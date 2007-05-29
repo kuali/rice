586d037880c62b96e9968eecb8eaaeb24f60de69
Jerry Neal <jkneal@indiana.edu>
2007-05-29 20:20:08 +0000

    Added null check for extra param NPE that was breaking multiple value lookups in KFS.

--- kns/src/main/java/org/kuali/core/web/ui/ExtraButton.java
+++ kns/src/main/java/org/kuali/core/web/ui/ExtraButton.java
@@ -15,42 +15,67 @@
  */
 package org.kuali.core.web.ui;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.Constants;
 import org.kuali.rice.KNSServiceLocator;
 
 /**
- * Represents an extra button that may appear on the lookups or bottom of a document page.
+ * Represents an extra button that may appear on the lookups or bottom of a
+ * document page.
  */
 public class ExtraButton {
-    private String extraButtonSource = "";
-    private String extraButtonAltText = "";
-    private String extraButtonParams = "";
-    private String extraButtonProperty = "";
+	private String extraButtonSource = "";
 
-    public String getExtraButtonAltText() {
-        return extraButtonAltText;
-    }
-    public void setExtraButtonAltText(String extraButtonAltText) {
-        this.extraButtonAltText = extraButtonAltText;
-    }
-    public String getExtraButtonParams() {
-        return extraButtonParams;
-    }
-    public void setExtraButtonParams(String extraButtonParams) {
-        this.extraButtonParams = extraButtonParams;
-    }
-    public String getExtraButtonProperty() {
-        return extraButtonProperty;
-    }
-    public void setExtraButtonProperty(String extraButtonProperty) {
-        this.extraButtonProperty = extraButtonProperty;
-    }
-    public String getExtraButtonSource() {
-        return extraButtonSource;
-    }
-    public void setExtraButtonSource(String extraButtonSource) {
-        this.extraButtonSource = extraButtonSource.replace("${kr.externalizable.images.url}", KNSServiceLocator.getKualiConfigurationService().getPropertyString(Constants.EXTERNALIZABLE_IMAGES_URL_KEY)).replace("${externalizable.images.url}", KNSServiceLocator.getKualiConfigurationService().getPropertyString(Constants.APPLICATION_EXTERNALIZABLE_IMAGES_URL_KEY));
-    }
-    
-    
+	private String extraButtonAltText = "";
+
+	private String extraButtonParams = "";
+
+	private String extraButtonProperty = "";
+
+	public String getExtraButtonAltText() {
+		return extraButtonAltText;
+	}
+
+	public void setExtraButtonAltText(String extraButtonAltText) {
+		this.extraButtonAltText = extraButtonAltText;
+	}
+
+	public String getExtraButtonParams() {
+		return extraButtonParams;
+	}
+
+	public void setExtraButtonParams(String extraButtonParams) {
+		this.extraButtonParams = extraButtonParams;
+	}
+
+	public String getExtraButtonProperty() {
+		return extraButtonProperty;
+	}
+
+	public void setExtraButtonProperty(String extraButtonProperty) {
+		this.extraButtonProperty = extraButtonProperty;
+	}
+
+	public String getExtraButtonSource() {
+		return extraButtonSource;
+	}
+
+	public void setExtraButtonSource(String extraButtonSource) {
+		if (StringUtils.isNotBlank(extraButtonSource)) {
+			this.extraButtonSource = extraButtonSource
+					.replace(
+							"${kr.externalizable.images.url}",
+							KNSServiceLocator
+									.getKualiConfigurationService()
+									.getPropertyString(
+											Constants.EXTERNALIZABLE_IMAGES_URL_KEY))
+					.replace(
+							"${externalizable.images.url}",
+							KNSServiceLocator
+									.getKualiConfigurationService()
+									.getPropertyString(
+											Constants.APPLICATION_EXTERNALIZABLE_IMAGES_URL_KEY));
+		}
+	}
+
 }

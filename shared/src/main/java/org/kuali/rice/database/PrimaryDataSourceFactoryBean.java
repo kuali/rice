6d96f5f8601ce8321d85d555cb4fd5eb677a194b
Eric Westfall <ewestfal@iu.edu>
2007-07-05 14:29:03 +0000

    Updated the PrimaryDataSourceFactoryBean so that it is more flexable and can be used to pull datasources from different config and jndi names.

--- shared/src/main/java/org/kuali/rice/database/PrimaryDataSourceFactoryBean.java
+++ shared/src/main/java/org/kuali/rice/database/PrimaryDataSourceFactoryBean.java
@@ -1,5 +1,8 @@
 package org.kuali.rice.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
@@ -12,10 +15,33 @@ import org.kuali.rice.lifecycle.Lifecycle;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 import org.springframework.jndi.JndiTemplate;
 
+/**
+ * A class that can be used to load the primary datasource for a Rice module
+ * from an object in the Config system or from a JNDI url specified by the
+ * Configuration system.  By default, it loads these values from the
+ * follwing properties:
+ *
+ * <ul>
+ *   <li>{@link Config#DATASOURCE_OBJ}</li>
+ *   <li>{@link Config#DATASOURCE_JNDI}</li>
+ * </ul>
+ *
+ * <p>The config properties checked can be overridden by setting the
+ *
+ *
+ * @author Eric Westfall
+ */
 public class PrimaryDataSourceFactoryBean extends AbstractFactoryBean {
+
+	private static final String DEFAULT_DATASOURCE_PARAM = Config.DATASOURCE_OBJ;
+	private static final String DEFAULT_DATASOURCE_JNDI_PARAM = Config.DATASOURCE_JNDI;
 
 	private TransactionManager transactionManager;
 	private JndiTemplate jndiTemplate;
+	private String defaultDataSourceParam = DEFAULT_DATASOURCE_PARAM;
+	private String defaultDataSourceJndiParam = DEFAULT_DATASOURCE_JNDI_PARAM;
+	private List<String> preferredDataSourceParams = new ArrayList<String>();
+	private List<String> preferredDataSourceJndiParams = new ArrayList<String>();
 
 	public PrimaryDataSourceFactoryBean() {
 		setSingleton(true);
@@ -28,21 +54,7 @@ public class PrimaryDataSourceFactoryBean extends AbstractFactoryBean {
 	@Override
 	protected Object createInstance() throws Exception {
 		Config config = Core.getCurrentContextConfig();
-		DataSource dataSource = (DataSource)config.getObject(Config.DATASOURCE_OBJ);
-		if (dataSource == null) {
-			String dataSourceJndiName = Core.getCurrentContextConfig().getProperty(Config.DATASOURCE_JNDI);
-			if (!StringUtils.isEmpty(dataSourceJndiName)) {
-				if (this.jndiTemplate == null) {
-				    this.jndiTemplate = new JndiTemplate();
-				}
-				try {
-					dataSource = (DataSource)this.jndiTemplate.lookup(dataSourceJndiName, DataSource.class);
-				} catch (NamingException e) {
-					throw new ConfigurationException("Could not locate the DataSource at the given JNDI location: '" + dataSourceJndiName + "'", e);
-				}
-			}
-
-		}
+		DataSource dataSource = createDataSource(config);
 		if (dataSource != null) {
 			return dataSource;
 		}
@@ -50,6 +62,62 @@ public class PrimaryDataSourceFactoryBean extends AbstractFactoryBean {
 			throw new ConfigurationException("A transactionManager must be specified!");
 		}
 		return createDefaultDataSource(config, this.transactionManager);
+	}
+
+	protected DataSource createDataSource(Config config) throws Exception {
+		DataSource dataSource = loadPreferredDataSourceFromConfig(config);
+		if (dataSource == null) {
+			Object dataSourceObject = config.getObject(getDefaultDataSourceParam());
+			if (dataSourceObject != null) {
+				validateDataSource(getDefaultDataSourceParam(), dataSourceObject);
+				dataSource = (DataSource)dataSourceObject;
+			} else {
+				dataSource = getDataSourceFromJndi(config, getDefaultDataSourceJndiParam());
+			}
+		}
+		return dataSource;
+	}
+
+	protected DataSource loadPreferredDataSourceFromConfig(Config config) {
+		for (String dataSourceParam : getPreferredDataSourceParams()) {
+			Object dataSource = config.getObject(dataSourceParam);
+			if (dataSource != null) {
+				validateDataSource(dataSourceParam, dataSource);
+				return (DataSource)dataSource;
+			}
+		}
+		if (this.jndiTemplate == null) {
+		    this.jndiTemplate = new JndiTemplate();
+		}
+		for (String dataSourceJndiParam : getPreferredDataSourceJndiParams()) {
+			DataSource dataSource = getDataSourceFromJndi(config, dataSourceJndiParam);
+			if (dataSource != null) {
+				return dataSource;
+			}
+		}
+		return null;
+	}
+
+	protected void validateDataSource(String paramName, Object dataSourceObject) {
+		if (!(dataSourceObject instanceof DataSource)) {
+			throw new ConfigurationException("DataSource configured for parameter '" + paramName + "' was not an instance of DataSource.  Was instead " + dataSourceObject.getClass().getName());
+		}
+	}
+
+	protected DataSource getDataSourceFromJndi(Config config, String dataSourceJndiParam) {
+		String jndiName = config.getProperty(dataSourceJndiParam);
+		if (!StringUtils.isBlank(jndiName)) {
+			try {
+				Object dataSource = getJndiTemplate().lookup(jndiName, DataSource.class);
+				if (dataSource != null) {
+					validateDataSource(dataSourceJndiParam, dataSource);
+					return (DataSource)dataSource;
+				}
+			} catch (NamingException e) {
+				throw new ConfigurationException("Could not locate the DataSource at the given JNDI location: '" + jndiName + "'", e);
+			}
+		}
+		return null;
 	}
 
 	protected DataSource createDefaultDataSource(Config config, TransactionManager transactionManager) throws Exception {
@@ -105,5 +173,40 @@ public class PrimaryDataSourceFactoryBean extends AbstractFactoryBean {
 	public void setJndiTemplate(JndiTemplate jndiTemplate) {
 		this.jndiTemplate = jndiTemplate;
 	}
+
+	public String getDefaultDataSourceJndiParam() {
+		return defaultDataSourceJndiParam;
+	}
+
+	public void setDefaultDataSourceJndiParam(String defaultDataSourceJndiParam) {
+		this.defaultDataSourceJndiParam = defaultDataSourceJndiParam;
+	}
+
+	public String getDefaultDataSourceParam() {
+		return defaultDataSourceParam;
+	}
+
+	public void setDefaultDataSourceParam(String defaultDataSourceParam) {
+		this.defaultDataSourceParam = defaultDataSourceParam;
+	}
+
+	public List<String> getPreferredDataSourceJndiParams() {
+		return preferredDataSourceJndiParams;
+	}
+
+	public void setPreferredDataSourceJndiParams(List<String> preferredDataSourceJndiParams) {
+		this.preferredDataSourceJndiParams = preferredDataSourceJndiParams;
+	}
+
+	public List<String> getPreferredDataSourceParams() {
+		return preferredDataSourceParams;
+	}
+
+	public void setPreferredDataSourceParams(List<String> preferredDataSourceParams) {
+		this.preferredDataSourceParams = preferredDataSourceParams;
+	}
+
+
+
 
 }

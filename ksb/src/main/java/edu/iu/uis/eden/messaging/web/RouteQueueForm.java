7a9a3d9fe4c72d1dfd8125dc086b9a332ec5cef8
Andrew Hollamon <andrew@dmentionsystems.com>
2007-07-14 00:31:57 +0000

    Resurrected the route queue screens.  The main screens work, though there is still a problem with the View Payload screen, that is still being worked on. KULRICE-269 KULRICE-381

--- ksb/src/main/java/edu/iu/uis/eden/messaging/web/RouteQueueForm.java
+++ ksb/src/main/java/edu/iu/uis/eden/messaging/web/RouteQueueForm.java
@@ -16,199 +16,211 @@
  */
 package edu.iu.uis.eden.messaging.web;
 
+import java.sql.Timestamp;
+import java.util.Collection;
+
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.struts.action.ActionForm;
+
+import edu.iu.uis.eden.EdenConstants;
+import edu.iu.uis.eden.messaging.PersistedMessage;
+import edu.iu.uis.eden.util.CodeTranslator;
+import edu.iu.uis.eden.util.converters.KEWSqlTimestampConverter;
 
 /**
  * Struts ActionForm for the {@link RouteQueueAction}.
  *
  * @author rkirkend
  */
-public class RouteQueueForm {
-//	extends ActionForm{
-//
-//    
-//	private static final long serialVersionUID = -247925603792980036L;
-//    private int maxRows;
-//    private Long routeQueueId;
-//    private String methodToCall = "";
-//    private Collection<PersistedMessage> routeQueueRows;
-//    private String showEdit;
-//    private String command;
-//    private PersistedMessage routeQueueFromDatabase;
-//    private PersistedMessage routeQueueFromForm;
-//    private String newQueueDate;
-//    private String existingQueueDate;
-//    
-//    //  filter stuff
-//    private String queueStatusFilter;
-//    private String ipNumberFilter;
-//    private String serviceNameFilter;
-//    private String messageEntityFilter;
-//    private String filterApplied;
-//    
-//    static {
-//        ConvertUtils.register(new KEWSqlTimestampConverter(), Timestamp.class);
-//    }
-//    
-//    public RouteQueueForm() {
-//        routeQueueFromForm = new PersistedMessage();
-//        maxRows = EdenConstants.MAX_RETURNED_ROWS;
-//    }
-//
-//    public String getMethodToCall() {
-//        return methodToCall;
-//    }
-//
-//    public void setMethodToCall(String methodToCall) {
-//        this.methodToCall = methodToCall;
-//    }
-//
-//    public Long getRouteQueueId() {
-//        return routeQueueId;
-//    }
-//
-//    public void setRouteQueueId(Long routeQueueId) {
-//        this.routeQueueId = routeQueueId;
-//    }
-//
-//    public Collection<PersistedMessage> getRouteQueueRows() {
-//        return routeQueueRows;
-//    }
-// 
-//    public void setRouteQueueRows(Collection<PersistedMessage> routeQueueRows) {
-//        this.routeQueueRows = routeQueueRows;
-//    }
-//    
-//    public int getRouteQueueRowsSize() {
-//        if (routeQueueRows == null) {
-//            return 0;
-//        }
-//        return routeQueueRows.size();
-//    }
-//
-//    public String getCommand() {
-//        return command;
-//    }
-//
-//    public void setCommand(String command) {
-//        this.command = command;
-//    }
-//
-//    public PersistedMessage getRouteQueueFromDatabase() {
-//        return routeQueueFromDatabase;
-//    }
-//
-//    public void setRouteQueueFromDatabase(PersistedMessage existingRouteQueue) {
-//        this.routeQueueFromDatabase = existingRouteQueue;
-//    }
-//  
-//    public PersistedMessage getRouteQueueFromForm() {
-//        return routeQueueFromForm;
-//    }
-//
-//    public void setRouteQueueFromForm(PersistedMessage routeQueue) {
-//        this.routeQueueFromForm = routeQueue;
-//    }
-//
-//    public String getShowEdit() {
-//        return showEdit;
-//    }
-//
-//    public void setShowEdit(String showEdit) {
-//        this.showEdit = showEdit;
-//    }
-//
-//    public String getExistingQueueDate() {
-//        return existingQueueDate;
-//    }
-//
-//    public void setExistingQueueDate(String existingQueueDate) {
-//        this.existingQueueDate = existingQueueDate;
-//    }
-//
-//    public String getNewQueueDate() {
-//        return newQueueDate;
-//    }
-//
-//    public void setNewQueueDate(String newQueueDate) {
-//        this.newQueueDate = newQueueDate;
-//    }
-//
-//    public int getMaxRows() {
-//        return maxRows;
-//    }
-//
-//    public void setMaxRows(int maxRows) {
-//        this.maxRows = maxRows;
-//    }
-//
-//    /**
-//     * @return the ipNumberFilter
-//     */
-//    public String getIpNumberFilter() {
-//      return ipNumberFilter;
-//    }
-//
-//    /**
-//     * @param ipNumberFilter the ipNumberFilter to set
-//     */
-//    public void setIpNumberFilter(String ipNumberFilter) {
-//      this.ipNumberFilter = ipNumberFilter;
-//    }
-//
-//    /**
-//     * @return the messageEntityFilter
-//     */
-//    public String getMessageEntityFilter() {
-//      return messageEntityFilter;
-//    }
-//
-//    /**
-//     * @param messageEntityFilter the messageEntityFilter to set
-//     */
-//    public void setMessageEntityFilter(String messageEntityFilter) {
-//      this.messageEntityFilter = messageEntityFilter;
-//    }
-//
-//    /**
-//     * @return the queueStatusFilter
-//     */
-//    public String getQueueStatusFilter() {
-//      return queueStatusFilter;
-//    }
-//
-//    /**
-//     * @param queueStatusFilter the queueStatusFilter to set
-//     */
-//    public void setQueueStatusFilter(String queueStatusFilter) {
-//      this.queueStatusFilter = queueStatusFilter;
-//    }
-//
-//    /**
-//     * @return the serviceNameFilter
-//     */
-//    public String getServiceNameFilter() {
-//      return serviceNameFilter;
-//    }
-//
-//    /**
-//     * @param serviceNameFilter the serviceNameFilter to set
-//     */
-//    public void setServiceNameFilter(String serviceNameFilter) {
-//      this.serviceNameFilter = serviceNameFilter;
-//    }
-//
-//    /**
-//     * @return the filterApplied
-//     */
-//    public String getFilterApplied() {
-//      return filterApplied;
-//    }
-//
-//    /**
-//     * @param filterApplied the filterApplied to set
-//     */
-//    public void setFilterApplied(String filterApplied) {
-//      this.filterApplied = filterApplied;
-//    }
+public class RouteQueueForm extends ActionForm {
+    
+	private static final long serialVersionUID = -247925603792980036L;
+    private int maxRows;
+    private Long routeQueueId;
+    private String methodToCall = "";
+    private Collection<PersistedMessage> routeQueueRows;
+    private String showEdit;
+    private String command;
+    private PersistedMessage routeQueueFromDatabase;
+    private PersistedMessage routeQueueFromForm;
+    private String newQueueDate;
+    private String existingQueueDate;
+    
+    //  filter stuff
+    private String queueStatusFilter;
+    private String ipNumberFilter;
+    private String serviceNameFilter;
+    private String messageEntityFilter;
+    private String filterApplied;
+    
+    static {
+        ConvertUtils.register(new KEWSqlTimestampConverter(), Timestamp.class);
+    }
+    
+    public RouteQueueForm() {
+        routeQueueFromForm = new PersistedMessage();
+        maxRows = EdenConstants.MAX_RETURNED_ROWS;
+    }
+
+    public String getRouteQueueStatusLabel(String statusCode) {
+        return CodeTranslator.getRouteQueueStatusLabel(statusCode);
+    }
+
+    public String getMethodToCall() {
+        return methodToCall;
+    }
+
+    public void setMethodToCall(String methodToCall) {
+        this.methodToCall = methodToCall;
+    }
+
+    public Long getRouteQueueId() {
+        return routeQueueId;
+    }
+
+    public void setRouteQueueId(Long routeQueueId) {
+        this.routeQueueId = routeQueueId;
+    }
+
+    public Collection<PersistedMessage> getRouteQueueRows() {
+        return routeQueueRows;
+    }
+ 
+    public void setRouteQueueRows(Collection<PersistedMessage> routeQueueRows) {
+        this.routeQueueRows = routeQueueRows;
+    }
+    
+    public int getRouteQueueRowsSize() {
+        if (routeQueueRows == null) {
+            return 0;
+        }
+        return routeQueueRows.size();
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public PersistedMessage getRouteQueueFromDatabase() {
+        return routeQueueFromDatabase;
+    }
+
+    public void setRouteQueueFromDatabase(PersistedMessage existingRouteQueue) {
+        this.routeQueueFromDatabase = existingRouteQueue;
+    }
+  
+    public PersistedMessage getRouteQueueFromForm() {
+        return routeQueueFromForm;
+    }
+
+    public void setRouteQueueFromForm(PersistedMessage routeQueue) {
+        this.routeQueueFromForm = routeQueue;
+    }
+
+    public String getShowEdit() {
+        return showEdit;
+    }
+
+    public void setShowEdit(String showEdit) {
+        this.showEdit = showEdit;
+    }
+
+    public String getExistingQueueDate() {
+        return existingQueueDate;
+    }
+
+    public void setExistingQueueDate(String existingQueueDate) {
+        this.existingQueueDate = existingQueueDate;
+    }
+
+    public String getNewQueueDate() {
+        return newQueueDate;
+    }
+
+    public void setNewQueueDate(String newQueueDate) {
+        this.newQueueDate = newQueueDate;
+    }
+
+    public int getMaxRows() {
+        return maxRows;
+    }
+
+    public void setMaxRows(int maxRows) {
+        this.maxRows = maxRows;
+    }
+
+    /**
+     * @return the ipNumberFilter
+     */
+    public String getIpNumberFilter() {
+      return ipNumberFilter;
+    }
+
+    /**
+     * @param ipNumberFilter the ipNumberFilter to set
+     */
+    public void setIpNumberFilter(String ipNumberFilter) {
+      this.ipNumberFilter = ipNumberFilter;
+    }
+
+    /**
+     * @return the messageEntityFilter
+     */
+    public String getMessageEntityFilter() {
+      return messageEntityFilter;
+    }
+
+    /**
+     * @param messageEntityFilter the messageEntityFilter to set
+     */
+    public void setMessageEntityFilter(String messageEntityFilter) {
+      this.messageEntityFilter = messageEntityFilter;
+    }
+
+    /**
+     * @return the queueStatusFilter
+     */
+    public String getQueueStatusFilter() {
+      return queueStatusFilter;
+    }
+
+    /**
+     * @param queueStatusFilter the queueStatusFilter to set
+     */
+    public void setQueueStatusFilter(String queueStatusFilter) {
+      this.queueStatusFilter = queueStatusFilter;
+    }
+
+    /**
+     * @return the serviceNameFilter
+     */
+    public String getServiceNameFilter() {
+      return serviceNameFilter;
+    }
+
+    /**
+     * @param serviceNameFilter the serviceNameFilter to set
+     */
+    public void setServiceNameFilter(String serviceNameFilter) {
+      this.serviceNameFilter = serviceNameFilter;
+    }
+
+    /**
+     * @return the filterApplied
+     */
+    public String getFilterApplied() {
+      return filterApplied;
+    }
+
+    /**
+     * @param filterApplied the filterApplied to set
+     */
+    public void setFilterApplied(String filterApplied) {
+      this.filterApplied = filterApplied;
+    }
 
 }

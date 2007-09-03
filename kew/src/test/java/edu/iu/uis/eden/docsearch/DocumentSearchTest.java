d536fce9fa10f40df4ed92faf04dc3dc46abd3b2
Eric Westfall <ewestfal@iu.edu>
2007-09-03 02:55:32 +0000

    Fixed DocumentSearchTest.

--- kew/src/test/java/edu/iu/uis/eden/docsearch/DocumentSearchTest.java
+++ kew/src/test/java/edu/iu/uis/eden/docsearch/DocumentSearchTest.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2005-2007 The Kuali Foundation.
- * 
- * 
+ *
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,14 +36,14 @@ import edu.iu.uis.eden.user.WorkflowUser;
 
 public class DocumentSearchTest extends WorkflowTestCase {
 
-    DocumentSearchService docSearchService; 
+    DocumentSearchService docSearchService;
     UserService userService;
 
     protected void setUpTransaction() throws Exception {
         docSearchService = (DocumentSearchService)KEWServiceLocator.getService(KEWServiceLocator.DOCUMENT_SEARCH_SERVICE);
         userService = (UserService)KEWServiceLocator.getService(KEWServiceLocator.USER_SERVICE);
     }
-    
+
     @Test public void testDocSearch() throws Exception {
         WorkflowUser user = userService.getWorkflowUser(new AuthenticationUserId("bmcgough"));
         DocSearchCriteriaVO criteria = new DocSearchCriteriaVO();
@@ -69,7 +69,7 @@ public class DocumentSearchTest extends WorkflowTestCase {
         savedSearchResults = docSearchService.getSavedSearchResults(user, "DocSearch.NamedSearch.for in accounts");
         assertNotNull(savedSearchResults);
     }
-    
+
     @Test public void testGetNamedDocSearches() throws Exception {
         WorkflowUser user = userService.getWorkflowUser(new AuthenticationUserId("bmcgough"));
         List namedSearches = docSearchService.getNamedSearches(user);
@@ -80,7 +80,7 @@ public class DocumentSearchTest extends WorkflowTestCase {
      * Test for https://test.kuali.org/jira/browse/KULWF-703
      */
     @Test public void testSearchEDENSERVICE_DOCS() throws Exception {
-        loadXmlStream(new FileInputStream("conf/bootstrap/BootstrapData.xml"));
+        loadXmlStream(new FileInputStream(getBaseDir()+"/src/main/config/bootstrap/BootstrapData.xml"));
 //        loadXmlStream(new FileInputStream("conf/bootstrap/BootstrapRuleTemplateContent.xml"));
 //        loadXmlStream(new FileInputStream("conf/bootstrap/BootstrapDocumentTypesContent.xml"));
 //        loadXmlStream(new FileInputStream("conf/bootstrap/BootstrapRuleContent.xml"));
@@ -97,7 +97,7 @@ public class DocumentSearchTest extends WorkflowTestCase {
         fieldValues.put(DocumentTypeLookupableImpl.DOCUMENT_TYPE_ID_PROPERTY_NAME, "");
         fieldValues.put(DocumentTypeLookupableImpl.DOC_TYP_NAME_PROPERTY_NAME, "EDENSERVICE-DOCS");
         fieldValues.put(DocumentTypeLookupableImpl.BACK_LOCATION_KEY_NAME, "http://localhost:8080/en-dev/DocumentSearch.do");
-        
+
         fieldValues.put(DocumentTypeLookupableImpl.DOC_FORM_KEY_NAME, "0");
 
         //String docTypReturn = (String) fieldConversions.get(DOC_TYP_FULL_NAME);
@@ -106,7 +106,7 @@ public class DocumentSearchTest extends WorkflowTestCase {
         assertNotNull(list);
         assertTrue(list.size() > 0);
     }
-    
+
     @Test public void testDefaultCreateDateSearchCriteria() throws Exception {
         WorkflowUser user = userService.getWorkflowUser(new AuthenticationUserId("bmcgough"));
         DocSearchCriteriaVO criteria = new DocSearchCriteriaVO();
@@ -115,7 +115,7 @@ public class DocumentSearchTest extends WorkflowTestCase {
         Calendar today = Calendar.getInstance();
         Calendar criteriaDate = Calendar.getInstance();
         criteriaDate.setTime(DocSearchUtils.convertStringDateToTimestamp(criteria.getFromDateCreated()));
-        assertEquals("Criteria date minus today's date should equal the constant value", EdenConstants.DOCUMENT_SEARCH_NO_CRITERIA_CREATE_DATE_DAYS_AGO.doubleValue(), getDifferenceInDays(today, criteriaDate));
+        assertEquals("Criteria date minus today's date should equal the constant value", EdenConstants.DOCUMENT_SEARCH_NO_CRITERIA_CREATE_DATE_DAYS_AGO.doubleValue(), getDifferenceInDays(today, criteriaDate), 0);
 
         criteria = new DocSearchCriteriaVO();
         criteria.setDocTitle("testing");
@@ -124,9 +124,9 @@ public class DocumentSearchTest extends WorkflowTestCase {
         today = Calendar.getInstance();
         criteriaDate = Calendar.getInstance();
         criteriaDate.setTime(DocSearchUtils.convertStringDateToTimestamp(criteria.getFromDateCreated()));
-        assertEquals("Criteria date minus today's date should equal the constant value", EdenConstants.DOCUMENT_SEARCH_DOC_TITLE_CREATE_DATE_DAYS_AGO.doubleValue(), getDifferenceInDays(today, criteriaDate));
+        assertEquals("Criteria date minus today's date should equal the constant value", EdenConstants.DOCUMENT_SEARCH_DOC_TITLE_CREATE_DATE_DAYS_AGO.doubleValue(), getDifferenceInDays(today, criteriaDate), 0);
     }
-     
+
     private static double getDifferenceInDays(Calendar startCalendar, Calendar endCalendar) {
         // First, get difference in whole days
         startCalendar.set(Calendar.HOUR_OF_DAY, 0);

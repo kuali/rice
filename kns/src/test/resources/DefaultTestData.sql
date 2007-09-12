1145a214e0a3ee26c793beaf423e9373bceea3f4
Aaron Hamid <aaron@incsw.com>
2007-09-12 16:55:51 +0000

    - recreate master sql - fixed up some bootstrap sql

--- kns/src/test/resources/DefaultTestData.sql
+++ kns/src/test/resources/DefaultTestData.sql
@@ -77,7 +77,7 @@ INSERT INTO kr_qrtz_locks values('STATE_ACCESS')
 ;
 INSERT INTO kr_qrtz_locks values('MISFIRE_ACCESS')
 ;
-INSERT INTO APPLICATIONS_T (ID, NAME, DESCRIPTION) VALUES (1, 'KIM', 'This record represents the actual KIM system and must always be loaded by default in order for the system to work properly.')
+INSERT INTO KIM_NAMESPACES_T (ID, NAME, DESCRIPTION) VALUES (1, 'KIM', 'This record represents the actual KIM system and must always be loaded by default in order for the system to work properly.') 
 ;
-INSERT INTO PERSONS_T (ID, USERNAME, PASSWORD) VALUES (1, 'admin', 'admin')
+INSERT INTO KIM_PERSONS_T (ID, USERNAME, PASSWORD) VALUES (1, 'admin', 'admin')
 ;

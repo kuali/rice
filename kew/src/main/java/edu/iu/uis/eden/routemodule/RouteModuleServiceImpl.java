ab777ca68299bfe1fae98e1d833b3fc6c4b272cc
Eric Westfall <ewestfal@iu.edu>
2007-09-25 16:40:10 +0000

    Fixed a bug in the route module service where it wasn't handling the loading of RouteModuleRemote correctly.

--- kew/src/main/java/edu/iu/uis/eden/routemodule/RouteModuleServiceImpl.java
+++ kew/src/main/java/edu/iu/uis/eden/routemodule/RouteModuleServiceImpl.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2005-2007 The Kuali Foundation.
- * 
- * 
+ *
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,10 @@ import org.kuali.rice.resourceloader.GlobalResourceLoader;
 
 import edu.iu.uis.eden.EdenConstants;
 import edu.iu.uis.eden.actionrequests.ActionRequestValue;
+import edu.iu.uis.eden.clientapp.RouteModuleRemote;
 import edu.iu.uis.eden.engine.node.RouteNode;
 import edu.iu.uis.eden.exception.ResourceUnavailableException;
+import edu.iu.uis.eden.exception.WorkflowRuntimeException;
 
 /**
  * @author ewestfal
@@ -43,26 +45,32 @@ public class RouteModuleServiceImpl implements RouteModuleService {
         }
         return routeModule;
     }
-    
+
     public RouteModule findRouteModule(ActionRequestValue actionRequest) throws ResourceUnavailableException {
         if (actionRequest.getNodeInstance() == null) {
             return null;
         }
         return findRouteModule(actionRequest.getNodeInstance().getRouteNode());
     }
-    
+
     private RouteModule getRouteModule(String routeMethodName) throws ResourceUnavailableException {
         if (routeMethodName == null) {
             return null;
         } else if ("".equals(routeMethodName.trim()) || EdenConstants.ROUTE_LEVEL_NO_ROUTE_MODULE.equals(routeMethodName)) {
                 return null;
         }
-        return (RouteModule) GlobalResourceLoader.getObject(new ObjectDefinition(routeMethodName));//SpringServiceLocator.getExtensionService().getRouteModule(routeMethodName);
+        Object routeModule = GlobalResourceLoader.getObject(new ObjectDefinition(routeMethodName));//SpringServiceLocator.getExtensionService().getRouteModule(routeMethodName);
+        if (routeModule instanceof RouteModule) {
+            return (RouteModule)routeModule;
+        } else if (routeModule instanceof RouteModuleRemote) {
+            return new RouteModuleRemoteAdapter((RouteModuleRemote)routeModule);
+        }
+        throw new WorkflowRuntimeException("Could not locate the Route Module with the given name: " + routeMethodName);
     }
 
     private RouteModule getFlexRMRouteModule(String ruleTemplateName) {
         return new FlexRMAdapter(ruleTemplateName);
     }
 
-    
+
 }

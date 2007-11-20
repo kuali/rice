c84719b77115d28c5abeef1b614884eeb54adc5f
Nate Johnson <natjohns@iu.edu>
2007-11-20 17:02:50 +0000

    Attempting to fix the 15 broken bus tests.

--- ksb/src/test/java/org/kuali/bus/test/KSBTestCase.java
+++ ksb/src/test/java/org/kuali/bus/test/KSBTestCase.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2007 The Kuali Foundation
- *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.opensource.org/licenses/ecl1.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
  */
 package org.kuali.bus.test;
 
@@ -49,152 +46,152 @@ public class KSBTestCase extends RiceTestCase {
 
     @Override
     public void setUp() throws Exception {
-		// because we're stopping and starting so many times we need to clear
-		// the core before
-		// another set of RLs get put in the core. This is because we are
-		// sometimes using
-		// the GRL to fetch a specific servers spring file out for testing
-		// purposes.
-	Core.destroy();
-	super.setUp();
-	if (startClient1() || startClient2()) {
-	    ((Runnable) KSBResourceLoaderFactory.getRemoteResourceLocator()).run();
-	}
-//	new SQLDataLoader("classpath:db/DefaultTestData.sql", ";").runSql();
+        // because we're stopping and starting so many times we need to clear
+        // the core before
+        // another set of RLs get put in the core. This is because we are
+        // sometimes using
+        // the GRL to fetch a specific servers spring file out for testing
+        // purposes.
+        Core.destroy();
+        super.setUp();
+        if (startClient1() || startClient2()) {
+            ((Runnable) KSBResourceLoaderFactory.getRemoteResourceLocator()).run();
+        }
+        // new SQLDataLoader("classpath:db/DefaultTestData.sql", ";").runSql();
     }
 
     @Override
     protected List<String> getConfigLocations() {
-	return Arrays.asList(new String[]{"classpath:META-INF/ksb-test-config.xml"});
+        return Arrays.asList(new String[] { "classpath:META-INF/ksb-test-config.xml" });
     }
 
     @Override
     protected String getDerbySQLFileLocation() {
-	return "classpath:db/derby/bus.sql";
+        return "classpath:db/derby/bus.sql";
     }
 
     @Override
     protected String getModuleName() {
-	return "ksb";
+        return "ksb";
     }
 
     @Override
     protected List<String> getTablesToClear() {
-	List<String> tables = new ArrayList<String>();
-	tables.add("EN_MSG_QUE_T");
-	tables.add("EN_MSG_PAYLOAD_T");
-	tables.add("EN_BAM_T");
-	tables.add("EN_BAM_PARAM_T");
-	tables.add("EN_SERVICE_DEF_DUEX_T");
-	return tables;
+        List<String> tables = new ArrayList<String>();
+        tables.add("EN_MSG_QUE_T");
+        tables.add("EN_MSG_PAYLOAD_T");
+        tables.add("EN_BAM_T");
+        tables.add("EN_BAM_PARAM_T");
+        tables.add("EN_SERVICE_DEF_DUEX_T");
+        return tables;
     }
 
     @Override
     protected List<Lifecycle> getPerTestLifecycles() {
-	List<Lifecycle> lifecycles = super.getPerTestLifecycles();
-		if (this.disableJta()) {
-			System.setProperty(BaseOjbConfigurer.OJB_PROPERTIES_PROP, "RiceNoJtaOJB.properties");
-			this.springContextResourceLoader = new SpringResourceLoader(new QName("ksbtestharness"), "KSBTestHarnessNoJtaSpring.xml");
-		} else {
-	this.springContextResourceLoader = new SpringResourceLoader(new QName("ksbtestharness"), "KSBTestHarnessSpring.xml");
-		}
+        List<Lifecycle> lifecycles = super.getSuiteLifecycles();
+        if (this.disableJta()) {
+            System.setProperty(BaseOjbConfigurer.OJB_PROPERTIES_PROP, "RiceNoJtaOJB.properties");
+            this.springContextResourceLoader = new SpringResourceLoader(new QName("ksbtestharness"), "KSBTestHarnessNoJtaSpring.xml");
+        } else {
+            this.springContextResourceLoader = new SpringResourceLoader(new QName("ksbtestharness"), "KSBTestHarnessSpring.xml");
+        }
 
-	lifecycles.add(this.springContextResourceLoader);
-	if (startClient1()) {
-	    this.testClient1 = new TestClient1();
-	    lifecycles.add(this.testClient1);
-	}
-	if (startClient2()) {
-	    this.testClient2 = new TestClient2();
-	    lifecycles.add(this.testClient2);
-	}
-	return lifecycles;
+        lifecycles.add(this.springContextResourceLoader);
+        if (startClient1()) {
+            this.testClient1 = new TestClient1();
+            lifecycles.add(this.testClient1);
+        }
+        if (startClient2()) {
+            this.testClient2 = new TestClient2();
+            lifecycles.add(this.testClient2);
+        }
+        return lifecycles;
     }
 
     public boolean startClient1() {
-	return false;
+        return false;
     }
 
     public boolean startClient2() {
-	return false;
+        return false;
     }
 
     public TestClient1 getTestClient1() {
-	return this.testClient1;
+        return this.testClient1;
     }
 
     public TestClient2 getTestClient2() {
-	return this.testClient2;
+        return this.testClient2;
     }
 
-	public static boolean verifyServiceCallsViaBam(QName serviceName, String methodName, boolean serverInvocation) throws Exception {
-	BAMService bamService = KSBServiceLocator.getBAMService();
-	List<BAMTargetEntry> bamCalls = null;
-	if (methodName == null) {
-	    bamCalls = bamService.getCallsForService(serviceName);
-	} else {
-	    bamCalls = bamService.getCallsForService(serviceName, methodName);
-	}
+    public static boolean verifyServiceCallsViaBam(QName serviceName, String methodName, boolean serverInvocation) throws Exception {
+        BAMService bamService = KSBServiceLocator.getBAMService();
+        List<BAMTargetEntry> bamCalls = null;
+        if (methodName == null) {
+            bamCalls = bamService.getCallsForService(serviceName);
+        } else {
+            bamCalls = bamService.getCallsForService(serviceName, methodName);
+        }
 
-	if (bamCalls.size() == 0) {
-	    return false;
-	}
-	for (BAMTargetEntry bamEntry : bamCalls) {
-	    if (bamEntry.getServerInvocation() && serverInvocation) {
-		return true;
-	    } else if (!serverInvocation) {
-		return true;
-	    }
-	}
-	return false;
+        if (bamCalls.size() == 0) {
+            return false;
+        }
+        for (BAMTargetEntry bamEntry : bamCalls) {
+            if (bamEntry.getServerInvocation() && serverInvocation) {
+                return true;
+            } else if (!serverInvocation) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Object getServiceFromWebAppResourceLoader(String serviceName) {
-	Map<ClassLoader, Config> configs = Core.getCONFIGS();
-	for (Map.Entry<ClassLoader, Config> configEntry : configs.entrySet()) {
-	    if (configEntry.getKey() instanceof WebAppClassLoader) {
-		ClassLoader old = Thread.currentThread().getContextClassLoader();
-		// to make GRL select services from correct classloader
-		Thread.currentThread().setContextClassLoader(configEntry.getKey());
-		try {
-		    return GlobalResourceLoader.getService(serviceName);
-		} finally {
-		    Thread.currentThread().setContextClassLoader(old);
-		}
-	    }
-	}
-	throw new RiceRuntimeException("Couldn't find service " + serviceName + " in WebApp Resource Loader");
+        Map<ClassLoader, Config> configs = Core.getCONFIGS();
+        for (Map.Entry<ClassLoader, Config> configEntry : configs.entrySet()) {
+            if (configEntry.getKey() instanceof WebAppClassLoader) {
+                ClassLoader old = Thread.currentThread().getContextClassLoader();
+                // to make GRL select services from correct classloader
+                Thread.currentThread().setContextClassLoader(configEntry.getKey());
+                try {
+                    return GlobalResourceLoader.getService(serviceName);
+                } finally {
+                    Thread.currentThread().setContextClassLoader(old);
+                }
+            }
+        }
+        throw new RiceRuntimeException("Couldn't find service " + serviceName + " in WebApp Resource Loader");
     }
 
     public static Object getServiceFromTestClient1SpringContext(String serviceName) {
-	Map<ClassLoader, Config> configs = Core.getCONFIGS();
-	for (Map.Entry<ClassLoader, Config> configEntry : configs.entrySet()) {
-	    if (configEntry.getKey() instanceof WebAppClassLoader) {
-		ClassLoader old = Thread.currentThread().getContextClassLoader();
-		// to make GRL select services from correct classloader
-		Thread.currentThread().setContextClassLoader(configEntry.getKey());
-		try {
-		    // TestClient1SpringContext found in web.xml of TestClient1
-					ApplicationContext appContext = (ApplicationContext) Core.getCurrentContextConfig().getObject("TestClient1SpringContext");
+        Map<ClassLoader, Config> configs = Core.getCONFIGS();
+        for (Map.Entry<ClassLoader, Config> configEntry : configs.entrySet()) {
+            if (configEntry.getKey() instanceof WebAppClassLoader) {
+                ClassLoader old = Thread.currentThread().getContextClassLoader();
+                // to make GRL select services from correct classloader
+                Thread.currentThread().setContextClassLoader(configEntry.getKey());
+                try {
+                    // TestClient1SpringContext found in web.xml of TestClient1
+                    ApplicationContext appContext = (ApplicationContext) Core.getCurrentContextConfig().getObject("TestClient1SpringContext");
 
-		    return appContext.getBean(serviceName);
-		} finally {
-		    Thread.currentThread().setContextClassLoader(old);
-		}
-	    }
-	}
-	throw new RiceRuntimeException("Couldn't find service " + serviceName + " in TestClient1 Spring Context");
+                    return appContext.getBean(serviceName);
+                } finally {
+                    Thread.currentThread().setContextClassLoader(old);
+                }
+            }
+        }
+        throw new RiceRuntimeException("Couldn't find service " + serviceName + " in TestClient1 Spring Context");
     }
 
     public ResourceLoader getSpringContextResourceLoader() {
-	return this.springContextResourceLoader;
+        return this.springContextResourceLoader;
     }
 
     public void setSpringContextResourceLoader(ResourceLoader testHarnessResourceLoader) {
-	this.springContextResourceLoader = testHarnessResourceLoader;
+        this.springContextResourceLoader = testHarnessResourceLoader;
     }
 
-	protected boolean disableJta() {
-		return false;
-}
+    protected boolean disableJta() {
+        return false;
+    }
 }

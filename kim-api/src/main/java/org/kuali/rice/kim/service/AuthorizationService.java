915065953af197d1c3929291fd8d6e46c9e00965
Aaron Godert <aaron@givegab.com>
2008-01-25 00:07:05 +0000

    KULRICE-1403 - updated services to involve the new Principal object and account for Namespace scoping

---
+++ kim-api/src/main/java/org/kuali/rice/kim/service/AuthorizationService.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kim.service;
+
+import java.util.Map;
+
+/**
+ * This service class is responsible for checking whether Persons, Groups, and Principals have the 
+ * authorization for performing certain actions (i.e. Permissions). 
+ * 
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ *
+ */
+public interface AuthorizationService {
+    /**
+     * Determines if a principal is authorized for a given permission.
+     * 
+     * @param   principalName        name identifying a principal
+     * @param   permissionName       name identifying a unique permission
+     * @param   namespaceName        The associated namespace to scope to
+     * @return                       boolean indicating if Principal is authorized
+     * 
+     */
+    public boolean isPrincipalAuthorized(String principalName, String permissionName, String namespaceName);
+    
+    /**
+     * Determines if a principal is authorized for a given permission.
+     * 
+     * @param   personId             person id identifying a unique KIM Person
+     * @param   permissionName       name identifying a unique permission
+     * @param   namespaceName        The associated namespace to scope to
+     * @return                       boolean indicating if Person is authorized
+     * 
+     */
+    public boolean isPersonAuthorized(Long personId, String permissionName, String namespaceName);
+    
+    /**
+     * Determines if a Person possesses a given permission and set of role attributes.
+     * 
+     * @param   principalName            name identifying a principal
+     * @param   permissionName           name identifying a unique permission
+     * @param   qualifiedRoleAttributes  Map<String, String> of role attribute name/value pairs
+     *                                   to qualify a Person
+     * @param   namespaceName            The associated namespace to scope to
+     * @return                           boolean indicating if Principal possesses the permission and 
+     *                                   all given Role attributes
+     * 
+     */
+    public boolean isPrincipalAuthorizedForQualifiedPermission(String principalName, String permissionName, 
+        Map<String, String> qualifiedRoleAttributes, String namespaceName);
+    
+    /**
+     * Determines if a Person possesses a given qualified permission.
+     * 
+     * @param   personId                 person id identifying a unique Person
+     * @param   permissionName           name identifying a unique permission
+     * @param   qualifiedRoleAttributes  Map<String, String> of role attribute name/value pairs
+     *                                   to qualify a Person
+     * @param   namespaceName            The associated namespace to scope to
+     * @return                           boolean indicating if Person possesses the permission and 
+     *                                   all given Role attributes
+     * 
+     */
+    public boolean isPersonAuthorizedForQualifiedPermission(Long personId, String permissionName, 
+        Map<String, String> qualifiedRoleAttributes, String namespaceName);
+    
+    /**
+     * Determines if a given principal has been assigned to a given role.
+     * 
+     * @param   principalName        name identifying a principal
+     * @param   roleName             name identifying a unique Role
+     * @return                       boolean indicating if Principal has been assigned to Role
+     * 
+     */
+    public boolean principalHasRole(String principalName, String roleName);
+    
+    /**
+     * Determines if a given person has been assigned to a given role.
+     * 
+     * @param   personId             person id uniquely identifying a person
+     * @param   roleName             name identifying a unique Role
+     * @return                       boolean indicating if Person has been assigned to Role
+     * 
+     */
+    public boolean personHasRole(Long personId, String roleName);
+    
+    /**
+     * Determines if a given group has been assigned to a given role.
+     * 
+     * @param   groupName            group name uniquely identifying a group
+     * @param   roleName             name identifying a unique Role
+     * @return                       boolean indicating if Group has been assigned to Role
+     * 
+     */
+    public boolean groupHasRole(Long personId, String roleName);
+    
+    /**
+     * Determines if a given principal possesses all given Role attributes.
+     * 
+     * @param   principalName               name identifying a principal
+     * @param   roleName                    name identifying a unique Role
+     * @param   qualifiedRoleAttributes     Map<String, String> of role attribute name/value pairs
+     *                                      to qualify a Role
+     * @return                              boolean indicating if principal possesses all given Role attributes
+     * 
+     */
+    public boolean principalHasQualifiedRole(String principalName, String roleName, 
+        Map<String, String> qualifiedRoleAttributes);
+    
+    /**
+     * Determines if a given principal possesses all given Role attributes.
+     * 
+     * @param   personId                    person id uniquely identifying a person
+     * @param   roleName                    name identifying a unique Role
+     * @param   qualifiedRoleAttributes     Map<String, String> of role attribute name/value pairs
+     *                                      to qualify a Role
+     * @return                              boolean indicating if Person possesses all given Role attributes
+     * 
+     */
+    public boolean personHasQualifiedRole(Long personId, String roleName, 
+        Map<String, String> qualifiedRoleAttributes);
+    
+    /**
+     * Determines if a given principal possesses all given Role attributes.
+     * 
+     * @param   groupName                   group name uniquely identifying a group
+     * @param   roleName                    name identifying a unique Role
+     * @param   qualifiedRoleAttributes     Map<String, String> of role attribute name/value pairs
+     *                                      to qualify a Role
+     * @return                              boolean indicating if a Group possesses all given Role attributes
+     * 
+     */
+    public boolean groupHasQualifiedRole(String groupName, String roleName, 
+        Map<String, String> qualifiedRoleAttributes);
+}

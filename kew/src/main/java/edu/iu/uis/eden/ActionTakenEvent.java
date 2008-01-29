9965319015f223397d90d562f8dae5a68aa9382c
Aaron Hamid <aaron@incsw.com>
2008-01-29 19:01:29 +0000

    added comment

--- kew/src/main/java/edu/iu/uis/eden/ActionTakenEvent.java
+++ kew/src/main/java/edu/iu/uis/eden/ActionTakenEvent.java
@@ -2,6 +2,10 @@ package edu.iu.uis.eden;
 
 import edu.iu.uis.eden.actiontaken.ActionTakenValue;
 
+/**
+ * Event sent to the postprocessor when an action has been taken
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ */
 public class ActionTakenEvent implements IDocumentEvent {
 
 	private static final long serialVersionUID = 2945081851810845704L;

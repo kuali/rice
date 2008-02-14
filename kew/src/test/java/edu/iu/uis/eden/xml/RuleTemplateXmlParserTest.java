5ac2713aaaeb50edacc8074a1effee202b93d525
Aaron Hamid <aaron@incsw.com>
2008-02-14 20:41:48 +0000

    removed extraneous import from KNS

--- kew/src/test/java/edu/iu/uis/eden/xml/RuleTemplateXmlParserTest.java
+++ kew/src/test/java/edu/iu/uis/eden/xml/RuleTemplateXmlParserTest.java
@@ -20,7 +20,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
-import org.kuali.core.service.impl.DocumentServiceImpl;
 import org.kuali.workflow.test.KEWTestCase;
 
 import edu.iu.uis.eden.EdenConstants;

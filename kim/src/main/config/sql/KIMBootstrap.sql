cb1ca5d6d900a5483c0ed07175249427fc77fab2
Aaron Godert <aaron@givegab.com>
2008-02-26 18:27:44 +0000

    KULRICE-1668 - didn't fix, but am cleaning up doc type codes that are being re-used throughout the application, each maint doc should have their own KULRICE-1667 - removed the NamespaceDefaultAttributeMaintenanceDocument.xml file b/c these attributes will be maintained by the parent, not one by one

--- kim/src/main/config/sql/KIMBootstrap.sql
+++ kim/src/main/config/sql/KIMBootstrap.sql
@@ -2,6 +2,8 @@ INSERT INTO KIM_NAMESPACES_T (ID, NAME, DESCRIPTION) VALUES (1, 'KIM', 'This rec
 /
 INSERT INTO KIM_ENTITY_TYPES_T values (1, 'Person', 'This entity represents a person in the system.', SYS_GUID(), 1)
 /
+INSERT INTO FP_DOC_TYPE_T values ('KNSD', SYS_GUID(), 1, 'KR', 'NAMESPACE', 'N', 'Y', 'N', 0, 'N', 'N')
+/
 INSERT INTO FP_DOC_TYPE_T values ('KPMD', SYS_GUID(), 1, 'KR', 'PRINCIPAL', 'N', 'Y', 'N', 0, 'N', 'N')
 /
 INSERT INTO FP_DOC_TYPE_T values ('KGMD', SYS_GUID(), 1, 'KR', 'GROUP', 'N', 'Y', 'N', 0, 'N', 'N')
@@ -9,4 +11,6 @@ INSERT INTO FP_DOC_TYPE_T values ('KGMD', SYS_GUID(), 1, 'KR', 'GROUP', 'N', 'Y'
 INSERT INTO FP_DOC_TYPE_T values ('KRMD', SYS_GUID(), 1, 'KR', 'ROLE', 'N', 'Y', 'N', 0, 'N', 'N')
 /
 INSERT INTO FP_DOC_TYPE_T values ('KGAM', SYS_GUID(), 1, 'KR', 'GROUP ATTRIBUTE', 'N', 'Y', 'N', 0, 'N', 'N')
+/
+INSERT INTO FP_DOC_TYPE_T values ('KATD', SYS_GUID(), 1, 'KR', 'ATTRIBUTE TYPE', 'N', 'Y', 'N', 0, 'N', 'N')
 /
---
+++ kom/src/main/config/sql/KOMBootstrap.sql
@@ -0,0 +1,6 @@
+INSERT INTO FP_DOC_TYPE_T values ('KOCD', SYS_GUID(), 1, 'KR', 'ORGANIZATION CATEGORY', 'N', 'Y', 'N', 0, 'N', 'N')
+/
+INSERT INTO FP_DOC_TYPE_T values ('KOCT', SYS_GUID(), 1, 'KR', 'ORGANIZATION CONTEXT', 'N', 'Y', 'N', 0, 'N', 'N')
+/
+INSERT INTO FP_DOC_TYPE_T values ('KORG', SYS_GUID(), 1, 'KR', 'ORGANIZATION', 'N', 'Y', 'N', 0, 'N', 'N')
+/

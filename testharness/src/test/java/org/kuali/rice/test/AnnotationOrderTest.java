070e61fc8f5c00bb62084a42d9ab65a13c62acb6
Aaron Hamid <aaron@incsw.com>
2008-02-28 20:08:41 +0000

    Added a test that tests annotation order preservation

---
+++ testharness/src/test/java/org/kuali/rice/test/AnnotationOrderTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.test;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Tests that ordering of annotation definitions is preserved 
+ * 
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ */
+public class AnnotationOrderTest {
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface TestAnnotation {
+        String value();
+    }
+
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface TestAnnotation1 {
+        String value();
+    }
+    
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface TestAnnotation2 {
+        String value();
+    }
+    
+    @Target({ElementType.TYPE, ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface TestAnnotationHolder {
+        TestAnnotation[] value();
+    }
+
+
+    @Test
+    @TestAnnotation1("11")
+    @TestAnnotationHolder({
+            @TestAnnotation("1"),
+            @TestAnnotation("2"),
+            @TestAnnotation("3"),
+            @TestAnnotation("4"),
+            @TestAnnotation("5"),
+            @TestAnnotation("6")
+    })
+    @TestAnnotation2("22")
+    public void testAnnotationOrder() throws Exception {
+        Method me = getClass().getMethod("testAnnotationOrder");
+        Annotation[] annotations = me.getAnnotations();
+        Assert.assertEquals(4, annotations.length);
+        Assert.assertEquals(Test.class, annotations[0].annotationType());
+        Assert.assertEquals(TestAnnotation1.class, annotations[1].annotationType());
+        Assert.assertEquals(TestAnnotationHolder.class, annotations[2].annotationType());
+        Assert.assertEquals(TestAnnotation2.class, annotations[3].annotationType());
+        
+        TestAnnotationHolder holder = me.getAnnotation(TestAnnotationHolder.class);
+        TestAnnotation[] children = holder.value();
+        Assert.assertEquals(6, children.length);
+        Assert.assertEquals("1", children[0].value());
+        Assert.assertEquals("2", children[1].value());
+        Assert.assertEquals("3", children[2].value());
+        Assert.assertEquals("4", children[3].value());
+        Assert.assertEquals("5", children[4].value());
+        Assert.assertEquals("6", children[5].value());
+    }
+}

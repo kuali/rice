0dcc61bab8eaf634d48cf4a57a56c7e07dccf298
Aaron Hamid <aaron@incsw.com>
2008-03-05 16:56:15 +0000

    KULRICE-1279 - added some KCB deliverer preferences to sample app data

--- kns/src/main/config/sql/rice_sample_app.sql
+++ kns/src/main/config/sql/rice_sample_app.sql
@@ -260,6 +260,16 @@ VALUES
 (4, 3)
 /
 
+-- Sample data that KCB contributes to the sample app --
+-- just add some deliverer configurations
+
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (1, 'TestUser6', 'KEW', 'mock', 0)
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (2, 'TestUser1', 'KEW', 'mock', 0)
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (3, 'TestUser2', 'KEW', 'mock', 0)
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (4, 'quickstart', 'KEW', 'mock', 0)
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (5, 'TestUser5', 'KEW', 'mock', 0)
+insert into KCB_RECIP_DELIVS (ID, RECIPIENT_ID, CHANNEL, DELIVERER_NAME, DB_LOCK_VER_NBR) values (6, 'TestUser4', 'KEW', 'mock', 0)
+
 
 commit
 /

6fbdb74d67907a633c78beec696fd99cfeaa8bc7
Aaron Hamid <aaron@incsw.com>
2008-03-20 17:49:33 +0000

    made abstract so unit test runner doesn't complain

--- kns/src/main/java/org/kuali/rice/testharness/KNSTestCase.java
+++ kns/src/main/java/org/kuali/rice/testharness/KNSTestCase.java
@@ -28,7 +28,7 @@ import edu.iu.uis.eden.batch.KEWXmlDataLoaderLifecycle;
  * 
  * @author Kuali Rice Team (kuali-rice@googlegroups.com)
  */
-public class KNSTestCase extends RiceTestCase {
+public abstract class KNSTestCase extends RiceTestCase {
 
 	private String contextName = "/SampleRiceClient";
 	private String relativeWebappRoot = "/src/test/webapp";

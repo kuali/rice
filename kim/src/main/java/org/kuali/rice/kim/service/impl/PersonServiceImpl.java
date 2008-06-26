5024795c1f5b88ea69d23ddc008bd4d7135540a4
George Lindholm <george.lindholm@ubc.ca>
2008-06-26 18:02:34 +0000

    KULRICE-1687: Implement the KIM PersonService

--- kim/src/main/java/org/kuali/rice/kim/service/impl/PersonServiceImpl.java
+++ kim/src/main/java/org/kuali/rice/kim/service/impl/PersonServiceImpl.java
@@ -15,111 +15,213 @@
  */
 package org.kuali.rice.kim.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.kim.bo.EntityAttribute;
+import org.kuali.rice.kim.bo.EntityType;
+import org.kuali.rice.kim.bo.Group;
+import org.kuali.rice.kim.bo.Namespace;
+import org.kuali.rice.kim.bo.Principal;
+import org.kuali.rice.kim.bo.PrincipalQualifiedRoleAttribute;
+import org.kuali.rice.kim.dao.KIMServicesDao;
 import org.kuali.rice.kim.dto.PersonAttributeDTO;
 import org.kuali.rice.kim.dto.PersonDTO;
 import org.kuali.rice.kim.service.PersonService;
 
 /**
- * This is the default KIM PersonService implementation that is provided by Rice.  This will mature over time as the KIM 
- * component is developed.
- * 
+ * This is the default KIM PersonService implementation that is provided by
+ * Rice. This will mature over time as the KIM component is developed.
+ *
  * @author Kuali Rice Team (kuali-rice@googlegroups.com)
  */
 public class PersonServiceImpl implements PersonService {
+	private KIMServicesDao kimServicesDao;
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getAllPersonIds()
-     */
-    public List<Long> getAllPersonIds() {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getAllPersonIds()
+	 */
+	public List<Long> getAllPersonIds() {
+		final Collection<Principal> persons = kimServicesDao.findAllPersons();
+		final ArrayList<Long> ids = new ArrayList<Long>(persons.size());
+		for (Principal p : persons) {
+			ids.add(p.getId());
+		}
+		return ids;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getAllPersons()
-     */
-    public List<PersonDTO> getAllPersons() {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getAllPersons()
+	 */
+	public List<PersonDTO> getAllPersons() {
+		final Collection<Principal> persons = kimServicesDao.findAllPersons();
+		final ArrayList<PersonDTO> ids = new ArrayList<PersonDTO>(persons
+				.size());
+		for (Principal p : persons) {
+			ids.add(Principal.toPersonDTO(p));
+		}
+		return ids;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getAttributeValue(java.lang.Long, java.lang.String, java.lang.String)
-     */
-    public String getAttributeValue(Long personId, String attributeName, String namespaceName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getAttributeValue(java.lang.Long,
+	 *      java.lang.String, java.lang.String)
+	 */
+	public String getAttributeValue(Long personId, String attributeName,
+			String namespaceName) {
+		return kimServicesDao.getPersonAttributeValue(personId, attributeName,
+				namespaceName);
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getPersonIdsWithAttributes(java.util.Map, java.lang.String)
-     */
-    public List<Long> getPersonIdsWithAttributes(Map<String, String> personAttributes, String namespaceName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getPersonIdsWithAttributes(java.util.Map,
+	 *      java.lang.String)
+	 */
+	public List<Long> getPersonIdsWithAttributes(
+			Map<String, String> personAttributes, String namespaceName) {
+		final Collection<Principal> persons = kimServicesDao.findAllPersons();
+		final ArrayList<Long> ids = new ArrayList<Long>(persons.size());
+		for (Principal p : persons) {
+			if (hasAttributes(p.getId(), personAttributes, namespaceName)) {
+				ids.add(p.getId());
+			}
+		}
+		return ids;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getPersonsWithAttributes(java.util.Map, java.lang.String)
-     */
-    public List<PersonDTO> getPersonsWithAttributes(Map<String, String> personAttributes, String namespaceName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getPersonsWithAttributes(java.util.Map,
+	 *      java.lang.String)
+	 */
+	public List<PersonDTO> getPersonsWithAttributes(
+			Map<String, String> personAttributes, String namespaceName) {
+		final Collection<Principal> principals = kimServicesDao
+				.findAllPersons();
+		final ArrayList<PersonDTO> persons = new ArrayList<PersonDTO>(
+				principals.size());
+		for (Principal p : principals) {
+			if (hasAttributes(p.getId(), personAttributes, namespaceName)) {
+				persons.add(Principal.toPersonDTO(p));
+			}
+		}
+		return persons;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#hasAttributes(java.lang.Long, java.util.Map, java.lang.String)
-     */
-    public boolean hasAttributes(Long personId, Map<String, String> personAttributes, String namespaceName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return false;
-    }
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#hasAttributes(java.lang.Long,
+	 *      java.util.Map, java.lang.String)
+	 */
+	public boolean hasAttributes(Long personId,
+			Map<String, String> personAttributes, String namespaceName) {
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#isMemberOfGroup(java.lang.Long, java.lang.String)
-     */
-    public boolean isMemberOfGroup(Long personId, String groupName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return false;
-    }
+		final Collection<EntityAttribute> attributeValues = kimServicesDao
+				.getPersonAttributesForNamespace(personId, namespaceName);
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getPersonAttributesByNamespace(java.lang.Long)
-     */
-    public HashMap<String, List<PersonAttributeDTO>> getPersonAttributesByNamespace(Long personId) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		if (attributeValues != null && attributeValues.size() > 0) {
+			return ImplUtils
+					.hasAllAttributes(personAttributes, attributeValues);
+		}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.PersonService#getPersonAttributesForNamespace(java.lang.Long, java.lang.String)
-     */
-    public HashMap<String, PersonAttributeDTO> getPersonAttributesForNamespace(Long personId, String namespaceName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
-    
-    
+		return false;
+	}
+
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#isMemberOfGroup(java.lang.Long,
+	 *      java.lang.String)
+	 */
+	public boolean isMemberOfGroup(Long personId, String groupName) {
+		final Principal principal = kimServicesDao.findPerson(personId);
+		if (principal == null) {
+			return false;
+		}
+
+		for (Group g : principal.getGroups()) {
+			if (g.getName().equals(groupName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getPersonAttributesByNamespace(java.lang.Long)
+	 */
+	public HashMap<String, List<PersonAttributeDTO>> getPersonAttributesByNamespace(
+			Long personId) {
+		final HashMap<String, List<PersonAttributeDTO>> attributes = new HashMap<String, List<PersonAttributeDTO>>(0);
+
+		final Principal principal = kimServicesDao.findPerson(personId);
+		if (principal == null) {
+			return attributes;
+		}
+
+		final Collection<Namespace> namespaces = (Collection<Namespace>) KNSServiceLocator
+				.getBusinessObjectService().findAll(Namespace.class);
+		for (Namespace namespace : namespaces) {
+			final Collection<EntityAttribute> attributeValues = kimServicesDao
+					.getPersonAttributesForNamespace(personId, namespace
+							.getName());
+			if (attributeValues != null && attributeValues.size() > 0) {
+				final ArrayList<PersonAttributeDTO> personAttributes = new ArrayList<PersonAttributeDTO>(
+						attributeValues.size());
+				for (EntityAttribute ea : attributeValues) {
+					personAttributes.add(EntityAttribute.toPersonDTO(ea));
+				}
+				attributes.put(namespace.getName(), personAttributes);
+			}
+		}
+		return attributes;
+	}
+
+	/**
+	 * This overridden method ...
+	 *
+	 * @see org.kuali.rice.kim.service.PersonService#getPersonAttributesForNamespace(java.lang.Long,
+	 *      java.lang.String)
+	 */
+	public HashMap<String, PersonAttributeDTO> getPersonAttributesForNamespace(
+			Long personId, String namespaceName) {
+
+		final HashMap<String, PersonAttributeDTO> eas = new HashMap<String, PersonAttributeDTO>(0);
+
+		final Collection<EntityAttribute> attributeValues = kimServicesDao
+				.getPersonAttributesForNamespace(personId, namespaceName);
+		if (attributeValues == null) {
+			return eas;
+		}
+
+		for (EntityAttribute ea : attributeValues) {
+			eas.put(ea.getAttributeName(), EntityAttribute.toPersonDTO(ea));
+		}
+		return eas;
+	}
+
+	/**
+	 * @param kimServicesDao
+	 *            the kimServicesDao to set
+	 */
+	public void setKimServicesDao(KIMServicesDao kimServicesDao) {
+		this.kimServicesDao = kimServicesDao;
+	}
+
 }
--- kim/src/test/java/org/kuali/rice/kim/service/PersonServiceTest.java
+++ kim/src/test/java/org/kuali/rice/kim/service/PersonServiceTest.java
@@ -15,26 +15,295 @@
  */
 package org.kuali.rice.kim.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
 import org.junit.Test;
+import org.kuali.rice.kim.dto.PersonAttributeDTO;
+import org.kuali.rice.kim.dto.PersonDTO;
 import org.kuali.rice.kim.test.KIMTestCase;
 import org.kuali.rice.resourceloader.GlobalResourceLoader;
 
 /**
  * Basic test to verify we can access the PersonService through the GRL.
- * 
- * @author Kuali Rice Team (kuali-rice@googlegroups.com)
  *
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
  */
 public class PersonServiceTest extends KIMTestCase {
-    private PersonService personService;
-    
+	private static final String EXPECTED_NAMESPACE_NAME = "KIM";
+	private static final String EXPECTED_ATTRIBUTE_NAME = "EmailAddress";
+	private static final String EXPECTED_ATTRIBUTE_VALUE = "kuali-rice@googlegroups.com";
+	private static final String EXPECTED_ATTRIBUTE_NAME2 = "Country";
+	private static final String EXPECTED_ATTRIBUTE_VALUE2 = "USA";
+	private static final Long EXPECTED_PERSON_ID = new Long(160);
+	private static final String EXPECTED_GROUP_NAME = "Group1";
+	private static final Map<String, String> EXPECTED_PERSON_ATTRIBUTE = new HashMap<String, String>();
+	private static final Map<String, String> EXPECTED_PERSON_ATTRIBUTES = new HashMap<String, String>();
+	private PersonService personService;
+	private PersonService personSoapService;
+
+	private static final String URI = EXPECTED_NAMESPACE_NAME;
+	private static final QName SOAP_SERVICE = new QName(URI,
+			"personSoapService");
+	private static final QName JAVA_SERVICE = new QName(URI, "personService");
+
 	public void setUp() throws Exception {
-	    super.setUp();
-	    personService = (PersonService)GlobalResourceLoader.getService("personService");
+		super.setUp();
+		personService = (PersonService) GlobalResourceLoader
+				.getService(JAVA_SERVICE);
+		personSoapService = (PersonService) GlobalResourceLoader
+				.getService(SOAP_SERVICE);
+		EXPECTED_PERSON_ATTRIBUTE.put(EXPECTED_ATTRIBUTE_NAME,
+				EXPECTED_ATTRIBUTE_VALUE);
+		EXPECTED_PERSON_ATTRIBUTES.put(EXPECTED_ATTRIBUTE_NAME,
+				EXPECTED_ATTRIBUTE_VALUE);
+		EXPECTED_PERSON_ATTRIBUTES.put(EXPECTED_ATTRIBUTE_NAME2,
+				EXPECTED_ATTRIBUTE_VALUE2);
 	}
-	
+
 	@Test
-	public void fakeTest() {
-	    assertTrue(true);
+	public void getAllPersonIds_Java() {
+		getAllPersonIds(personService);
 	}
+
+	@Test
+	public void getAllPersonIds_Soap() {
+		getAllPersonIds(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getAllPersonIds(final PersonService personService) {
+		List<Long> personIds = personService.getAllPersonIds();
+		assertNotNull("Found no personsIds", personIds);
+		assertEquals("Wrong number of persons found", 2, personIds.size());
+		assertEquals("Wrong person found", EXPECTED_PERSON_ID, personIds.get(0));
+	}
+
+	@Test
+	public void getAllPersons_Java() {
+		getAllPersons(personService);
+	}
+
+	@Test
+	public void getAllPersons_Soap() {
+		getAllPersons(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getAllPersons(final PersonService personService) {
+		List<PersonDTO> persons = personService.getAllPersons();
+		assertNotNull("Found no persons", persons);
+		assertEquals("Wrong number of persons found", 2, persons.size());
+		assertEquals("Wrong person found", EXPECTED_PERSON_ID, persons.get(0)
+				.getId());
+	}
+
+	@Test
+	public void getAttributeValue_Java() {
+		getAttributeValue(personService);
+	}
+
+	@Test
+	public void getAttributeValue_Soap() {
+		getAttributeValue(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getAttributeValue(final PersonService personService) {
+		String value = personService.getAttributeValue(EXPECTED_PERSON_ID,
+				EXPECTED_ATTRIBUTE_NAME, EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no attribute value", value);
+		assertEquals("Wrong attribute value found", EXPECTED_ATTRIBUTE_VALUE,
+				value);
+	}
+
+	@Test
+	public void getPersonIdsWithAttributes_Java() {
+		getPersonIdsWithAttributes(personService);
+	}
+
+	@Test
+	public void getPersonIdsWithAttributes_Soap() {
+		getPersonIdsWithAttributes(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getPersonIdsWithAttributes(
+			final PersonService personService) {
+		List<Long> personsIds = personService.getPersonIdsWithAttributes(
+				EXPECTED_PERSON_ATTRIBUTE, EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no PersonIds with attributes", personsIds);
+		assertEquals("Found wrong number of persons", 2, personsIds.size());
+		assertEquals("Wrong personsId (principalId", EXPECTED_PERSON_ID,
+				personsIds.get(0));
+
+		personsIds = personService.getPersonIdsWithAttributes(
+				EXPECTED_PERSON_ATTRIBUTES, EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no PersonIds with attributes", personsIds);
+		assertEquals("Found wrong number of persons with multiple attributes",
+				1, personsIds.size());
+		assertEquals("Wrong personsId (principalId", EXPECTED_PERSON_ID,
+				personsIds.get(0));
+	}
+
+	@Test
+	public void getPersonsWithAttributes_Java() {
+		getPersonsWithAttributes(personService);
+	}
+
+	@Test
+	public void getPersonsWithAttributes_Soap() {
+		getPersonsWithAttributes(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getPersonsWithAttributes(
+			final PersonService personService) {
+		List<PersonDTO> persons = personService.getPersonsWithAttributes(
+				EXPECTED_PERSON_ATTRIBUTE, EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no persons with attributes", persons);
+		assertEquals("Found wrong number of persons", 2, persons.size());
+		assertEquals("Wrong personsId (principalId", EXPECTED_PERSON_ID,
+				persons.get(0).getId());
+
+		persons = personService.getPersonsWithAttributes(
+				EXPECTED_PERSON_ATTRIBUTES, EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no persons with attributes", persons);
+		assertEquals("Found wrong number of persons with multiple attributes",
+				1, persons.size());
+		assertEquals("Wrong personsId (principalId", EXPECTED_PERSON_ID,
+				persons.get(0).getId());
+	}
+
+	@Test
+	public void hasAttributes_Java() {
+		hasAttributes(personService);
+	}
+
+	@Test
+	public void hasAttributes_Soap() {
+		hasAttributes(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void hasAttributes(final PersonService personService) {
+		boolean hasAttributes = personService.hasAttributes(EXPECTED_PERSON_ID,
+				EXPECTED_PERSON_ATTRIBUTE, EXPECTED_NAMESPACE_NAME);
+		assertTrue("Does not have attributes", hasAttributes);
+
+		hasAttributes = personService.hasAttributes(EXPECTED_PERSON_ID,
+				EXPECTED_PERSON_ATTRIBUTE, EXPECTED_NAMESPACE_NAME + "x");
+		assertFalse("Should not have attributes", hasAttributes);
+	}
+
+	@Test
+	public void isMemberOfGroup_Java() {
+		isMemberOfGroup(personService);
+	}
+
+	@Test
+	public void isMemberOfGroup_Soap() {
+		isMemberOfGroup(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void isMemberOfGroup(final PersonService personService) {
+		final boolean isMember = personService.isMemberOfGroup(
+				EXPECTED_PERSON_ID, EXPECTED_GROUP_NAME);
+		assertTrue("Is not a member", isMember);
+	}
+
+	@Test
+	public void getPersonAttributesForNamespace_Java() {
+		getPersonAttributesForNamespace(personService);
+
+	}
+
+	@Test
+	public void getPersonAttributesForNamespace_Soap() {
+		getPersonAttributesForNamespace(personSoapService);
+
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getPersonAttributesForNamespace(
+			final PersonService personService) {
+		HashMap<String, PersonAttributeDTO> attributes = personService
+				.getPersonAttributesForNamespace(EXPECTED_PERSON_ID,
+						EXPECTED_NAMESPACE_NAME);
+		assertNotNull("Found no person attributes for namespace", attributes);
+		assertEquals("Wrong number of attributes found", 2, attributes.size());
+		assertTrue("Wrong attribute found", attributes
+				.containsKey(EXPECTED_ATTRIBUTE_NAME));
+		assertEquals("Wrong attribute value found", EXPECTED_ATTRIBUTE_VALUE,
+				attributes.get(EXPECTED_ATTRIBUTE_NAME).getValue());
+
+		attributes = personService.getPersonAttributesForNamespace(
+				EXPECTED_PERSON_ID, EXPECTED_NAMESPACE_NAME + "x");
+		assertEquals("Found attributes for non-existent namespace", 0, attributes.size());
+
+		attributes = personService.getPersonAttributesForNamespace(01234445L,
+				EXPECTED_NAMESPACE_NAME);
+		assertEquals("Found attributes for non-existent person", 0, attributes.size());
+	}
+
+	@Test
+	public void getPersonAttributesByNamespace_Java() {
+		getPersonAttributesByNamespace(personService);
+	}
+
+	@Test
+	public void getPersonAttributesByNamespace_Soap() {
+		getPersonAttributesByNamespace(personSoapService);
+	}
+
+	/**
+	 * This method ...
+	 *
+	 */
+	private static void getPersonAttributesByNamespace(
+			final PersonService personService) {
+		HashMap<String, List<PersonAttributeDTO>> attributes = personService
+				.getPersonAttributesByNamespace(EXPECTED_PERSON_ID);
+		assertNotNull("Found no person attributes by namespace", attributes);
+		assertEquals("Wrong number of namespaces found", 1, attributes.size());
+		assertNotNull("No namespace list found", attributes
+				.containsKey(EXPECTED_NAMESPACE_NAME));
+		final List<PersonAttributeDTO> pas = attributes
+				.get(EXPECTED_NAMESPACE_NAME);
+		assertTrue("Wrong number of person attributes for namespace found", pas
+				.size() == 2);
+		assertEquals("Wrong person attribute found", EXPECTED_ATTRIBUTE_NAME,
+				pas.get(0).getAttributeName());
+		assertEquals("Wrong person attribute value found",
+				EXPECTED_ATTRIBUTE_VALUE, pas.get(0).getValue());
+	}
+
 }

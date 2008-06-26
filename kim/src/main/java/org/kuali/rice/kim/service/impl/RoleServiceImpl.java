fcb9f73d1775a9dac1b85547bd1d0a1bad14d4a1
George Lindholm <george.lindholm@ubc.ca>
2008-06-26 18:04:18 +0000

    KULRICE-1690: Implement the KIM RoleService

--- kim/src/main/java/org/kuali/rice/kim/service/impl/RoleServiceImpl.java
+++ kim/src/main/java/org/kuali/rice/kim/service/impl/RoleServiceImpl.java
@@ -15,9 +15,21 @@
  */
 package org.kuali.rice.kim.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.kim.bo.Entity;
+import org.kuali.rice.kim.bo.Group;
+import org.kuali.rice.kim.bo.GroupQualifiedRole;
+import org.kuali.rice.kim.bo.Permission;
+import org.kuali.rice.kim.bo.Principal;
+import org.kuali.rice.kim.bo.PrincipalQualifiedRole;
+import org.kuali.rice.kim.bo.Role;
+import org.kuali.rice.kim.dao.KIMServicesDao;
 import org.kuali.rice.kim.dto.EntityDTO;
 import org.kuali.rice.kim.dto.GroupDTO;
 import org.kuali.rice.kim.dto.GroupQualifiedRoleDTO;
@@ -29,191 +41,382 @@ import org.kuali.rice.kim.dto.RoleDTO;
 import org.kuali.rice.kim.service.RoleService;
 
 /**
- * This is the default KIM RoleService implementation that is provided by Rice.  This will mature over time as the KIM 
- * component is developed.
- * 
+ * This is the default KIM RoleService implementation that is provided by Rice.
+ * This will mature over time as the KIM component is developed.
+ *
  * @author Kuali Rice Team (kuali-rice@googlegroups.com)
  */
 public class RoleServiceImpl implements RoleService {
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getAllRoleNames()
-     */
-    public List<String> getAllRoleNames() {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	KIMServicesDao kimServicesDao;
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getAllRoles()
-     */
-    public List<RoleDTO> getAllRoles() {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
-    
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getRole(java.lang.Long)
-     */
-    public RoleDTO getRole(Long roleId) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	public RoleServiceImpl() {
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getRole(java.lang.String)
-     */
-    public RoleDTO getRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getAllRoleNames()
+	 */
+	public List<String> getAllRoleNames() {
+		final ArrayList<String> names = new ArrayList<String>(0);
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getGroupNamesWithRole(java.lang.String)
-     */
-    public List<String> getGroupNamesWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		final Collection<Role> roles = (Collection<Role>) KNSServiceLocator
+				.getBusinessObjectService().findAll(Role.class);
+		if (roles == null) {
+			return names;
+		}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getGroupQualifiedRoles(java.lang.String, java.util.Map)
-     */
-    public List<GroupQualifiedRoleDTO> getGroupQualifiedRoles(String roleName, Map<String, String> qualifiedRoleAttributes) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		names.ensureCapacity(roles.size());
+		for (Role role : roles) {
+			names.add(role.getName());
+		}
+		return names;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getGroupQualifiedRoles(java.lang.String)
-     */
-    public List<GroupQualifiedRoleDTO> getGroupQualifiedRoles(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getAllRoles()
+	 */
+	public List<RoleDTO> getAllRoles() {
+		ArrayList<RoleDTO> dto = new ArrayList<RoleDTO>(0);
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getGroupsWithRole(java.lang.String)
-     */
-    public List<GroupDTO> getGroupsWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		final Collection<Role> roles = (Collection<Role>) KNSServiceLocator
+				.getBusinessObjectService().findAll(Role.class);
+		if (roles == null) {
+			return dto;
+		}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPermissionNamesForRole(java.lang.String)
-     */
-    public List<String> getPermissionNamesForRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		dto.ensureCapacity(roles.size());
+		for (Role role : roles) {
+			dto.add(Role.toDTO(role));
+		}
+		return dto;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPermissionsForRole(java.lang.String)
-     */
-    public List<PermissionDTO> getPermissionsForRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getRole(java.lang.Long)
+	 */
+	public RoleDTO getRole(Long roleId) {
+		final HashMap<String, Long> id = new HashMap<String, Long>();
+		id.put("ID", roleId);
+		final Role role = (Role) KNSServiceLocator.getBusinessObjectService()
+				.findByPrimaryKey(Role.class, id);
+		if (role == null) {
+			return null;
+		}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPersonIdsWithRole(java.lang.String)
-     */
-    public List<Long> getPersonIdsWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		return Role.toDTO(role);
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPersonsWithRole(java.lang.String)
-     */
-    public List<PersonDTO> getPersonsWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getRole(java.lang.String)
+	 */
+	public RoleDTO getRole(String roleName) {
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPrincipalNamesWithRole(java.lang.String)
-     */
-    public List<String> getPrincipalNamesWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		Role role = findRoleByName(roleName);
+		return Role.toDTO(role);
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPrincipalsWithRole(java.lang.String)
-     */
-    public List<PrincipalDTO> getPrincipalsWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getGroupNamesWithRole(java.lang.String)
+	 */
+	public List<String> getGroupNamesWithRole(String roleName) {
+		final ArrayList<String> groups = new ArrayList<String>();
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPrincipalQualifiedRoles(java.lang.String)
-     */
-    public List<PrincipalQualifiedRoleDTO> getPrincipalQualifiedRoles(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return groups;
+		}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getPrincipalQualifiedRoles(java.lang.String, java.util.Map)
-     */
-    public List<PrincipalQualifiedRoleDTO> getPrincipalQualifiedRoles(String roleName, Map<String, String> qualifiedRoleAttributes) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+		groups.ensureCapacity(role.getGroups().size());
+		for (Group group : role.getGroups()) {
+			groups.add(group.getName());
+		}
+		return groups;
+	}
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getEntityIdsWithRole(java.lang.String)
-     */
-    public List<Long> getEntityIdsWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getGroupQualifiedRoles(java.lang.String,
+	 *      java.util.Map)
+	 */
+	public List<GroupQualifiedRoleDTO> getGroupQualifiedRoles(String roleName,
+			Map<String, String> qualifiedRoleAttributes) {
+		Collection<GroupQualifiedRole> groupsWithRole = kimServicesDao
+				.findAllGroupsWithQualifiedRole(roleName);
 
-    /**
-     * This overridden method ...
-     * 
-     * @see org.kuali.rice.kim.service.RoleService#getEntitysWithRole(java.lang.String)
-     */
-    public List<EntityDTO> getEntitysWithRole(String roleName) {
-        // TODO ag266 - THIS METHOD NEEDS JAVADOCS
-        return null;
-    }
-    
-    
+		final ArrayList<GroupQualifiedRoleDTO> groups = new ArrayList<GroupQualifiedRoleDTO>(
+				groupsWithRole.size());
+
+		for (GroupQualifiedRole attr : groupsWithRole) {
+			if (ImplUtils.hasAllQualifiedAttributes(qualifiedRoleAttributes,
+					attr.getQualifiedRoleAttributes())) {
+				groups.add(GroupQualifiedRole.toDTO(attr));
+			}
+		}
+		return groups;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getGroupQualifiedRoles(java.lang.String)
+	 */
+	public List<GroupQualifiedRoleDTO> getGroupQualifiedRoles(String roleName) {
+		Collection<GroupQualifiedRole> groupsWithRole = kimServicesDao
+				.findAllGroupsWithQualifiedRole(roleName);
+
+		final ArrayList<GroupQualifiedRoleDTO> groups = new ArrayList<GroupQualifiedRoleDTO>(
+				groupsWithRole.size());
+
+		for (GroupQualifiedRole attr : groupsWithRole) {
+			groups.add(GroupQualifiedRole.toDTO(attr));
+		}
+		return groups;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getGroupsWithRole(java.lang.String)
+	 */
+	public List<GroupDTO> getGroupsWithRole(String roleName) {
+		final ArrayList<GroupDTO> groups = new ArrayList<GroupDTO>(0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return groups;
+		}
+
+		groups.ensureCapacity(role.getGroups().size());
+		for (Group group : role.getGroups()) {
+			groups.add(Group.toDTO(group));
+		}
+		return groups;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPermissionNamesForRole(java.lang.String)
+	 */
+	public List<String> getPermissionNamesForRole(String roleName) {
+		final ArrayList<String> permissions = new ArrayList<String>(0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return permissions;
+		}
+
+		permissions.ensureCapacity(role.getPermissions().size());
+		for (Permission permission : role.getPermissions()) {
+			permissions.add(permission.getName());
+		}
+		return permissions;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPermissionsForRole(java.lang.String)
+	 */
+	public List<PermissionDTO> getPermissionsForRole(String roleName) {
+		final ArrayList<PermissionDTO> permissions = new ArrayList<PermissionDTO>(
+				0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return permissions;
+		}
+
+		permissions.ensureCapacity(role.getPermissions().size());
+		for (Permission permission : role.getPermissions()) {
+			permissions.add(Permission.toDTO(permission));
+		}
+		return permissions;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPersonIdsWithRole(java.lang.String)
+	 */
+	public List<Long> getPersonIdsWithRole(String roleName) {
+		ArrayList<Long> personIds = new ArrayList<Long>(0);
+
+		final Collection<Principal> personPrincipals = kimServicesDao.findAllPersons();
+
+		personIds.ensureCapacity(personPrincipals.size());
+		for (Principal person : personPrincipals) {
+			for (Role role : person.getRoles()) {
+				if (role.getName().equalsIgnoreCase(roleName)) {
+					personIds.add(person.getId());
+				}
+			}
+		}
+		return personIds;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPersonsWithRole(java.lang.String)
+	 */
+	public List<PersonDTO> getPersonsWithRole(String roleName) {
+		final Collection<Principal> personPrincipals = kimServicesDao.findAllPersons();
+
+		ArrayList<PersonDTO> persons = new ArrayList<PersonDTO>();
+		for (Principal person : personPrincipals) {
+			for (Role role : person.getRoles()) {
+				if (role.getName().equalsIgnoreCase(roleName)) {
+					persons.add(Principal.toPersonDTO(person));
+				}
+			}
+		}
+		return persons;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPrincipalNamesWithRole(java.lang.String)
+	 */
+	public List<String> getPrincipalNamesWithRole(String roleName) {
+		final ArrayList<String> principals = new ArrayList<String>(0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return principals;
+		}
+
+		principals.ensureCapacity(role.getPrincipals().size());
+		for (Principal principal : role.getPrincipals()) {
+			principals.add(principal.getName());
+		}
+		return principals;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPrincipalsWithRole(java.lang.String)
+	 */
+	public List<PrincipalDTO> getPrincipalsWithRole(String roleName) {
+		final ArrayList<PrincipalDTO> principals = new ArrayList<PrincipalDTO>(
+				0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return principals;
+		}
+
+		principals.ensureCapacity(role.getPrincipals().size());
+		for (Principal principal : role.getPrincipals()) {
+			principals.add(Principal.toDTO(principal));
+		}
+		return principals;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPrincipalQualifiedRoles(java.lang.String)
+	 */
+	public List<PrincipalQualifiedRoleDTO> getPrincipalQualifiedRoles(
+			String roleName) {
+		final ArrayList<PrincipalQualifiedRoleDTO> principals = new ArrayList<PrincipalQualifiedRoleDTO>(
+				0);
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return principals;
+		}
+
+		principals.ensureCapacity(role.getPrincipals().size());
+		for (PrincipalQualifiedRole pqr : role.getPrincipalQualifiedRoles()) {
+			principals.add(PrincipalQualifiedRole.toDTO(pqr));
+		}
+
+		return principals;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getPrincipalQualifiedRoles(java.lang.String,
+	 *      java.util.Map)
+	 */
+	public List<PrincipalQualifiedRoleDTO> getPrincipalQualifiedRoles(
+			String roleName, Map<String, String> qualifiedRoleAttributes) {
+		final ArrayList<PrincipalQualifiedRoleDTO> pqrs = new ArrayList<PrincipalQualifiedRoleDTO>(
+				0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return pqrs;
+		}
+
+		for (PrincipalQualifiedRole p : role.getPrincipalQualifiedRoles()) {
+			if (ImplUtils.hasAllQualifiedAttributes(qualifiedRoleAttributes, p
+					.getPrincipalQualifiedRoleAttributes())) {
+				pqrs.add(PrincipalQualifiedRole.toDTO(p));
+			}
+		}
+		return pqrs;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getEntityIdsWithRole(java.lang.String)
+	 */
+	public List<Long> getEntityIdsWithRole(String roleName) {
+		final ArrayList<Long> entitys = new ArrayList<Long>(0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return entitys;
+		}
+
+		entitys.ensureCapacity(role.getPrincipals().size());
+		for (Principal principal : role.getPrincipals()) {
+			entitys.add(principal.getEntityId());
+		}
+
+		return entitys;
+	}
+
+	/**
+	 *
+	 * @see org.kuali.rice.kim.service.RoleService#getEntitysWithRole(java.lang.String)
+	 */
+	public List<EntityDTO> getEntitysWithRole(String roleName) {
+		final ArrayList<EntityDTO> entitys = new ArrayList<EntityDTO>(0);
+
+		final Role role = findRoleByName(roleName);
+		if (role == null) {
+			return entitys;
+		}
+
+		entitys.ensureCapacity(role.getPrincipals().size());
+		for (Principal principal : role.getPrincipals()) {
+			entitys.add(Entity.toDTO(principal.getEntity()));
+		}
+
+		return entitys;
+	}
+
+	/**
+	 *
+	 * This method ...
+	 *
+	 * @param roleName
+	 * @return
+	 */
+	private static Role findRoleByName(final String roleName) {
+		final HashMap<String, String> name = new HashMap<String, String>();
+		name.put("NAME", roleName);
+		return ((Collection<Role>) KNSServiceLocator.getBusinessObjectService()
+				.findMatching(Role.class, name)).iterator().next();
+	}
+
+	/**
+	 *
+	 * @param kimServicesDao
+	 *            the kimServicesDao to set
+	 */
+	public void setKimServicesDao(KIMServicesDao kimServicesDao) {
+		this.kimServicesDao = kimServicesDao;
+	}
+
 }

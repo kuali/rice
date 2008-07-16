46079002c9c7bf7623cef92219f090ad3b3b348b
Shubhangi Timble <shubhangi_timble@syntelinc.com>
2008-07-16 06:03:18 +0000

    JIRA 1996 and 1994 Removed Schema Names from Alter statements to drop DOC_OVRD_IND and DOC_LOCK_CD columns from EN_DOC_HDR_T table.

--- scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
@@ -1,2 +1,2 @@
-alter table "KRICE"."EN_DOC_HDR_T" drop column "DOC_OVRD_IND";
-alter table "KRICE"."EN_DOC_HDR_T" drop column "DOC_LOCK_CD";
+alter table "EN_DOC_HDR_T" drop column "DOC_OVRD_IND";
+alter table "EN_DOC_HDR_T" drop column "DOC_LOCK_CD";

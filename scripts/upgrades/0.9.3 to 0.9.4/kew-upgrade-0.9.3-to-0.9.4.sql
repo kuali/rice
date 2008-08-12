4ce9ba7605d7f7f708e40daafc6e006a8ec30613
Eric Westfall <ewestfal@iu.edu>
2008-08-12 19:37:59 +0000

    Updated some of the sql upgrade scripts for 0.9.4.

--- scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
@@ -1,2 +1,4 @@
-alter table "EN_DOC_HDR_T" drop column "DOC_OVRD_IND";
-alter table "EN_DOC_HDR_T" drop column "DOC_LOCK_CD";
+alter table "EN_DOC_HDR_T" drop column "DOC_OVRD_IND"
+/
+alter table "EN_DOC_HDR_T" drop column "DOC_LOCK_CD"
+/

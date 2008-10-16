92b011d180c3c55fe63a2f4e73c10bdd74f64a2c
Shubhangi Timble <shubhangi_timble@syntelinc.com>
2008-10-16 13:28:46 +0000

    KULRICE-2358 Remove DTYPE column from Rice tables

--- scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
@@ -4,3 +4,9 @@ alter table "EN_DOC_HDR_T" drop column "DOC_LOCK_CD"
 /
 alter table "EN_RTE_NODE_T" drop column "CONTENT_FRAGMENT"
 /
+alter table "EN_DOC_HDR_T" drop column "DTYPE"
+/
+alter table "EN_ACTN_ITM_T" drop column "DTYPE"
+/
+alter table "EN_USR_T" drop column "DTYPE"
+/

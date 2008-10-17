fd60630b507d058d9201afde1aa6161bce4eadeb
Shubhangi Timble <shubhangi_timble@syntelinc.com>
2008-10-17 09:43:30 +0000

    KULRICE-2160 Remove unused columns from EN_DOC_TYP_T

--- scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/kew-upgrade-0.9.3-to-0.9.4.sql
@@ -10,3 +10,9 @@ alter table "EN_ACTN_ITM_T" drop column "DTYPE"
 /
 alter table "EN_USR_T" drop column "DTYPE"
 /
+alter table "EN_DOC_TYP_T" drop column "CSTM_ACTN_LIST_ATTRIB_CLS_NM"
+/
+alter table "EN_DOC_TYP_T" drop column "CSTM_ACTN_EMAIL_ATTRIB_CLS_NM"
+/
+alter table "KRICE"."EN_DOC_TYP_T" drop column "CSTM_DOC_NTE_ATTRIB_CLS_NM" 
+/

6e2e380ac67da1d65036e0318d310b1b7f3d78ad
Shubhangi Timble <shubhangi_timble@syntelinc.com>
2008-10-30 08:54:59 +0000

    KULRICE-2395 Rename references to "messageEntity' to "serviceNamespace"

--- impl/src/main/java/org/kuali/rice/kew/actions/ActionTakenEvent.java
+++ impl/src/main/java/org/kuali/rice/kew/actions/ActionTakenEvent.java
@@ -210,7 +210,7 @@ public abstract class ActionTakenEvent {
 	 * Asynchronously queues the documented to be processed by the workflow engine.
 	 */
 	protected void queueDocumentProcessing() {
-		QName documentServiceName = new QName(getRouteHeader().getDocumentType().getMessageEntity(), MessageServiceNames.DOCUMENT_ROUTING_SERVICE);
+		QName documentServiceName = new QName(getRouteHeader().getDocumentType().getServiceNamespace(), MessageServiceNames.DOCUMENT_ROUTING_SERVICE);
 		KSBXMLService documentRoutingService = (KSBXMLService) MessageServiceNames.getServiceAsynchronously(documentServiceName, getRouteHeader());
 		try {
 //			String content = String.valueOf(getRouteHeaderId());

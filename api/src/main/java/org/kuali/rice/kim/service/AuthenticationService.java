71c195e9e31eeec5aea7a6e9123527d143d4963b
Jonathan Keller <keller.jonathan@gmail.com>
2008-11-13 19:19:25 +0000

    Removed the unnecessary AuthenticationServiceBase.

--- api/src/main/java/org/kuali/rice/kim/service/AuthenticationService.java
+++ api/src/main/java/org/kuali/rice/kim/service/AuthenticationService.java
@@ -1,6 +1,9 @@
 package org.kuali.rice.kim.service;
 
+import javax.servlet.http.HttpServletRequest;
 
-
-public interface AuthenticationService extends AuthenticationServiceBase {
+public interface AuthenticationService {
+    public String getPrincipalName(HttpServletRequest request);
+    
+    public boolean validatePassword();
 }
--- api/src/main/java/org/kuali/rice/kim/service/AuthenticationServiceBase.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.rice.kim.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface AuthenticationServiceBase {
-    public String getPrincipalName(HttpServletRequest request);
-    
-    public boolean validatePassword();
-}

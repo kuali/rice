c5732fa0c3ab3d1ed0874bdde231834c95c8ea98
Poonam Bhargava <bhargavp@indiana.edu>
2008-12-03 15:42:30 +0000

    Constants added and modified according to KRIM_ATTR_DEFN_T.

--- impl/src/main/java/org/kuali/rice/kim/util/KimConstants.java
+++ impl/src/main/java/org/kuali/rice/kim/util/KimConstants.java
@@ -45,20 +45,25 @@ public class KimConstants {
 	public static final String KIM_ATTRIB_ACTION_DETAILS_AT_ROLE_MEMBER_LEVEL ="actionDetailsAtRoleMemberLevel";	
 	public static final String KIM_ATTRIB_REQUIRED ="required";	
 	public static final String KIM_ATTRIB_BEAN_NAME ="beanName";	
-	public static final String KIM_ATTRIB_CHART_CODE ="Chart Code";
+	public static final String KIM_ATTRIB_CHART_CODE ="chartOfAccountsCode";
+	public static final String KIM_ATTRIB_ORGANIZATION_CODE = "organizationCode";
+	public static final String KIM_ATTRIB_CAMPUS_CODE = "campusCode";
 	public static final String KIM_ATTRIB_TYPE_CODE ="typeCd";
 	public static final String KIM_ATTRIB_CREATED_SELF_ONLY ="createdBySelfOnly";	
-	public static final String KIM_ATTRIB_ORGANIZATION_CODE = "Organization Code";
-	public static final String KIM_ATTRIB_CAMPUS_CODE = "Campus Code";
 	public static final String KIM_ATTRIB_NAMESPACE_CODE = "Namespace Code";
 	public static final String KIM_ATTRIB_DOCUMENT_TYPE_NAME = "Document Type Name";
 	public static final String KIM_ATTRIB_ROUTE_STATUS_CODE = "Route Status Code";
-	public static final String KIM_ATTRIB_ROUTE_NODE_NAME = "Route Node Name";
-	public static final String KIM_ATTRIB_DOCUMENT_NUMBER = "Document Number";
-	public static final String KIM_ATTRIB_PROPERTY_NAME = "Property Name";
+	public static final String KIM_ATTRIB_ROUTE_NODE_NAME = "routeNodeName";
+	public static final String KIM_ATTRIB_DOCUMENT_NUMBER = "documentNumber";
+	public static final String KIM_ATTRIB_PROPERTY_NAME = "propertyName";
 	public static final String KIM_ATTRIB_COMPONENT_NAME = "Component";
 	public static final String KIM_ATTRIB_EXISTING_RECORDS_ONLY = "existingRecordsOnly";
+	public static final String KIM_ATTRIB_PRINCIPAL_ID = "principalId";
+	public static final String KIM_ATTRIB_DESCEND_HIERARCHY = "descendHierarchy";
 	
+	public static final String KIM_ROLE_NAME_USER = "User";
+	public static final String KIM_ROLE_NAME_BILLER = "Biller";
+	public static final String KIM_ROLE_NAME_PROCESSOR = "Processor";	
 	public static final String DEFAULT_PERMISSION_TYPE_SERVICE = "defaultPermissionTypeService";
 
 	@Deprecated

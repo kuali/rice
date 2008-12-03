a6998ca47c098289112f4ec1f728dd722570491f
Aaron Hamid <aaron@incsw.com>
2008-12-03 23:17:08 +0000

    KULRICE-2110 - rice service injection unit test; for now this lives in the KEW test suite

---
+++ kew/src/test/java/org/kuali/rice/core/util/ClientDocumentServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.core.util;
+
+import org.kuali.rice.kns.dao.DocumentDao;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.service.DateTimeService;
+import org.kuali.rice.kns.service.DictionaryValidationService;
+import org.kuali.rice.kns.service.DocumentAuthorizationService;
+import org.kuali.rice.kns.service.DocumentHeaderService;
+import org.kuali.rice.kns.service.DocumentTypeService;
+import org.kuali.rice.kns.service.KualiRuleService;
+import org.kuali.rice.kns.service.MaintenanceDocumentService;
+import org.kuali.rice.kns.service.NoteService;
+import org.kuali.rice.kns.service.impl.DocumentServiceImpl;
+import org.kuali.rice.kns.workflow.service.WorkflowDocumentService;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Extends DocumentServiceImpl to exercise providing an overridden bean in the client
+ * without having to redefine and inject every Rice bean dependency.
+ * {@link RiceService} annotation and {@link GRLServiceInjectionPostProcessor} should
+ * inject everything we need. 
+ * 
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ */
+public class ClientDocumentServiceImpl extends DocumentServiceImpl implements InitializingBean {
+    public static ClientDocumentServiceImpl me;
+
+    public void afterPropertiesSet() throws Exception {
+        me = this;
+    }
+
+    // some member fields are private at the moment; do the best we can
+
+    public WorkflowDocumentService getWorkflowDocumentService() {
+        return workflowDocumentService;
+    }
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+    public DocumentAuthorizationService getDocumentAuthorizationService() {
+        return documentAuthorizationService;
+    }
+    public DocumentDao getDocumentDao() {
+        return documentDao;
+    }
+}
---
+++ kew/src/test/java/org/kuali/rice/core/util/RiceServiceInjectedObject.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.core.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.rice.kew.actionlist.service.ActionListService;
+import org.kuali.rice.kew.actionrequest.service.ActionRequestService;
+import org.kuali.rice.kew.mail.service.ActionListEmailService;
+import org.springframework.beans.factory.BeanNameAware;
+
+
+/**
+ * This is an object that is defined as a bean in the TestKewSpringBeans.xml (the test "client") and
+ * uses RiceService annotations to have Rice services from the Rice context injected into it instead
+ * of Spring wiring.  
+ * 
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ */
+public class RiceServiceInjectedObject implements BeanNameAware {
+    // hack for testing purposes
+    public static Map<String, RiceServiceInjectedObject> beans = new HashMap<String, RiceServiceInjectedObject>();
+    
+    public void setBeanName(String name) {
+        beans.put(name, this);
+    }
+
+    @RiceService(name="enActionListService")
+    public ActionListService als;
+    
+    /**
+     * Set by {@link #setActionListEmailService()} annotation
+     */
+    public ActionListEmailService ales;
+    
+    /**
+     * Set by {@link #getActionRequestService()} annotation
+     */
+    public ActionRequestService ars;
+    
+    @RiceService(name="enApplicationConstantsService")
+    public Object wireMeInSpring;
+
+    // DON'T WIRE THIS IN SPRING
+    @RiceService(resourceLoader="KEW_SPRING+PLUGIN_REGISTRY_CONTAINER_RESOURCE_LOADER", name="enActionListEmailService") // specify the resource loader by name here
+    public void setActionListEmailService(ActionListEmailService ales) {
+        this.ales = ales;
+    }
+
+
+    @RiceService(name="enActionRequestService")
+    public ActionRequestService getActionRequestService() {
+        return ars;
+    }
+    
+    public void setActionRequestService(ActionRequestService ars) {
+        this.ars = ars;
+    }
+    
+
+    
+    public void setWireMeInSpring(Object o) {
+        this.wireMeInSpring = o;
+    }
+
+}

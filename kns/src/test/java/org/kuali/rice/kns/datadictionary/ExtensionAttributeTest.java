23d2aaeb6ec90008fa3973d82023b266b5883ffe
Eric Westfall <ewestfal@iu.edu>
2008-12-08 17:12:12 +0000

    Fixed extension attribute test so that it's loading some of the KIM dd files which are referenced from the core KNS data dictionary files.

--- kns/src/test/java/org/kuali/rice/kns/datadictionary/ExtensionAttributeTest.java
+++ kns/src/test/java/org/kuali/rice/kns/datadictionary/ExtensionAttributeTest.java
@@ -54,6 +54,11 @@ public class ExtensionAttributeTest extends KNSTestBase {
 		dd = new DataDictionary();
 		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kns/bo/datadictionary");
 		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kns/document/datadictionary");
+		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kim/bo/datadictionary/PersonImpl.xml");
+		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kim/bo/datadictionary/KimBaseBeans.xml");
+		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kim/bo/datadictionary/KimGroupImpl.xml");
+		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kim/bo/datadictionary/KimRoleImpl.xml");
+		dd.addConfigFileLocation("file:" + getBaseDir() + "/../impl/src/main/resources/org/kuali/rice/kim/bo/datadictionary/KimTypeImpl.xml");
 		dd.addConfigFileLocation("classpath:org/kuali/rice/kns/test/document");
         dd.parseDataDictionaryConfigurationFiles( false );
 	}

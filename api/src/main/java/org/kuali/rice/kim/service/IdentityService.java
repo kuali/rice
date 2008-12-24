9535d9c45396de77fe73b3de5ea5e1b3ab31f426
Jonathan Keller <keller.jonathan@gmail.com>
2008-12-24 00:02:55 +0000

    Merged IdentityService interfaces.

--- api/src/main/java/org/kuali/rice/kim/service/IdentityService.java
+++ api/src/main/java/org/kuali/rice/kim/service/IdentityService.java
@@ -8,7 +8,7 @@ import org.kuali.rice.kim.bo.entity.KimEntity;
 import org.kuali.rice.kim.bo.entity.KimPrincipal;
 import org.kuali.rice.kim.bo.entity.NamePrincipalName;
 
-public interface IdentityService extends IdentityServiceBase {
+public interface IdentityService {
 	
 	/** Get an entity object from the main entity ID */ 
 	KimEntity getEntity( String entityId );
@@ -32,4 +32,20 @@ public interface IdentityService extends IdentityServiceBase {
     public Map<String, NamePrincipalName> getDefaultNamesForPrincipalIds(List<String> principalIds);
     
     public Map<String, EntityName> getDefaultNamesForEntityIds(List<String> entityIds);
+
+	/** Return the entity ID for the given principal */
+	String getEntityIdByPrincipalId( String principalId );
+	
+	/** Return the entity ID for a given principal name */
+	String getEntityIdByPrincipalName( String principalName );
+
+	/** Return the principal ID for a given principal name */
+	String getPrincipalIdByPrincipalName( String principalName );
+
+	/** Find entity IDs based on the given criteria. */
+	public List<String> lookupEntityIds(Map<String,String> searchCriteria);
+    
+    // Do we need APIs here to pull the extended Entity information that we don't need to load always?
+    // like addresses, external identifiers, citizenship?
+
 }

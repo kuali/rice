588374e6320bffb8a697a19cb266513d59d2d690
George Lindholm <george.lindholm@ubc.ca>
2009-01-13 22:30:09 +0000

    ResponsibilityIdDAOJpa - Created unit test

---
+++ kew/src/test/java/org/kuali/rice/kew/responsibility/ResponsibilityIdServiceTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kew.responsibility;
+
+import org.junit.Test;
+import org.kuali.rice.kew.responsibility.service.ResponsibilityIdService;
+import org.kuali.rice.kew.service.KEWServiceLocator;
+import org.kuali.rice.kew.test.KEWTestCase;
+
+/**
+ * Test the Responsibility ID Service
+ *
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ *
+ */
+public class ResponsibilityIdServiceTest extends KEWTestCase {
+    @Test
+    public void testResponsibilityIdService() {
+        ResponsibilityIdService idService = (ResponsibilityIdService)KEWServiceLocator.getService("enResponsibilityIdService");
+        final Long firstId = idService.getNewResponsibilityId();
+        assertNotNull("Did not get a responsibility id", firstId);
+        final Long secondId = idService.getNewResponsibilityId();
+        assertNotNull("Did not get a second responsibility id", secondId);
+        assertTrue("Both responsibilityIds are equal", firstId.compareTo(secondId) != 0);
+    }
+}

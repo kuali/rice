75013c0d7554145b717f86d8434af11c06dd03db
David Elyea <delyea@iu.edu>
2009-01-22 20:33:08 +0000

    KULRICE-2507 - corrected the ordering of the new date fields for export

--- impl/src/main/java/org/kuali/rice/kew/xml/export/RuleXmlExporter.java
+++ impl/src/main/java/org/kuali/rice/kew/xml/export/RuleXmlExporter.java
@@ -67,6 +67,12 @@ public class RuleXmlExporter implements XmlExporter, XmlConstants {
             renderer.renderTextElement(ruleElement, NAME, rule.getName());
         }
         renderer.renderTextElement(ruleElement, DOCUMENT_TYPE, rule.getDocTypeName());
+        if(rule.getToDateString()!=null){
+            renderer.renderTextElement(ruleElement, TO_DATE, rule.getToDateString());
+        }
+        if(rule.getFromDateString()!=null){
+            renderer.renderTextElement(ruleElement, FROM_DATE, rule.getFromDateString());
+        }
         if (rule.getRuleTemplateName() != null) {
             renderer.renderTextElement(ruleElement, RULE_TEMPLATE, rule.getRuleTemplateName());
         }
@@ -83,14 +89,7 @@ public class RuleXmlExporter implements XmlExporter, XmlConstants {
         if (delegation != null) {
             renderer.renderTextElement(ruleElement, DELEGATION_TYPE, delegation.getDelegationType());
         }
-        if(rule.getToDateString()!=null){
-        	renderer.renderTextElement(ruleElement, TO_DATE, rule.getToDateString());
-        }
-        if(rule.getFromDateString()!=null){
-        	renderer.renderTextElement(ruleElement, FROM_DATE, rule.getFromDateString());
-        }
-        
-        
+
     }
 
     private void exportRuleExtensions(Element parent, List ruleExtensions) {

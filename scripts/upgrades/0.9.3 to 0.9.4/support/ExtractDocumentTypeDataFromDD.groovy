c09c5894590560c1ecedab51687e594dd3a7d771
Eric Westfall <ewestfal@iu.edu>
2009-01-25 03:13:29 +0000

    Committed an updated version of the DD doc type extract script which includes help urls

--- scripts/upgrades/0.9.3 to 0.9.4/support/ExtractDocumentTypeDataFromDD.groovy
+++ scripts/upgrades/0.9.3 to 0.9.4/support/ExtractDocumentTypeDataFromDD.groovy
@@ -6,6 +6,9 @@ inputDir = '.'
 outputFileName = 'document-type-data-extract.sql'
 fileNameSuffix = 'Document.xml'
 
+labelUpdates = []
+helpUrlUpdates = []
+
 count = 0
 for (arg in args) {
 	if (arg == '-in') inputDir = args[count + 1]
@@ -15,7 +18,7 @@ for (arg in args) {
 }	
 
 
-def processDir( outputFile, dir ) {
+def processDir( dir ) {
     //println "Processing Directory: " + dir
     def files = new File(dir).list()
 	    
@@ -24,32 +27,53 @@ def processDir( outputFile, dir ) {
         File file = new File(dir, fileName)
         //println "Processing File: " + file.getAbsolutePath()
         if ( file.isDirectory() ) {
-        	processDir(outputFile, file.getAbsolutePath() )
+        	processDir( file.getAbsolutePath() )
         } else if (fileName.endsWith( fileNameSuffix )){
         	//println "Processing XML "+fileName  
         	def builder     = DocumentBuilderFactory.newInstance().newDocumentBuilder()
         	def inputStream = new FileInputStream(file)
         	def elements     = builder.parse(inputStream).documentElement
 				
-        	XPathAPI.selectNodeList(elements, '/beans/bean').each{ processBean(outputFile, it, file) }
+        	XPathAPI.selectNodeList(elements, '/beans/bean').each{ processBean(it, file) }
 			
         }
     }
+
 }
 
-def processBean(outputFile, bean, file) {
+def processBean(bean, file) {
 	def parentAttribute = bean.getAttribute('parent')
 	if (parentAttribute.endsWith("DocumentEntry")) {
-		println 'Processing DocumentEntry for file ' + file.getAbsolutePath()
+		//println 'Processing DocumentEntry for file ' + file.getAbsolutePath()
 		def label = getBeanPropertyValue(bean, 'label')
 		def documentTypeName = getBeanPropertyValue(bean, 'documentTypeName')
-		outputFile.append(generateUpdateStatement(label, documentTypeName))
+		labelUpdates.add(generateLabelUpdateStatement(label, documentTypeName))
+		processHelpDefinition(bean, documentTypeName)
 	}
 }
 
-def generateUpdateStatement(label, documentTypeName) {
-	// TODO add help url
-	return "UPDATE KREW_DOC_TYP_T SET LBL='" + label + "' WHERE DOC_TYP_NM='" + documentTypeName + "'\n\\\n"
+def processHelpDefinition(beanElement, documentTypeName) {
+	def helpDefinitionNode = XPathAPI.selectSingleNode(beanElement, 'property[@name=\'helpDefinition\']')
+	if (helpDefinitionNode != null) {
+		def helpDefBean = XPathAPI.selectSingleNode(helpDefinitionNode, 'bean')
+		def helpParamName = getBeanPropertyValue(helpDefBean, 'parameterName')
+		if (helpParamName == null || helpParamName == '') {
+			throw new RuntimeException("Failed to find help parameter name for HelpDefinition")
+		}
+		helpUrlUpdates.add(generateHelpUrlUpdateStatement(helpParamName, documentTypeName))
+	} else {
+		println 'Did not find HelpDefinition for document type: ' + documentTypeName
+	}
+}
+
+def generateLabelUpdateStatement(label, documentTypeName) {
+	return "UPDATE KREW_DOC_TYP_T SET LBL='" + label + "' WHERE DOC_TYP_NM='" + documentTypeName + "'\n\\"
+}
+
+def generateHelpUrlUpdateStatement(helpParameterName, documentTypeName) {
+	return "UPDATE KREW_DOC_TYP_T SET HLP_URL=" + 
+		"(SELECT TXT FROM KRNS_PARM_T WHERE PARM_NM='" + helpParameterName + "' AND PARM_TYP_CD='HELP') " +
+		"WHERE DOC_TYP_NM='" + documentTypeName + "'\n\\"
 }
 
 def getBeanPropertyValue(beanElement, propertyName) {
@@ -68,4 +92,14 @@ def sqlFile = new File (outputFileName)
 if (sqlFile.exists()) {
 	sqlFile.delete();
 }
-processDir(sqlFile, inputDir)
+processDir(inputDir)
+sqlFile.append('-- Start Label Updates\n\n')
+labelUpdates.each {
+	String statement ->
+	sqlFile.append(statement + "\n")
+}
+sqlFile.append('\n\n\n\n\n-- Start Help URL Updates\n\n')
+helpUrlUpdates.each {
+	String statement ->
+	sqlFile.append(statement + "\n")
+}

d05ce0fe0e238ee5f500b7b7e490291a0cf925a0
Ailish Byrne <abyrne@iu.edu>
2009-01-25 00:20:21 +0000

    rolling back change to role route module to try to fix grouping of action requests cause it backfired

--- impl/src/main/java/org/kuali/rice/kew/role/RoleRouteModule.java
+++ impl/src/main/java/org/kuali/rice/kew/role/RoleRouteModule.java
@@ -180,22 +180,18 @@ public class RoleRouteModule implements RouteModule {
 	
 	
 	class ResponsibilitySet {
-		private String roleId;
 		private String actionRequestCode;
 		private String approvePolicy;
-		private Integer priorityNumber;
 		private List<ResponsibilityActionInfo> responsibilities = new ArrayList<ResponsibilityActionInfo>();
 
 		public ResponsibilitySet(ResponsibilityActionInfo responsibility) {
-			this.roleId = responsibility.getRoleId();
 			this.actionRequestCode = responsibility.getActionTypeCode();
 			this.approvePolicy = responsibility.getActionPolicyCode();
-			this.priorityNumber = responsibility.getPriorityNumber();
 		}
 		
 		public boolean matches(ResponsibilityActionInfo responsibility) {
-			return  responsibility.getRoleId().equals(roleId) && responsibility.getActionTypeCode().equals(actionRequestCode) &&
-				responsibility.getActionPolicyCode().equals(approvePolicy) && responsibility.getPriorityNumber().equals(priorityNumber);
+			return responsibility.getActionTypeCode().equals(actionRequestCode) &&
+				responsibility.getActionPolicyCode().equals(approvePolicy);
 		}
 
 		public String getActionRequestCode() {

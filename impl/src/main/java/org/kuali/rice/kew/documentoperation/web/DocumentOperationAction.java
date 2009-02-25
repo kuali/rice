609a9c722c116b494d31c2c886268896a7bd76f6
Peter Giles <g1l3sp@gmail.com>
2009-02-25 22:00:50 +0000

    KULRICE-2708
    DocumentOperationAction:
    - Removed empty overriding checkAuthorization(...) method
    
    struts-config.xml:
    - set contextRelative="true" for exceptionIncidentHandler global forward

--- impl/src/main/java/org/kuali/rice/kew/documentoperation/web/DocumentOperationAction.java
+++ impl/src/main/java/org/kuali/rice/kew/documentoperation/web/DocumentOperationAction.java
@@ -753,16 +753,6 @@ public class DocumentOperationAction extends KewKualiAction {
 		}
 	}
 	
-	/**
-	 * chb: 12Jan2009
-	 * This needs to be fleshed out after permissions are built
-	 */
-	//TODO
-	@Override
-	public void checkAuthorization(ActionForm form, String methodToCall) throws AuthorizationException
-	{
-	}
-
 	public ActionForward moveDocument(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		try {
 			DocumentOperationForm docForm = (DocumentOperationForm) form;

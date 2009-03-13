13fc9ab1dc640096cdacab6f294f7aff6b97dc97
Jeremy Hanson <jjhanso@iastate.edu>
2009-03-13 18:02:04 +0000

    KULRICE-2731 KULRICE-2790
    Initial commit to allow BusinessObjectService to persist more than one data source, and split the rice datasource configuration for KNS across client-side and server-side.
    
    Everything is coded and tested with a single datasource.  Will test multiple datasources next week at F2F.

---
+++ impl/src/main/java/org/kuali/rice/kns/service/impl/KNSModuleService.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kns.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.bo.ExternalizableBusinessObject;
+import org.kuali.rice.kns.util.ExternalizableBusinessObjectUtils;
+
+/**
+ * This is a description of what this class does - jjhanso don't forget to fill this in.
+ *
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ *
+ */
+public class KNSModuleService extends ModuleServiceBase {
+
+    protected List<String> businessObjects;
+
+    @Override
+    public boolean isResponsibleFor(Class businessObjectClass) {
+        if (businessObjects.contains(businessObjectClass.getName())) {
+            return true;
+        }
+        if (ExternalizableBusinessObject.class.isAssignableFrom(businessObjectClass)) {
+            Class externalizableBusinessObjectInterface = ExternalizableBusinessObjectUtils.determineExternalizableBusinessObjectSubInterface(businessObjectClass);
+            if (externalizableBusinessObjectInterface != null) {
+                for (String prefix : getModuleConfiguration().getPackagePrefixes()) {
+                    if (externalizableBusinessObjectInterface.getPackage().getName().startsWith(prefix)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<String> getBusinessObjects() {
+        return this.businessObjects;
+    }
+
+    public void setBusinessObjects(List<String> businessObjects) {
+        this.businessObjects = businessObjects;
+    }
+}

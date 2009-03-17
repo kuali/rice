7d358ed53625cc59ccd9383fc50c3c8c99e7f584
Tom Bradford <tbradford@kuali.org>
2009-03-17 12:54:35 +0000

    [KULRICE-2865] Extracted the Campus getter methods into an ExternalizableBusinessObject interface (CampusEBO)

---
+++ impl/src/main/java/org/kuali/rice/kns/bo/CampusEBO.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kns.bo;
+
+/**
+ * Campus Externalizable Business Object 
+ * 
+ * @author Kuali Rice Team (kuali-rice@googlegroups.com)
+ *
+ */
+public interface CampusEBO extends ExternalizableBusinessObject {
+
+	/**
+	 * Gets the campusCode attribute.
+	 * 
+	 * @return Returns the campusCode
+	 * 
+	 */
+	public String getCampusCode();
+
+	/**
+	 * Gets the campusName attribute.
+	 * 
+	 * @return Returns the campusName
+	 * 
+	 */
+	public String getCampusName();
+
+	/**
+	 * Gets the campusShortName attribute.
+	 * 
+	 * @return Returns the campusShortName
+	 * 
+	 */
+	public String getCampusShortName();
+
+	/**
+	 * Gets the campusTypeCode attribute.
+	 * 
+	 * @return Returns the campusTypeCode
+	 * 
+	 */
+	public String getCampusTypeCode();
+
+	/**
+	 * Gets the campusType attribute. 
+	 * @return Returns the campusType.
+	 */
+	public CampusType getCampusType();
+
+	/**
+	 * @return the active
+	 */
+	public boolean isActive();
+
+}

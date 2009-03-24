7713239535eaeb3cc2e6b27d1bdf1b104d86d535
Jean Zhour <zjzhou@ucdavis.edu>
2009-03-24 22:21:32 +0000

    KFSMI-2875, KULRICE-2862 Field values remains lowercase by the time the rule class is executed

--- impl/src/main/java/org/kuali/rice/kns/util/FieldUtils.java
+++ impl/src/main/java/org/kuali/rice/kns/util/FieldUtils.java
@@ -626,12 +626,20 @@ public class FieldUtils {
                     try {
                     	//convert to upperCase based on data dictionary
                     	Class businessObjectClass = bo.getClass();
-                    	//boolean upperCase = getDataDictionaryService().getAttributeForceUppercase(businessObjectClass, propertyName);
+                    	boolean upperCase = false;
+                         try {
+                        	 upperCase = getDataDictionaryService().getAttributeForceUppercase(businessObjectClass, propertyName);
+                         }
+                         catch (UnknownBusinessClassAttributeException t) {
+                             boolean catchme = true;
+                             // throw t;
+                         }
+                    	
                     	Object fieldValue = fieldValues.get(propertyName);
                     	
-                    	//if(upperCase && fieldValue instanceof String){
-                    	//	fieldValue = ((String) fieldValue).toUpperCase(); 
-                    	//}
+                    	if(upperCase && fieldValue instanceof String){
+                    		fieldValue = ((String) fieldValue).toUpperCase(); 
+                    	}
                         ObjectUtils.setObjectProperty(bo, propertyName, type, fieldValue);
                     }
                     catch (FormatException e) {

6f77a9d86b52e6571882a09a0584a2daefedf42c
Tom Bradford <tbradford@kuali.org>
2009-05-21 23:19:23 +0000

    Pulling the PersistenceBroker / EntityManager from the BusinessObject class that is passed in

--- impl/src/main/java/org/kuali/rice/kns/dao/jdbc/SequenceAccessorDaoJdbc.java
+++ impl/src/main/java/org/kuali/rice/kns/dao/jdbc/SequenceAccessorDaoJdbc.java
@@ -17,33 +17,70 @@ package org.kuali.rice.kns.dao.jdbc;
 
 import javax.persistence.EntityManager;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.ojb.broker.PBKey;
 import org.apache.ojb.broker.PersistenceBroker;
+import org.kuali.rice.core.config.ConfigurationException;
+import org.kuali.rice.core.util.OrmUtils;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.bo.DocumentHeader;
+import org.kuali.rice.kns.bo.ModuleConfiguration;
 import org.kuali.rice.kns.dao.SequenceAccessorDao;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.kuali.rice.kns.service.KualiModuleService;
+import org.kuali.rice.kns.service.ModuleService;
+import org.springmodules.orm.ojb.OjbFactoryUtils;
 
 /**
  * This class uses the KualiDBPlatform to get the next number from a given sequence.
  */
 public class SequenceAccessorDaoJdbc extends PlatformAwareDaoBaseJdbc implements SequenceAccessorDao {
-	private EntityManager entityManager;
-	private PersistenceBroker broker;
+	private KualiModuleService kualiModuleService;
+	
+	public Long getNextAvailableSequenceNumber(String sequenceName, 
+			Class<? extends BusinessObject> clazz) {
+		
+        ModuleService moduleService = getKualiModuleService().getResponsibleModuleService(clazz);
+        if ( moduleService == null )
+        	throw new ConfigurationException("moduleService is null");
+        	        	
+        ModuleConfiguration moduleConfig = moduleService.getModuleConfiguration();
+        if ( moduleConfig == null )
+        	throw new ConfigurationException("moduleConfiguration is null");
+        
+    	String dataSourceName = moduleConfig.getDataSourceName();
+    	EntityManager entityManager = moduleConfig.getEntityManager();
+
+        if ( StringUtils.isEmpty(dataSourceName) ) 
+        	throw new ConfigurationException("dataSourceName is not set");       	
+        	
+    	if ( OrmUtils.isJpaAnnotated(clazz) && OrmUtils.isJpaEnabled() ) {
+            if ( entityManager != null ) 
+            	return getDbPlatform().getNextValSQL(sequenceName, entityManager);
+            else
+            	throw new ConfigurationException("EntityManager is null");
+        } 
+    	else {
+    		PBKey key = new PBKey(dataSourceName);
+    		PersistenceBroker broker = OjbFactoryUtils.getPersistenceBroker(key, true);
+    		if ( broker != null )
+    			return getDbPlatform().getNextValSQL(sequenceName, broker);
+    		else
+    			throw new ConfigurationException("PersistenceBroker is null");            			
+        }
+	}
 	
     /**
      * @see org.kuali.rice.kns.dao.SequenceAccessorDao#getNextAvailableSequenceNumber(java.lang.String)
      */
     public Long getNextAvailableSequenceNumber(String sequenceName) {
-    	if ( broker != null )
-    		return getDbPlatform().getNextValSQL(sequenceName, broker);
-    	else if ( entityManager != null ) 
-    		return getDbPlatform().getNextValSQL(sequenceName, entityManager);
-    	else
-    		throw new RuntimeException("Either broker or entityManager must be set");
+    	// Use DocumentHeader to get the dataSourceName associated with KNS
+    	return getNextAvailableSequenceNumber(sequenceName, DocumentHeader.class);
     }
-
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
-	
-	public void setPersistenceBroker(PersistenceBroker broker) {
-		this.broker = broker;
-	}
+    
+    private KualiModuleService getKualiModuleService() {
+        if ( kualiModuleService == null ) 
+            kualiModuleService = KNSServiceLocator.getKualiModuleService();
+        return kualiModuleService;
+    }
 }

05a49858ce7c9b22d43ab5e178dcbda358f9e9e2
Ge Zhang <g1zhang@ucsd.edu>
2009-07-03 02:45:13 +0000

    fix unit test

--- kew/src/test/java/org/kuali/rice/kew/docsearch/CustomDocumentSearchGeneratorTest.java
+++ kew/src/test/java/org/kuali/rice/kew/docsearch/CustomDocumentSearchGeneratorTest.java
@@ -88,6 +88,8 @@ public class CustomDocumentSearchGeneratorTest extends DocumentSearchTestBase {
         // delete the parameter
         KNSServiceLocator.getBusinessObjectService().delete(KNSServiceLocator.getParameterService().retrieveParameter(KEWConstants.KEW_NAMESPACE, KNSConstants.DetailTypes.DOCUMENT_SEARCH_DETAIL_TYPE, KEWConstants.DOC_SEARCH_RESULT_CAP));
 
+        adjustResultSetCapApplicationConstantValue(CustomDocumentSearchGenerator.RESULT_SET_LIMIT);
+        
         KEWServiceLocator.getDocumentSearchService().getList(user.getPrincipalId(), criteria);
         assertEquals("Criteria threshold should equal custom generator class threshold", CustomDocumentSearchGenerator.RESULT_SET_LIMIT, criteria.getThreshold().intValue());
     }

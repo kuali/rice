721d5be6d91b64b1315e0361be9f413c9989a3aa
Eric Westfall <ewestfal@iu.edu>
2009-07-06 21:06:13 +0000

    KULRICE-3278

--- scripts/upgrades/0.9.3 to 0.9.4/db-updates-during-qa/07-06-2009.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/db-updates-during-qa/07-06-2009.sql
@@ -2,5 +2,24 @@ DROP TABLE KRIM_PERM_RQRD_ATTR_T
 /
 DROP TABLE KRIM_RSP_RQRD_ATTR_T
 /
+
+-- KULRICE-3287
+
 DELETE FROM KRNS_PARM_T where NMSPC_CD='KR-WKFLW' AND PARM_DTL_TYP_CD='All' AND PARM_NM='APPLICATION_CONTEXT'
 /
+
+-- KULRICE-3278
+
+DECLARE
+
+    ref_perm_id VARCHAR2(40);
+
+BEGIN
+
+    SELECT perm_id INTO ref_perm_id FROM krim_perm_attr_data_t WHERE attr_val = 'org.kuali.rice.kew.web.backdoor.AdministrationAction';
+    DELETE FROM krim_perm_attr_data_t WHERE perm_id = ref_perm_id;
+    DELETE FROM krim_role_perm_t WHERE perm_id = ref_perm_id;
+    DELETE FROM krim_perm_t WHERE perm_id = ref_perm_id;
+
+END;
+/ 

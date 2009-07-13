f0c7c6e6a14912bb509927de6c9c3a4fa3332710
Chad Hagstrom <cah292@cornell.edu>
2009-07-13 17:51:31 +0000

    KULRICE-3349: Included an SQL statement for adding a valid doc handler URL to the Campus, Campus Type, Country, County, Postal Code, and State document types.

--- scripts/upgrades/0.9.3 to 0.9.4/db-updates-during-qa/07-13-2009.sql
+++ scripts/upgrades/0.9.3 to 0.9.4/db-updates-during-qa/07-13-2009.sql
@@ -1,3 +1,6 @@
-
 -- KULRICE-3126 This will turn off field level help by default.
 UPDATE KRNS_PARM_T SET TXT = 'N' WHERE NMSPC_CD = 'KR-NS' AND PARM_DTL_TYP_CD = 'All' AND PARM_NM = 'ENABLE_FIELD_LEVEL_HELP_IND'
+/
+-- KULRICE-3349: Add the doc handler URL to the Campus, Campus Type, Country, County, Postal Code, and State document types.
+UPDATE KREW_DOC_TYP_T SET DOC_HDLR_URL='${kr.url}/maintenance.do?methodToCall=docHandler' WHERE DOC_TYP_NM='CampusMaintenanceDocument' OR DOC_TYP_NM='CampusTypeMaintenanceDocument' OR DOC_TYP_NM='CountryMaintenanceDocument' OR DOC_TYP_NM='CountyMaintenanceDocument' OR DOC_TYP_NM='PostalCodeMaintenanceDocument' OR DOC_TYP_NM='StateMaintenanceDocument'
+/

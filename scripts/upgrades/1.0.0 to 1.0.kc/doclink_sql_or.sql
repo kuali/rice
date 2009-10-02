7c3c9effd590746eb00b7ac19823737f6c822d2a
Ge Zhang <g1zhang@ucsd.edu>
2009-10-02 18:04:33 +0000

    KULRICE-3073 doc linking db updates

---
+++ scripts/upgrades/1.0.0 to 1.0.kc/doclink_sql_or.sql
@@ -0,0 +1,18 @@
+drop sequence krew_doc_lnk_s 
+
+create sequence krew_doc_lnk_s increment by 1 start with 2000 cache 20
+
+drop index KREW_DOC_LNK_TI1 on krew_doc_lnk_t
+
+drop table krew_doc_lnk_t
+
+create table krew_doc_lnk_t(
+
+           DOC_LNK_ID NUMBER(19),
+           ORGN_DOC_ID NUMBER(14) NOT NULL,
+           DEST_DOC_ID NUMBER(14) NOT NULL, 
+           
+           CONSTRAINT KREW_DOC_LNK_TP1 PRIMARY KEY (DOC_LNK_ID)
+)
+
+create INDEX KREW_DOC_LNK_TI1 on krew_doc_lnk_t(ORGN_DOC_ID)

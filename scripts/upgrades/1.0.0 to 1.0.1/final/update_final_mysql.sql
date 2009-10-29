89c99e7f87e5f79ca4b22fbdd3d6d1414a883c61
Peter Giles <g1l3sp@gmail.com>
2009-10-29 23:13:55 +0000

    KULRICE-3653: No context-sensitive help available for Rice documents https://test.kuali.org/jira/browse/KULRICE-3653
    Added this sql for setting up context-sensitive help to the final rollup for mysql.

--- scripts/upgrades/1.0.0 to 1.0.1/final/update_final_mysql.sql
+++ scripts/upgrades/1.0.0 to 1.0.1/final/update_final_mysql.sql
@@ -881,6 +881,41 @@ go
 UPDATE KRNS_PARM_T SET TXT='MM/dd/yyyy hh:mm a;MM/dd/yyyy;MM/dd/yyyy HH:mm:ss;MM/dd/yy;MM-dd-yy;MMMM dd;yyyy;MMddyy' WHERE NMSPC_CD='KR-NS' AND PARM_DTL_TYP_CD='All' AND PARM_NM='STRING_TO_TIMESTAMP_FORMATS' AND APPL_NMSPC_CD='KUALI'
 go
 
-
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fperson.htm' where doc_typ_nm = 'IdentityManagementPersonDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fgroup.htm' where doc_typ_nm = 'IdentityManagementGroupDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Frole.htm' where doc_typ_nm = 'IdentityManagementRoleDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fpermission.htm' where doc_typ_nm = 'IdentityManagementGenericPermissionMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fresponsibility.htm' where doc_typ_nm = 'IdentityManagementReviewResponsibilityMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fcampus.htm' where doc_typ_nm = 'CampusMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fcountry.htm' where doc_typ_nm = 'CountryMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fcounty.htm' where doc_typ_nm = 'CountyMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fpostalcode.htm' where doc_typ_nm = 'PostalCodeMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fstate.htm' where doc_typ_nm = 'StateMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Faddresstype.htm' where doc_typ_nm = 'PMAT' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fcampustype.htm' where doc_typ_nm = 'CampusTypeMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fphonetype.htm' where doc_typ_nm = 'PMPT' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fdocumenttype.htm' where doc_typ_nm = 'DocumentTypeDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fparameter.htm' where doc_typ_nm = 'ParameterMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fparametercomponent.htm' where doc_typ_nm = 'ParameterDetailTypeMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fparametertype.htm' where doc_typ_nm = 'ParameterTypeMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/
+update krew_doc_typ_t set help_def_url = 'default.htm?turl=WordDocuments%2Fnamespace.htm' where doc_typ_nm = 'NamespaceMaintenanceDocument' and actv_ind = 1 and cur_ind = 1
+/ 
 
 

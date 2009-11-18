d2aa47f041948f69f53894b5b392052bb3c86adf
Jonathan Keller <keller.jonathan@gmail.com>
2009-11-18 22:14:36 +0000

    KULRICE-3702 : added caching of document type-based permission checks which are not going to change unless document types are re-ingested since the permission check takes no role qualifiers.

--- impl/src/main/java/org/kuali/rice/kew/doctype/service/impl/DocumentTypeServiceImpl.java
+++ impl/src/main/java/org/kuali/rice/kew/doctype/service/impl/DocumentTypeServiceImpl.java
@@ -25,6 +25,7 @@ import java.util.List;
 import org.jdom.Element;
 import org.kuali.rice.kew.doctype.bo.DocumentType;
 import org.kuali.rice.kew.doctype.dao.DocumentTypeDAO;
+import org.kuali.rice.kew.doctype.service.DocumentTypePermissionService;
 import org.kuali.rice.kew.doctype.service.DocumentTypeService;
 import org.kuali.rice.kew.dto.DTOConverter;
 import org.kuali.rice.kew.dto.DocumentTypeDTO;
@@ -171,6 +172,7 @@ public class DocumentTypeServiceImpl implements DocumentTypeService {
     	LOG.info("clearing DocumentType cache because of local update");
     	KEWServiceLocator.getCacheAdministrator().flushGroup(DOCUMENT_TYPE_ID_CACHE_GROUP);
     	KEWServiceLocator.getCacheAdministrator().flushGroup(DOCUMENT_TYPE_NAME_CACHE_GROUP);
+    	KEWServiceLocator.getCacheAdministrator().flushGroup(DocumentTypePermissionService.DOC_TYPE_PERM_CACHE_GROUP);
     	KEWServiceLocator.getCacheAdministrator().flushEntry(CURRENT_ROOTS_IN_CACHE_KEY);
     }
 
@@ -201,7 +203,9 @@ public class DocumentTypeServiceImpl implements DocumentTypeService {
         		    Integer maxVersionNumber = documentTypeDAO.getMaxVersionNumber(documentType.getName());
         			documentType.setVersion((maxVersionNumber != null) ? new Integer(maxVersionNumber.intValue() + 1) : new Integer(0));
         			oldDocumentType.setCurrentInd(Boolean.FALSE);
-        			LOG.info("Saving old document type Id " + oldDocumentType.getDocumentTypeId() + " name '" + oldDocumentType.getName() + "' (current = " + oldDocumentType.getCurrentInd() + ")");
+        			if ( LOG.isInfoEnabled() ) { 
+        				LOG.info("Saving old document type Id " + oldDocumentType.getDocumentTypeId() + " name '" + oldDocumentType.getName() + "' (current = " + oldDocumentType.getCurrentInd() + ")");
+        			}
         			save(oldDocumentType, false);
         		}
     		}
@@ -215,7 +219,9 @@ public class DocumentTypeServiceImpl implements DocumentTypeService {
             documentType.setPreviousVersionId(existingDocTypeId);
             documentType.setCurrentInd(Boolean.TRUE);
     		save(documentType, false);
-            LOG.info("Saved current document type Id " + documentType.getDocumentTypeId() + " name '" + documentType.getName() + "' (current = " + documentType.getCurrentInd() + ")");
+    		if ( LOG.isInfoEnabled() ) { 
+    			LOG.info("Saved current document type Id " + documentType.getDocumentTypeId() + " name '" + documentType.getName() + "' (current = " + documentType.getCurrentInd() + ")");
+    		}
     		//attach the children to this new parent.  cloning the children would probably be a better way to go here...
     		if (ObjectUtils.isNotNull(existingDocTypeId)) {
     		    // documentType.getPreviousVersion() should not be null at this point
@@ -224,7 +230,9 @@ public class DocumentTypeServiceImpl implements DocumentTypeService {
     				DocumentType child = (DocumentType) iterator.next();
     				child.setDocTypeParentId(documentType.getDocumentTypeId());
     				save(child, false);
-    	            LOG.info("Saved child document type Id " + child.getDocumentTypeId() + " name '" + child.getName() + "' (parent = " + child.getDocTypeParentId() + ", current = " + child.getCurrentInd() + ")");
+    				if ( LOG.isInfoEnabled() ) { 
+    					LOG.info("Saved child document type Id " + child.getDocumentTypeId() + " name '" + child.getName() + "' (parent = " + child.getDocTypeParentId() + ", current = " + child.getCurrentInd() + ")");
+    				}
     			}
     		}
     		// initiate a save of this document type's parent document type, this will force a
@@ -235,7 +243,9 @@ public class DocumentTypeServiceImpl implements DocumentTypeService {
     		if (documentType.getDocTypeParentId() != null) {
     			DocumentType parent = getDocumentTypeDAO().findByDocId(documentType.getDocTypeParentId());
     			save(parent, false);
-                LOG.info("Saved parent document type Id " + parent.getDocumentTypeId() + " name '" + parent.getName() + "' (current = " + parent.getCurrentInd() + ")");
+    			if ( LOG.isInfoEnabled() ) { 
+    				LOG.info("Saved parent document type Id " + parent.getDocumentTypeId() + " name '" + parent.getName() + "' (current = " + parent.getCurrentInd() + ")");
+    			}
     		}
 
     		// finally, flush the cache and notify the rule cache of the DocumentType change

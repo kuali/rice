398ead86a0735c13cc49bd711344ebc365b7ccd5
Jeremy Hanson <jjhanso@iastate.edu>
2009-12-02 19:17:43 +0000

    KULRICE-3685 - Kim entity tables missing foreign-key relationships KULRICE-3522 - Drop application URL from KIM attribute definition table

--- scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/11-12-2009.sql
+++ scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/11-12-2009.sql
@@ -1,17 +1,3 @@
--- create new service def value table
---CREATE TABLE KRSB_SVC_DEF_VAL_T
---(
---      SVC_DEF_ID NUMBER(14) not null,
---      SVC_DEF CLOB,
---      CONSTRAINT KRSB_SVC_DEF_T PRIMARY KEY (SVC_DEF_ID)
---)
---/
-
---insert into KRSB_SVC_DEF_VAL_T (SVC_DEF_ID, SVC_DEF) (select SVC_DEF_ID, SVC_DEF from KRSB_SVC_DEF_T)
---/
---alter table KRSB_SVC_DEF_T drop COLUMN SVC_DEF
---/
-
 -- KULRICE-3522 -- Drop application URL from KIM attribute definition table
 alter table krim_attr_defn_t drop column appl_url
 / 

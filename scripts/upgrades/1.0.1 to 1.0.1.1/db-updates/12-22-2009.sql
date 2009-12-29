c03c1a9a4aa1647c7d8e8e6bfd6996c052042365
Peter Giles <g1l3sp@gmail.com>
2009-12-29 22:32:42 +0000

    KULRICE-3754:
    resolving auto-merge conflict (I think I committed the conflicted file somehow)

--- scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/12-22-2009.sql
+++ scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/12-22-2009.sql
@@ -1,6 +1,2 @@
 # changes for KULRICE-3754
-<<<<<<< HEAD:scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/12-22-2009.sql
-alter table KREW_DOC_TYP_T add column DOC_SEARCH_HELP_URL VARCHAR(4000) DEFAULT NULL;
-=======
 alter table KREW_DOC_TYP_T add DOC_SEARCH_HELP_URL VARCHAR(4000) DEFAULT NULL;
->>>>>>> KULRICE-3754:scripts/upgrades/1.0.1 to 1.0.1.1/db-updates/12-22-2009.sql

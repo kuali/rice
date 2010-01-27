9244e891cf5a002a1567a4d7a8a95fbec719fc4c
Jonathan Keller <keller.jonathan@gmail.com>
2010-01-27 18:43:45 +0000

    Added oracle dialect.  Updated MySQL to MySQL5

--- impl/src/main/java/org/kuali/rice/core/jpa/dialect/OracleDialect.java
+++ impl/src/main/java/org/kuali/rice/core/jpa/dialect/OracleDialect.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2008-2009 The Kuali Foundation
- * 
- * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,14 @@
  */
 package org.kuali.rice.core.jpa.dialect;
 
-import java.sql.Types;
+import org.hibernate.dialect.Oracle10gDialect;
 
-public class MySQLDialect extends org.hibernate.dialect.MySQL5Dialect {
-	
-	public MySQLDialect() {
-		super();
-		registerColumnType( Types.BIT, "char(1)" );
-	}
+/**
+ * This is a description of what this class does - jonathan don't forget to fill this in. 
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ *
+ */
+public class OracleDialect extends Oracle10gDialect {
 
 }

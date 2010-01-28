0f819fe9d627d651fe59e0d9e827badf9cabb255
Dan Seibert <dseibert@vivantech.com>
2010-01-28 22:53:54 +0000

    KULRICE-3886 changed name of TypeDefs "rice_active_inactive" and "rice_encrypt_decrypt"

--- impl/src/main/java/org/kuali/rice/kns/util/package-info.java
+++ impl/src/main/java/org/kuali/rice/kns/util/package-info.java
@@ -1,10 +1,10 @@
 @org.hibernate.annotations.TypeDefs ({
 	@org.hibernate.annotations.TypeDef(
-		name="rice_kns_encrypt_decrypt",
+		name="rice_encrypt_decrypt",
 		typeClass=org.kuali.rice.kns.util.HibernateImmutableValueUserType.class
 	),
 	@org.hibernate.annotations.TypeDef(
-		name="active_inactive",
+		name="rice_active_inactive",
 		typeClass=org.kuali.rice.kns.util.HibernateKualiCharBooleanAIType.class
 	),
 	@org.hibernate.annotations.TypeDef(

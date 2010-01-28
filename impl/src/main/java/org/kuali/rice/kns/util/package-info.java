e7505e53770017afce30e3418154a50d48c91239
Dan Seibert <dseibert@vivantech.com>
2010-01-28 21:42:14 +0000

    KULRICE-3886 added TypeDefs for HibernateKualiHashType, and HibernateCharBooleanAIType.   At this time they are just place holders and are NOT correctly implemented.

--- impl/src/main/java/org/kuali/rice/kns/util/package-info.java
+++ impl/src/main/java/org/kuali/rice/kns/util/package-info.java
@@ -2,15 +2,11 @@
 	@org.hibernate.annotations.TypeDef(
 		name="rice_kns_encrypt_decrypt",
 		typeClass=org.kuali.rice.kns.util.HibernateImmutableValueUserType.class
-	)
-})
-@org.hibernate.annotations.TypeDefs ({
+	),
 	@org.hibernate.annotations.TypeDef(
 		name="active_inactive",
 		typeClass=org.kuali.rice.kns.util.HibernateKualiCharBooleanAIType.class
-	)
-})
-@org.hibernate.annotations.TypeDefs ({
+	),
 	@org.hibernate.annotations.TypeDef(
 		name="rice_hash",
 		typeClass=org.kuali.rice.kns.util.HibernateKualiHashType.class

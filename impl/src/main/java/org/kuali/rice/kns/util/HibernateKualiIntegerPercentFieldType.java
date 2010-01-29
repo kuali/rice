5513744396e4231a4569aa714345abf2d9865a0c
Dan Seibert <dseibert@vivantech.com>
2010-01-29 21:03:41 +0000

    KULRICE-3886 type cleanup

--- impl/src/main/java/org/kuali/rice/kns/util/HibernateKualiIntegerPercentFieldType.java
+++ impl/src/main/java/org/kuali/rice/kns/util/HibernateKualiIntegerPercentFieldType.java
@@ -15,79 +15,75 @@
  */
 package org.kuali.rice.kns.util;
 
-import java.security.GeneralSecurityException;
+import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
 import org.hibernate.HibernateException;
-import org.hibernate.usertype.UserType;
-import org.kuali.rice.kns.service.KNSServiceLocator;
 
 /**
- * Hibernate UserType to encrypt and decript data on its way to the database 
+ * This is a description of what this class does - g1zhang don't forget to fill this in. 
  * 
  * @author Kuali Rice Team (rice.collab@kuali.org)
  *
  */
-public class HibernateKualiIntegerPercentFieldType extends HibernateImmutableValueUserType implements UserType {
-	/**
-	 * Retrieves a value from the given ResultSet and decrypts it
-	 * 
-	 * @see org.hibernate.usertype.UserType#nullSafeGet(java.sql.ResultSet, java.lang.String[], java.lang.Object)
-	 */
-	public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
-		String value = rs.getString(names[0]);
-		String converted = null;
-
-		if (value != null) {
-	        try {
-	            converted = KNSServiceLocator.getEncryptionService().decrypt(value);
-	        }
-	        catch (GeneralSecurityException gse) {
-	            throw new RuntimeException("Unable to decrypt value from db: " + gse.getMessage());
-	        }
-	        
-	        if (converted == null) {
-				converted = value;
-			}
-		}
-
-        return converted;
-	}
+public class HibernateKualiIntegerPercentFieldType extends
+		HibernateImmutableValueUserType {
 
 	/**
-	 * Encrypts the value if possible and then sets that on the PreparedStatement
+	 * This overridden method ...
 	 * 
-	 * @see org.hibernate.usertype.UserType#nullSafeSet(java.sql.PreparedStatement, java.lang.Object, int)
+	 * @see org.kuali.rice.kns.util.HibernateImmutableValueUserType#nullSafeGet(java.sql.ResultSet, java.lang.String[], java.lang.Object)
 	 */
-	public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
-		String converted = null;
+	@Override
+	public Object nullSafeGet(ResultSet rs, String[] names, Object source)
+			throws HibernateException, SQLException {
+        // Check for null, and verify object type.
+        // Do conversion if our type is correct (BigDecimal).
+        if (source != null && source instanceof BigDecimal) {
+            BigDecimal converted = (BigDecimal) source;
 
-		if (value != null) {
-	        try {
-	            converted = KNSServiceLocator.getEncryptionService().encrypt(value);
-	        }
-	        catch (GeneralSecurityException gse) {
-	            throw new RuntimeException("Unable to encrypt value to db: " + gse.getMessage());
-	        }
-		}
-        
-        if (converted == null) {
-        	st.setNull(index, Types.VARCHAR);
-        } else {
-        	st.setString(index, converted);
+            // Once we have converted, we need to convert again to KualiPercent.
+            KualiPercent percentConverted = new KualiPercent((BigDecimal) converted);
+
+            return percentConverted;
+
+        }
+        else {
+            return null;
         }
 	}
 
 	/**
-	 * Returns String.class
+	 * This overridden method ...
 	 * 
-	 * @see org.hibernate.usertype.UserType#returnedClass()
+	 * @see org.kuali.rice.kns.util.HibernateImmutableValueUserType#nullSafeSet(java.sql.PreparedStatement, java.lang.Object, int)
+	 */
+	@Override
+	public void nullSafeSet(PreparedStatement st, Object source, int index)
+			throws HibernateException, SQLException {
+	       Object converted = source;
+
+	        if (source instanceof KualiPercent) {
+	            converted = ((KualiPercent) source).bigDecimalValue();
+	        }
+
+	        if (converted == null) {
+	        	st.setNull(index, Types.DECIMAL); 
+	        } else {
+	        	st.setBigDecimal(index, ((BigDecimal)converted));
+	        }
+	}
+
+	/**
+	 * This overridden method ...
+	 * 
+	 * @see org.kuali.rice.kns.util.HibernateImmutableValueUserType#returnedClass()
 	 */
 	public Class returnedClass() {
-		return String.class;
+		return BigDecimal.class;
 	}
 
 	/**
@@ -96,7 +92,7 @@ public class HibernateKualiIntegerPercentFieldType extends HibernateImmutableVal
 	 * @see org.hibernate.usertype.UserType#sqlTypes()
 	 */
 	public int[] sqlTypes() {
-		return new int[] { Types.VARCHAR };
+		return new int[] { Types.DECIMAL };
 	}
 
 }

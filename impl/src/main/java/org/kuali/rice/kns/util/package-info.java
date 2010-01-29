c273c6da39fb4871c0464ea4c07c6bb003182a4b
Dan Seibert <dseibert@vivantech.com>
2010-01-29 17:42:50 +0000

    KULRICE-3886 Added typdefs for rice specific hibernate jpa user types.

--- impl/src/main/java/org/kuali/rice/kns/util/package-info.java
+++ impl/src/main/java/org/kuali/rice/kns/util/package-info.java
@@ -1,7 +1,7 @@
 @org.hibernate.annotations.TypeDefs ({
 	@org.hibernate.annotations.TypeDef(
 		name="rice_encrypt_decrypt",
-		typeClass=org.kuali.rice.kns.util.HibernateImmutableValueUserType.class
+		typeClass=org.kuali.rice.kns.util.HibernateKualiEncryptDecryptUserType.class
 	),
 	@org.hibernate.annotations.TypeDef(
 		name="rice_active_inactive",
@@ -10,6 +10,26 @@
 	@org.hibernate.annotations.TypeDef(
 		name="rice_hash",
 		typeClass=org.kuali.rice.kns.util.HibernateKualiHashType.class
+	),
+	@org.hibernate.annotations.TypeDef(
+		name="rice_decimal",
+		typeClass=org.kuali.rice.kns.util.HibernateKualiDecimalType.class
+	),
+	@org.hibernate.annotations.TypeDef(
+		name="rice_percent",
+		typeClass=org.kuali.rice.kns.util.HibernateKualiPercentType.class
+	),
+	@org.hibernate.annotations.TypeDef(
+		name="rice_percentage",
+		typeClass=org.kuali.rice.kns.util.HibernateKualiPercentageType.class
+	),
+	@org.hibernate.annotations.TypeDef(
+		name="rice_integer",
+		typeClass=org.kuali.rice.kns.util.HibernateKualiIntegerType.class
+	),
+	@org.hibernate.annotations.TypeDef(
+		name="rice_integer_percentage",
+		typeClass=org.kuali.rice.kns.util.HibernateIntegerPercentageType.class
 	)
 })
 

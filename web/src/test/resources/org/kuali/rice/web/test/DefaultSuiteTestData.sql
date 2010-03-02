904d957b07dc0e79bc4a4f2a0f6e0ebba3b4d767
Peter Giles <g1l3sp@gmail.com>
2010-03-02 19:28:11 +0000

    KULRICE-3003: Fix broken KENWebServiceTest
    web/src/test/resources/org/kuali/rice/web/test/DefaultSuiteTestData.sql:
    - Removed references to columns that had been removed from the database

--- web/src/test/resources/org/kuali/rice/web/test/DefaultSuiteTestData.sql
+++ web/src/test/resources/org/kuali/rice/web/test/DefaultSuiteTestData.sql
@@ -13,29 +13,29 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 -- 
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (1, 'fred', '5EA45238F5528846E0404F8189D840B8')
+insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (1, 'fred')
 ;
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (2, 'fran', '5EA45238F5528846E0404F8189D840B8')
+insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (2, 'fran')
 ;
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (3, 'frank', '5EA45238F5528846E0404F8189D840B8')
+insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (3, 'frank')
 ;
-insert into TRV_ACCT_TYPE values ('CAT', 'Clearing Account Type', '5EA45238F5528846E0404F8189D840B8', 1)
+insert into TRV_ACCT_TYPE values ('CAT', 'Clearing Account Type')
 ;
-insert into TRV_ACCT_TYPE values ('EAT', 'Expense Account Type', '5EA45238F5528846E0404F8189D840B8', 1)
+insert into TRV_ACCT_TYPE values ('EAT', 'Expense Account Type')
 ;
-insert into TRV_ACCT_TYPE values ('IAT', 'Income Account Type', '5EA45238F5528846E0404F8189D840B8', 1)
+insert into TRV_ACCT_TYPE values ('IAT', 'Income Account Type')
 ;
-insert into TRV_ACCT values ('a1', 'a1', 'CAT', 1, '5EF45238F5528846E0404F8189D840B8', 1)
+insert into TRV_ACCT values ('a1', 'a1', 'CAT', 1)
 ;
-insert into TRV_ACCT values ('a2', 'a2', 'EAT', 2, '5EF45238F5528846E0404F8189D840B9', 1)
+insert into TRV_ACCT values ('a2', 'a2', 'EAT', 2)
 ;
-insert into TRV_ACCT values ('a3', 'a3', 'IAT', 3, '5EF45238F5528846E0404F8189D840BA', 1)
+insert into TRV_ACCT values ('a3', 'a3', 'IAT', 3)
 ;
-insert into TRV_ACCT_EXT values ('a1', 'CAT', '5EF45238F5528846E0404F8189D840B8', 1)
+insert into TRV_ACCT_EXT values ('a1', 'CAT')
 ;
-insert into TRV_ACCT_EXT values ('a2', 'EAT', '5EF45238F5528846E0404F8189D840B9', 1)
+insert into TRV_ACCT_EXT values ('a2', 'EAT')
 ;
-insert into TRV_ACCT_EXT values ('a3', 'IAT', '5EF45238F5528846E0404F8189D840BA', 1)
+insert into TRV_ACCT_EXT values ('a3', 'IAT')
 ;
 insert into TRV_DOC_ACCT (DOC_HDR_ID, ACCT_NUM) values (1, 'a1')
 ;

ba166cb1f628745912df88536daa7aeafa947f87
Jonathan Keller <keller.jonathan@gmail.com>
2010-04-23 19:02:11 +0000

    New login filter which just logs the user in as Admin.

---
+++ impl/src/main/java/org/kuali/rice/kns/web/filter/DummyLoginFilter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2005-2008 The Kuali Foundation
+ * 
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kns.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * A login filter which forwards to a login page that allows for the desired
+ * authentication ID to be entered without the need for a password.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DummyLoginFilter implements Filter {
+	
+	private String user = "admin";
+	
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest hsreq = (HttpServletRequest) request;
+				request = new HttpServletRequestWrapper(hsreq) {
+					public String getRemoteUser() {
+						return user;
+					}
+				};
+		}
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {}
+}

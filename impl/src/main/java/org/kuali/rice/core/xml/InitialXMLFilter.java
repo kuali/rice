9067d94fb0627a64fa024f93a5ac5918ec0526da
Tom Bradford <tbradford@kuali.org>
2010-05-19 19:46:02 +0000

    KULRICE-2634 : Adding InitialXMLFilter which automatically does the work of linking in the necessary ChainedXMLFilter instances based on information returned from the XMLImportExportService

---
+++ impl/src/main/java/org/kuali/rice/core/xml/InitialXMLFilter.java
@@ -0,0 +1,46 @@
+package org.kuali.rice.core.xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+/**
+ * Acts as the first step in a Chain of XML Filters.  This node does the work
+ * of identifying the Document's Schema URI and linking in the ChainedXMLFilter
+ * instances that would be required to upgrade the input stream. 
+ */
+public class InitialXMLFilter extends ChainedXMLFilterBase {
+    private XMLImportExportServiceBase xmlService;
+    private String schemaUri = null;
+
+    public InitialXMLFilter() {
+        setNextFilter(new TerminalXMLFilter());
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName,
+                             Attributes atts) throws SAXException {
+        if ( schemaUri == null ) {
+            schemaUri = atts.getValue("http://www.w3.org/2001/XMLSchema-instance",
+                                      "noNamespaceSchemaLocation");
+            if ( schemaUri != null ) {
+                XMLImportExportServiceBase xmlService = getXMLImportExportService();
+                ChainedXMLFilter startFilter = xmlService.getFilterForSchemaURI(schemaUri);
+                if ( startFilter != null )
+                    setNextFilter(startFilter);
+            }
+        }
+        next().startElement(uri, localName, qName, atts);
+    }
+
+    public String getSchemaURI() {
+        return schemaUri;
+    }
+    
+    public XMLImportExportServiceBase getXMLImportExportService() {
+        return xmlService;
+    }
+
+    public void setXMLImportExportService(XMLImportExportServiceBase xmlService) {
+        this.xmlService = xmlService;
+    }
+}

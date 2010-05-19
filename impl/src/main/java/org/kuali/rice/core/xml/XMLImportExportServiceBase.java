400cfa5027801146361fcbe77ec540d24de4f5e5
Tom Bradford <tbradford@kuali.org>
2010-05-19 16:27:10 +0000

    KULRICE-2634 : Some cleanup

--- impl/src/main/java/org/kuali/rice/core/xml/XMLImportExportServiceBase.java
+++ impl/src/main/java/org/kuali/rice/core/xml/XMLImportExportServiceBase.java
@@ -1,5 +1,6 @@
 package org.kuali.rice.core.xml;
 
+import org.apache.log4j.Logger;
 import org.xml.sax.XMLFilter;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
  * an interface when completed.
  */
 public class XMLImportExportServiceBase {
+    private static final Logger LOG = Logger.getLogger(XMLImportExportServiceBase.class);
+            
     private enum State {
         INIT,
         LINKING
@@ -32,14 +35,14 @@ public class XMLImportExportServiceBase {
      * @return The starting ChainedXMLFilter for transforming the Document
      */
     protected ChainedXMLFilter getFilterForSchemaURI(String schemaURI) {
-        State state = State.INIT;
-        ChainedXMLFilter result = null;
-        ChainedXMLFilter prevFilter = null;
+        ChainedXMLFilter result = null, prevFilter = null;
+        List<XMLInputFilterEntry> filters = getFilters();
         if ( filters != null ) {
-            for ( XMLInputFilterEntry entry : getFilters() ) {
+            State state = State.INIT;            
+            for ( XMLInputFilterEntry entry : filters ) {
                 if ( state == State.INIT && entry.getSchemaURI().equals(schemaURI) ) {
                     // We actually want to skip this entry and start linking
-                    // at the next entry.
+                    // at the next one, as we don't need to convert to self.
                     state = State.LINKING;
                 }
                 else if ( state == State.LINKING ) {
@@ -49,16 +52,16 @@ public class XMLImportExportServiceBase {
                             filter = entry.getFilterClass().newInstance();
                         }
                         catch ( Exception e ) {
-                            //LOG.error("Could not instantiate ChainedXMLFilter", e);
+                            LOG.error("Could not instantiate ChainedXMLFilter", e);
                         }
                     }
-                    if ( filter == null )
-                        filter = new PassthruXMLFilter();                    
-                    if ( prevFilter != null )
-                        prevFilter.setNextFilter(filter);
-                    if ( result == null )
-                        result = filter;
-                    prevFilter = filter;
+                    if ( filter != null ) {
+                        if ( prevFilter != null )
+                            prevFilter.setNextFilter(filter);
+                        if ( result == null )
+                            result = filter;
+                        prevFilter = filter;
+                    }
                 }
             }
         }

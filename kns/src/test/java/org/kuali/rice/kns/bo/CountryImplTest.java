f82a914ccbff72284147df08cd10e1b546cdb812
Changpil Lee <changpil@gmail.com>
2010-08-11 20:49:09 +0000

    KULRICE-4187

--- kns/src/test/java/org/kuali/rice/kns/bo/CountryImplTest.java
+++ kns/src/test/java/org/kuali/rice/kns/bo/CountryImplTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2007 The Kuali Foundation
- *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl1.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kuali.rice.kns.bo;
 
 
@@ -29,7 +14,7 @@ import org.kuali.test.KNSTestCase;
  * @author Kuali Rice Team (rice.collab@kuali.org)
  *
  */
-public class CountryImpTest extends KNSTestCase{
+public class CountryImplTest extends KNSTestCase{
 
 	Country countryOne;
 	Country countryTwo;

78a2e9f13f811be8ba2cc53161b49d698e4ad008
Garey Taylor <gpt@umd.edu>
2010-08-12 20:12:30 +0000

    KULRICE-4425: Rolling back changes for KFS issues.

--- impl/src/main/java/org/kuali/rice/kns/workflow/attribute/DataDictionaryDocumentSearchCustomizer.java
+++ impl/src/main/java/org/kuali/rice/kns/workflow/attribute/DataDictionaryDocumentSearchCustomizer.java
@@ -25,77 +25,68 @@ import org.kuali.rice.kew.docsearch.DocSearchCriteriaDTO;
 import org.kuali.rice.kew.docsearch.DocSearchDTO;
 import org.kuali.rice.kew.docsearch.DocumentSearchContext;
 import org.kuali.rice.kew.docsearch.DocumentSearchGenerator;
-import org.kuali.rice.kew.docsearch.DocumentSearchResultComponents;
-import org.kuali.rice.kew.docsearch.DocumentSearchResultProcessor;
 import org.kuali.rice.kew.docsearch.SearchableAttribute;
 import org.kuali.rice.kew.docsearch.SearchableAttributeValue;
+import org.kuali.rice.kew.docsearch.StandardDocumentSearchGenerator;
+import org.kuali.rice.kew.docsearch.StandardDocumentSearchResultProcessor;
 import org.kuali.rice.kew.exception.WorkflowServiceError;
 import org.kuali.rice.kew.rule.WorkflowAttributeValidationError;
-import org.kuali.rice.kew.service.KEWServiceLocator;
 import org.kuali.rice.kns.util.MessageMap;
 import org.kuali.rice.kns.web.ui.Row;
 
-public class DataDictionaryDocumentSearchCustomizer implements DocumentSearchResultProcessor, SearchableAttribute, DocumentSearchGenerator {
-    
-    private DocumentSearchGenerator documentSearchGenerator; 
-    private DocumentSearchResultProcessor documentSearchResultProcessor;
-    private SearchableAttribute searchableAttribute = new DataDictionarySearchableAttribute();
+public class DataDictionaryDocumentSearchCustomizer extends StandardDocumentSearchResultProcessor implements SearchableAttribute, DocumentSearchGenerator {
+    // SEARCH GENERATOR IMPLEMENTATION
+    private StandardDocumentSearchGenerator documentSearchGenerator = new StandardDocumentSearchGenerator();
 
-    protected DocumentSearchGenerator getStandardDocumentSearchGenerator() { 
-	    if (documentSearchGenerator == null) { 
-	    	documentSearchGenerator = KEWServiceLocator.getDocumentSearchService().getStandardDocumentSearchGenerator(); 
-	    } 
-	    return documentSearchGenerator; 
-    } 
-
-    protected DocumentSearchResultProcessor getStandardDocumentSearchResultProcessor() { 
-	    if (documentSearchResultProcessor == null) { 
-	    	documentSearchResultProcessor = KEWServiceLocator.getDocumentSearchService().getStandardDocumentSearchResultProcessor(); 
-	    } 
-	    return documentSearchResultProcessor; 
-    } 
     public DocSearchCriteriaDTO clearSearch(DocSearchCriteriaDTO searchCriteria) {
-        return getStandardDocumentSearchGenerator().clearSearch(searchCriteria);
+        return documentSearchGenerator.clearSearch(searchCriteria);
     }
 
     public String generateSearchSql(DocSearchCriteriaDTO searchCriteria) {
-        return getStandardDocumentSearchGenerator().generateSearchSql(searchCriteria);
+        return documentSearchGenerator.generateSearchSql(searchCriteria);
     }
 
     public int getDocumentSearchResultSetLimit() {
-        return getStandardDocumentSearchGenerator().getDocumentSearchResultSetLimit();
+        return documentSearchGenerator.getDocumentSearchResultSetLimit();
     }
 
     public List<WorkflowServiceError> performPreSearchConditions(String principalId, DocSearchCriteriaDTO searchCriteria) {
-        return getStandardDocumentSearchGenerator().performPreSearchConditions(principalId, searchCriteria);
+        return documentSearchGenerator.performPreSearchConditions(principalId, searchCriteria);
     }
 
     public List<DocSearchDTO> processResultSet(Statement searchAttributeStatement, ResultSet resultSet, DocSearchCriteriaDTO searchCriteria, String principalId) throws SQLException {
-        return getStandardDocumentSearchGenerator().processResultSet(searchAttributeStatement, resultSet, searchCriteria, principalId);
+        return documentSearchGenerator.processResultSet(searchAttributeStatement, resultSet, searchCriteria, principalId);
     }
 
     @Deprecated
     public List<DocSearchDTO> processResultSet(Statement searchAttributeStatement, ResultSet resultSet, DocSearchCriteriaDTO searchCriteria) throws SQLException {
-        return getStandardDocumentSearchGenerator().processResultSet(searchAttributeStatement, resultSet, searchCriteria);
-    }    
+        return documentSearchGenerator.processResultSet(searchAttributeStatement, resultSet, searchCriteria);
+    }
+
+    public void setSearchableAttributes(List<SearchableAttribute> searchableAttributes) {
+        documentSearchGenerator.setSearchableAttributes(searchableAttributes);
+    }
 
     public void setSearchingUser(String principalId) {
-    	getStandardDocumentSearchGenerator().setSearchingUser(principalId);
+        documentSearchGenerator.setSearchingUser(principalId);
     }
 
     public List<WorkflowServiceError> validateSearchableAttributes(DocSearchCriteriaDTO searchCriteria) {
-        return getStandardDocumentSearchGenerator().validateSearchableAttributes(searchCriteria);
+        return documentSearchGenerator.validateSearchableAttributes(searchCriteria);
     }
 
     public boolean isProcessResultSet() {
-        return getStandardDocumentSearchGenerator().isProcessResultSet();
+        return documentSearchGenerator.isProcessResultSet();
     }
     public void setProcessResultSet(boolean isProcessResultSet){
-    	getStandardDocumentSearchGenerator().setProcessResultSet(isProcessResultSet);
+        documentSearchGenerator.setProcessResultSet(isProcessResultSet);
     }
     public MessageMap getMessageMap(DocSearchCriteriaDTO searchCriteria) {
-        return getStandardDocumentSearchGenerator().getMessageMap(searchCriteria);
+        return documentSearchGenerator.getMessageMap(searchCriteria);
     }
+
+    // SEARCHABLE ATTRIBUTE IMPLEMENTATION
+    private SearchableAttribute searchableAttribute = new DataDictionarySearchableAttribute();
 
     public String getSearchContent(DocumentSearchContext documentSearchContext) {
         return searchableAttribute.getSearchContent(documentSearchContext);
@@ -112,22 +103,5 @@ public class DataDictionaryDocumentSearchCustomizer implements DocumentSearchRes
     public List<WorkflowAttributeValidationError> validateUserSearchInputs(Map<Object, Object> paramMap, DocumentSearchContext searchContext) {
         return searchableAttribute.validateUserSearchInputs(paramMap, searchContext);
     }
-
-	public boolean isProcessFinalResults() {		
-		return getStandardDocumentSearchResultProcessor().isProcessFinalResults();
-	}
-
-	public DocumentSearchResultComponents processIntoFinalResults(
-			List<DocSearchDTO> docSearchResultRows,
-			DocSearchCriteriaDTO criteria, String principalId)
-			throws SQLException {
-		
-		return getStandardDocumentSearchResultProcessor().processIntoFinalResults(docSearchResultRows, criteria, principalId);
-	}
-
-	public void setProcessFinalResults(boolean isProcessFinalResults) {
-		getStandardDocumentSearchResultProcessor().setProcessFinalResults(isProcessFinalResults);
-		
-	}
 
 }

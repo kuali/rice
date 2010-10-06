28e0a1967fc0f991aaef876a56fee627ec9c486e
William Balderamos <wbalderamos@gmail.com>
2010-10-06 14:48:10 +0000

    KULRICE-4466 - Create Unit Test for org.kuali.rice.kns.util.PatternedStringBuilder

--- kns/src/test/java/org/kuali/rice/kns/util/PatternedStringBuilderTest.java
+++ kns/src/test/java/org/kuali/rice/kns/util/PatternedStringBuilderTest.java
@@ -4,22 +4,20 @@ import org.junit.Test;
 import org.kuali.test.KNSTestCase;
 
 /**
- *  
- * 
  * @author Kuali Rice Team (rice.collab@kuali.org)
- *
  */
-public class PatternedStringBuilderTest extends KNSTestCase{
- 
- @Test
- public void testSprintf(){
-  double pi = Math.PI;
-  PatternedStringBuilder patterenedStringBuilder = new PatternedStringBuilder("pi = %5.3f");
-  
-  //patterenedStringBuilder.setPattern("int");
-  String expectedVal = patterenedStringBuilder.sprintf(pi);
-  assertEquals("pi = 3.142",expectedVal);
- }
- 
+public class PatternedStringBuilderTest extends KNSTestCase {
 
+    @Test
+    public void testSprintf() {
+        double pi = Math.PI;
+        PatternedStringBuilder patterenedStringBuilder = new PatternedStringBuilder("pi = %5.3f");
+        String expectedVal = patterenedStringBuilder.sprintf(pi);
+        assertEquals("pi = 3.142", expectedVal);
+        patterenedStringBuilder.setPattern("%4$2s %3$2s %2$2s %1$2s");
+        assertEquals(" z  y  x  w", patterenedStringBuilder.sprintf("w", "x", "y", "z"));
+        patterenedStringBuilder.setPattern("");
+        assertEquals("", patterenedStringBuilder.sprintf("somethingElse"));
+        // This basically replicates tests done via jdk to test java.util.Formatter.java
+    }
 }

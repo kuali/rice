3484304a50dd76820022d1efcd77f170d4eeb5d8
William Balderamos <wbalderamos@gmail.com>
2010-11-02 18:19:28 +0000

    KULRICE-4696 - Parameter lookup reports all rows when only searching by Component. Unbounding search results returned to accomodate larger datasets found on the client side as opposed to the server.

--- impl/src/main/java/org/kuali/rice/kns/lookup/ParameterLookupableHelperServiceImpl.java
+++ impl/src/main/java/org/kuali/rice/kns/lookup/ParameterLookupableHelperServiceImpl.java
@@ -103,7 +103,7 @@ public class ParameterLookupableHelperServiceImpl extends KualiLookupableHelperS
             // we're filtering in memory, so remove this criteria
             fieldValues.remove("parameterDetailType.parameterDetailTypeName");
             
-            results = super.getSearchResults(fieldValues);
+            results = super.getSearchResultsUnbounded(fieldValues);
             // attach the DD detail types to your results before we filter (else filtering won't work correctly)
             attachDataDictionaryDetailTypes(results, ddDetailTypes);
             // filter down to just results with matching parameter component (ParameterDetailType)
@@ -114,7 +114,7 @@ public class ParameterLookupableHelperServiceImpl extends KualiLookupableHelperS
             });
         }
         else {
-            results = super.getSearchResults(fieldValues);
+            results = super.getSearchResultsUnbounded(fieldValues);
             attachDataDictionaryDetailTypes(results, ddDetailTypes);
         }
         return results;

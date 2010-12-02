ad45162335d92193bc192efe102adfbb53ae96e0
Travis Schneeberger <tschneeb@gmail.com>
2010-12-02 18:40:06 +0000

    removing ken lifecycle

--- core/api/src/main/java/org/kuali/rice/core/lifecycle/LifecycleBean.java
+++ core/api/src/main/java/org/kuali/rice/core/lifecycle/LifecycleBean.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.ken.core;
+package org.kuali.rice.core.lifecycle;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;

a2c2e5eec7fdc2a5607732c677e1f6c8d00469c9
Jeremy Hanson <jjhanso@iastate.edu>
2011-02-25 22:28:25 +0000

    KULRICE-4983 - remove value finder caching.

--- shareddata/rice-shareddata-framework/src/main/java/org/kuali/rice/shareddata/framework/campus/CampusValuesFinder.java
+++ shareddata/rice-shareddata-framework/src/main/java/org/kuali/rice/shareddata/framework/campus/CampusValuesFinder.java
@@ -15,19 +15,15 @@
  */
 package org.kuali.rice.shareddata.framework.campus;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.kuali.rice.core.util.KeyValue;
 import org.kuali.rice.core.util.ConcreteKeyValue;
+import org.kuali.rice.core.util.KeyValue;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
 import org.kuali.rice.shareddata.api.campus.Campus;
 import org.kuali.rice.shareddata.api.campus.CampusService;
-import org.kuali.rice.shareddata.api.country.CountryService;
 import org.kuali.rice.shareddata.api.services.SharedDataApiServiceLocator;
-import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
-import org.kuali.rice.kns.service.KNSServiceLocator;
-import org.kuali.rice.kns.service.KeyValuesService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class...
@@ -35,33 +31,20 @@ import org.kuali.rice.kns.service.KeyValuesService;
  * 
  */
 public class CampusValuesFinder extends KeyValuesBase {
-
-	protected static List<KeyValue> campusCache = null;
 	
     /*
      * @see org.kuali.keyvalues.KeyValuesFinder#getKeyValues()
      */
 	@Override
 	public List<KeyValue> getKeyValues() {
-    	if ( campusCache == null ) {
-    		synchronized ( this.getClass() ) {
 				
-    			CampusService campusService = SharedDataApiServiceLocator.getCampusService();
-		        List<Campus> campuses = campusService.findAllCampuses();
-		        List<KeyValue> labels = new ArrayList<KeyValue>();
- 		        labels.add(new ConcreteKeyValue("", ""));
-		        for ( Campus campus : campuses ) {
-		            labels.add(new ConcreteKeyValue(campus.getCode(), campus.getCode() + " - " + campus.getName()));
-		        }
-		
-		        campusCache = labels;
-			}
-    	}
-    	return campusCache;
-    }
-    
-    @Override
-    public void clearInternalCache() {
-    	campusCache = null;
+            CampusService campusService = SharedDataApiServiceLocator.getCampusService();
+            List<Campus> campuses = campusService.findAllCampuses();
+            List<KeyValue> labels = new ArrayList<KeyValue>();
+            labels.add(new ConcreteKeyValue("", ""));
+            for ( Campus campus : campuses ) {
+                labels.add(new ConcreteKeyValue(campus.getCode(), campus.getCode() + " - " + campus.getName()));
+            }
+            return labels;
     }
 }

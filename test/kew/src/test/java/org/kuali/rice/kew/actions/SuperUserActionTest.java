3afcce227cb94551cdb85771f74bb819f33d9db0
Travis Schneeberger <tschneeb@gmail.com>
2011-03-14 20:11:00 +0000

    fixing a kew tests

--- test/kew/src/test/java/org/kuali/rice/kew/actions/SuperUserActionTest.java
+++ test/kew/src/test/java/org/kuali/rice/kew/actions/SuperUserActionTest.java
@@ -1,13 +1,12 @@
 /*
- * Copyright 2005-2007 The Kuali Foundation
- * 
- * 
+ * Copyright 2006-2011 The Kuali Foundation
+ *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl2.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +26,7 @@ import org.kuali.rice.kew.service.WorkflowInfo;
 import org.kuali.rice.kew.test.KEWTestCase;
 import org.kuali.rice.kew.test.TestUtilities;
 import org.kuali.rice.kew.util.KEWConstants;
+import org.kuali.rice.test.BaselineTestCase;
 
 import java.util.Iterator;
 import java.util.List;
@@ -36,6 +36,7 @@ import static org.junit.Assert.*;
 /**
  * Tests the super user actions available on the API.
  */
+@BaselineTestCase.BaselineMode(BaselineTestCase.Mode.NONE)
 public class SuperUserActionTest extends KEWTestCase {
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(SuperUserActionTest.class);
 

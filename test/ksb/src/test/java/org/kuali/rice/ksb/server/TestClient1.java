e7c1a604571cfe4d22d392a03f927a3dfc1ccd98
Travis Schneeberger <tschneeb@gmail.com>
2011-03-16 16:23:52 +0000

    cleaning up old jetty server stuff

--- test/ksb/src/test/java/org/kuali/rice/ksb/server/TestClient1.java
+++ test/ksb/src/test/java/org/kuali/rice/ksb/server/TestClient1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 The Kuali Foundation
+ * Copyright 2006-2011 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,13 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.kuali.rice.ksb.server;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.kuali.rice.core.config.ConfigContext;
-import org.kuali.rice.test.BaseTestServer;
+import org.kuali.rice.ksb.BaseTestServer;
 
 public class TestClient1 extends BaseTestServer {
 	

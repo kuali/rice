4400d1a5231b06fc8922a91bf7480ed2508072b3
Eric Westfall <ewestfal@iu.edu>
2011-03-20 23:56:45 +0000

    KULRICE-5032 - Ugh, trying to fix Calendar comparison failure which doesn't happen locally but happens in CI.

--- core/api/src/test/java/org/kuali/rice/core/api/criteria/CriteriaTest.java
+++ core/api/src/test/java/org/kuali/rice/core/api/criteria/CriteriaTest.java
@@ -19,10 +19,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -37,7 +35,7 @@ import org.kuali.rice.core.test.JAXBAssert;
  */
 public class CriteriaTest {
 
-	private static final String XML = "<criteria><like propertyPath=\"display\"><stringValue>*Eric*</stringValue></like><greaterThan propertyPath=\"birthDate\"><dateTimeValue>1980-09-01T05:00:00Z</dateTimeValue></greaterThan><lessThan propertyPath=\"birthDate\"><dateTimeValue>1980-10-01T05:00:00Z</dateTimeValue></lessThan><or><equal propertyPath=\"name.first\"><stringValue>Eric</stringValue></equal><equal propertyPath=\"name.last\"><stringValue>Westfall</stringValue></equal></or></criteria>";
+	private static final String XML = "<criteria><like propertyPath=\"display\"><stringValue>*Eric*</stringValue></like><greaterThan propertyPath=\"birthDate\"><dateTimeValue>1980-09-01T00:00:00Z</dateTimeValue></greaterThan><lessThan propertyPath=\"birthDate\"><dateTimeValue>1980-10-01T00:00:00Z</dateTimeValue></lessThan><or><equal propertyPath=\"name.first\"><stringValue>Eric</stringValue></equal><equal propertyPath=\"name.last\"><stringValue>Westfall</stringValue></equal></or></criteria>";
 	
 	@Test
 	public void testCriteria() {
@@ -66,18 +64,29 @@ public class CriteriaTest {
 	// TODO add a test which tests jaxb with all possible combinations of expressions to make sure they are all getting serialized properly
 	
 	private static Criteria create() throws Exception {
-		Date gtBirthDate = new SimpleDateFormat("yyyyMMdd").parse("19800901");
-		Date ltBirthDate = new SimpleDateFormat("yyyyMMdd").parse("19801001");
-		// normalize timezone to GMT for purpose of the tests
-		Calendar gtCalendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		gtCalendar.setTimeInMillis(gtBirthDate.getTime());
-		Calendar ltCalendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		ltCalendar.setTimeInMillis(ltBirthDate.getTime());
+		
+		// try to create this in a way that won't break based on machine locale or system clock
+		Calendar gtBirthDate = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		gtBirthDate.set(Calendar.MONTH, 8);
+		gtBirthDate.set(Calendar.DAY_OF_MONTH, 1);
+		gtBirthDate.set(Calendar.YEAR, 1980);
+		gtBirthDate.set(Calendar.HOUR_OF_DAY, 0);
+		gtBirthDate.set(Calendar.MINUTE, 0);
+		gtBirthDate.set(Calendar.SECOND, 0);
+		gtBirthDate.set(Calendar.MILLISECOND, 0);
+		Calendar ltBirthDate = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		ltBirthDate.set(Calendar.MONTH, 9);
+		ltBirthDate.set(Calendar.DAY_OF_MONTH, 1);
+		ltBirthDate.set(Calendar.YEAR, 1980);
+		ltBirthDate.set(Calendar.HOUR_OF_DAY, 0);
+		ltBirthDate.set(Calendar.MINUTE, 0);
+		ltBirthDate.set(Calendar.SECOND, 0);
+		ltBirthDate.set(Calendar.MILLISECOND, 0);
 		
 		CriteriaBuilder<Person> builder = CriteriaBuilder.newCriteriaBuilder(Person.class);
 		builder.like("display", "*Eric*");
-		builder.greaterThan("birthDate", gtCalendar);
-		builder.lessThan("birthDate", ltCalendar);
+		builder.greaterThan("birthDate", gtBirthDate);
+		builder.lessThan("birthDate", ltBirthDate);
 		CriteriaBuilder<Person> orCriteria = builder.or();
 		orCriteria.equal("name.first", "Eric");
 		orCriteria.equal("name.last", "Westfall");

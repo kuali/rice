0d2c9d4ce38991a7565295e2530c7036b3af5cf5
Eric Westfall <ewestfal@iu.edu>
2011-04-04 19:31:00 +0000

    KULRICE-5062 - filled in more detail on the ModelBuilder javadoc.

--- core/api/src/main/java/org/kuali/rice/core/api/mo/ModelBuilder.java
+++ core/api/src/main/java/org/kuali/rice/core/api/mo/ModelBuilder.java
@@ -1,9 +1,25 @@
 package org.kuali.rice.core.api.mo;
 
 /**
- * This is an interface that defines a builder.
+ * This is an interface that defines a builder.  A builder is an object used to
+ * assemble and construct an instance of another object.  Typically the object
+ * being constructed will be an immutable object (and therefore does not
+ * contain "setters" which can be used to mutate it's state).  The builder
+ * pattern is a creation pattern that can be used to aid in the construction of
+ * these complex immutable objects.
+ * 
+ * <p>This interface only defines a common {@link #build()} method which is
+ * used to return an instance of the object once state has been set on the
+ * builder to a point where construction of an object instance is deemed
+ * acceptable by the client code.  Definition of type-specific setter methods
+ * are defined by the classes which implement this interface.
  *
- * See Effective Java 2nd ed. page 15 for more information.
+ * <p>This version of the builder pattern is proposed by Joshua Bloch in his
+ * book "Effective Java".  See "Effective Java" 2nd ed. page 15 for more
+ * information.
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ * 
  */
 public interface ModelBuilder {
 	
@@ -16,5 +32,5 @@ public interface ModelBuilder {
 	 * @return an instance of the object being built by this builder,
 	 * should never return null
 	 */
-     /*<T> T*/ Object build();
+     Object build();
 }

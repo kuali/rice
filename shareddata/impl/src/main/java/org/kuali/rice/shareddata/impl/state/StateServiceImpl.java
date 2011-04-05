510460d7691b29213174e2fb507cb472aaf5ef2c
Travis Schneeberger <tschneeb@gmail.com>
2011-04-05 15:26:25 +0000

    format imports

--- shareddata/impl/src/main/java/org/kuali/rice/shareddata/impl/state/StateServiceImpl.java
+++ shareddata/impl/src/main/java/org/kuali/rice/shareddata/impl/state/StateServiceImpl.java
@@ -22,7 +22,12 @@ import org.kuali.rice.kns.service.BusinessObjectService;
 import org.kuali.rice.shareddata.api.state.State;
 import org.kuali.rice.shareddata.api.state.StateService;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StateServiceImpl implements StateService {
 

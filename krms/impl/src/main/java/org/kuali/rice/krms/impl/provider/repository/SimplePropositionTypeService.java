6639ea5796bdd38da987e98126edd407b493fa8a
Eric Westfall <ewestfal@iu.edu>
2011-04-14 05:44:28 +0000

    KULRICE-4746 - committed scaffolding code for the SimplePropositionTypeService

---
+++ krms/impl/src/main/java/org/kuali/rice/krms/impl/provider/repository/SimplePropositionTypeService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krms.impl.provider.repository;
+
+import org.kuali.rice.krms.api.repository.PropositionDefinition;
+import org.kuali.rice.krms.framework.engine.Proposition;
+import org.kuali.rice.krms.framework.type.PropositionTypeService;
+
+/**
+ * TODO... 
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ *
+ */
+public class SimplePropositionTypeService implements PropositionTypeService {
+
+	private RepositoryToEngineTranslator translator;
+	
+	@Override
+	public Proposition loadProposition(PropositionDefinition propositionDefinition) {
+		// TODO...
+		throw new UnsupportedOperationException("TODO - implement me!!!");
+	}
+	
+	public void setTranslator(RepositoryToEngineTranslator translator) {
+		this.translator = translator;
+	}
+
+}

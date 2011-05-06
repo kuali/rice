8559687af997500970995a18cfe6255f20b5f3fb
Jeremy Hanson <jjhanso@iastate.edu>
2011-05-06 18:59:44 +0000

    KULRICE-4794 - fixes for kns unit tests

--- test/kns/src/test/resources/org/kuali/rice/kns/test/DefaultSuiteTestData.sql
+++ test/kns/src/test/resources/org/kuali/rice/kns/test/DefaultSuiteTestData.sql
@@ -25,11 +25,11 @@ insert into TRV_ACCT_TYPE values ('EAT', 'Expense Account Type', 'B0B067E2D30B48
 ;
 insert into TRV_ACCT_TYPE values ('IAT', 'Income Account Type', '37DA37109BCB421BAF78E6F8824921B6', 1)
 ;
-insert into TRV_ACCT values ('a1', 'a1', 'CAT', 1, '7003409C8F824FDD836C799F87A2C04C', 1)
+insert into TRV_ACCT values ('a1', 'a1', 'CAT', 1, '7003409C8F824FDD836C799F87A2C04C', 1, null, null, null, null)
 ;
-insert into TRV_ACCT values ('a2', 'a2', 'EAT', 2, 'DC6F5FE5003B483092F21D99AF4C701A', 1)
+insert into TRV_ACCT values ('a2', 'a2', 'EAT', 2, 'DC6F5FE5003B483092F21D99AF4C701A', 1, null, null, null, null)
 ;
-insert into TRV_ACCT values ('a3', 'a3', 'IAT', 3, 'C68B618C117A4234A33F0C3146E92439', 1)
+insert into TRV_ACCT values ('a3', 'a3', 'IAT', 3, 'C68B618C117A4234A33F0C3146E92439', 1, null, null, null, null)
 ;
 insert into TRV_ACCT_EXT values ('a1', 'CAT', '8AE92DD8F6F248179F40BB9E4C095B9D', 1)
 ;

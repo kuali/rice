ad7947f6e5179cf645df4e59d1bc65fa1666dc37
Jeremy Hanson <jjhanso@iastate.edu>
2011-05-10 19:15:25 +0000

    KULRICE-5049 - fix for last commit.  Group.xml and groupUpdateServiceImplTest were not committed.

---
+++ impl/src/test/groovy/org/kuali/rice/kim/impl/group/GroupUpdateServiceImplTest.groovy
@@ -0,0 +1,79 @@
+package org.kuali.rice.kim.impl.group
+
+import org.junit.Test
+import org.junit.BeforeClass
+import java.sql.Timestamp
+import org.kuali.rice.kim.impl.type.KimTypeBo
+import groovy.mock.interceptor.MockFor
+import org.kuali.rice.kns.service.BusinessObjectService
+import org.junit.Before
+import org.kuali.rice.kim.api.group.GroupService
+import org.kuali.rice.kim.service.impl.GroupUpdateServiceImpl
+import org.kuali.rice.kim.api.group.GroupUpdateService
+import org.kuali.rice.kns.bo.PersistableBusinessObject
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jjhanso
+ * Date: 5/9/11
+ * Time: 12:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class GroupUpdateServiceImplTest {
+
+    //importing the should fail method since I don't want to extend
+    //GroovyTestCase which is junit 3 style
+    private final shouldFail = new GroovyTestCase().&shouldFail
+
+    MockFor mock
+    BusinessObjectService bos
+
+    GroupServiceImpl groupServiceImpl
+    GroupService groupService
+    GroupUpdateService groupUpdateService
+    GroupUpdateServiceImpl groupUpdateServiceImpl
+
+    @Before
+    void setupMockContext() {
+        mock = new MockFor(BusinessObjectService.class)
+
+    }
+
+    @Before
+    void setupServiceUnderTest() {
+        groupServiceImpl = new GroupServiceImpl()
+        groupService = groupServiceImpl    //assign Interface type to implementation reference for unit test only
+
+        groupUpdateServiceImpl = new GroupUpdateServiceImpl()
+        groupUpdateService = groupUpdateServiceImpl
+    }
+
+    void injectBusinessObjectServiceIntoGroupUpdateService() {
+        bos = mock.proxyDelegateInstance()
+        groupUpdateServiceImpl.setBusinessObjectService(bos)
+    }
+
+    @Test
+    void test_createGroupNullGroup(){
+        injectBusinessObjectServiceIntoGroupUpdateService()
+
+        shouldFail(IllegalArgumentException.class) {
+            groupUpdateService.createGroup(null)
+        }
+        mock.verify(bos)
+
+    }
+
+    @Test
+    void test_updateGroupNullGroup(){
+        injectBusinessObjectServiceIntoGroupUpdateService()
+
+        shouldFail(IllegalArgumentException.class) {
+            groupUpdateService.updateGroup(null, null)
+        }
+        mock.verify(bos)
+
+    }
+
+
+}

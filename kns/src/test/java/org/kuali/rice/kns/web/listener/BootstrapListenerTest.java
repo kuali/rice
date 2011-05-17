01afadf0abcd709122dcd593c1cace5f848b66fd
Shannon Hess <ShannonHess18@gmail.com>
2011-05-17 19:19:58 +0000

    KULRICE-5114 : Allow config.xml files to include HTTPSessionListeners ;  Moving the location of the JUnit tests to prevent errors.

--- kns/src/test/java/org/kuali/rice/kns/web/listener/BootstrapListenerTest.java
+++ kns/src/test/java/org/kuali/rice/kns/web/listener/BootstrapListenerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.web;
+package org.kuali.rice.kns.web.listener;
 
 import java.net.URL;
 import java.net.URLClassLoader;

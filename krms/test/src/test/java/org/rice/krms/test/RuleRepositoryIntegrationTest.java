95bc3e877d8d04c2b918a213bda27dd8000ade66
Dan Seibert <dseibert@vivantech.com>
2011-05-25 17:46:56 +0000

    KULRICE-5139 Changed Action used in test. Added 2 assertions at end of test.

--- krms/test/src/test/java/org/rice/krms/test/RuleRepositoryIntegrationTest.java
+++ krms/test/src/test/java/org/rice/krms/test/RuleRepositoryIntegrationTest.java
@@ -1,5 +1,7 @@
 package org.rice.krms.test;
 
+import static junit.framework.Assert.assertTrue;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -9,19 +11,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.kuali.rice.krms.api.engine.Engine;
 import org.kuali.rice.krms.api.engine.EngineResults;
+import org.kuali.rice.krms.api.engine.ExecutionFlag;
 import org.kuali.rice.krms.api.engine.ExecutionOptions;
 import org.kuali.rice.krms.api.engine.ResultEvent;
 import org.kuali.rice.krms.api.engine.SelectionCriteria;
 import org.kuali.rice.krms.api.engine.Term;
 import org.kuali.rice.krms.api.engine.TermSpecification;
 import org.kuali.rice.krms.api.repository.RuleRepositoryService;
+import org.kuali.rice.krms.api.repository.type.KrmsTypeRepositoryService;
 import org.kuali.rice.krms.framework.engine.ProviderBasedEngine;
+import org.kuali.rice.krms.framework.engine.ResultLogger;
+import org.kuali.rice.krms.framework.engine.result.EngineResultListener;
+import org.kuali.rice.krms.framework.engine.result.Log4jResultListener;
 import org.kuali.rice.krms.impl.provider.repository.RepositoryToEngineTranslator;
 import org.kuali.rice.krms.impl.provider.repository.RepositoryToEngineTranslatorImpl;
 import org.kuali.rice.krms.impl.provider.repository.RuleRepositoryContextProvider;
+import org.kuali.rice.krms.impl.provider.repository.SimplePropositionTypeService;
+import org.kuali.rice.krms.impl.repository.KrmsTypeBoServiceImpl;
 import org.kuali.rice.krms.impl.repository.RuleRepositoryServiceImpl;
 import org.kuali.rice.krms.impl.repository.TermBoService;
 import org.kuali.rice.krms.impl.repository.TermBoServiceImpl;
+import org.kuali.rice.krms.impl.type.KrmsTypeResolverImpl;
 import org.kuali.rice.test.BaselineTestCase.BaselineMode;
 import org.kuali.rice.test.BaselineTestCase.Mode;
 import org.kuali.rice.test.data.UnitTestData;
@@ -39,7 +49,10 @@ public class RuleRepositoryIntegrationTest extends AbstractBoTest {
 	private ProviderBasedEngine engine;
 	private RepositoryToEngineTranslator repositoryToEngineTranslator;
 	private TermBoService termBoService;
+    private KrmsTypeRepositoryService krmsTypeRepository;
 
+    private ResultLogger resultLogger = ResultLogger.getInstance();
+    
 	@Before
 	public void setup() {
 		super.setup();
@@ -53,15 +66,32 @@ public class RuleRepositoryIntegrationTest extends AbstractBoTest {
 		termBoService = new TermBoServiceImpl();
 		((TermBoServiceImpl)termBoService).setBusinessObjectService(getBoService());
 		
+		krmsTypeRepository = new KrmsTypeBoServiceImpl();
+		((KrmsTypeBoServiceImpl)krmsTypeRepository).setBusinessObjectService(getBoService());
+
+        KrmsTypeResolverImpl typeResolver = new KrmsTypeResolverImpl();
+        typeResolver.setTypeRepositoryService(krmsTypeRepository);
+
+        SimplePropositionTypeService simplePropositionTypeService = new SimplePropositionTypeService();
+        simplePropositionTypeService.setTypeResolver(typeResolver);
+        simplePropositionTypeService.setTermBoService(termBoService);
+        typeResolver.setDefaultSimplePropositionTypeService(simplePropositionTypeService);
+        
 		repositoryToEngineTranslator = new RepositoryToEngineTranslatorImpl();
 		((RepositoryToEngineTranslatorImpl)repositoryToEngineTranslator).setTermBoService(termBoService);
-		
+        ((RepositoryToEngineTranslatorImpl)repositoryToEngineTranslator).setRuleRepositoryService(ruleRepositoryService);
+        ((RepositoryToEngineTranslatorImpl)repositoryToEngineTranslator).setTypeRepositoryService(krmsTypeRepository);
+        ((RepositoryToEngineTranslatorImpl)repositoryToEngineTranslator).setTypeResolver(typeResolver);
+	
 		contextProvider = new RuleRepositoryContextProvider();
 		contextProvider.setRuleRepositoryService(ruleRepositoryService);
 		contextProvider.setRepositoryToEngineTranslator(repositoryToEngineTranslator);
 		
 		engine = new ProviderBasedEngine();
 		engine.setContextProvider(contextProvider);
+
+	    resultLogger.addListener(new EngineResultListener());
+        resultLogger.addListener(new Log4jResultListener());	
 	}
 	
 	@Test
@@ -73,16 +103,23 @@ public class RuleRepositoryIntegrationTest extends AbstractBoTest {
 		contextQualifiers.put("Context1Qualifier", "BLAH");
 		Map<String,String> agendaQualifiers = new HashMap<String,String>();
 		Date now = new Date();
-		SelectionCriteria sc = SelectionCriteria.createCriteria("Earthquake", now, contextQualifiers, agendaQualifiers);
+		SelectionCriteria sc = SelectionCriteria.createCriteria("EARTHQUAKE", now, contextQualifiers, agendaQualifiers);
 		
 		Map<Term, Object> facts = new HashMap<Term, Object>();
-		TermSpecification campusCodeTermSpec = new TermSpecification("campusCodeTermSpec", "campusCodeType");
+		TermSpecification campusCodeTermSpec = new TermSpecification("campusCodeTermSpec", "java.lang.String");
 		Term myCampusCode = new Term( campusCodeTermSpec );
 		facts.put(myCampusCode, "BL");
 		
 		ExecutionOptions xOptions = new ExecutionOptions();
+		xOptions.setFlag(ExecutionFlag.LOG_EXECUTION, true);
 		EngineResults eResults = engine.execute(sc, facts, xOptions);
 		List<ResultEvent> rEvents = eResults.getAllResults();
+		
+	    List<ResultEvent> ruleEvaluationResults = eResults.getResultsOfType(ResultEvent.RuleEvaluated.toString());
+	    
+	    assertTrue("We should have results for one rule", ruleEvaluationResults.size() == 1);
+	    assertTrue("rule should have evaluated to true", ruleEvaluationResults.get(0).getResult() == true);
+
 	}
 	
 }
--- krms/test/src/test/resources/org/kuali/rice/test/RuleRepositoryTestData.sql
+++ krms/test/src/test/resources/org/kuali/rice/test/RuleRepositoryTestData.sql
@@ -30,7 +30,7 @@ values ('T2', 'CAMPUS', 'KRMS_TEST', 'myCampusService', 'Y', 1)
 
 insert into krms_typ_t 
 (typ_id, nm, nmspc_cd, srvc_nm, actv, ver_nbr)
-values ('T3', 'SAY_HELLO_ACTION', 'KRMS_TEST', 'myHelloSpamService', 'Y', 1)
+values ('T3', 'KrmsActionResolverType', 'KRMS_TEST', 'testActionTypeService', 'Y', 1)
 ;
 
 insert into krms_typ_t 
@@ -53,39 +53,6 @@ insert into krms_typ_attr_t
 values ('T5A', 1, 'T5', 'Q33001', 'Y', 1)
 ;
 
-insert into krms_rule_t
-(rule_id, nmspc_cd, nm, typ_id, prop_id, actv, ver_nbr, descr_txt)
-values ('R201', 'KRMS_TEST', 'Rule1', 'T2', null, 'Y', 1, 'Bloomington Campus Code Rule')
-;
-
-insert into krms_prop_t(prop_id, desc_txt, typ_id, dscrm_typ_cd, rule_id, ver_nbr)
-values ('P101', 'is campus bloomington', 'T2', 'S','R201',1)
-;
-
-update krms_rule_t
-set prop_id = 'P101' where rule_id = 'R201'
-;
-
-insert into krms_prop_parm_t 
-(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
-values ('101A', 'P101', 'campusCode', 'T', 1, 1)
-;
-
-insert into krms_prop_parm_t 
-(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
-values ('101B', 'P101', '=', 'O', 1, 1)
-;
-
-insert into krms_prop_parm_t 
-(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
-values ('101C', 'P101', 'BL', 'C', 1, 1)
-;
-
-insert into krms_actn_t
-(actn_id, nm, nmspc_cd, desc_txt, typ_id, rule_id, seq_no, ver_nbr)
-values ( 'action2001', 'Say Hello', 'KRMS_TEST', 'Send Hello SPAM', 'T3', 'R201', 1, 1)
-;
-
 insert into krms_cntxt_t
 (cntxt_id, nmspc_cd, nm, typ_id, actv, ver_nbr)
 values ('CONTEXT1','KRMS_TEST', 'Context1', 'T4', 'Y', 1)
@@ -94,6 +61,48 @@ values ('CONTEXT1','KRMS_TEST', 'Context1', 'T4', 'Y', 1)
 insert into krms_cntxt_attr_t
 (cntxt_attr_id, cntxt_id, attr_val, attr_defn_id, ver_nbr)
 values('C1ATTR1', 'CONTEXT1', 'BLAH', 'Q44001', 1)
+;
+
+
+insert into krms_rule_t
+(rule_id, nmspc_cd, nm, typ_id, prop_id, actv, ver_nbr, descr_txt)
+values ('R201', 'KRMS_TEST', 'Rule1', 'T2', null, 'Y', 1, 'Bloomington Campus Code Rule')
+;
+
+insert into krms_prop_t(prop_id, desc_txt, typ_id, dscrm_typ_cd, rule_id, ver_nbr)
+values ('P101', 'is campus bloomington', null, 'S','R201',1)
+;
+
+update krms_rule_t
+set prop_id = 'P101' where rule_id = 'R201'
+;
+
+insert into krms_term_spec_t
+(term_spec_id, cntxt_id, nm, typ, actv, ver_nbr)
+values ('TERMSPEC_001', 'CONTEXT1', 'campusCodeTermSpec', 'java.lang.String', 'Y', 1);
+
+insert into krms_term_t
+(term_id, term_spec_id, ver_nbr)
+values ('TERM_001', 'TERMSPEC_001', 1);
+
+insert into krms_prop_parm_t 
+(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
+values ('101A', 'P101', 'TERM_001', 'T', 1, 1)
+;
+
+insert into krms_prop_parm_t 
+(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
+values ('101C', 'P101', 'BL', 'C', 2, 1)
+;
+
+insert into krms_prop_parm_t 
+(prop_parm_id, prop_id, parm_val, parm_typ_cd, seq_no, ver_nbr)
+values ('101B', 'P101', '=', 'O', 3, 1)
+;
+
+insert into krms_actn_t
+(actn_id, nm, nmspc_cd, desc_txt, typ_id, rule_id, seq_no, ver_nbr)
+values ( 'action2001', 'testAction', 'KRMS_TEST', 'Action Stub for Testing', 'T3', 'R201', 1, 1)
 ;
 
 insert into krms_agenda_t

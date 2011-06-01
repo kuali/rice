43d0efc2b1d81d51a84095a4bd598610d7eb00de
Eric Westfall <ewestfal@iu.edu>
2011-06-01 00:17:10 +0000

    KULRICE-4816 - javadoc'd ServiceInfoContract

--- ksb/api/src/main/java/org/kuali/rice/ksb/api/registry/ServiceInfoContract.java
+++ ksb/api/src/main/java/org/kuali/rice/ksb/api/registry/ServiceInfoContract.java
@@ -2,30 +2,102 @@ package org.kuali.rice.ksb.api.registry;
 
 import javax.xml.namespace.QName;
 
+import org.kuali.rice.core.api.CoreConstants;
 import org.kuali.rice.core.api.mo.common.Versioned;
+import org.kuali.rice.ksb.api.bus.ServiceConfiguration;
 
 public interface ServiceInfoContract extends Versioned {
 
+	/**
+	 * Returns the identifier for the service.
+	 * 
+	 * @return the identifier for the service, will only be null if the
+	 * service has not yet been published to the registry
+	 */
 	public String getServiceId();
 
+	/**
+	 * Returns the name of the service as a qualified name consisting of a
+	 * namespace and a name.
+	 * 
+	 * @return the name of the service, should never be null
+	 */
 	public QName getServiceName();
 
+	/**
+	 * Returns the URL of the service as a string.
+	 * 
+	 * @return the url of the service, should never be null or blank
+	 */
 	public String getEndpointUrl();
 	
+	/**
+	 * Returns the id of the instance that published and owns the service.
+	 * 
+	 * @return the instance id of this service, should never be null or blank
+	 */
 	public String getInstanceId();
 	
+	/**
+	 * Returns the namespace of the application that published and owns the service.
+	 * 
+	 * @return the application namespace of this service, should never be null or blank
+	 */
 	public String getApplicationNamespace();
 
+	/**
+	 * Return the IP address of the server on which the application is running which
+	 * published and owns the service.  This value could be either an IPv4 or
+	 * IPv6 address, but it should never return a null or empty string.
+	 * 
+	 * @return the IP address of this service, should never be null or blank
+	 */
 	public String getServerIpAddress();
 	
+	/**
+	 * Returns the type of this service.  Will generally distinguish the format
+	 * of the data being brokered by the service (i.e. SOAP, REST, Java
+	 * Serialization, etc.)
+	 * 
+	 * @return the type of this service, should never be null or blank
+	 */
 	public String getType();
 	
+	/**
+	 * Returns the version information of this service.  The publisher of the
+	 * service can use any value they choose for the service versions.
+	 * However, there is one standard version which represents a service
+	 * without any version information, and that is {@link CoreConstants.Versions#UNSPECIFIED}.
+	 * 
+	 * @return the version of this service, or {@link CoreConstants.Versions#UNSPECIFIED}
+	 * if no version has been secified, should never return a null or blank value
+	 */
 	public String getServiceVersion();
 	
+	/**
+	 * Return the status of the service endpoint represented by this service.
+	 * 
+	 * @return the status of this service
+	 */
 	public ServiceEndpointStatus getStatus();
 	
+	/**
+	 * Returns the id of the service descriptor for this service.  This id can
+	 * be used to help locate the {@link ServiceDescriptorContract} for this
+	 * service which includes more detailed information on this service.
+	 * 
+	 * @return the id of the service descriptor for this service, will only return
+	 * a null value if the service has not yet been published
+	 */
 	public String getServiceDescriptorId();
 
+	/**
+	 * Returns a checksum value for the {@link ServiceConfiguration} stored in the
+	 * {@link ServiceDescriptorContract} for this service.  This allows for fast
+	 * comparison of services during various registry operations.
+	 * 
+	 * @return the checksum for this service, should never return a null or blank value
+	 */
 	public String getChecksum();
 	
 }

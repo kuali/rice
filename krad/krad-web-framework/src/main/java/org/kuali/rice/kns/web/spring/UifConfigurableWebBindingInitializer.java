dc7f7370caee5fccaa4ee5f3c902747dd94d4b4a
Samuel Holtzkampf <samuel@opencollab.co.za>
2011-06-01 19:45:10 +0000

    KULRICE-5179 Convert KNS Formatters to Spring PropertyEditors for use in KRAD

--- krad/krad-web-framework/src/main/java/org/kuali/rice/kns/web/spring/UifConfigurableWebBindingInitializer.java
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/kns/web/spring/UifConfigurableWebBindingInitializer.java
@@ -1,57 +1,69 @@
 /*
- * Copyright 2007 The Kuali Foundation
- *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl1.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2007 The Kuali Foundation Licensed under the Educational Community
+ * License, Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.opensource.org/licenses/ecl1.php Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 package org.kuali.rice.kns.web.spring;
 
-import java.sql.Date;
+import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.text.DecimalFormat;
 
 import org.kuali.rice.core.util.type.AbstractKualiDecimal;
 import org.kuali.rice.core.util.type.KualiDecimal;
 import org.kuali.rice.core.util.type.KualiInteger;
 import org.kuali.rice.core.util.type.KualiPercent;
-import org.kuali.rice.core.web.format.BigDecimalFormatter;
-import org.kuali.rice.core.web.format.CurrencyFormatter;
-import org.kuali.rice.core.web.format.DateFormatter;
-import org.kuali.rice.core.web.format.DateViewTimestampObjectFormatter;
-import org.kuali.rice.core.web.format.KualiIntegerCurrencyFormatter;
-import org.kuali.rice.core.web.format.PercentageFormatter;
+import org.springframework.beans.propertyeditors.CustomNumberEditor;
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.context.request.WebRequest;
 
 /**
- * Registers standard PropertyEditors used in binding for all http requests. 
+ * Registers standard PropertyEditors used in binding for all http requests.
  * 
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public class UifConfigurableWebBindingInitializer extends ConfigurableWebBindingInitializer {
 
-	@Override
+    @Override
     public void initBinder(WebDataBinder binder, WebRequest request) {
-        binder.registerCustomEditor(AbstractKualiDecimal.class, new UifKnsFormatterPropertyEditor(BigDecimalFormatter.class));
-        binder.registerCustomEditor(KualiDecimal.class, new UifKnsFormatterPropertyEditor(CurrencyFormatter.class)); 
-        binder.registerCustomEditor(KualiInteger.class, new UifKnsFormatterPropertyEditor(KualiIntegerCurrencyFormatter.class));
-        binder.registerCustomEditor(KualiPercent.class, new UifKnsFormatterPropertyEditor(PercentageFormatter.class));
-        binder.registerCustomEditor(Date.class, new UifKnsFormatterPropertyEditor(DateFormatter.class));
-        binder.registerCustomEditor(Timestamp.class, new UifKnsFormatterPropertyEditor(DateViewTimestampObjectFormatter.class));
-        
+
+
+        binder.registerCustomEditor(KualiDecimal.class, new UifCurrencyEditor());
+        binder.registerCustomEditor(KualiInteger.class, new UifKualiIntegerCurrencyEditor());
+
+        binder.registerCustomEditor(KualiPercent.class, new UifPercentageEditor());
+
+        binder.registerCustomEditor(java.sql.Date.class, new UifDateEditor());
+        binder.registerCustomEditor(java.util.Date.class, new UifDateEditor());
+        binder.registerCustomEditor(Timestamp.class, new UifDateViewTimestampEditor());
+
         // TODO do we need this since we are switching to spring tags
         binder.registerCustomEditor(boolean.class, new UifBooleanEditor());
-        
-	    super.initBinder(binder, request);
+        binder.registerCustomEditor(Boolean.class, new UifBooleanEditor());
+        binder.registerCustomEditor(Boolean.TYPE, new UifBooleanEditor());
+
+        // Use the spring custom number editor for Big decimals
+        DecimalFormat bigIntFormatter = new DecimalFormat();
+        bigIntFormatter.setMaximumFractionDigits(340);
+        binder.registerCustomEditor(BigDecimal.class, new CustomNumberEditor(BigDecimal.class, bigIntFormatter, true));
+        binder.registerCustomEditor(AbstractKualiDecimal.class, new CustomNumberEditor(AbstractKualiDecimal.class,
+                bigIntFormatter, true));
+
+        // Use the spring StringTrimmerEditor editor for Strings
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+        // Use the StringArrayPropertyEditor for string arrays with "," as the
+        // separator
+        binder.registerCustomEditor(String[].class, new StringArrayPropertyEditor(",", false));
+
+        super.initBinder(binder, request);
     }
 
 }
---
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/kns/web/spring/UifDateEditor.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2007 The Kuali Foundation Licensed under the Educational Community
+ * License, Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.opensource.org/licenses/ecl1.php Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.kuali.rice.kns.web.spring;
+
+import java.beans.PropertyEditorSupport;
+import java.sql.Date;
+import java.text.ParseException;
+import java.util.Calendar;
+
+import org.kuali.rice.core.api.CoreConstants;
+import org.kuali.rice.core.api.datetime.DateTimeService;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.util.RiceKeyConstants;
+import org.kuali.rice.core.web.format.FormatException;
+
+/**
+ * This PropertyEditor converts between date display strings and
+ * <code>java.sql.Date</code> objects.
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class UifDateEditor extends PropertyEditorSupport {
+
+    /** The date time service. */
+    private transient DateTimeService dateTimeService;
+
+    /**
+     * This overridden method uses the
+     * <code>org.kuali.rice.core.api.datetime.DateTimeService</code> to convert
+     * the date object to the display string.
+     * 
+     * @see java.beans.PropertyEditorSupport#getAsText()
+     */
+    @Override
+    public String getAsText() {
+        if (this.getValue() == null) {
+            return null;
+        }
+        if ("".equals(this.getValue())) {
+            return null;
+        }
+        return getDateTimeService().toDateString((java.util.Date) this.getValue());
+    }
+
+    /**
+     * Gets the date time service.
+     * 
+     * @return the date time service
+     */
+    protected DateTimeService getDateTimeService() {
+        if (this.dateTimeService == null) {
+            this.dateTimeService = GlobalResourceLoader.getService(CoreConstants.Services.DATETIME_SERVICE);
+        }
+        return this.dateTimeService;
+    }
+
+    /**
+     * This overridden method converts the display string to a
+     * <code>java.sql.Date</code> object using the
+     * <code>org.kuali.rice.core.api.datetime.DateTimeService</code>.
+     * 
+     * @see java.beans.PropertyEditorSupport#setAsText(java.lang.String)
+     */
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        this.setValue(convertToObject(text));
+    }
+
+    /**
+     * Convert display text to <code>java.sql.Date</code> object using the
+     * <code>org.kuali.rice.core.api.datetime.DateTimeService</code>.
+     * 
+     * @param text
+     *            the display text
+     * @return the <code>java.sql.Date</code> object
+     * @throws IllegalArgumentException
+     *             the illegal argument exception
+     */
+    protected Object convertToObject(String text) throws IllegalArgumentException {
+        try {
+            Date result = getDateTimeService().convertToSqlDate(text);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(result);
+            if (calendar.get(Calendar.YEAR) < 1000 && verbatimYear(text).length() < 4) {
+                throw new FormatException("illegal year format", RiceKeyConstants.ERROR_DATE, text);
+            }
+            return result;
+        } catch (ParseException e) {
+            throw new FormatException("parsing", RiceKeyConstants.ERROR_DATE, text, e);
+        }
+    }
+
+    /**
+     * For a given user input date, this method returns the exact string the
+     * user entered after the last slash. This allows the formatter to
+     * distinguish between ambiguous values such as "/06" "/6" and "/0006"
+     * 
+     * @param date
+     * @return
+     */
+    private String verbatimYear(String date) {
+        String result = "";
+
+        int pos = date.lastIndexOf("/");
+        if (pos >= 0) {
+            result = date.substring(pos);
+        }
+
+        return result;
+    }
+
+}

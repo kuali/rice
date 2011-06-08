1b1bea216830de08627bb3c603ed6502aa478f8f
Eric Westfall <ewestfal@iu.edu>
2011-06-08 18:51:08 +0000

    Made a minor improvement to the Versioned interface javadoc

--- core/api/src/main/java/org/kuali/rice/core/api/mo/common/Versioned.java
+++ core/api/src/main/java/org/kuali/rice/core/api/mo/common/Versioned.java
@@ -17,7 +17,7 @@ package org.kuali.rice.core.api.mo.common;
 
 /**
  * This interface can be used to identify a model object which has a version number
- * that is used for the purpose of optimistic locking.
+ * that is used for the purposes of optimistic locking.
  * 
  * @author Kuali Rice Team (rice.collab@kuali.org)
  *
@@ -28,6 +28,7 @@ public interface Versioned {
 	/**
 	 * Returns the version number for this object.  In general, this value should only
 	 * be null if the object has not yet been stored to a persistent data store.
+	 * This version number is generally used for the purposes of optimistic locking.
 	 * 
 	 * @return the version number, or null if one has not been assigned yet
 	 */

9e9c7bc77c33c21c5f3c7136c7d48a63d49b14c1
David Elyea <delyea@iu.edu>
2011-06-11 06:33:12 +0000

    KULRICE-5155 - Initial commit of character level validation classes for KRAD

---
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/kns/datadictionary/validation/constraint/AlphaPatternConstraint.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2005-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kns.datadictionary.validation.constraint;
+
+import org.apache.commons.lang.StringUtils;
+
+
+/**
+ * Pattern for matching alpha characters
+ * 
+ * 
+ */
+public class AlphaPatternConstraint extends ValidCharactersPatternConstraint {
+    protected boolean allowWhitespace = false;
+
+
+    /**
+     * @return allowWhitespace
+     */
+    public boolean getAllowWhitespace() {
+        return allowWhitespace;
+    }
+
+    /**
+     * @param allowWhitespace
+     */
+    public void setAllowWhitespace(boolean allowWhitespace) {
+        this.allowWhitespace = allowWhitespace;
+    }
+
+
+    /**
+     * @see org.kuali.rice.kns.datadictionary.validation.ValidationPattern#getRegexString()
+     */
+    protected String getRegexString() {
+        StringBuffer regexString = new StringBuffer("[A-Za-z");
+
+        if (allowWhitespace) {
+            regexString.append("\\s");
+        }
+        regexString.append("]");
+
+        return regexString.toString();
+    }
+
+	/**
+	 * 
+	 * @see org.kuali.rice.kns.datadictionary.validation.constraint.BaseConstraint#getLabelKey()
+	 */
+	@Override
+	public String getLabelKey() {
+		String labelKey = super.getLabelKey();
+		if (StringUtils.isNotEmpty(labelKey)) {
+			return labelKey;
+		}
+		StringBuilder key = new StringBuilder("");
+		key.append("alphaPattern,");
+		if (getAllowWhitespace()) {
+			key.append("whitespace");
+		}
+		return key.toString();
+	}
+
+//	@Override
+//	protected String getValidationErrorMessageKeyOptions() {
+//		if (getAllowWhitespace()) {
+//			return ".allowWhitespace";
+//		}
+//		return KNSConstants.EMPTY_STRING;
+//	}
+
+}
---
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/kns/datadictionary/validation/constraint/RegexPatternConstraint.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.kns.datadictionary.validation.constraint;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kns.datadictionary.validation.ValidationPattern.ValidationPatternException;
+
+/**
+ * This class is used to set a regex pattern of characters to match. This string should not
+ * include the start(^) and end($) symbols or any length related symbols (*, {0,}, etc)
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RegexPatternConstraint extends ValidCharactersPatternConstraint {
+
+    @XmlElement
+    protected String pattern;
+	
+    @XmlElement
+    private String validationErrorMessageKey;
+	
+	/**
+	 * @return the pattern
+	 */
+	public String getPattern() {
+		return this.pattern;
+	}
+
+	/**
+	 * @param pattern the pattern to set
+	 */
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+
+	/**
+	 * @return the validationErrorMessageKey
+	 */
+	public String getValidationErrorMessageKey() {
+		return this.validationErrorMessageKey;
+	}
+
+	/**
+	 * @param validationErrorMessageKey the validationErrorMessageKey to set
+	 */
+	public void setValidationErrorMessageKey(String validationErrorMessageKey) {
+		this.validationErrorMessageKey = validationErrorMessageKey;
+	}
+
+	/**
+	 * This overridden method ...
+	 * 
+	 * @see org.kuali.rice.kns.datadictionary.validation.constraint.BaseConstraint#getLabelKey()
+	 */
+	@Override
+	public String getLabelKey() {
+		if (StringUtils.isBlank(getValidationErrorMessageKey())) {
+			throw new ValidationPatternException("Regex Validation Patterns must have a validation error message key defined");
+		}
+		return getValidationErrorMessageKey();
+	}
+
+	/**
+	 * This overridden method returns the regex pattern as set by the application
+	 * 
+	 * @see org.kuali.rice.kns.datadictionary.validation.constraint.ValidCharactersPatternConstraint#getRegexString()
+	 */
+	@Override
+	protected String getRegexString() {
+		return getPattern();
+	}
+
+}

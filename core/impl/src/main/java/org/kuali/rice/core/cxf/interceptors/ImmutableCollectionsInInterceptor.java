4669aff4e3256228ac7ef8d5349972a92cfbdb65
Jason Whaley <jwhaley@savoirtech.com>
2011-06-13 14:24:09 +0000

    KULRICE-5188:  committing the actual interceptor used to replayce Immutable*Adapters

---
+++ core/impl/src/main/java/org/kuali/rice/core/cxf/interceptors/ImmutableCollectionsInInterceptor.java
@@ -0,0 +1,50 @@
+package org.kuali.rice.core.cxf.interceptors;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+/**
+ * A CXF Interceptor that binds itself to the USER_LOGICAL phase to be used on inbound
+ * messages.  This interceptor is invoked in the interceptor chain after unmarshalling
+ * from XML to Java has occurred.  The role of this interceptor is to ensure that any
+ * Collection (and specifically List or Set) used in a @WebMethod is ultimately of the
+ * type returned from
+ */
+@SuppressWarnings("unused")
+public class ImmutableCollectionsInInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    /**
+     * Instantiates an ImmutableCollectionsInInterceptor
+     */
+    public ImmutableCollectionsInInterceptor() {
+        super(Phase.USER_LOGICAL);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void handleMessage(final Message message) throws Fault {
+        List contents = message.getContent(List.class);
+        for (int i = 0; i < contents.size(); i++) {
+            Object o = contents.get(i);
+            if (o instanceof List) {
+                List unmodifiable = Collections.unmodifiableList((List) o);
+                contents.set(i, unmodifiable);
+            }
+            else if (o instanceof Set) {
+                Set unmodifiable = Collections.unmodifiableSet((Set) o);
+                contents.set(i, unmodifiable);
+            }
+            else if (o instanceof Collection) {
+                Collection unmodifiable = Collections.unmodifiableCollection((Collection) o);
+                contents.set(i,unmodifiable);
+            }
+        }
+    }
+}

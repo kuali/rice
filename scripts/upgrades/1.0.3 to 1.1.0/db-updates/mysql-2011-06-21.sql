9625f2e6fbaef848ab9c3455aef71ebbb95423d5
Travis Schneeberger <tschneeb@gmail.com>
2011-06-22 13:55:12 +0000

    KULRICE-5116: removing web module

---
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/mysql-2011-06-21.sql
@@ -0,0 +1,7 @@
+alter table KRIM_PERM_TMPL_T change column NMSPC_CD NMSPC_CD varchar2(40) not null;
+alter table KRIM_PERM_TMPL_T change column NM NM varchar2(100) not null;
+alter table KRIM_PERM_TMPL_T add constraint KRIM_PERM_TMPL_TC1 unique (NM, NMSPC_CD);
+
+alter table KRIM_RSP_TMPL_T hange column NMSPC_CD NMSPC_CD varchar2(40) not null;
+alter table KRIM_RSP_TMPL_T change column NM NM varchar2(100) not null;
+alter table KRIM_RSP_TMPL_T add constraint KRIM_RSP_TMPL_TC1 unique (NM, NMSPC_CD);

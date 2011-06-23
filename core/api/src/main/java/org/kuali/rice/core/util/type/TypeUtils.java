51076e40402b274768a0adca85e1241e5a1509e3
Travis Schneeberger <tschneeb@gmail.com>
2011-06-23 21:05:19 +0000

    using concurrenthashmap

--- core/api/src/main/java/org/kuali/rice/core/util/type/TypeUtils.java
+++ core/api/src/main/java/org/kuali/rice/core/util/type/TypeUtils.java
@@ -18,186 +18,185 @@ package org.kuali.rice.core.util.type;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Provides utilities for checking the types of objects.
  */
-@SuppressWarnings("all")
 public class TypeUtils {
-	private static final Class[] BOOLEAN_CLASSES = { Boolean.class, Boolean.TYPE };
-	private static final Class[] INTEGRAL_CLASSES = { Byte.class, Byte.TYPE, Short.class, Short.TYPE, Integer.class,
-			Integer.TYPE, Long.class, Long.TYPE, BigInteger.class, KualiInteger.class };
-	// KRACOEUS-1493 : add AbstractKualiDecimal for budgetdecimal
-	private static final Class[] DECIMAL_CLASSES = { Float.class, Float.TYPE, Double.class, Double.TYPE,
-			BigDecimal.class, KualiDecimal.class, KualiPercent.class, AbstractKualiDecimal.class };
-	private static final Class[] TEMPORAL_CLASSES = { java.util.Date.class, java.sql.Date.class,
-			java.sql.Timestamp.class };
-	private static final Class[] STRING_CLASSES = { String.class };
-	private static final Class[] CLASS_CLASSES = { Class.class };
+    private static final Collection<Class<?>> BOOLEAN_CLASSES;
+    private static final Collection<Class<?>> INTEGRAL_CLASSES;
+    private static final Collection<Class<?>> DECIMAL_CLASSES;
+    private static final Collection<Class<?>> TEMPORAL_CLASSES;
+    private static final Collection<Class<?>> STRING_CLASSES = Collections.<Class<?>>singleton(CharSequence.class);
+    private static final Collection<Class<?>> CLASS_CLASSES = Collections.<Class<?>>singleton(Class.class);
+    private static final Collection<Class<?>> SIMPLE_CLASSES;
 
-	private static final HashMap<Class, Boolean> isBooleanCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isIntegralCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isDecimalCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isTemporalCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isStringCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isSimpleCache = new HashMap<Class, Boolean>();
-	private static final HashMap<Class, Boolean> isClassCache = new HashMap<Class, Boolean>();
+    static {
+        final Collection<Class<?>> temp = new ArrayList<Class<?>>();
+        temp.add(Boolean.class);
+        temp.add(Boolean.TYPE);
+        BOOLEAN_CLASSES = Collections.unmodifiableCollection(temp);
+    }
 
-	public static class JoinType {
+    static {
+        final Collection<Class<?>> temp = new ArrayList<Class<?>>();
+        temp.add(Byte.class);
+        temp.add(Byte.TYPE);
+        temp.add(Short.class);
+        temp.add(Short.TYPE);
+        temp.add(Integer.class);
+        temp.add(Integer.TYPE);
+        temp.add(Long.class);
+        temp.add(Long.TYPE);
+        temp.add(BigInteger.class);
+        temp.add(KualiInteger.class);
+        INTEGRAL_CLASSES = Collections.unmodifiableCollection(temp);
+    }
 
-	}
+    static {
+        final Collection<Class<?>> temp = new ArrayList<Class<?>>();
+        temp.add(Float.class);
+        temp.add(Float.TYPE);
+        temp.add(Double.class);
+        temp.add(Double.TYPE);
+        temp.add(BigDecimal.class);
+        temp.add(AbstractKualiDecimal.class);
+        DECIMAL_CLASSES = Collections.unmodifiableCollection(temp);
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is an join type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isJoinClass(Class clazz) {
-		return clazz.isAssignableFrom(JoinType.class);
-	}
+    static {
+        final Collection<Class<?>> temp = new ArrayList<Class<?>>();
+        temp.add(java.util.Date.class);
+        temp.add(java.sql.Date.class);
+        temp.add(java.sql.Timestamp.class);
+        TEMPORAL_CLASSES = Collections.unmodifiableCollection(temp);
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is an boolean type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isBooleanClass(Class clazz) {
-		Boolean result = isBooleanCache.get(clazz);
-		if (result == null) {
-			result = isa(BOOLEAN_CLASSES, clazz);
-			synchronized (isBooleanCache) {
-				isBooleanCache.put(clazz, result);
-			}
-		}
+    static {
+        final Collection<Class<?>> temp = new ArrayList<Class<?>>();
+        temp.addAll(BOOLEAN_CLASSES);
+        temp.addAll(INTEGRAL_CLASSES);
+        temp.addAll(DECIMAL_CLASSES);
+        temp.addAll(TEMPORAL_CLASSES);
+        temp.addAll(STRING_CLASSES);
+        SIMPLE_CLASSES = Collections.unmodifiableCollection(temp);
+    }
 
-		return result;
-	}
+    //use ConcurrentReferenceHashMap if it makes it into the jdk, since Class tokens always use identity for equality
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_BOOLEAN_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_INTEGRAL_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_DECIMAL_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_TEMPORAL_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_STRING_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_SIMPLE_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
+    private static final ConcurrentHashMap<Class<?>, Boolean> IS_CACHE_CACHE = new ConcurrentHashMap<Class<?>, Boolean>();
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is an integral type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isIntegralClass(Class clazz) {
-		Boolean result = isIntegralCache.get(clazz);
-		if (result == null) {
-			result = isa(INTEGRAL_CLASSES, clazz);
-			synchronized (isIntegralCache) {
-				isIntegralCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
+    private TypeUtils() {
+        throw new UnsupportedOperationException("do not call.");
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is a decimal type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isDecimalClass(Class clazz) {
-		Boolean result = isDecimalCache.get(clazz);
-		if (result == null) {
-			result = isa(DECIMAL_CLASSES, clazz);
-			synchronized (isDecimalCache) {
-				isDecimalCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is an boolean type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isBooleanClass(Class<?> clazz) {
+        return is(clazz, BOOLEAN_CLASSES, IS_BOOLEAN_CACHE);
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is a temporal type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isTemporalClass(Class clazz) {
-		Boolean result = isTemporalCache.get(clazz);
-		if (result == null) {
-			result = isa(TEMPORAL_CLASSES, clazz);
-			synchronized (isTemporalCache) {
-				isTemporalCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is an integral type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isIntegralClass(Class<?> clazz) {
+        return is(clazz, INTEGRAL_CLASSES, IS_INTEGRAL_CACHE);
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is a string type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isStringClass(Class clazz) {
-		Boolean result = isStringCache.get(clazz);
-		if (result == null) {
-			result = isa(STRING_CLASSES, clazz);
-			synchronized (isStringCache) {
-				isStringCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
-	
-	/**
-	 * @param clazz
-	 * @return true if the given Class is a Class type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isClassClass(Class clazz) {
-		Boolean result = isClassCache.get(clazz);
-		if (result == null) {
-			result = isa(CLASS_CLASSES, clazz);
-			synchronized (isClassCache) {
-				isClassCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is a decimal type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isDecimalClass(Class<?> clazz) {
+        return is(clazz, DECIMAL_CLASSES, IS_DECIMAL_CACHE);
+    }
 
-	/**
-	 * @param clazz
-	 * @return true if the given Class is a "simple" - one of the primitive
-	 *         types, their wrappers, or a temporal type
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	public static boolean isSimpleType(Class clazz) {
-		Boolean result = isSimpleCache.get(clazz);
-		if (result == null) {
-			result = isa(STRING_CLASSES, clazz) || isa(DECIMAL_CLASSES, clazz) || isa(INTEGRAL_CLASSES, clazz)
-					|| isa(BOOLEAN_CLASSES, clazz) || isa(TEMPORAL_CLASSES, clazz);
-			synchronized (isSimpleCache) {
-				isSimpleCache.put(clazz, result);
-			}
-		}
-		return result;
-	}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is a temporal type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isTemporalClass(Class<?> clazz) {
+        return is(clazz, TEMPORAL_CLASSES, IS_TEMPORAL_CACHE);
+    }
 
-	/**
-	 * @param types
-	 * @param type
-	 * @return true if the given Class is assignable from one of the classes in
-	 *         the given Class[]
-	 * @throws IllegalArgumentException
-	 *             if the given Class is null
-	 */
-	private static boolean isa(Class[] types, Class type) {
-		if (type == null) {
-			throw new IllegalArgumentException("illegal (null) type class");
-		}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is a string type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isStringClass(Class<?> clazz) {
+        return is(clazz, STRING_CLASSES, IS_STRING_CACHE);
+    }
 
-		boolean isa = false;
-		for (int i = 0; !isa && (i < types.length); ++i) {
-			isa = types[i].isAssignableFrom(type);
-		}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is a Class type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isClassClass(Class<?> clazz) {
+        return is(clazz, CLASS_CLASSES, IS_CACHE_CACHE);
+    }
 
-		return isa;
-	}
+    /**
+     * @param clazz class token
+     * @return true if the given Class is a "simple" - one of the primitive
+     *         types, their wrappers, or a temporal type
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    public static boolean isSimpleType(Class<?> clazz) {
+        return is(clazz, SIMPLE_CLASSES, IS_SIMPLE_CACHE);
+    }
+
+    private static boolean is(Class<?> clazz, Collection<Class<?>> clazzes, ConcurrentHashMap<Class<?>, Boolean> cache) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("clazz is null");
+        }
+
+        if (clazzes == null) {
+            throw new IllegalArgumentException("clazzes is null");
+        }
+
+        if (cache == null) {
+            throw new IllegalArgumentException("cache is null");
+        }
+
+        Boolean result = cache.get(clazz);
+        if (result == null) {
+            result = isa(clazzes, clazz);
+            cache.putIfAbsent(clazz, result);
+        }
+        return result;
+    }
+
+    /**
+     * @param types class tokens to check against
+     * @param type class token
+     * @return true if the given Class is assignable from one of the classes in
+     *         the given Class[]
+     * @throws IllegalArgumentException if the given Class is null
+     */
+    private static boolean isa(Collection<Class<?>> types, Class<?> type) {
+        for (Class<?> cur : types) {
+            if (cur.isAssignableFrom(type)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

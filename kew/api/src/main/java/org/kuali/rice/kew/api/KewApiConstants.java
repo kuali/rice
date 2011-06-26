0652fa82419c5f662f51147025c70042bde9b47f
Eric Westfall <ewestfal@iu.edu>
2011-06-26 06:39:56 +0000

    KULRICE-4555 - added missing license headers on KEW api classes

--- kew/api/src/main/java/org/kuali/rice/kew/api/KewApiConstants.java
+++ kew/api/src/main/java/org/kuali/rice/kew/api/KewApiConstants.java
@@ -1,24 +1,40 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.rice.kew.api;
 
 import org.kuali.rice.core.api.CoreConstants;
 import org.kuali.rice.core.api.CoreConstants.Versions;
 
 public final class KewApiConstants {
-	
+
     public static final String MACHINE_GENERATED_RESPONSIBILITY_ID = "0";
     public static final String ADHOC_REQUEST_RESPONSIBILITY_ID = "-1";
     public static final String EXCEPTION_REQUEST_RESPONSIBILITY_ID = "-2";
     public static final String SAVED_REQUEST_RESPONSIBILITY_ID = "-3";
-    
+
     public static final int TITLE_MAX_LENGTH = 255;
-    
+
     public static final String DOCUMENT_CONTENT_ELEMENT = "documentContent";
     public static final String ATTRIBUTE_CONTENT_ELEMENT = "attributeContent";
     public static final String SEARCHABLE_CONTENT_ELEMENT = "searchableContent";
     public static final String APPLICATION_CONTENT_ELEMENT = "applicationContent";
-    public static final String DEFAULT_DOCUMENT_CONTENT = "<"+DOCUMENT_CONTENT_ELEMENT+"/>";
-    public static final String DEFAULT_DOCUMENT_CONTENT2 = "<"+DOCUMENT_CONTENT_ELEMENT+"></"+DOCUMENT_CONTENT_ELEMENT+">";
-    
+    public static final String DEFAULT_DOCUMENT_CONTENT = "<" + DOCUMENT_CONTENT_ELEMENT + "/>";
+    public static final String DEFAULT_DOCUMENT_CONTENT2 = "<" + DOCUMENT_CONTENT_ELEMENT + "></"
+            + DOCUMENT_CONTENT_ELEMENT + ">";
+
     // receive future action request constants
     public static final String RECEIVE_FUTURE_REQUESTS_BRANCH_STATE_KEY = "_receive_future_requests";
     public static final String DEACTIVATED_FUTURE_REQUESTS_BRANCH_STATE_KEY = "_deactivated_future_requests";
@@ -26,27 +42,25 @@ public final class KewApiConstants {
     public static final String RECEIVE_FUTURE_REQUESTS_BRANCH_STATE_VALUE = "YES";
     public static final String CLEAR_FUTURE_REQUESTS_BRANCH_STATE_VALUE = "CLEAR";
 
-	
-	public static final class DocumentContentVersions {
-		public static final int ROUTE_LEVEL = 0;
-		public static final int NODAL = 1;
-		public static final int CURRENT = NODAL;
-	}
-	
+    public static final class DocumentContentVersions {
+        public static final int ROUTE_LEVEL = 0;
+        public static final int NODAL = 1;
+        public static final int CURRENT = NODAL;
+    }
+
     public static final class Namespaces {
 
-    	public static final String KEW_NAMESPACE_PREFIX = CoreConstants.Namespaces.ROOT_NAMESPACE_PREFIX + "/kew";
+        public static final String KEW_NAMESPACE_PREFIX = CoreConstants.Namespaces.ROOT_NAMESPACE_PREFIX + "/kew";
 
-    	/**
-    	 * Namespace for the kew module which is compatible with Kuali Rice 2.0.x.
-    	 */
-    	public static final String KEW_NAMESPACE_2_0 = KEW_NAMESPACE_PREFIX + "/" + Versions.VERSION_2_0;
+        /**
+         * Namespace for the kew module which is compatible with Kuali Rice 2.0.x.
+         */
+        public static final String KEW_NAMESPACE_2_0 = KEW_NAMESPACE_PREFIX + "/" + Versions.VERSION_2_0;
 
     }
 
-	
-	private KewApiConstants() {
-		throw new UnsupportedOperationException("Should never be called.");
-	}
+    private KewApiConstants() {
+        throw new UnsupportedOperationException("Should never be called.");
+    }
 
 }
--- kew/api/src/main/java/org/kuali/rice/kew/api/action/DelegationType.java
+++ kew/api/src/main/java/org/kuali/rice/kew/api/action/DelegationType.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.rice.kew.api.action;
 
 import javax.xml.bind.annotation.XmlEnum;
@@ -10,38 +25,41 @@ import org.kuali.rice.core.api.mo.common.Coded;
 @XmlRootElement(name = "delegationType")
 @XmlType(name = "DelegationTypeType")
 @XmlEnum
-public enum DelegationType implements Coded {	
-	
-	@XmlEnumValue("P") PRIMARY("P", "Primary"),
-	@XmlEnumValue("S") SECONDARY("S", "Secondary");
-	
-	private final String code;
-	private final String label;
-	
-	DelegationType(String code, String label) {
-		this.code = code;
-		this.label = label;
-	}
-	
-	@Override
-	public String getCode() {
-		return code;
-	}
-	
-	public String getLabel() {
-		return label;
-	}
-		
-	public static DelegationType fromCode(String code) {
-		if (code == null) {
-			return null;
-		}
-		for (DelegationType value : values()) {
-			if (value.code.equals(code)) {
-				return value;
-			}
-		}
-		throw new IllegalArgumentException("Failed to locate the DelegationType with the given code: " + code);
-	}
-	
+public enum DelegationType implements Coded {
+
+    @XmlEnumValue("P")
+    PRIMARY("P", "Primary"),
+
+    @XmlEnumValue("S")
+    SECONDARY("S", "Secondary");
+
+    private final String code;
+    private final String label;
+
+    DelegationType(String code, String label) {
+        this.code = code;
+        this.label = label;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static DelegationType fromCode(String code) {
+        if (code == null) {
+            return null;
+        }
+        for (DelegationType value : values()) {
+            if (value.code.equals(code)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Failed to locate the DelegationType with the given code: " + code);
+    }
+
 }

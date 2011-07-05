53fcd6c1e15dbaa9edacfd7c55ba3c5d7e218cf3
Brian Smith <bsmith83@ad3.ucdavis.edu>
2011-07-05 22:26:48 +0000

    Layout View for UX to test various layouts with

---
+++ sampleapp/src/main/java/edu/sampleu/travel/krad/form/UILayoutTestForm.java
@@ -0,0 +1,491 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.sampleu.travel.krad.form;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.kuali.rice.krad.web.form.UifFormBase;
+
+/**
+ * Form for Test UI Page
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class UILayoutTestForm extends UifFormBase {
+    private static final long serialVersionUID = -7525378097732916418L;
+    
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+    private String field5;
+    private String field6;
+    private String field7;
+    private String field8;
+    private String field9;
+    private String field10;
+    private String field11;
+    private String field12;
+    private String field13;
+    private String field14;
+    private String field15;
+    private String field16;
+    private String field17;
+    private String field18;
+    private String field19;
+    private String field20;
+    private String field21;
+    private String field22;
+    private String field23;
+    private String field24;
+    private String field25;
+    private String field26;
+    private String field27;
+    private String field28;
+    private String field29;
+    private String field30;
+
+    public UILayoutTestForm() {
+        super();
+    }
+
+    @Override
+    public void postBind(HttpServletRequest request) {
+        super.postBind(request);
+    }
+
+    /**
+     * @return the field1
+     */
+    public String getField1() {
+        return this.field1;
+    }
+
+    /**
+     * @param field1 the field1 to set
+     */
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    /**
+     * @return the field2
+     */
+    public String getField2() {
+        return this.field2;
+    }
+
+    /**
+     * @param field2 the field2 to set
+     */
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    /**
+     * @return the field3
+     */
+    public String getField3() {
+        return this.field3;
+    }
+
+    /**
+     * @param field3 the field3 to set
+     */
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+
+    /**
+     * @return the field4
+     */
+    public String getField4() {
+        return this.field4;
+    }
+
+    /**
+     * @param field4 the field4 to set
+     */
+    public void setField4(String field4) {
+        this.field4 = field4;
+    }
+
+    /**
+     * @return the field5
+     */
+    public String getField5() {
+        return this.field5;
+    }
+
+    /**
+     * @param field5 the field5 to set
+     */
+    public void setField5(String field5) {
+        this.field5 = field5;
+    }
+
+    /**
+     * @return the field6
+     */
+    public String getField6() {
+        return this.field6;
+    }
+
+    /**
+     * @param field6 the field6 to set
+     */
+    public void setField6(String field6) {
+        this.field6 = field6;
+    }
+
+    /**
+     * @return the field7
+     */
+    public String getField7() {
+        return this.field7;
+    }
+
+    /**
+     * @param field7 the field7 to set
+     */
+    public void setField7(String field7) {
+        this.field7 = field7;
+    }
+
+    /**
+     * @return the field8
+     */
+    public String getField8() {
+        return this.field8;
+    }
+
+    /**
+     * @param field8 the field8 to set
+     */
+    public void setField8(String field8) {
+        this.field8 = field8;
+    }
+
+    /**
+     * @return the field9
+     */
+    public String getField9() {
+        return this.field9;
+    }
+
+    /**
+     * @param field9 the field9 to set
+     */
+    public void setField9(String field9) {
+        this.field9 = field9;
+    }
+
+    /**
+     * @return the field10
+     */
+    public String getField10() {
+        return this.field10;
+    }
+
+    /**
+     * @param field10 the field10 to set
+     */
+    public void setField10(String field10) {
+        this.field10 = field10;
+    }
+
+    /**
+     * @return the field11
+     */
+    public String getField11() {
+        return this.field11;
+    }
+
+    /**
+     * @param field11 the field11 to set
+     */
+    public void setField11(String field11) {
+        this.field11 = field11;
+    }
+
+    /**
+     * @return the field12
+     */
+    public String getField12() {
+        return this.field12;
+    }
+
+    /**
+     * @param field12 the field12 to set
+     */
+    public void setField12(String field12) {
+        this.field12 = field12;
+    }
+
+    /**
+     * @return the field13
+     */
+    public String getField13() {
+        return this.field13;
+    }
+
+    /**
+     * @param field13 the field13 to set
+     */
+    public void setField13(String field13) {
+        this.field13 = field13;
+    }
+
+    /**
+     * @return the field14
+     */
+    public String getField14() {
+        return this.field14;
+    }
+
+    /**
+     * @param field14 the field14 to set
+     */
+    public void setField14(String field14) {
+        this.field14 = field14;
+    }
+
+    /**
+     * @return the field15
+     */
+    public String getField15() {
+        return this.field15;
+    }
+
+    /**
+     * @param field15 the field15 to set
+     */
+    public void setField15(String field15) {
+        this.field15 = field15;
+    }
+
+    /**
+     * @return the field16
+     */
+    public String getField16() {
+        return this.field16;
+    }
+
+    /**
+     * @param field16 the field16 to set
+     */
+    public void setField16(String field16) {
+        this.field16 = field16;
+    }
+
+    /**
+     * @return the field17
+     */
+    public String getField17() {
+        return this.field17;
+    }
+
+    /**
+     * @param field17 the field17 to set
+     */
+    public void setField17(String field17) {
+        this.field17 = field17;
+    }
+
+    /**
+     * @return the field18
+     */
+    public String getField18() {
+        return this.field18;
+    }
+
+    /**
+     * @param field18 the field18 to set
+     */
+    public void setField18(String field18) {
+        this.field18 = field18;
+    }
+
+    /**
+     * @return the field19
+     */
+    public String getField19() {
+        return this.field19;
+    }
+
+    /**
+     * @param field19 the field19 to set
+     */
+    public void setField19(String field19) {
+        this.field19 = field19;
+    }
+
+    /**
+     * @return the field20
+     */
+    public String getField20() {
+        return this.field20;
+    }
+
+    /**
+     * @param field20 the field20 to set
+     */
+    public void setField20(String field20) {
+        this.field20 = field20;
+    }
+
+    /**
+     * @return the field21
+     */
+    public String getField21() {
+        return this.field21;
+    }
+
+    /**
+     * @param field21 the field21 to set
+     */
+    public void setField21(String field21) {
+        this.field21 = field21;
+    }
+
+    /**
+     * @return the field22
+     */
+    public String getField22() {
+        return this.field22;
+    }
+
+    /**
+     * @param field22 the field22 to set
+     */
+    public void setField22(String field22) {
+        this.field22 = field22;
+    }
+
+    /**
+     * @return the field23
+     */
+    public String getField23() {
+        return this.field23;
+    }
+
+    /**
+     * @param field23 the field23 to set
+     */
+    public void setField23(String field23) {
+        this.field23 = field23;
+    }
+
+    /**
+     * @return the field24
+     */
+    public String getField24() {
+        return this.field24;
+    }
+
+    /**
+     * @param field24 the field24 to set
+     */
+    public void setField24(String field24) {
+        this.field24 = field24;
+    }
+
+    /**
+     * @return the field25
+     */
+    public String getField25() {
+        return this.field25;
+    }
+
+    /**
+     * @param field25 the field25 to set
+     */
+    public void setField25(String field25) {
+        this.field25 = field25;
+    }
+
+    /**
+     * @return the field26
+     */
+    public String getField26() {
+        return this.field26;
+    }
+
+    /**
+     * @param field26 the field26 to set
+     */
+    public void setField26(String field26) {
+        this.field26 = field26;
+    }
+
+    /**
+     * @return the field27
+     */
+    public String getField27() {
+        return this.field27;
+    }
+
+    /**
+     * @param field27 the field27 to set
+     */
+    public void setField27(String field27) {
+        this.field27 = field27;
+    }
+
+    /**
+     * @return the field28
+     */
+    public String getField28() {
+        return this.field28;
+    }
+
+    /**
+     * @param field28 the field28 to set
+     */
+    public void setField28(String field28) {
+        this.field28 = field28;
+    }
+
+    /**
+     * @return the field29
+     */
+    public String getField29() {
+        return this.field29;
+    }
+
+    /**
+     * @param field29 the field29 to set
+     */
+    public void setField29(String field29) {
+        this.field29 = field29;
+    }
+
+    /**
+     * @return the field30
+     */
+    public String getField30() {
+        return this.field30;
+    }
+
+    /**
+     * @param field30 the field30 to set
+     */
+    public void setField30(String field30) {
+        this.field30 = field30;
+    }
+    
+    
+}
--- sampleapp/src/main/webapp/WEB-INF/tags/rice-portal/channel/main/kradTestLinks.tag
+++ sampleapp/src/main/webapp/WEB-INF/tags/rice-portal/channel/main/kradTestLinks.tag
@@ -19,6 +19,7 @@
 <div class="body">
   <strong>Screen Element Testing</strong>
   <ul class="chan">
+  	<li><portal:portalLink displayTitle="true" title="Layout Test" url="${ConfigProperties.application.url}/spring/uilayouttest?viewId=LayoutTestView&methodToCall=start" /></li>
 	 <li><portal:portalLink displayTitle="true" title="Test View 1" url="${ConfigProperties.application.url}/spring/uitest?viewId=Travel-testView1&methodToCall=start" /></li>
      <li><portal:portalLink displayTitle="true" title="Test View 2" url="${ConfigProperties.application.url}/spring/uitest?viewId=Travel-testView2&methodToCall=start" /></li>
    </ul>

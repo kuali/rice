929caae1546177054ed1d953e563618a1c5170b1
Jason Whaley <jwhaley@savoirtech.com>
2011-07-07 17:32:24 +0000

    KULRICE-5340:  changed reflection techniques on ImmutableCollectionsInInterceptor to truly modify fields of the response wrapper class that are List, Set or Collection classes

--- core/impl/src/main/java/org/kuali/rice/core/cxf/interceptors/ImmutableCollectionsInInterceptor.java
+++ core/impl/src/main/java/org/kuali/rice/core/cxf/interceptors/ImmutableCollectionsInInterceptor.java
@@ -5,6 +5,7 @@ import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -16,39 +17,69 @@ import java.util.Set;
  * messages.  This interceptor is invoked in the interceptor chain after unmarshalling
  * from XML to Java has occurred.  The role of this interceptor is to ensure that any
  * Collection (and specifically List, Set, or Map) used in a @WebMethod is ultimately of the
- * type returned from
+ * expected immutable type returned from the local service.
  */
 @SuppressWarnings("unused")
 public class ImmutableCollectionsInInterceptor extends AbstractPhaseInterceptor<Message> {
 
     /**
-     * Instantiates an ImmutableCollectionsInInterceptor
+     * Instantiates an ImmutableCollectionsInInterceptor and adds it to the USER_LOGICAL phase.
      */
     public ImmutableCollectionsInInterceptor() {
         super(Phase.USER_LOGICAL);
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void handleMessage(final Message message) throws Fault {
-        List contents = message.getContent(List.class);
-        for (int i = 0; i < contents.size(); i++) {
-            Object o = contents.get(i);
-            if (o instanceof List) {
-                List unmodifiable = Collections.unmodifiableList((List) o);
-                contents.set(i, unmodifiable);
+        try {
+            List contents = message.getContent(List.class);
+            for (Object o : contents) {
+                makeCollectionFieldsImmutable(o);
             }
-            else if (o instanceof Set) {
-                Set unmodifiable = Collections.unmodifiableSet((Set) o);
-                contents.set(i, unmodifiable);
+        } catch (Exception e) {
+            throw new Fault(e);
+        }
+    }
+
+    /**
+     * Accepts an object whose fields of type List, Set, or Collection need to be made immutable irrespective of field
+     * visibility.  The passed in object is mutated as a result.
+     *
+     * @param o - The object whose List, Set, or Collection fields will be made immutable
+     * @throws IllegalAccessException
+     */
+    @SuppressWarnings("unchecked")
+    void makeCollectionFieldsImmutable(Object o) throws IllegalAccessException {
+        Class<?> targetClass = o.getClass();
+        for (Field f : targetClass.getDeclaredFields()) {
+
+            f.setAccessible(true);
+
+            if (f.getType().isAssignableFrom(List.class)) {
+                List original = (List) f.get(o);
+                if (original == null) {
+                    original = Collections.emptyList();
+                }
+                List immutable = Collections.unmodifiableList(original);
+                f.set(o, immutable);
+
+            } else if (f.getType().isAssignableFrom(Set.class)) {
+                Set original = (Set) f.get(o);
+                if (original == null) {
+                    original = Collections.emptySet();
+                }
+                Set immutable = Collections.unmodifiableSet(original);
+                f.set(o, immutable);
+            } else if (f.getType().isAssignableFrom(Collection.class)) {
+                Collection original = (Collection) f.get(o);
+                if (original == null) {
+                    original = Collections.emptyList();
+                }
+                Collection immutable = Collections.unmodifiableCollection(original);
+                f.set(o, immutable);
             }
-            else if (o instanceof Collection) {
-                Collection unmodifiable = Collections.unmodifiableCollection((Collection) o);
-                contents.set(i,unmodifiable);
-            } else if (o instanceof Map) {
-                Map unmodifiable = Collections.unmodifiableMap((Map) o);
-                contents.set(i, unmodifiable);
-            }
+
+            f.setAccessible(false);
         }
     }
 }

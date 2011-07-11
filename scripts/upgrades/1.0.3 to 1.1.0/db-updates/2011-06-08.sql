bff0597603d58a4cbef8668ea66aa735ab720b94
Peter Giles <g1l3sp@gmail.com>
2011-07-11 00:23:20 +0000

    KULRICE-5238: build agenda tree UI components
    * Committing changes to agenda editor view for M6.  
    * Also sneaking in a tweak to a db update script.

--- scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-08.sql
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-08.sql
@@ -1,5 +1,5 @@
 -- make krms_rule_t.prop_id nullable
-alter table krms_rule_t modify prop_id varchar2(40) DEFAULT NULL;
+alter table krms_rule_t modify (prop_id NULL);
 
 -- add krms_actn_t.nmspc_cd
 alter table krms_actn_t add nmspc_cd varchar2(40) not null;
@@ -8,7 +8,7 @@ alter table krms_actn_t add nmspc_cd varchar2(40) not null;
 alter table krms_agenda_t modify actv varchar2(1) DEFAULT 'Y';
 
 -- make krms_prop_t.typ_id nullable 
-alter table krms_prop_t modify typ_id varchar2(40) DEFAULT NULL;
+alter table krms_prop_t modify (typ_id NULL);
 
 -- change krms_rule_t.descr_txt to desc_t for consistency
 alter table krms_rule_t rename column descr_txt to desc_txt;

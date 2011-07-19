28081c18d02d35680638ad9a58035f7d43ba81a6
Matthew Wuertz <mwuertz@kuali.org>
2011-07-19 01:50:08 +0000

    KULRICE-5343 - Adding another unit tests for IdentityService.

--- impl/src/test/groovy/org/kuali/rice/kim/service/impl/IdentityServiceImplTest.groovy
+++ impl/src/test/groovy/org/kuali/rice/kim/service/impl/IdentityServiceImplTest.groovy
@@ -13,6 +13,7 @@ import org.kuali.rice.kim.impl.identity.privacy.EntityPrivacyPreferencesBo
 import org.kuali.rice.kim.impl.identity.personal.EntityBioDemographicsBo
 import java.text.SimpleDateFormat
 import org.kuali.rice.krad.service.PersistenceService
+import org.kuali.rice.kim.impl.identity.principal.PrincipalBo
 
 class IdentityServiceImplTest {
     private final shouldFail = new GroovyTestCase().&shouldFail
@@ -35,11 +36,18 @@ class IdentityServiceImplTest {
         Date birthDate = formatter.parse(birthDateString);
         Date deceasedDate = formatter.parse(deceasedDateString);
         EntityBioDemographicsBo firstEntityBioDemographicsBo = new EntityBioDemographicsBo(entityId: "AAA", birthDateValue: birthDate, genderCode: "M", deceasedDateValue: deceasedDate, maritalStatusCode: "S", primaryLanguageCode: "EN", secondaryLanguageCode: "FR", countryOfBirthCode: "US", birthStateCode: "IN", cityOfBirth: "Bloomington", geographicOrigin: "None", suppressPersonal: false);
-        EntityBo firstEntityBo = new EntityBo(active: true, id: "AAA", privacyPreferences: firstEntityPrivacyPreferencesBo, bioDemographics: firstEntityBioDemographicsBo);
+        PrincipalBo firstEntityPrincipal = new PrincipalBo(entityId: "AAA", principalId: "P1", active: true, principalName: "FirstPrincipal", versionNumber: 1);
+        List<PrincipalBo> firstPrincipals = new ArrayList<PrincipalBo>();
+        firstPrincipals.add(firstEntityPrincipal);
+        EntityBo firstEntityBo = new EntityBo(active: true, id: "AAA", privacyPreferences: firstEntityPrivacyPreferencesBo, bioDemographics: firstEntityBioDemographicsBo, principals: firstPrincipals);
 
         EntityPrivacyPreferencesBo secondEntityPrivacyPreferencesBo = new EntityPrivacyPreferencesBo(entityId: "BBB", suppressName: true, suppressEmail: true, suppressAddress: true, suppressPhone: true, suppressPersonal: false);
         EntityBioDemographicsBo secondEntityBioDemographicsBo = new EntityBioDemographicsBo(entityId: "BBB", birthDateValue: birthDate, genderCode: "M", deceasedDateValue: deceasedDate, maritalStatusCode: "S", primaryLanguageCode: "EN", secondaryLanguageCode: "FR", countryOfBirthCode: "US", birthStateCode: "IN", cityOfBirth: "Bloomington", geographicOrigin: "None", suppressPersonal: false);
-        EntityBo secondEntityBo = new EntityBo(active: true, id: "BBB", privacyPreferences: secondEntityPrivacyPreferencesBo, bioDemographics: secondEntityBioDemographicsBo);
+        PrincipalBo secondEntityPrincipal = new PrincipalBo(entityId: "BBB", principalId: "P2", active: true, principalName: "SecondPrincipal", versionNumber: 1);
+        List<PrincipalBo> secondPrincipals = new ArrayList<PrincipalBo>();
+        secondPrincipals.add(secondEntityPrincipal);
+        EntityBo secondEntityBo = new EntityBo(active: true, id: "BBB", privacyPreferences: secondEntityPrivacyPreferencesBo, bioDemographics: secondEntityBioDemographicsBo, principals: secondPrincipals);
+
         for (bo in [firstEntityBo, secondEntityBo]) {
             sampleEntities.put(bo.id, bo)
         }
@@ -125,5 +133,29 @@ class IdentityServiceImplTest {
     @Test(expected = IllegalArgumentException.class)
     public void testGetEntityByPrincipalIdWithNullIdFails() {
         Entity entity = identityService.getEntityByPrincipalId(null);
+    }
+
+    @Test
+    public void testGetEntityByPrincipalIdSucceeds() {
+        mockBoService.demand.findMatching(1..sampleEntities.size()) {
+            Class clazz, Map map -> for (EntityBo entityBo in sampleEntities.values()) {
+                for (PrincipalBo principalBo in entityBo.principals) {
+                    if (principalBo.principalId.equals(map.get("principals.principalId")))
+                    {
+                        Collection<EntityBo> entities = new ArrayList<EntityBo>();
+                        entities.add(entityBo);
+                        return entities;
+                    }
+                }
+            }
+        }
+
+        injectBusinessObjectServiceIntoIdentityService();
+
+        for (EntityBo entityBo in sampleEntities.values()) {
+            Assert.assertEquals(EntityBo.to(entityBo), identityService.getEntityByPrincipalId(entityBo.principals[0].principalId));
+        }
+
+        mockBoService.verify(boService)
     }
 }

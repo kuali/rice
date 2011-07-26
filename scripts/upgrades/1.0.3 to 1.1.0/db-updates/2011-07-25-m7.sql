e2ad29a6d406197253bf0ea3a66ac622e313df24
James Renfro <jrenfro@u.washington.edu>
2011-07-26 04:30:46 +0000

    KULRICE-5215 : Adding scripts to remove RIA tables

---
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-07-25-m7.sql
@@ -0,0 +1,3 @@
+drop table KREW_RIA_DOC_T;
+drop sequence KREW_RIA_DOCTYPE_MAP_ID_S; 
+drop table KREW_RIA_DOCTYPE_MAP_T;
---
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/mysql-2011-07-25-m7.sql
@@ -0,0 +1,3 @@
+drop table krew_ria_doc_t;
+drop table krew_ria_doctype_map_id_s;
+drop table krew_ria_doctype_map_t;

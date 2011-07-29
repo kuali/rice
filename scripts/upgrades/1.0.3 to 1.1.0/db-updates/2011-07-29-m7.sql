f2460240801c576106e7e05c7135f1e70af34fcd
Jeremy Hanson <jjhanso@iastate.edu>
2011-07-29 15:35:38 +0000

    KULRICE-5052 - correcting typo

---
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-07-29-m7.sql
@@ -0,0 +1,2 @@
+update KRIM_PERM_T t set NM='Take Requested Approve Action' where PERM_ID = '170'
+/
---
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/mysql-2011-07-29-m7.sql
@@ -0,0 +1,1 @@
+update KRIM_PERM_T t set NM='Take Requested Approve Action' where PERM_ID = '170';

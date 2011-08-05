e53f48c18e8ca6c5db239aa0d93988152c322a72
Eric Westfall <ewestfal@iu.edu>
2011-08-05 01:42:48 +0000

    KULRICE-5056 - Added a lookup attribute field

---
+++ core/api/src/main/java/org/kuali/rice/core/api/uif/LookupAttributeField.java
@@ -0,0 +1,33 @@
+package org.kuali.rice.core.api.uif;
+
+/**
+ * Describes an attribute field which is used as part of a lookup.  Includes the base {@code RemotableAttributeField}
+ * definition as well as additional information about the field and how it gets rendered and handled as part of a
+ * lookup.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public interface LookupAttributeField {
+
+    /**
+     * Returns the {@code RemotableAttributeField} definition for this lookup attribute.  Cannot be null.
+     *
+     * @return the attribute field definition, cannot be null
+     */
+    RemotableAttributeField getAttributeField();
+
+    /**
+     * Returns true if this field should be included as part of the lookup criteria, false if not.
+     *
+     * @return true if this field should be included as part of the lookup criteria, false if not
+     */
+    boolean isInCriteria();
+
+    /**
+     * Returns true if this field should be included in the result set of the lookup, false if not.
+     *
+     * @return true if this field should be included in the result set of the lookup, false if not
+     */
+    boolean isInResults();
+
+}

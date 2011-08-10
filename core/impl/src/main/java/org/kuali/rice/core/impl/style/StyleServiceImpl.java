4f25dc9f09f01d27b74239cb64174793d44f8e8c
Travis Schneeberger <tschneeb@gmail.com>
2011-08-10 19:24:18 +0000

    KULRICE-5357: removing some caching

--- core/impl/src/main/java/org/kuali/rice/core/impl/style/StyleServiceImpl.java
+++ core/impl/src/main/java/org/kuali/rice/core/impl/style/StyleServiceImpl.java
@@ -16,15 +16,6 @@
 
 package org.kuali.rice.core.impl.style;
 
-import java.io.StringReader;
-import java.util.List;
-import java.util.Properties;
-
-import javax.xml.transform.Templates;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamSource;
-
 import org.apache.log4j.Logger;
 import org.kuali.rice.core.api.style.Style;
 import org.kuali.rice.core.api.style.StyleRepositoryService;
@@ -33,6 +24,14 @@ import org.kuali.rice.core.framework.services.CoreFrameworkServiceLocator;
 import org.kuali.rice.kew.util.KEWConstants;
 import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.ksb.api.cache.RiceCacheAdministrator;
+
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Properties;
 
 
 /**
@@ -58,7 +57,6 @@ public class StyleServiceImpl implements StyleService {
      * Loads the named style from the database, or (if configured) imports it from a file
      * specified via a configuration parameter with a name of the format edl.style.&lt;styleName&gt;
      * {@inheritDoc}
-     * @see org.kuali.rice.edl.impl.service.StyleService#getStyle(java.lang.String)
      */
     @Override
     public Style getStyle(String styleName) {
@@ -70,40 +68,38 @@ public class StyleServiceImpl implements StyleService {
 
     @Override
     public Templates getStyleAsTranslet(String name) throws TransformerConfigurationException {
-        if (name == null) return null;
-        Templates translet = fetchTemplatesFromCache(name);
-        if (translet == null) {
-            Style style = getStyle(name);
-            if (style == null) {
-                return null;
-            }
-
-            boolean useXSLTC = CoreFrameworkServiceLocator.getParameterService().getParameterValueAsBoolean(KEWConstants.KEW_NAMESPACE, KRADConstants.DetailTypes.EDOC_LITE_DETAIL_TYPE, KEWConstants.EDL_USE_XSLTC_IND);
-            if (useXSLTC) {
-                LOG.info("using xsltc to compile stylesheet");
-                String key = "javax.xml.transform.TransformerFactory";
-                String value = "org.apache.xalan.xsltc.trax.TransformerFactoryImpl";
-                Properties props = System.getProperties();
-                props.put(key, value);
-                System.setProperties(props);
-            }
-
-            TransformerFactory factory = TransformerFactory.newInstance();
-            factory.setURIResolver(new StyleUriResolver(this));
-
-            if (useXSLTC) {
-                factory.setAttribute("translet-name",name);
-                factory.setAttribute("generate-translet",Boolean.TRUE);
-                String debugTransform = CoreFrameworkServiceLocator.getParameterService().getParameterValueAsString(KEWConstants.KEW_NAMESPACE, KRADConstants.DetailTypes.EDOC_LITE_DETAIL_TYPE, KEWConstants.EDL_DEBUG_TRANSFORM_IND);
-                if (debugTransform.trim().equals("Y")) {
-                    factory.setAttribute("debug", Boolean.TRUE);
-                }
-            }
-
-            translet = factory.newTemplates(new StreamSource(new StringReader(style.getXmlContent())));
-            putTemplatesInCache(name, translet);
+        if (name == null) {
+            return null;
         }
-        return translet;
+
+        Style style = getStyle(name);
+        if (style == null) {
+            return null;
+        }
+
+        boolean useXSLTC = CoreFrameworkServiceLocator.getParameterService().getParameterValueAsBoolean(KEWConstants.KEW_NAMESPACE, KRADConstants.DetailTypes.EDOC_LITE_DETAIL_TYPE, KEWConstants.EDL_USE_XSLTC_IND);
+        if (useXSLTC) {
+            LOG.info("using xsltc to compile stylesheet");
+            String key = "javax.xml.transform.TransformerFactory";
+            String value = "org.apache.xalan.xsltc.trax.TransformerFactoryImpl";
+            Properties props = System.getProperties();
+            props.put(key, value);
+            System.setProperties(props);
+        }
+
+        TransformerFactory factory = TransformerFactory.newInstance();
+        factory.setURIResolver(new StyleUriResolver(this));
+
+        if (useXSLTC) {
+            factory.setAttribute("translet-name",name);
+            factory.setAttribute("generate-translet",Boolean.TRUE);
+            String debugTransform = CoreFrameworkServiceLocator.getParameterService().getParameterValueAsString(KEWConstants.KEW_NAMESPACE, KRADConstants.DetailTypes.EDOC_LITE_DETAIL_TYPE, KEWConstants.EDL_DEBUG_TRANSFORM_IND);
+            if (debugTransform.trim().equals("Y")) {
+                factory.setAttribute("debug", Boolean.TRUE);
+            }
+        }
+
+        return factory.newTemplates(new StreamSource(new StringReader(style.getXmlContent())));
     }
 
     @Override
@@ -115,22 +111,4 @@ public class StyleServiceImpl implements StyleService {
     public List<String> getAllStyleNames() {
         return styleRepositoryService.getAllStyleNames();
     }
-        
-    // cache helper methods
-
-    /**
-     * Returns the key to be used for caching the Templates for the given style name.
-     */
-    protected String getTemplatesCacheKey(String styleName) {
-        return StyleRepositoryServiceImpl.TEMPLATES_CACHE_GROUP_NAME + ":" + styleName;
-    }
-
-    protected Templates fetchTemplatesFromCache(String styleName) {
-        return (Templates) cache.getFromCache(getTemplatesCacheKey(styleName));
-    }
-
-    protected void putTemplatesInCache(String styleName, Templates templates) {
-        cache.putInCache(getTemplatesCacheKey(styleName), templates, StyleRepositoryServiceImpl.TEMPLATES_CACHE_GROUP_NAME);
-    }
-
 }

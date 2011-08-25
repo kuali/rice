baee0645ae586f96e3461444d3b8524a4a84feda
Jeremy Hanson <jjhanso@iastate.edu>
2011-08-25 14:55:34 +0000

    made delimiters consistent for oracle scripts

--- scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-13-m6.sql
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-13-m6.sql
@@ -1,7 +1,8 @@
-update krew_doc_typ_t set post_prcsr = 'org.kuali.rice.krad.workflow.postprocessor.KualiPostProcessor' where post_prcsr = 'org.kuali.rice.kns.workflow.postprocessor.KualiPostProcessor';
-
-update krew_rule_attr_t set cls_nm = 'org.kuali.rice.krad.workflow.attribute.KualiXmlSearchableAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlSearchableAttributeImpl';
-
-update krew_rule_attr_t set cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXMLBooleanTranslatorSearchableAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXMLBooleanTranslatorSearchableAttributeImpl';
-
-update krew_rule_attr_t set cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlRuleAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlRuleAttributeImpl';
+update krew_doc_typ_t set post_prcsr = 'org.kuali.rice.krad.workflow.postprocessor.KualiPostProcessor' where post_prcsr = 'org.kuali.rice.kns.workflow.postprocessor.KualiPostProcessor'
+/
+update krew_rule_attr_t set cls_nm = 'org.kuali.rice.krad.workflow.attribute.KualiXmlSearchableAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlSearchableAttributeImpl'
+/
+update krew_rule_attr_t set cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXMLBooleanTranslatorSearchableAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXMLBooleanTranslatorSearchableAttributeImpl'
+/
+update krew_rule_attr_t set cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlRuleAttributeImpl' where cls_nm = 'org.kuali.rice.kns.workflow.attribute.KualiXmlRuleAttributeImpl'
+/
--- scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-21.sql
+++ scripts/upgrades/1.0.3 to 1.1.0/db-updates/2011-06-21.sql
@@ -1,7 +1,13 @@
-alter table KRIM_PERM_TMPL_T modify NMSPC_CD varchar2(40) not null;
-alter table KRIM_PERM_TMPL_T modify NM varchar2(100) not null;
-alter table KRIM_PERM_TMPL_T add constraint KRIM_PERM_TMPL_TC1 unique (NM, NMSPC_CD);
+alter table KRIM_PERM_TMPL_T modify NMSPC_CD varchar2(40) not null
+/
+alter table KRIM_PERM_TMPL_T modify NM varchar2(100) not null
+/
+alter table KRIM_PERM_TMPL_T add constraint KRIM_PERM_TMPL_TC1 unique (NM, NMSPC_CD)
+/
 
-alter table KRIM_RSP_TMPL_T modify NMSPC_CD varchar2(40) not null;
-alter table KRIM_RSP_TMPL_T modify NM varchar2(100) not null;
-alter table KRIM_RSP_TMPL_T add constraint KRIM_RSP_TMPL_TC1 unique (NM, NMSPC_CD);
+alter table KRIM_RSP_TMPL_T modify NMSPC_CD varchar2(40) not null
+/
+alter table KRIM_RSP_TMPL_T modify NM varchar2(100) not null
+/
+alter table KRIM_RSP_TMPL_T add constraint KRIM_RSP_TMPL_TC1 unique (NM, NMSPC_CD)
+/

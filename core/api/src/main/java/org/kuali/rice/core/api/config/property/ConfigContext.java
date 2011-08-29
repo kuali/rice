2784b20781859794f1225827b2b4633e4f474439
Travis Schneeberger <tschneeb@gmail.com>
2011-08-29 14:21:46 +0000

    KULRICE-5357: changes from code review, removing unused code.

--- core/api/src/main/java/org/kuali/rice/core/api/config/property/ConfigContext.java
+++ core/api/src/main/java/org/kuali/rice/core/api/config/property/ConfigContext.java
@@ -16,15 +16,10 @@
 
 package org.kuali.rice.core.api.config.property;
 
-import org.apache.log4j.Logger;
-import org.kuali.rice.core.api.util.ClassLoaderUtils;
-
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Singleton that holds references to global engine objects.
@@ -33,14 +28,7 @@ import java.util.Set;
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public class ConfigContext {
-    private static final Logger LOG = Logger.getLogger(ConfigContext.class);
-
-    /**
-     * Concurrency utility which allows other, loosely coupled, components to wait for configuration initialization
-     * to complete before proceeding (namely the SpringServiceLocator, before it initializes Spring)
-     */
-    private static final ContextualConfigLock initialized = new ContextualConfigLock("ConfigurationInitialized");
-    private static final Map<ClassLoader, Config> CONFIGS = new HashMap<ClassLoader, Config>();
+    private static final ConcurrentHashMap<ClassLoader, Config> CONFIGS = new ConcurrentHashMap<ClassLoader, Config>();
 
     private ConfigContext() {
         // nothing to do here
@@ -51,40 +39,26 @@ public class ConfigContext {
      * implementation.
      * @param rootCfg the root config
      */
-    public synchronized static void init(Config rootCfg) {
+    public static void init(Config rootCfg) {
     	init(Thread.currentThread().getContextClassLoader(), rootCfg);
     }
 
     /**
      * Initializes the ConfigContext with the given Config and binds it to the given ClassLoader.
      */
-    public synchronized static void init(ClassLoader classLoader, Config config) {
+    public static void init(ClassLoader classLoader, Config config) {
     	CONFIGS.put(classLoader, config);
-    	initialized.fire();
     }
 
-    public synchronized static boolean isInitialized() {
-    	return initialized.hasFired();
+    public static boolean isInitialized() {
+    	return !CONFIGS.isEmpty();
     }
     
     /**
      * Destroy method (mostly to aid testing, as core needs to be torn down appropriately).
      */
-    public synchronized static void destroy() {
-        if (!initialized.hasFired()) {
-            LOG.warn("Destroy on un-initialized ConfigContext was ignored.");
-            return;
-        }
+    public static void destroy() {
         CONFIGS.clear();
-        initialized.reset();
-    }
-
-    /**
-     * Returns the Condition that allows waiting on configuration to complete
-     * @return the Condition that allows waiting on configuration to complete
-     */
-    public static ContextualConfigLock getInitializedCondition() {
-        return initialized;
     }
 
     /**
@@ -93,58 +67,16 @@ public class ConfigContext {
      * returned, being created first if it does not yet exist.
      * @return the Config object which is associated with the caller's context classloader
      */
-    public synchronized static Config getCurrentContextConfig() {
-    	return CONFIGS.get(Thread.currentThread().getContextClassLoader());
+    public static Config getCurrentContextConfig() {
+    	return getConfig(Thread.currentThread().getContextClassLoader());
     }
 
     /**
      * @param cl the classloader whose Config to return
      * @return the Config of a particular class loader
      */
-    public synchronized static Config getConfig(ClassLoader cl) {
+    private static Config getConfig(ClassLoader cl) {
         return CONFIGS.get(cl);
-    }
-
-    public synchronized static Object getObjectFromConfigHierarchy(String name) {
-    	return getObjectFromClassLoader(name, ClassLoaderUtils.getDefaultClassLoader());
-    }
-
-    private static Object getObjectFromClassLoader(String name, ClassLoader classLoader) {
-    	if (classLoader instanceof ConfigHolder) {
-    		Object object = ((ConfigHolder)classLoader).getConfig().getObject(name);
-    		if (object != null) {
-    			return object;
-    		}
-    	} else {
-    		Object object = getCurrentContextConfig().getObject(name);
-    		if (object != null) {
-    			return object;
-    		}
-    		return null;
-    	}
-    	return getObjectFromClassLoader(name, classLoader.getParent());
-    }
-
-    public static List<Object> getObjectsFromConfigHierarchy(String name) {
-    	List<Object> objects = new ArrayList<Object>();
-    	getObjectFromClassLoader(name, ClassLoaderUtils.getDefaultClassLoader(), objects);
-    	return objects;
-    }
-
-    private static void getObjectFromClassLoader(String name, ClassLoader classLoader, List<Object> objects) {
-    	if (classLoader instanceof ConfigHolder) {
-    		Object object = ((ConfigHolder)classLoader).getConfig().getObject(name);
-    		if (object != null) {
-    			objects.add(object);
-    		}
-    	} else {
-    		Object object = getCurrentContextConfig().getObject(name);
-    		if (object != null) {
-    			objects.add(object);
-    		}
-    		return;
-    	}
-    	getObjectFromClassLoader(name, classLoader.getParent(), objects);
     }
 
     /**

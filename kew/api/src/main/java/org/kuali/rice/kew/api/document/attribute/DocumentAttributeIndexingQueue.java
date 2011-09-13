117ae1a3815de7cf3293a8d71ef23af3baab70db
Eric Westfall <ewestfal@iu.edu>
2011-09-13 18:05:39 +0000

    KULRICE-5056 - improved javadoc on DocumentAttributeIndexingQueue interface

--- kew/api/src/main/java/org/kuali/rice/kew/api/document/attribute/DocumentAttributeIndexingQueue.java
+++ kew/api/src/main/java/org/kuali/rice/kew/api/document/attribute/DocumentAttributeIndexingQueue.java
@@ -16,6 +16,7 @@
  */
 package org.kuali.rice.kew.api.document.attribute;
 
+import org.kuali.rice.core.api.exception.RiceIllegalArgumentException;
 import org.kuali.rice.kew.api.KewApiConstants;
 
 import javax.jws.WebMethod;
@@ -26,7 +27,9 @@ import javax.jws.soap.SOAPBinding;
 import javax.xml.bind.annotation.XmlElement;
 
 /**
- * Defines the contract for a message queue that handles indexing of workflow documents.
+ * Defines the contract for a message queue that handles indexing of workflow documents.  The indexing process is
+ * intended to look at the content associated with the workflow document and extra requested attributes for indexing
+ * alongside the document.  These values can then be accessed and/or searched.
  *
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
@@ -34,7 +37,17 @@ import javax.xml.bind.annotation.XmlElement;
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public interface DocumentAttributeIndexingQueue {
 
+    /**
+     * Perform document attribute indexing on the document with the given id.  The given document id must be valid,
+     * otherwise a {@code RiceIllegalArgumentException} will be thrown.
+     *
+     * @param documentId the id of the document to index, cannot be a null or blank value and must be an accurate
+     * identifier for an existing document
+     *
+     * @throws RiceIllegalArgumentException if the given document id is a null or blank value, or if it does not
+     * resolve to a valid document
+     */
     @WebMethod(operationName = "indexDocument")
-	void indexDocument(@WebParam(name = "documentId") String documentId);
+	void indexDocument(@WebParam(name = "documentId") String documentId) throws RiceIllegalArgumentException;
 
 }

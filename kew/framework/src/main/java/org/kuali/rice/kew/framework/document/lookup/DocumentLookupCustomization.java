a032247ba46d1883372e05304d7b315d3a6159a7
Eric Westfall <ewestfal@iu.edu>
2011-09-14 23:46:17 +0000

    KULRICE-5056 - javdoc'd DocumentLookupCustomization

--- kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupCustomization.java
+++ kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupCustomization.java
@@ -4,14 +4,34 @@ import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * An enumeration which defines the set of available document lookup customizations.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
 @XmlRootElement(name = "documentLookupCustomization")
 @XmlType(name = "DocumentLookupCustomizationType")
 @XmlEnum
 public enum DocumentLookupCustomization {
 
+    /**
+     * Customization of the document lookup criteria.
+     */
     CRITERIA,
+
+    /**
+     * Customization of clearing of the document lookup criteria.
+     */
     CLEAR_CRITERIA,
+
+    /**
+     * Customization of document lookup results.
+     */
     RESULTS,
+
+    /**
+     * Customization of the document lookup result attribute fields.
+     */
     RESULT_SET_FIELDS;
 
 }

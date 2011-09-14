64269711966879ccc5d7c02cfd5de11b8bb146be
Eric Westfall <ewestfal@iu.edu>
2011-09-14 23:25:27 +0000

    KULRICE-5056 - javadoc'd DocumentLookupCriteriaConfigurationContract

--- kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupCriteriaConfigurationContract.java
+++ kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupCriteriaConfigurationContract.java
@@ -3,12 +3,19 @@ package org.kuali.rice.kew.framework.document.lookup;
 import java.util.List;
 
 /**
- * TODO...
+ * Defines the contract which specifies attribute fields that should be included as part of document lookup criteria
+ * on the document lookup user interface.
  *
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public interface DocumentLookupCriteriaConfigurationContract {
 
+    /**
+     * Returns the additional attribute fields that should be included as part of the document lookup criteria on the
+     * document lookup user interface.
+     *
+     * @return the search attribute fields
+     */
     List<AttributeFields> getSearchAttributeFields();
 
 }

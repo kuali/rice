17dfd2b48a6452bfded91750d17a95d5fbb95a0f
Eric Westfall <ewestfal@iu.edu>
2011-09-14 03:05:31 +0000

    KULRICE-5056 - added javadoc on DocumentLookupResult

--- kew/api/src/main/java/org/kuali/rice/kew/api/document/lookup/DocumentLookupResult.java
+++ kew/api/src/main/java/org/kuali/rice/kew/api/document/lookup/DocumentLookupResult.java
@@ -23,6 +23,12 @@ import org.kuali.rice.kew.api.document.attribute.DocumentAttributeContract;
 import org.kuali.rice.kew.api.document.attribute.DocumentAttributeFactory;
 import org.w3c.dom.Element;
 
+/**
+ * An immutable data transfer object implementation of the {@link DocumentLookupResultContract}.  Instances of this
+ * class should be constructed using the nested {@link Builder} class.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
 @XmlRootElement(name = DocumentLookupResult.Constants.ROOT_ELEMENT_NAME)
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = DocumentLookupResult.Constants.TYPE_NAME, propOrder = {
@@ -70,6 +76,14 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
         return this.documentAttributes;
     }
 
+    /**
+     * Returns an unmodifiable list of all document attributes on this result which have the given name.  It is legal
+     * for a result to contain more than one attribute of the same name.  In these cases, this represents a document
+     * attribute which has more than one value.
+     *
+     * @param attributeName the attribute name of document attributes to retrieve
+     * @return an unmodifiable list of document attributes with the given name, will never be null but may be empty
+     */
     public List<DocumentAttribute> getDocumentAttributeByName(String attributeName) {
         List<DocumentAttribute> namedAttributes = new ArrayList<DocumentAttribute>();
         for (DocumentAttribute attribute : getDocumentAttributes()) {
@@ -80,6 +94,15 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
         return Collections.unmodifiableList(namedAttributes);
     }
 
+    /**
+     * Returns a single document attribute from this result which has the given name.  If there is more than one
+     * document attribute on this result with the given name, only a single one will be returned (though it is
+     * undeterministic which one will this will be).  If there are no attributes on this result with the given name
+     * then this method will return null.
+     *
+     * @param attributeName the attribute name of the document attribute to retrieve
+     * @return a single document attribute with the given name, or null if one does not exist
+     */
     public DocumentAttribute getSingleDocumentAttributeByName(String attributeName) {
         List<DocumentAttribute> namedAttributes = getDocumentAttributeByName(attributeName);
         if (namedAttributes.isEmpty()) {
@@ -89,7 +112,8 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
     }
 
     /**
-     * A builder which can be used to construct {@link DocumentLookupResult} instances.  Enforces the constraints of the {@link DocumentLookupResultContract}.
+     * A builder which can be used to construct {@link DocumentLookupResult} instances.  Enforces the constraints of the
+     * {@link DocumentLookupResultContract}.
      */
     public final static class Builder implements Serializable, ModelBuilder, DocumentLookupResultContract {
 
@@ -101,10 +125,29 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
             setDocumentAttributes(new ArrayList<DocumentAttribute.AbstractBuilder<?>>());
         }
 
+        /**
+         * Create a builder for the document lookup result and initialize it with the given document builder.
+         * Additionally initializes the list of document attribute builders on the new instance to an empty list.
+         *
+         * @param document the document builder with which to initialize the returned builder instance
+         *
+         * @return a builder instance initialized with the given document builder
+         *
+         * @throws IllegalArgumentException if the given document builder is null
+         */
         public static Builder create(Document.Builder document) {
             return new Builder(document);
         }
 
+        /**
+         * Creates a new builder instance initialized with copies of the properties from the given contract.
+         *
+         * @param contract the contract from which to copy properties
+         *
+         * @return a builder instance initialized with properties from the given contract
+         *
+         * @throws IllegalArgumentException if the given contract is null
+         */
         public static Builder create(DocumentLookupResultContract contract) {
             if (contract == null) {
                 throw new IllegalArgumentException("contract was null");
@@ -134,6 +177,9 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
         }
 
         public void setDocument(Document.Builder document) {
+            if (document == null) {
+                throw new IllegalArgumentException("document was null");
+            }
             this.document = document;
         }
 
@@ -142,7 +188,6 @@ public final class DocumentLookupResult extends AbstractDataTransferObject imple
         }
 
     }
-
 
     /**
      * Defines some internal constants used on this class.

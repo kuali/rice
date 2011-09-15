91af15a8990f7dc6966cdfd139656dec83e55192
Eric Westfall <ewestfal@iu.edu>
2011-09-15 01:53:41 +0000

    KULRICE-5056 - more docs

--- kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupResultValuesContract.java
+++ kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupResultValuesContract.java
@@ -2,8 +2,20 @@ package org.kuali.rice.kew.framework.document.lookup;
 
 import java.util.List;
 
+/**
+ * Defines the contract for an object containing result values that are used to customize document lookup results.
+ * Defines a set of additional custom values for document lookup results that can be defined and returned by an
+ * application which is customizing the document lookup for a specific document type.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
 public interface DocumentLookupResultValuesContract {
 
+    /**
+     * Returns an unmodifiable list of the result values, one for each customized document.
+     *
+     * @return the list of customized document lookup result values, will never be null but may be empty
+     */
     List<? extends DocumentLookupResultValueContract> getResultValues();
 
 }

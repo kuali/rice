d342801ff46a0ae6c9fb7e95836b84df8fcbdbee
Eric Westfall <ewestfal@iu.edu>
2011-09-15 02:00:10 +0000

    KULRICE-5056 - javadoc'd DocumentLookupResultValueContract

--- kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupResultValueContract.java
+++ kew/framework/src/main/java/org/kuali/rice/kew/framework/document/lookup/DocumentLookupResultValueContract.java
@@ -5,10 +5,26 @@ import org.kuali.rice.kew.api.document.attribute.DocumentAttributeContract;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Defines the contract for an object containing a customized result value for a specific document which is part of a
+ * set of document lookup results.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
 public interface DocumentLookupResultValueContract {
 
+    /**
+     * Returns the id of the document for which this customized result value applies.
+     *
+     * @return the document id of the customized document lookup result
+     */
     String getDocumentId();
 
+    /**
+     * Returns the customized document attribute values for this document lookup result.
+     *
+     * @return the customized document attribute values for this document lookup result
+     */
     List<? extends DocumentAttributeContract> getDocumentAttributes();
 
 }

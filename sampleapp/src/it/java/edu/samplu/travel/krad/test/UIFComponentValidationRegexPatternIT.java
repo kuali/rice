c4b2e1e1bfc4da1faaa1a618e8e0bcb8f6e30b55
Viral Chauhan <vchauhan@vivantech.com>
2011-09-16 13:14:02 +0000

    KULRICE-5412 Selenium Test updated verifying UIF Component's Validation Regex patterns

--- sampleapp/src/it/java/edu/samplu/travel/krad/test/UIFComponentValidationRegexPatternIT.java
+++ sampleapp/src/it/java/edu/samplu/travel/krad/test/UIFComponentValidationRegexPatternIT.java
@@ -52,10 +52,10 @@ public class UIFComponentValidationRegexPatternIT {
         assertEquals("Login", selenium.getTitle());
         selenium.type("__login_user", "admin");
         selenium.click("//input[@value='Login']");
-        selenium.waitForPageToLoad("30000");
+        selenium.waitForPageToLoad("50000");
         assertEquals("Kuali Portal Index", selenium.getTitle());
         selenium.click("link=Uif Components (Kitchen Sink)");
-        selenium.waitForPageToLoad("30000");
+        selenium.waitForPageToLoad("50000");
         assertEquals("Kuali Portal Index", selenium.getTitle());
         selenium.selectFrame("iframeportlet");
         selenium.click("link=Validation - Regex");
@@ -64,7 +64,6 @@ public class UIFComponentValidationRegexPatternIT {
         
        
         //---------------------------------------------Fixed Point------------------------------//
-       //clearFixedPointText();
         clearText("//input[@name='field50']");
         selenium.type("//input[@name='field50']", "127.344");
         selenium.focus("//input[@name='field51']");
@@ -92,28 +91,53 @@ public class UIFComponentValidationRegexPatternIT {
         //---------------------------------------------Floating Point------------------------------//
         clearText("//input[@name='field51']");
         selenium.type("//input[@name='field51']", "127.");
-        selenium.focus("//input[@name='field52']");
+        selenium.focus("//input[@name='field77']");
         Thread.sleep(100);               
         assertTrue(selenium.isTextPresent("Must be a positive or negative number, with any number of digits to the right of the decimal."));
         
         clearText("//input[@name='field51']");
         selenium.type("//input[@name='field51']", "1234.4123"); 
-        selenium.focus("//input[@name='field52']");
+        selenium.focus("//input[@name='field77']");
         Thread.sleep(100);  
         assertTrue(! selenium.isTextPresent("Must be a positive or negative number, with any number of digits to the right of the decimal."));
         
         clearText("//input[@name='field51']");
         selenium.type("//input[@name='field51']", "1234()98"); 
-        selenium.focus("//input[@name='field52']");
+        selenium.focus("//input[@name='field77']");
         Thread.sleep(100);       
         assertTrue(selenium.isTextPresent("Must be a positive or negative number, with any number of digits to the right of the decimal."));
         
         clearText("//input[@name='field51']");
         selenium.type("//input[@name='field51']", "-123.67");        
-        selenium.focus("//input[@name='field52']");      
+        selenium.focus("//input[@name='field77']");      
         Thread.sleep(100);        
         assertTrue(! selenium.isTextPresent("Must be a positive or negative number, with any number of digits to the right of the decimal."));
 
+        
+      //---------------------------------------------Integer Pattern constraint------------------------------//
+        clearText("//input[@name='field77']");
+        selenium.type("//input[@name='field77']", "127.");
+        selenium.focus("//input[@name='field52']");
+        Thread.sleep(100);               
+        assertTrue(selenium.isTextPresent("Must be a positive or negative whole number"));
+        
+        clearText("//input[@name='field77']");
+        selenium.type("//input[@name='field77']", "1234.4123"); 
+        selenium.focus("//input[@name='field52']");
+        Thread.sleep(100);  
+        assertTrue(selenium.isTextPresent("Must be a positive or negative whole number"));
+        
+        clearText("//input[@name='field77']");
+        selenium.type("//input[@name='field77']", "123E123"); 
+        selenium.focus("//input[@name='field52']");
+        Thread.sleep(100);       
+        assertTrue(selenium.isTextPresent("Must be a positive or negative whole number"));
+        
+        clearText("//input[@name='field77']");
+        selenium.type("//input[@name='field77']", "-123");        
+        selenium.focus("//input[@name='field52']");      
+        Thread.sleep(100);        
+        assertTrue(! selenium.isTextPresent("Must be a positive or negative whole number"));
         
         //---------------------------------------------Phone Text------------------------------//
         clearText("//input[@name='field52']");
@@ -193,33 +217,77 @@ public class UIFComponentValidationRegexPatternIT {
         //---------------------------------------------Email Text------------------------------//
         clearText("//input[@name='field54']");
         selenium.type("//input[@name='field54']", "123@123.123");
-        selenium.focus("//input[@name='field55']");
+        selenium.focus("//input[@name='field84']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a properly formatted email address."));
         
         clearText("//input[@name='field54']");
         selenium.type("//input[@name='field54']", "email.com@emailServer");
-        selenium.focus("//input[@name='field55']");
+        selenium.focus("//input[@name='field84']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a properly formatted email address."));
         
         clearText("//input[@name='field54']");
         selenium.type("//input[@name='field54']", "emailemailServer@.com"); 
-        selenium.focus("//input[@name='field55']");
+        selenium.focus("//input[@name='field84']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a properly formatted email address."));
         
         clearText("//input[@name='field54']");
         selenium.type("//input[@name='field54']", "email@emailServercom"); 
-        selenium.focus("//input[@name='field55']");
+        selenium.focus("//input[@name='field84']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a properly formatted email address."));
         
         clearText("//input[@name='field54']");
         selenium.type("//input[@name='field54']", "email@emailServer.com");        
-        selenium.focus("//input[@name='field55']");      
+        selenium.focus("//input[@name='field84']");      
         Thread.sleep(100);
         assertTrue(! selenium.isTextPresent("Must be a properly formatted email address."));
+        
+        
+        //---------------------------------------------URL pattern Text------------------------------//
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "www.google.com");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "https:www.google.com");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "ftp://www.google.comsdfa123!#@");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "ftp:/www.google.coms");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "ftp://www.google.com");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "https://www.google.com");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
+        
+        clearText("//input[@name='field84']");
+        selenium.type("//input[@name='field84']", "http://www.google.com");
+        selenium.focus("//input[@name='field55']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid url beginning with http, https, or ftp"));
         
         
         //---------------------------------------------Date pattern Text------------------------------//
@@ -344,27 +412,93 @@ public class UIFComponentValidationRegexPatternIT {
         //---------------------------------------------BasicDate pattern Text------------------------------//
         clearText("//input[@name='field75']");
         selenium.type("//input[@name='field75']", "12122012");
-        selenium.focus("//input[@name='field56']");
+        selenium.focus("//input[@name='field82']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a date in the following format(s): MM/dd/yy, MM/dd/yyyy, MM-dd-yy, MM-dd-yyyy"));
         
         clearText("//input[@name='field75']");
         selenium.type("//input[@name='field75']", "13-12-34");
-        selenium.focus("//input[@name='field56']");
+        selenium.focus("//input[@name='field82']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a date in the following format(s): MM/dd/yy, MM/dd/yyyy, MM-dd-yy, MM-dd-yyyy"));
         
         clearText("//input[@name='field75']");
         selenium.type("//input[@name='field75']", "12:12:2034");
-        selenium.focus("//input[@name='field56']");
+        selenium.focus("//input[@name='field82']");
         Thread.sleep(100);
         assertTrue(selenium.isTextPresent("Must be a date in the following format(s): MM/dd/yy, MM/dd/yyyy, MM-dd-yy, MM-dd-yyyy"));
         
         clearText("//input[@name='field75']");
         selenium.type("//input[@name='field75']", "12-12-2034");
-        selenium.focus("//input[@name='field56']");
+        selenium.focus("//input[@name='field82']");
         Thread.sleep(100);
         assertTrue(! selenium.isTextPresent("Must be a date in the following format(s): MM/dd/yy, MM/dd/yyyy, MM-dd-yy, MM-dd-yyyy"));
+        
+        
+        //---------------------------------------------Time12H Pattern Text------------------------------//
+        clearText("//input[@name='field82']");
+        selenium.type("//input[@name='field82']", "13:00:12");
+        selenium.focus("//input[@name='field83']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 12 hour time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field82']");
+        selenium.type("//input[@name='field82']", "09:00:");
+        selenium.focus("//input[@name='field83']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 12 hour time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field82']");
+        selenium.type("//input[@name='field82']", "3-00:12");
+        selenium.focus("//input[@name='field83']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 12 hour time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field82']");
+        selenium.type("//input[@name='field82']", "3:00:34");
+        selenium.focus("//input[@name='field83']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid 12 hour time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field82']");
+        selenium.type("//input[@name='field82']", "3:00");
+        selenium.focus("//input[@name='field83']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid 12 hour time in HH:mm format, seconds are optional"));
+       
+        
+        //---------------------------------------------Time24H Pattern Text------------------------------//
+        clearText("//input[@name='field83']");
+        selenium.type("//input[@name='field83']", "24:00:12");
+        selenium.focus("//input[@name='field56']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 24 hour (0-23) time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field83']");
+        selenium.type("//input[@name='field83']", "14:00:");
+        selenium.focus("//input[@name='field56']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 24 hour (0-23) time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field83']");
+        selenium.type("//input[@name='field83']", "13:00:76");
+        selenium.focus("//input[@name='field56']");
+        Thread.sleep(100);
+        assertTrue(selenium.isTextPresent("Must be a valid 24 hour (0-23) time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field83']");
+        selenium.type("//input[@name='field83']", "13:00:23");
+        selenium.focus("//input[@name='field56']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid 24 hour (0-23) time in HH:mm format, seconds are optional"));
+        
+        clearText("//input[@name='field83']");
+        selenium.type("//input[@name='field83']", "23:00:12");
+        selenium.focus("//input[@name='field56']");
+        Thread.sleep(100);
+        assertTrue(! selenium.isTextPresent("Must be a valid 24 hour (0-23) time in HH:mm format, seconds are optional"));
+       
+        
         
         
         //---------------------------------------------Timestamp pattern Text------------------------------//

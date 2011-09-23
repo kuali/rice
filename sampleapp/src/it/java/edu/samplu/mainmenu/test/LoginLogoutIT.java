3338173697eb511e56d5de1fa54cc1c0bbb9204f
Leo Przybylski <leo@rsmart.com>
2011-09-23 17:36:14 +0000

    KULRICE-5613 adding login logout

---
+++ sampleapp/src/it/java/edu/samplu/mainmenu/test/LoginLogoutIT.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.mainmenu.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
+
+/**
+ * TODO Administrator don't forget to fill this in. 
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LoginLogoutIT {
+        @Before
+        public void setUp() throws Exception {
+                selenium = new DefaultSelenium("localhost", 4444, "*firefox", System.getProperty("remote.public.url"));
+                selenium.start();
+        }
+
+        @Test
+        public void testBlah() throws Exception {
+                selenium.open("/portal.do");
+                selenium.type("name=__login_user", "quickstart");
+                selenium.click("css=input[type=\"submit\"]");
+                selenium.waitForPageToLoad("30000");
+                selenium.click("link=Main Menu");
+                selenium.waitForPageToLoad("30000");
+        }
+
+        @After
+        public void tearDown() throws Exception {
+                selenium.stop();
+        }
+}
+
+

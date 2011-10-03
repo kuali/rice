a6a2be99ce2778d55e0711c5ea30b1724a65f898
Viral Chauhan <vchauhan@vivantech.com>
2011-10-03 10:39:02 +0000

    KULRICE-5644 Selenium Smoke Test Creation: More Complex Create-Blanket Approve Documents Tests

---
+++ sampleapp/src/it/java/edu/samplu/admin/test/LocationCampusBlanketAppIT.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.admin.test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+/**
+ * TODO Administrator don't forget to fill this in. 
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LocationCampusBlanketAppIT {
+    private Selenium selenium;
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", System.getProperty("remote.public.url"));
+        selenium.start();
+    }
+
+    @Test
+    public void testCampus() throws Exception {
+        selenium.open(System.getProperty("remote.public.url"));
+        assertEquals("Login", selenium.getTitle());
+        selenium.type("name=__login_user", "admin");
+        selenium.click("//input[@value='Login']");
+        selenium.waitForPageToLoad("30000");
+        assertEquals("Kuali Portal Index", selenium.getTitle());
+        selenium.click("link=Administration");
+        selenium.waitForPageToLoad("30000");
+        assertEquals("Kuali Portal Index", selenium.getTitle());
+        selenium.click("link=Campus");
+        selenium.waitForPageToLoad("30000");
+        assertEquals("Kuali Portal Index", selenium.getTitle());
+        selenium.selectFrame("iframeportlet");
+        selenium.click("//img[@alt='create new']");        
+        selenium.waitForPageToLoad("30000");
+        assertTrue(selenium.isElementPresent("name=methodToCall.cancel"));
+        String docId = selenium.getText("//div[@id='headerarea']/div/table/tbody/tr[1]/td[1]");
+       
+        selenium.type("id=document.documentHeader.documentDescription", "Validation Test Campus");
+        selenium.type("id=document.newMaintainableObject.code", "VC");
+        selenium.type("id=document.newMaintainableObject.name", "Validation Test Campus");
+        selenium.type("id=document.newMaintainableObject.shortName", "VTC");
+        selenium.select("id=document.newMaintainableObject.campusTypeCode", "label=B - BOTH");
+        selenium.click("name=methodToCall.blanketApprove");
+        selenium.waitForPageToLoad("30000");
+        selenium.selectWindow("null");
+        selenium.click("//img[@alt='doc search']");
+        selenium.waitForPageToLoad("30000");
+        assertEquals("Kuali Portal Index", selenium.getTitle());
+        selenium.selectFrame("iframeportlet");
+        selenium.click("//input[@name='methodToCall.search' and @value='search']");
+        selenium.waitForPageToLoad("30000");
+        docId= "link=" + docId;
+        assertTrue(selenium.isElementPresent(docId));       
+        if(selenium.isElementPresent(docId)){            
+            assertEquals("FINAL", selenium.getText("//table[@id='row']/tbody/tr[1]/td[4]"));
+        }else{
+            assertEquals(docId, selenium.getText("//table[@id='row']/tbody/tr[1]/td[1]"));            
+            assertEquals("FINAL", selenium.getText("//table[@id='row']/tbody/tr[1]/td[4]"));
+        }
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+}

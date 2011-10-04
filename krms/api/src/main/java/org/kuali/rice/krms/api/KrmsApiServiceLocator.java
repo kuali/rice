4c226335cb2a092dda6d0a99929bd49f2a7736ba
Eric Westfall <ewestfal@iu.edu>
2011-10-04 02:59:15 +0000

    KULRICE-5595 - added publishing of the KrmsTypeRepositoryService in support of testing end-to-end testing of KEW to KRMS integration

--- krms/api/src/main/java/org/kuali/rice/krms/api/KrmsApiServiceLocator.java
+++ krms/api/src/main/java/org/kuali/rice/krms/api/KrmsApiServiceLocator.java
@@ -3,6 +3,7 @@ package org.kuali.rice.krms.api;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.krms.api.engine.Engine;
 import org.kuali.rice.krms.api.repository.RuleRepositoryService;
+import org.kuali.rice.krms.api.repository.type.KrmsTypeRepositoryService;
 import org.kuali.rice.ksb.api.bus.ServiceBus;
 import org.kuali.rice.ksb.api.messaging.MessageHelper;
 import org.kuali.rice.ksb.api.registry.ServiceRegistry;
@@ -17,7 +18,8 @@ import javax.xml.namespace.QName;
 public class KrmsApiServiceLocator {
 
 	public static final String ENGINE = "rice.krms.engine";
-	public static final QName RULE_REPOSITORY_SERVICE = new QName(KrmsConstants.KRMS_NAMESPACE, "ruleRepositoryServiceSoap");
+	public static final QName RULE_REPOSITORY_SERVICE = new QName(KrmsConstants.Namespaces.KRMS_NAMESPACE_2_0, "ruleRepositoryServiceSoap");
+    public static final QName KRMS_TYPE_REPOSITORY_SERVICE = new QName(KrmsConstants.Namespaces.KRMS_NAMESPACE_2_0, "krmsTypeRepositoryServiceSoap");
 
     static <T> T getService(String serviceName) {
         return GlobalResourceLoader.<T>getService(serviceName);
@@ -33,6 +35,10 @@ public class KrmsApiServiceLocator {
     
     public static RuleRepositoryService getRuleRepositoryService() {
     	return getService(RULE_REPOSITORY_SERVICE);
+    }
+
+    public static KrmsTypeRepositoryService getKrmsTypeRepositoryService() {
+        return getService(KRMS_TYPE_REPOSITORY_SERVICE);
     }
 
 }

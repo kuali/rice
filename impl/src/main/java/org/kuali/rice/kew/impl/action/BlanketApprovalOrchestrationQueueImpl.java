caf725cc74953d6d571bc006f78e4afd34fa580e
Eric Westfall <ewestfal@iu.edu>
2011-10-12 04:48:02 +0000

    KULRICE-5209 - moved BlanketApprovalOrchestrationQueueImpl to the appropriate package

--- impl/src/main/java/org/kuali/rice/kew/impl/action/BlanketApprovalOrchestrationQueueImpl.java
+++ impl/src/main/java/org/kuali/rice/kew/impl/action/BlanketApprovalOrchestrationQueueImpl.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.kew.actions.asyncservices;
+package org.kuali.rice.kew.impl.action;
 
 import java.util.HashSet;
 import java.util.Set;

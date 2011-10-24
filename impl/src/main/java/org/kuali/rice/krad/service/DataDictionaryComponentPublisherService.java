6126f3bde3165c4b6047cda3c9ec7c44d8b6976f
Eric Westfall <ewestfal@iu.edu>
2011-10-24 12:41:18 +0000

    KULRICE-5762 - implemented DataDictionaryComponentPublisherService

---
+++ impl/src/main/java/org/kuali/rice/krad/service/DataDictionaryComponentPublisherService.java
@@ -0,0 +1,12 @@
+package org.kuali.rice.krad.service;
+
+/**
+ * A service which can be used to publish components to the Rice core component system from the data dicationary.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public interface DataDictionaryComponentPublisherService {
+
+    void publishAllComponents();
+
+}
---
+++ impl/src/main/java/org/kuali/rice/krad/service/impl/DataDictionaryComponentPublisherServiceImpl.java
@@ -0,0 +1,16 @@
+package org.kuali.rice.krad.service.impl;
+
+import org.kuali.rice.krad.service.DataDictionaryComponentPublisherService;
+
+/**
+ * Reference implementation of the {@code DataDictionaryComponentPublisherService}.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DataDictionaryComponentPublisherServiceImpl implements DataDictionaryComponentPublisherService {
+
+    @Override
+    public void publishAllComponents() {
+        // TODO - implement me!
+    }
+}

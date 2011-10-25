2ce5857603bfa297482d1848f2a3ef170ab4b2b1
Eric Westfall <ewestfal@iu.edu>
2011-10-25 19:58:45 +0000

    KULRICE-5746 - migrated use of document lookup to document search on DocumentSearchCustomizer

--- kew/framework/src/main/java/org/kuali/rice/kew/framework/document/search/DocumentSearchCustomizer.java
+++ kew/framework/src/main/java/org/kuali/rice/kew/framework/document/search/DocumentSearchCustomizer.java
@@ -19,17 +19,17 @@ import org.kuali.rice.kew.api.document.search.DocumentSearchResult;
 import java.util.List;
 
 /**
- * Provides various mechanisms for an application to customize the behavior of the Document Lookup functionality for
+ * Provides various mechanisms for an application to customize the behavior of the Document Search functionality for
  * their document types.  Client applications should provide implementations of this interface if they need to
- * enact some of these customizations on document lookups.  These customizers then get mapped to the appropriate
+ * enact some of these customizations on document searches.  These customizers then get mapped to the appropriate
  * document types via the KEW extension framework (see
  * {@link org.kuali.rice.kew.api.extension.ExtensionRepositoryService}).
  *
  * <p>The customization functionality provided by this interface includes:</p>
  *
  * <ul>
- *     <li>The ability to customize document lookup criteria before it gets submitted.</li>
- *     <li>The ability to customize how document lookup criteria is cleared.</li>
+ *     <li>The ability to customize document search criteria before it gets submitted.</li>
+ *     <li>The ability to customize how document search criteria is cleared.</li>
  *     <li>The ability to customize how result data is processed and presented.</li>
  * </ul>
  *
@@ -37,17 +37,17 @@ import java.util.List;
  * that it is permitted that an implementation of this interface could be tied to more than one document type via the
  * extension framework.  This is why information about the specific document type for which the customizations is being
  * applied is passed to all methods on this interface (note that the document type information is available from the
- * {@code DocumentLookupCriteria} as well.</p>
+ * {@code DocumentSearchCriteria} as well.</p>
  *
  * <p>Furthermore, the customization hooks afforded by this interface will only be executed when a valid document type
- * has been specified as input to the document lookup.  This is because the customizer is tied to the document type and
- * the document lookup must therefore be able to resolve a valid document type from the user-supplied criteria in order
+ * has been specified as input to the document search.  This is because the customizer is tied to the document type and
+ * the document search must therefore be able to resolve a valid document type from the user-supplied criteria in order
  * to perform these customizations.</p>
  *
  * <p>Since some of the operations on this component could potentially add expense to the overall search process in the
  * cases where customization is only done on certain document types or only certain customization features are being
  * utilized, this interface provides for a set of boolean operations which indicate which customization
- * features should be activated by the document lookup framework for a given document type.  It's expected that KEW
+ * features should be activated by the document search framework for a given document type.  It's expected that KEW
  * will check each of these flags prior to invoking the corresponding method that it "gaurds" and, if the customization
  * flag is disabled, it should refrain from executing that method.</p>
  *
@@ -62,10 +62,10 @@ import java.util.List;
 public interface DocumentSearchCustomizer {
 
     /**
-     * Performs customization on the given document lookup criteria.  This method should return the customized version
+     * Performs customization on the given document search criteria.  This method should return the customized version
      * of the criteria, or null if no customizations were performed.  This customization is invoked prior the actual
-     * lookup being executed, and the resulting customized criteria is what gets passed to the search implementation.
-     * It is also invoked in situtations where the document lookup is performed via the api or via the user interface.
+     * search being executed, and the resulting customized criteria is what gets passed to the search implementation.
+     * It is also invoked in situtations where the document search is performed via the api or via the user interface.
      *
      * <p>It is guaranteed that the document type name on the given criteria will never be null and will always
      * represent a valid document type.</p>
@@ -77,13 +77,13 @@ public interface DocumentSearchCustomizer {
     DocumentSearchCriteria customizeCriteria(DocumentSearchCriteria documentSearchCriteria);
 
     /**
-     * Performs a customized "clear" of the given document lookup criteria.  Clearing of the criteria is typically
-     * invoked whenever the user of the document lookup user interface clicks the "clear" button.  By default all of
-     * the lookup criteria is cleared, but this method allows for certain criteria data to be preserved on a clear if
+     * Performs a customized "clear" of the given document search criteria.  Clearing of the criteria is typically
+     * invoked whenever the user of the document search user interface clicks the "clear" button.  By default all of
+     * the search criteria is cleared, but this method allows for certain criteria data to be preserved on a clear if
      * desired.
      *
      * <p>A common use of this feature is to preserve the document type that has been selected when clearing criteria
-     * for a customized document lookup.
+     * for a customized document search.
      *
      * <p>It is guaranteed that the document type name on the given criteria will never be null and will always
      * represent a valid document type.</p>
@@ -95,32 +95,32 @@ public interface DocumentSearchCustomizer {
     DocumentSearchCriteria customizeClearCriteria(DocumentSearchCriteria documentSearchCriteria);
 
     /**
-     * Performs customization of the given list of document lookup results.  This method is invoked after the search is
-     * executed but before results are returned to the caller.  It is executed when a document lookup is executed via
-     * the api or from the document lookup user interface.
+     * Performs customization of the given list of document search results.  This method is invoked after the search is
+     * executed but before results are returned to the caller.  It is executed when a document search is executed via
+     * the api or from the document search user interface.
      *
-     * <p>This method returns a {@code DocumentLookupResultValues} object which contains a list of
+     * <p>This method returns a {@code DocumentSearchResultValues} object which contains a list of
      * {@link DocumentSearchResultValue} objects.  Each of these result values maps to a specific document id and
      * contains a list of {@link org.kuali.rice.kew.api.document.attribute.DocumentAttribute} values which can be used
-     * to modify existing document attributes or create new ones that are included as part of the lookup results.  It
+     * to modify existing document attributes or create new ones that are included as part of the search results.  It
      * is important to note that in order for these custom attribute values to be displayed in the result set in the
-     * document lookup user interface, there must be a corresponding entry in the
+     * document search user interface, there must be a corresponding entry in the
      * {@link DocumentSearchResultSetConfiguration} returned by the
      * {@link #customizeResultSetConfiguration(org.kuali.rice.kew.api.document.search.DocumentSearchCriteria)} method
      * on this customizer implementation.</p>
      *
      * <p>It is permissible that implementations of this method may not return result values for all of the document
-     * provided in the given list of document lookup results.  It is important to note however that ommision from the
-     * returned result values does not filter or remove the result from the lookup results.  Generally speaking, this
+     * provided in the given list of document search results.  It is important to note however that ommision from the
+     * returned result values does not filter or remove the result from the search results.  Generally speaking, this
      * method cannot be used to remove results from the result set.</p>
      *
      * <p>It is guaranteed that the document type name on the given criteria will never be null and will always
      * represent a valid document type.</p>
      *
-     * @param documentSearchCriteria the criteria against which the document lookup was executed
-     * @param defaultResults the results that were returned by the execution of the document lookup
+     * @param documentSearchCriteria the criteria against which the document search was executed
+     * @param defaultResults the results that were returned by the execution of the document search
      * 
-     * @return customized lookup result values for any of the document results requiring custom values, or null if no
+     * @return customized search result values for any of the document results requiring custom values, or null if no
      * customization was performed
      */
     DocumentSearchResultValues customizeResults(DocumentSearchCriteria documentSearchCriteria, List<DocumentSearchResult> defaultResults);
@@ -131,13 +131,13 @@ public interface DocumentSearchCustomizer {
      * attributes that are defined for the document type.  Generally speaking, this controls which "columns" of data are
      * displayed in the document search results.
      *
-     * <p>This method is only invoked by the document lookup user interface whenever it is rendering document lookup
-     * results.  It is not invoked when invoking document lookup using only the api.</p>
+     * <p>This method is only invoked by the document search user interface whenever it is rendering document search
+     * results.  It is not invoked when invoking document search using only the api.</p>
      *
      * <p>It is guaranteed that the document type name on the given criteria will never be null and will always
      * represent a valid document type.</p>
      *
-     * @param documentSearchCriteria the criteria against which the document lookup was executed
+     * @param documentSearchCriteria the criteria against which the document search was executed
      * @return the customized result set configuration, or null if no customization was performed
      */
     DocumentSearchResultSetConfiguration customizeResultSetConfiguration(DocumentSearchCriteria documentSearchCriteria);

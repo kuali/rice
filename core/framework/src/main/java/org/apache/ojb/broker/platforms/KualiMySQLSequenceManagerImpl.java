740b847781d9e9c5408fd26840c13f9aa4ecf8c1
Jeff Caddel <jcaddel@rsmart.com>
2011-11-02 19:28:27 +0000

    KULRICE-5623 The semi-colon in the middle of the license header was confusing the maven-license-plugin

--- core/framework/src/main/java/org/apache/ojb/broker/platforms/KualiMySQLSequenceManagerImpl.java
+++ core/framework/src/main/java/org/apache/ojb/broker/platforms/KualiMySQLSequenceManagerImpl.java
@@ -4,7 +4,6 @@
  * $Source: /opt/cvs/rice/shared/src/main/java/org/apache/ojb/broker/platforms/KualiMySQLSequenceManagerImpl.java,v $
  * 
  * Licensed under the Educational Community License, Version 2.0 (the "License")
- ;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 

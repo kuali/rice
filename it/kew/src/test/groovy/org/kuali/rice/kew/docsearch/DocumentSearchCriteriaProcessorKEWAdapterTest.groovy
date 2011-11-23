8137cd533e22634861aebfd089baac4a16559518
Aaron Hamid <aaron@incsw.com>
2011-11-23 08:41:45 +0000

    KULRICE-5635 fix test

--- it/kew/src/test/groovy/org/kuali/rice/kew/docsearch/DocumentSearchCriteriaProcessorKEWAdapterTest.groovy
+++ it/kew/src/test/groovy/org/kuali/rice/kew/docsearch/DocumentSearchCriteriaProcessorKEWAdapterTest.groovy
@@ -78,7 +78,7 @@ class DocumentSearchCriteriaProcessorKEWAdapterTest extends KEWTestCase {
         "documentId",
         "applicationDocumentId",
         "statusCode",
-        "applicationDocumentStatusCode",
+        "applicationDocumentStatus",
         "routeNodeName",
         "routeNodeLogic",
         "dateCreated",

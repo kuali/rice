559fa26571babae5b3cf8b610269b1236cfbbdab
Claus Niesen <cniesen@iastate.edu>
2011-11-28 22:48:14 +0000

    KULRICE-5996 Display error after Context lookup if the user is not authorized for it.

--- krms/impl/src/main/java/org/kuali/rice/krms/impl/ui/AgendaEditorController.java
+++ krms/impl/src/main/java/org/kuali/rice/krms/impl/ui/AgendaEditorController.java
@@ -21,12 +21,16 @@ import org.kuali.rice.krad.document.MaintenanceDocument;
 import org.kuali.rice.krad.service.KRADServiceLocator;
 import org.kuali.rice.krad.service.SequenceAccessorService;
 import org.kuali.rice.krad.uif.UifParameters;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.util.ObjectUtils;
 import org.kuali.rice.krad.web.controller.MaintenanceDocumentController;
 import org.kuali.rice.krad.web.form.MaintenanceForm;
 import org.kuali.rice.krad.web.form.UifFormBase;
+import org.kuali.rice.krms.api.KrmsConstants;
 import org.kuali.rice.krms.api.repository.rule.RuleDefinition;
 import org.kuali.rice.krms.api.repository.type.KrmsAttributeDefinition;
+import org.kuali.rice.krms.impl.authorization.AgendaAuthorizationService;
 import org.kuali.rice.krms.impl.repository.ActionAttributeBo;
 import org.kuali.rice.krms.impl.repository.ActionBo;
 import org.kuali.rice.krms.api.repository.LogicalOperator;
@@ -41,6 +45,7 @@ import org.kuali.rice.krms.impl.repository.PropositionBo;
 import org.kuali.rice.krms.impl.repository.RuleBo;
 import org.kuali.rice.krms.impl.repository.RuleBoService;
 import org.kuali.rice.krms.impl.rule.AgendaEditorBusRule;
+import org.kuali.rice.krms.impl.util.KRMSPropertyConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -79,9 +84,15 @@ public class AgendaEditorController extends MaintenanceDocumentController {
             HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView modelAndView = super.refresh(form, result, request, response);
 
-        // handle return from context lookup and update the namespace on all agenda related objects if the contest has been changed
+        // handle return from context lookup
         MaintenanceForm maintenanceForm = (MaintenanceForm) form;
         AgendaEditor agendaEditor = ((AgendaEditor) maintenanceForm.getDocument().getNewMaintainableObject().getDataObject());
+        // verify that user has permission for the context
+        if (!getAgendaAuthorizationService().isAuthorized(KrmsConstants.MAINTAIN_KRMS_AGENDA, agendaEditor.getAgenda().getContextId())) {
+            GlobalVariables.getMessageMap().putErrorWithoutFullErrorPath(KRADConstants.MAINTENANCE_NEW_MAINTAINABLE
+                    + KRMSPropertyConstants.Agenda.CONTEXT, "error.agenda.unauthorizedContext");
+        }
+        // update the namespace on all agenda related objects if the contest has been changed
         if (!StringUtils.equals(agendaEditor.getOldContextId(), agendaEditor.getAgenda().getContextId())) {
             agendaEditor.setOldContextId(agendaEditor.getAgenda().getContextId());
 
@@ -1890,5 +1901,9 @@ public class AgendaEditorController extends MaintenanceDocumentController {
         rule.refreshPropositionTree(false);
 
         return super.updateComponent(form, result, request, response);
+    }
+
+    public AgendaAuthorizationService getAgendaAuthorizationService() {
+        return KrmsRepositoryServiceLocator.getAgendaAuthorizationService();
     }
 }

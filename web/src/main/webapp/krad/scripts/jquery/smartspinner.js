8048c9b0d06761137bb0fa28478b9aa57c7a5984
Samuel Holtzkampf <samuel@opencollab.co.za>
2011-11-28 10:35:01 +0000

    Fix smartspinner.js header

--- web/src/main/webapp/krad/scripts/jquery/smartspinner.js
+++ web/src/main/webapp/krad/scripts/jquery/smartspinner.js
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2006-2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2005-2011 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
@@ -13,7 +29,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-﻿(function($) {
+
+(function($) {
     $.fn.extend({
         spinit: function(options) {
             var settings = $.extend({ min: 0, max: 100, callback: null, stepInc: 1, pageInc: 10, width: 50, height: 15, btnWidth: 10, mask: '' }, options);

a7cadd6e80d64799156092c38a671ffab274de09
Erik Meade <github-kuali-m@eghm.net>
2011-12-27 21:39:49 +0000

    KULRICE-5994 - Plugable Comparison Operator - unit test

---
+++ krms/framework/src/test/java/org/kuali/rice/krms/framework/engine/expression/ComparisonOperatorTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2005-2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krms.framework.engine.expression;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+
+public class ComparisonOperatorTest {
+    @Test
+    public void testEquals() {
+        ComparisonOperator op = ComparisonOperator.fromCode(ComparisonOperator.EQUALS.toString());
+        assertTrue(op.compare("StringOne", "StringOne"));
+        assertTrue(op.compare(123, "123"));
+    }
+
+    @Test
+    public void testLess() {
+        ComparisonOperator op = ComparisonOperator.fromCode(ComparisonOperator.LESS_THAN.toString());
+        assertTrue(op.compare(123, "124"));
+        assertTrue(op.compare(new Double(123.2), "124"));
+        assertFalse(op.compare(123, "123"));
+        assertFalse(op.compare(new Integer(123), "122"));
+        assertFalse(op.compare(122.1, "122"));
+    }
+
+    @Test
+    public void testLessThanEqual() {
+        ComparisonOperator op = ComparisonOperator.fromCode(ComparisonOperator.LESS_THAN_EQUAL.toString());
+        assertTrue(op.compare(123, "124"));
+        assertTrue(op.compare(123.1, "123.1"));
+        assertFalse(op.compare(new Double(123.1), "123.01"));
+        assertFalse(op.compare(123, "122"));
+    }
+}

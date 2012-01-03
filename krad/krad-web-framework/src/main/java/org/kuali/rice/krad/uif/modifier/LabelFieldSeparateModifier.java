aa81d744bd4936bc853bbcaa8ce2d7306d5e94a4
Jerry Neal <jkneal@indiana.edu>
2012-01-03 16:20:03 +0000

    KULRICE-6414 - adjusted displaywith id to include the collection suffix

--- krad/krad-web-framework/src/main/java/org/kuali/rice/krad/uif/modifier/LabelFieldSeparateModifier.java
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/krad/uif/modifier/LabelFieldSeparateModifier.java
@@ -69,7 +69,7 @@ public class LabelFieldSeparateModifier extends ComponentModifierBase {
 
 				// pull out label field
 				if (field.getLabelField() != null && field.getLabelField().isRender()) {
-				    field.getLabelField().addStyleClass("displayWith-" + field.getFactoryId());
+				    field.getLabelField().addStyleClass("displayWith-" + field.getId());
                     if (!field.isRender() && StringUtils.isBlank(field.getProgressiveRender())) {
                        field.getLabelField().setRender(false);
                     }

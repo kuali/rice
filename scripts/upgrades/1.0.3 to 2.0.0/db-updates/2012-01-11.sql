8355d4c4173b2072f923f2e1ab07c075b98acd0f
Peter Giles <g1l3sp@gmail.com>
2012-01-11 16:48:01 +0000

    KULRICE-6452: Rename KRMS_CNTXT_TERM_SPEC_PREREQ_S to KRMS_CNTXT_VLD_TERM_SPEC_S

---
+++ scripts/upgrades/1.0.3 to 2.0.0/db-updates/2012-01-11.sql
@@ -0,0 +1,5 @@
+-- KULRICE-6452
+
+drop table krms_cntxt_vld_event_t;
+drop sequence krms_cntxt_vld_event_s;
+rename krms_cntxt_term_spec_prereq_s to krms_cntxt_vld_term_spec_s;
---
+++ scripts/upgrades/1.0.3 to 2.0.0/db-updates/mysql-2012-01-11.sql
@@ -0,0 +1,5 @@
+-- KULRICE-6452
+
+drop table krms_cntxt_vld_event_t;
+drop table krms_cntxt_vld_event_s;
+rename table krms_cntxt_term_spec_prereq_s to krms_cntxt_vld_term_spec_s;

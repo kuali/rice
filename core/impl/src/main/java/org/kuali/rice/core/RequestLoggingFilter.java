2042b78b76ea48eb71bfa8187e1e837162ea6570
Erik Meade <github-kuali-m@eghm.net>
2012-02-01 17:32:35 +0000

    KULRICE-6605 - RequestLoggerFilter moved to core-impl.

--- core/impl/src/main/java/org/kuali/rice/core/RequestLoggingFilter.java
+++ core/impl/src/main/java/org/kuali/rice/core/RequestLoggingFilter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.krms.impl;
+package org.kuali.rice.core;
 
 import org.apache.log4j.Logger;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;

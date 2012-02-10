d777e45912c237f89d7a145db9811f60016cc364
Viral Chauhan <vchauhan@vivantech.com>
2012-02-10 12:27:06 +0000

    Modified createproject.groovy and config-pom templates to enable project creation with sampleapp as well as empty project with rice dependencies. Still fixing it to create standalone as it still wont work for it.

--- scripts/createproject.groovy
+++ scripts/createproject.groovy
@@ -23,9 +23,9 @@
  * Additionally, the generated project can be imported into Eclipse.
  */
 
-if (args.length < 2 || args.length > 9) { 
+if (args.length < 2 || args.length > 9) {
 	println 'usage: groovy createproject.groovy -name PROJECT_NAME [-pdir PROJECT_DIR] [-rdir RICE_DIR] [-mdir MAVEN_HOME] [-sampleapp] [-standalone]'
-	System.exit(1)	
+	System.exit(1)
 }
 
 PROJECT_DIR = '/java/projects'
@@ -37,13 +37,13 @@ STANDALONE = false
 count = 0
 for (arg in args) {
 	if (arg == '-name') PROJECT_NAME = args[count + 1]
-   	if (arg == '-pdir') PROJECT_DIR = args[count + 1]
+	if (arg == '-pdir') PROJECT_DIR = args[count + 1]
 	if (arg == '-rdir') RICE_DIR = args[count + 1]
 	if (arg == '-mdir') MAVEN_HOME = args[count + 1]
 	if (arg == '-sampleapp') SAMPLEAPP = true
 	if (arg == '-standalone') STANDALONE = true
 	count++
-}	
+}
 
 PROJECT_PATH = PROJECT_DIR + '/' + PROJECT_NAME
 
@@ -53,15 +53,20 @@ riceVersion = pom.version.text()
 projectNameUpper = PROJECT_NAME.toUpperCase()
 
 if (SAMPLEAPP) {
-  projectNameUpper = "TRAVEL"
-}  
+	projectNameUpper = "TRAVEL"
+}
 TEMPLATE_BINDING = [
-	"\${PROJECT_NAME}":"$PROJECT_NAME",
-	"\${APP_NAMESPACE}":projectNameUpper,
-	"\${RICE_VERSION}":riceVersion,
-	"\${USER_HOME}":System.getProperty('user.home'),
-	"\${bootstrap.spring.file}":"SpringBeans.xml"
-] 
+			"\${PROJECT_NAME}":"$PROJECT_NAME",
+			"\${APP_NAMESPACE}":projectNameUpper,
+			"\${RICE_VERSION}":riceVersion,
+			"\${USER_HOME}":System.getProperty('user.home'),
+			"\${bootstrap.spring.file}":"classpath:SpringBeans.xml", 
+			"\${monitoring.spring.import}":"", 			
+			"\${monitoring.filter}":"",
+			"\${monitoring.listener}":"",
+			"\${monitoring.mapping}":"",
+			"sample-app":PROJECT_NAME
+		]
 
 println warningtext()
 
@@ -88,77 +93,98 @@ removeFile("${System.getProperty('user.home')}/kuali/main/dev/rice.keystore")
 buildDir("${System.getProperty('user.home')}/kuali/main/dev/")
 buildDir("${PROJECT_DIR}")
 
-ant = new AntBuilder() 
+ant = new AntBuilder()
 
-ant.delete(dir:PROJECT_PATH) 
+ant.delete(dir:PROJECT_PATH)
 
 def springTemplateFile = null;
 
 if (SAMPLEAPP) {
 	// copy the Sample Application Files
-	ant.copy(todir:PROJECT_PATH + '/src/main/java') { 
-    	fileset(dir:RICE_DIR + '/sampleapp/src/main/java')
+	ant.copy(todir:PROJECT_PATH + '/src/main/java') {
+		fileset(dir:RICE_DIR + '/sampleapp/src/main/java')
 	}
 	/*ant.copy(todir:PROJECT_PATH + '/src/main/resources') {
-		fileset(dir:RICE_DIR + '/web/src/test/resources') 
-	}*/
-	
+	 fileset(dir:RICE_DIR + '/web/src/test/resources') 
+	 }*/
+
 	ant.copy(todir:PROJECT_PATH + '/src/main/resources') {
-		fileset(dir:RICE_DIR + '/sampleapp/src/main/resources', includes:'META-INF/*') 
+		fileset(dir:RICE_DIR + '/sampleapp/src/main/resources', includes:'META-INF/*,edu/sampleu/**/*,OJB-repository-sampleapp.xml')
 	}
 	
-	// copy sample-app-config.xml
-	/*ant.copy(todir:PROJECT_PATH + '/src/main/resources') { 
-    	fileset(dir:RICE_DIR + '/sampleapp/src/main/resources', includes:'META-INF/*') 
-	}*/
+	// Remove if Sample app not required on Homepage
+	ant.copy(todir:PROJECT_PATH + '/src/main/webapp/WEB-INF/jsp') {
+		fileset(dir:RICE_DIR + '/sampleapp/src/main/webapp/WEB-INF/jsp')
+	}
 	
+	
+	// copy main channels from sampleapp to new project
+	ant.copy(todir:PROJECT_PATH + '/src/main/webapp/WEB-INF/tags/rice-portal/channel/main') {
+		fileset(dir:RICE_DIR + '/sampleapp/src/main/webapp/WEB-INF/tags/rice-portal/channel/main')
+	}
+	
+	//Copy tag to new project to enable sample-app channel
+	ant.copy(todir:PROJECT_PATH + '/src/main/webapp/WEB-INF/tags/rice-portal/') {
+		fileset(file:RICE_DIR + '/sampleapp/src/main/webapp/WEB-INF/tags/rice-portal/mainTab.tag')
+	}	// copy sample-app-config.xml
+	/*ant.copy(todir:PROJECT_PATH + '/src/main/resources') { 
+	 fileset(dir:RICE_DIR + '/sampleapp/src/main/resources', includes:'META-INF/*') 
+	 }*/
+
 	// copy other configuration files
 	/*ant.copy(todir:PROJECT_PATH + '/src/main/resources') { 
-    	fileset(dir:RICE_DIR + '/web/src/test/resources', includes:'configurationServiceData.xml, *Resources.properties, OJB-*.xml')
-    }*/
-	
+	 fileset(dir:RICE_DIR + '/web/src/test/resources', includes:'configurationServiceData.xml, *Resources.properties, OJB-*.xml')
+	 }*/
+
 	// copy the Sample Application scripts. Like database create sql
 	ant.copy(todir:PROJECT_PATH + '/scripts') {
 		fileset(dir:RICE_DIR + '/sampleapp/scripts')
 	}
-	
+
 	springTemplateFile = new File(RICE_DIR + '/config/templates/createproject.SampleAppBeans.template.xml')
 } else {
 	// copy an empty java file to /src/main/java
 	ant.copy(file:RICE_DIR + "/config/templates/PutJavaCodeHere.java",
-		 tofile:PROJECT_PATH + "/src/main/java/PutJavaCodeHere.java")
-		 
+			tofile:PROJECT_PATH + "/src/main/java/PutJavaCodeHere.java")
+
 	// copy configuration files
-	ant.copy(todir:PROJECT_PATH + '/src/main/resources') { 
-    	fileset(dir:RICE_DIR + '/web/src/main/resources', includes:'configurationServiceData.xml, META-INF/*') 
+	ant.copy(todir:PROJECT_PATH + '/src/main/resources') {
+		fileset(dir:RICE_DIR + '/web/src/main/resources', includes:'configurationServiceData.xml, META-INF/*')
 	}
 	/*ant.copy(todir:PROJECT_PATH + '/src/main/resources') { 
-    	fileset(dir:RICE_DIR + '/web/src/test/resources', includes:'KR-ApplicationResources.properties')
-    }*/
-    
+	 fileset(dir:RICE_DIR + '/web/src/test/resources', includes:'KR-ApplicationResources.properties')
+	 }*/
+	
+	//copies meta-inf folder to empty rice project skeleton
+	ant.copy(todir:PROJECT_PATH + '/src/main/resources/META-INF') {
+		fileset(dir:RICE_DIR + '/sampleapp/src/main/resources/META-INF')
+	}
+	
+	
 	springTemplateFile = new File(RICE_DIR + '/config/templates/createproject.SpringBeans.template.xml')
 }
 
 // copy standard Rice Spring configuration files to project and rename
- 
+
 ant.copy(file:RICE_DIR + "/core/impl/src/main/resources/org/kuali/rice/core/RiceJTASpringBeans.xml",
-		 tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceJTASpringBeans.xml")
+		tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceJTASpringBeans.xml")
 if (STANDALONE) {
-    new File(PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceDataSourceSpringBeans.xml") << templateReplace(new File(RICE_DIR + '/impl/src/main/resources/org/kuali/rice/core/RiceDataSourceStandaloneClientSpringBeans.xml.template'))
+	new File(PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceDataSourceSpringBeans.xml") << templateReplace(new File(RICE_DIR + '/core/impl/src/main/resources/org/kuali/rice/core/RiceDataSourceStandaloneClientSpringBeans.xml.template'))
 	new File(PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceSpringBeans.xml") << templateReplace(new File(RICE_DIR + '/web/src/main/resources/org/kuali/rice/config/RiceStandaloneClientSpringBeans.xml.template'))
 } else {
-	ant.copy(file:RICE_DIR + "/core/impl/src/main/resources/org/kuali/rice/core/RiceDataSourceSpringBeans.xml",
-		 tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceDataSourceSpringBeans.xml")
+	new File(PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceDataSourceSpringBeans.xml") << templateReplace(new File(RICE_DIR + '/core/impl/src/main/resources/org/kuali/rice/core/RiceDataSourceSpringBeans.xml'))
+	/*ant.copy(file:RICE_DIR + "/core/impl/src/main/resources/org/kuali/rice/core/RiceDataSourceSpringBeans.xml",
+	 tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceDataSourceSpringBeans.xml")*/
 	ant.copy(file:RICE_DIR + "/impl/src/main/resources/org/kuali/rice/config/RiceSpringBeans.xml",
-		 tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceSpringBeans.xml")
+			tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-RiceSpringBeans.xml")
 }
 
 if (SAMPLEAPP) {
-    // [tbradford] TODO: There are some classes in the web portion of rice that
-    // need to be copied in addition, as they no longer seem to be bundled in 
-    // any of the Maven artifacts.
+	// [tbradford] TODO: There are some classes in the web portion of rice that
+	// need to be copied in addition, as they no longer seem to be bundled in
+	// any of the Maven artifacts.
 	ant.copy(file:RICE_DIR + "/sampleapp/src/main/resources/SampleAppModuleBeans.xml",
-			 tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-SampleAppModuleBeans.xml")
+			tofile:PROJECT_PATH + "/src/main/resources/${PROJECT_NAME}-SampleAppModuleBeans.xml")
 }
 
 // create SpringBeans.xml based on the defined springTemplateFile
@@ -170,18 +196,18 @@ new File(PROJECT_PATH + "/src/main/resources/SpringBeans.xml") << templateReplac
 // elegant way of directing people to the impex process for importing the
 // sample application data.
 
-// ant.copy(todir:PROJECT_PATH + '/src/main/config/sql') { 
-//     fileset(dir:RICE_DIR + '/impl/src/main/config/sql', includes:'**/*', excludes:'.svn,CVS*,.cvs*') 
+// ant.copy(todir:PROJECT_PATH + '/src/main/config/sql') {
+//     fileset(dir:RICE_DIR + '/impl/src/main/config/sql', includes:'**/*', excludes:'.svn,CVS*,.cvs*')
 // }
 
-// ant.copy(todir:PROJECT_PATH + '/src/main/config/xml') { 
-//     fileset(dir:RICE_DIR + '/impl/src/main/config/xml', includes:'**/*', excludes:'.svn,CVS*,.cvs*') 
+// ant.copy(todir:PROJECT_PATH + '/src/main/config/xml') {
+//     fileset(dir:RICE_DIR + '/impl/src/main/config/xml', includes:'**/*', excludes:'.svn,CVS*,.cvs*')
 // }
 
 // copy web application
 
-ant.copy(todir:PROJECT_PATH + '/src/main/webapp') { 
-    fileset(dir:RICE_DIR + '/web/src/main/webapp', includes:'**/*', excludes:'.svn,CVS*,.cvs*') 
+ant.copy(todir:PROJECT_PATH + '/src/main/webapp') {
+	fileset(dir:RICE_DIR + '/web/src/main/webapp', includes:'**/*', excludes:'.svn,CVS*,.cvs*')
 }
 
 // [tbradford] There's already an index.jsp in the portal that redirects to portal.jsp
@@ -192,8 +218,8 @@ ant.copy(todir:PROJECT_PATH + '/src/main/webapp') {
 // }
 
 // copy the keystore files
-ant.copy(todir:"${System.getProperty('user.home')}/kuali/main/dev") { 
-    fileset(dir:RICE_DIR + '/security', includes:'rice.keystore', excludes:'.svn,CVS*,.cvs*') 
+ant.copy(todir:"${System.getProperty('user.home')}/kuali/main/dev") {
+	fileset(dir:RICE_DIR + '/security', includes:'rice.keystore', excludes:'.svn,CVS*,.cvs*')
 }
 
 // execute variable replacement on web.xml to give it the correct SpringBeans.xml
@@ -228,17 +254,19 @@ if (SAMPLEAPP) {
 	config.delete()
 	config = new File(PROJECT_PATH + "/src/main/resources/META-INF/${PROJECT_NAME}-config.xml")
 	config << configtext
+} else {
+	//Rename config file and replace module name from config file  
+	new File(PROJECT_PATH + "/src/main/resources/META-INF/${PROJECT_NAME}-config.xml") << templateReplace(new File(RICE_DIR + '/sampleapp/src/main/resources/META-INF/sample-app-config.xml'))
 }
 
 // fix the links in index.jsp
 
 index = new File(PROJECT_PATH + '/src/main/webapp/index.jsp')
 indextext = ""
-index.eachLine { 
-    line -> 
-	    line = line.replace('kr-dev', "${PROJECT_NAME}-dev")
-	    line = line.replace('-dev/portal.do', "-dev/index.jsp")
-    	indextext += line + "\n" 
+index.eachLine {  line ->
+	line = line.replace('kr-dev', "${PROJECT_NAME}-dev")
+	line = line.replace('-dev/portal.do', "-dev/index.jsp")
+	indextext += line + "\n"
 }
 index.delete()
 index = new File(PROJECT_PATH + "/src/main/webapp/index.jsp")
@@ -261,24 +289,24 @@ if (maven) {
 		System.exit(1)
 	}
 
-    println("\nAdjusting Classpath Entries...")
-    def facetXml = new File(PROJECT_PATH + '/.classpath')
-    def facets = new XmlParser().parse(facetXml)
+	println("\nAdjusting Classpath Entries...")
+	def facetXml = new File(PROJECT_PATH + '/.classpath')
+	def facets = new XmlParser().parse(facetXml)
 
-    facets.classpathentry.each {
-      // con here is part of the classpath entry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER found in the .classpath file
-      // at this time, it uniquely ids the classpath entry for the jre
-      if (it.'@kind' == 'con') {
-        def tempNode = it
-        facets.remove(it)
-        println('\nMoving ' + it.'@path' + 'to the top of the classpath...')
-        facets.children().add(0, tempNode)
-      }
-    }
-    println("\nBacking up the .classpath file... \n")
-    new File(PROJECT_PATH, '/.classpath.bkp').delete()
-    facetXml.renameTo(new File(PROJECT_PATH, '/.classpath.bkp'))
-    new XmlNodePrinter(new PrintWriter(new FileWriter(PROJECT_PATH + '/.classpath'))).print(facets)
+	facets.classpathentry.each {
+		// con here is part of the classpath entry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER found in the .classpath file
+		// at this time, it uniquely ids the classpath entry for the jre
+		if (it.'@kind' == 'con') {
+			def tempNode = it
+			facets.remove(it)
+			println('\nMoving ' + it.'@path' + 'to the top of the classpath...')
+			facets.children().add(0, tempNode)
+		}
+	}
+	println("\nBacking up the .classpath file... \n")
+	new File(PROJECT_PATH, '/.classpath.bkp').delete()
+	facetXml.renameTo(new File(PROJECT_PATH, '/.classpath.bkp'))
+	new XmlNodePrinter(new PrintWriter(new FileWriter(PROJECT_PATH + '/.classpath'))).print(facets)
 }
 
 new File(PROJECT_PATH + '/instructions.txt') << instructionstext()
@@ -289,13 +317,13 @@ System.exit(0)
 
 def removeFile(path) {
 	if (new File(path).exists()) {
-		new File(path).delete() 
+		new File(path).delete()
 	}
 }
 
 def buildDir(path) {
 	if (! new File(path).exists()) {
-		new File(path).mkdirs() 
+		new File(path).mkdirs()
 	}
 }
 
@@ -304,11 +332,11 @@ def buildDir(path) {
 def detectMaven(mvnDir) {
 	mvnPath = ""
 	mvnExec = (System.getProperty("os.name") =~ /^Windows/) ? "/bin/mvn.bat" : "/bin/mvn"
-	
+
 	mvnHome1 = System.getProperty("M2_HOME")
 	mvnHome2 = System.getProperty("m2.home")
 	mvnHome3 = System.getenv("M2_HOME")
-	
+
 	if (mvnDir) {
 		mvnPath = mvnDir + mvnExec
 	} else if (mvnHome1) {
@@ -347,13 +375,13 @@ def detectMaven(mvnDir) {
 }
 
 def writeProcessOutput(process) {
-    tempReader = new InputStreamReader(new BufferedInputStream(process.getInputStream()))
-    reader = new BufferedReader(tempReader)
-    while (true) {
-    	line = reader.readLine()
-        if (line == null) break
-        println line
-    }		
+	tempReader = new InputStreamReader(new BufferedInputStream(process.getInputStream()))
+	reader = new BufferedReader(tempReader)
+	while (true) {
+		line = reader.readLine()
+		if (line == null) break
+			println line
+	}
 }
 
 def pomtext() {
@@ -375,19 +403,18 @@ def userhomeconfigtext() {
 
 def templateReplace(file) {
 	def replaced = ""
-	file.eachLine { 
-    line -> 
-    	for ( binding in TEMPLATE_BINDING ) {
-          line = line.replace(binding.key, binding.value)
-        }
-    	replaced += line + "\n" 
+	file.eachLine {  line ->
+		for ( binding in TEMPLATE_BINDING ) {
+			line = line.replace(binding.key, binding.value)
+		}
+		replaced += line + "\n"
 	}
 	return replaced
 }
 
 def instructionstext() {
 	def datasetDirectory = (SAMPLEAPP)? "/database/demo-server-dataset" : "database/bootstrap-server-dataset"
-"""
+	"""
 ==================================================================
         Instructions to complete Rice Template Install
 ==================================================================
@@ -415,7 +442,7 @@ def instructionstext() {
 }
 
 def warningtext() {
-"""
+	"""
 ==================================================================
                             WARNING 
 ==================================================================
@@ -434,7 +461,7 @@ Do you want to continue (yes/no)?"""
 }
 
 def mavenwarningtext() {
-"""
+	"""
 ==================================================================
                             WARNING 
 ==================================================================

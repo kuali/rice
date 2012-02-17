035b67c93b2ba3b05d465398f74fcb92a55e477a
Scott Gibson <sgibson@umd.edu>
2012-02-17 15:12:06 +0000

    KULRICE-6649 changed dhtml setRecipient to default to not escape html

--- web/src/main/webapp/kr/scripts/dhtml.js
+++ web/src/main/webapp/kr/scripts/dhtml.js
@@ -186,9 +186,9 @@ function setRecipientValue(recipientBase, value, isError ) {
     var containerDiv = document.getElementById(recipientBase + divSuffix);
     if (containerDiv) {
 		if (value == '') {
-			DWRUtil.setValue( containerDiv.id, "&nbsp;" );
+			DWRUtil.setValue( containerDiv.id, "&nbsp;", {escapeHtml:false} );
 		} else {
-			DWRUtil.setValue( containerDiv.id, value, isError?null:{escapeHtml:true} );
+			DWRUtil.setValue( containerDiv.id, value, isError?{escapeHtml:false}:{escapeHtml:true} );
 		}
 	}
     if (containerHidden) {

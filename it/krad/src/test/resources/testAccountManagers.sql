6b84af05899f020237618adf0c2ba71a5c1e71de
Leo Przybylski <leo@rsmart.com>
2012-02-23 01:31:57 +0000

    Rolling back license header changes

--- it/krad/src/test/resources/testAccountManagers.sql
+++ it/krad/src/test/resources/testAccountManagers.sql
@@ -1,5 +1,5 @@
 --
--- Copyright 2005-2012 The Kuali Foundation
+-- Copyright 2005-2011 The Kuali Foundation
 --
 -- Licensed under the Educational Community License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-
 insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (101, 'fo-101', '5EA45238F5528846E0404F8189D840B0');
 insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (102, 'fo-102', '5EA45238F5528846E0404F8189D840B1');
 insert into trv_acct_fo (acct_fo_id, acct_fo_user_name, OBJ_ID) values (103, 'fo-103', '5EA45238F5528846E0404F8189D840B2');

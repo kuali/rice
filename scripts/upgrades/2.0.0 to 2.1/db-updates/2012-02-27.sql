05011e80744b79dc4f6be0013a0a28eba762eb29
Jeff Ruch <jeff.ruch@colostate.edu>
2012-02-27 17:25:17 +0000

    KULRICE-6842: Don't allow requests for null principals.

---
+++ scripts/upgrades/2.0.0 to 2.1/db-updates/2012-02-27.sql
@@ -0,0 +1,28 @@
+--
+-- Copyright 2005-2012 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+--
+-- KULRICE-6842: Don't allow requests for null principals
+--
+
+ALTER TABLE
+   krim_grp_mbr_t
+MODIFY
+   (
+  MBR_ID VARCHAR(40) NOT NULL
+   )
+;

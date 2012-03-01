5ec83c1712166b75336085f3d74750eab602f455
Peter Giles <g1l3sp@gmail.com>
2012-03-01 21:54:53 +0000

    KULRICE-6710
    adding explanatory comment at Chitra's request

--- scripts/upgrades/1.0.3 to 2.0.0/db-updates/mysql-2012-02-14.sql
+++ scripts/upgrades/1.0.3 to 2.0.0/db-updates/mysql-2012-02-14.sql
@@ -19,6 +19,10 @@
 -- KULRICE-6710: Drop krms_cntxt_vld_rule_t, krms_cntxt_vld_actn_t and krms_cntxt_vld_agenda_t tables
 --
 
+-- NOTE that these tables should have been renamed in the master db, but mysteriously still were present.
+-- deleting here.  If you get errors that these tables and sequences don't exist, you can omit these statements
+-- without concern.
+
 drop table krms_cntxt_vld_actn_t;
 drop table krms_cntxt_vld_actn_s;
 drop table krms_cntxt_vld_agenda_t;

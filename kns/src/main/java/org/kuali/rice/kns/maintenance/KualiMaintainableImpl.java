ffda68df2db9ecd7e63dd3228100513ebad248aa
Peter Giles <g1l3sp@gmail.com>
2012-03-07 18:59:02 +0000

    KULRICE-6899: Maintainable.saveBusinessObject() never gets called
    RoutingRuleDelegationMaintainable.java, RoutingRuleMaintainable.java:
    * removed saveDataObject methods
    
    GenericPermissionMaintainable.java:
    * removed old saveBusinessObject method
    * cleaned up saveDataObject and renamed to saveBusinessObject
    
    KualiMaintainableImpl.java:
    * added saveDataObject method that delegates to saveBusinessObject

--- kns/src/main/java/org/kuali/rice/kns/maintenance/KualiMaintainableImpl.java
+++ kns/src/main/java/org/kuali/rice/kns/maintenance/KualiMaintainableImpl.java
@@ -298,7 +298,16 @@ public class KualiMaintainableImpl extends MaintainableImpl implements Maintaina
 		getBusinessObjectService().linkAndSave(businessObject);
 	}
 
-	/**
+    /**
+     * delegate this call to KNS' {@link org.kuali.rice.kns.maintenance.Maintainable#saveBusinessObject()} in order
+     * to support KNS maintainables.
+     */
+    @Override
+    public void saveDataObject() {
+        super.saveDataObject();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    /**
 	 * Retrieves title for maintenance document from data dictionary
 	 */
 	public String getMaintainableTitle() {

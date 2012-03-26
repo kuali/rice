c1a0abd748f414c600f08134b8f365f9a9691b51
Sona Sona <sonam@kuali.org>
2012-03-26 17:12:25 +0000

    KULRICE-6969 Added currency control check in DataDictionaryTypeServiceHelper.toRemotableAbstractControlBuilder(). Added null check in AttributesMapBuilder.

--- impl/src/main/java/org/kuali/rice/kns/datadictionary/exporter/AttributesMapBuilder.java
+++ impl/src/main/java/org/kuali/rice/kns/datadictionary/exporter/AttributesMapBuilder.java
@@ -206,7 +206,9 @@ public class AttributesMapBuilder {
         }
         else if (control.isText()) {
             controlMap.set("text", "true");
-            controlMap.set("size", control.getSize().toString());
+            if (control.getSize() != null) {
+                controlMap.set("size", control.getSize().toString());
+            }
             controlMap.set("datePicker", Boolean.valueOf(control.isDatePicker()).toString());
             controlMap.set("ranged", Boolean.valueOf(control.isRanged()).toString());
         }
@@ -218,7 +220,9 @@ public class AttributesMapBuilder {
         }
         else if (control.isCurrency()) {
             controlMap.set("currency", "true");
-            controlMap.set("size", control.getSize().toString());
+            if (control.getSize() != null) {
+                controlMap.set("size", control.getSize().toString());
+            }
             controlMap.set("formattedMaxLength", ((CurrencyControlDefinition) control).getFormattedMaxLength().toString());
         }
         else if (control.isLookupHidden()) {

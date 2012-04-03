17abee1566036a5c286e1a4ae009ea85835756c1
Aaron Hamid <aaron@incsw.com>
2012-04-03 19:07:27 +0000

    KULRICE-5733 this turned out to be trivial, committing sql script

--- scripts/upgrades/2.0.0 to 2.1/scripts/delete_old_docsearch_attributes.sql
+++ scripts/upgrades/2.0.0 to 2.1/scripts/delete_old_docsearch_attributes.sql
@@ -5,5 +5,5 @@ select * from KREW_RULE_ATTR_T where RULE_ATTR_TYP_CD="DocumentSearchCriteriaPro
 /
 -- delete from KREW_DOC_TYP_ATTR_T where RULE_ATTR_ID in (select RULE_ATTR_ID from KREW_RULE_ATTR_T where RULE_ATTR_TYP_CD="DocumentSearchCriteriaProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchGeneratorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchResultProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchXMLResultProcessorAttribute")
 -- /
--- delete * from KREW_RULE_ATTR_T where RULE_ATTR_TYP_CD="DocumentSearchCriteriaProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchGeneratorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchResultProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchXMLResultProcessorAttribute"
+-- delete from KREW_RULE_ATTR_T where RULE_ATTR_TYP_CD="DocumentSearchCriteriaProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchGeneratorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchResultProcessorAttribute" or RULE_ATTR_TYP_CD="DocumentSearchXMLResultProcessorAttribute"
 -- /

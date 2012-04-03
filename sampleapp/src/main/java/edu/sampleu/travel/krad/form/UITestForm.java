527907b852ca8e3d1cefa7d288852f38cace0b2e
Jerry Neal <jkneal@indiana.edu>
2012-04-03 20:11:02 +0000

    Initialize field2 to prevent NPE on save of test view 1

--- sampleapp/src/main/java/edu/sampleu/travel/krad/form/UITestForm.java
+++ sampleapp/src/main/java/edu/sampleu/travel/krad/form/UITestForm.java
@@ -30,7 +30,7 @@ public class UITestForm extends UifFormBase {
 	private static final long serialVersionUID = -2054046347823986319L;
 
 	private String field1;
-	private String field2;
+	private String field2 = "";
 	private String field3;
 	private String field4;
 	private String field5;

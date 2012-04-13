2a17edda954d396fd51aca07b39e0f57299917f1
Eric Njogu <enjogu@moderas.org>
2012-04-13 12:05:28 +0000

    KULRICE-7053: changes to selenium tests

--- sampleapp/src/it/java/edu/samplu/travel/krad/test/ConfigurationTestViewIT.java
+++ sampleapp/src/it/java/edu/samplu/travel/krad/test/ConfigurationTestViewIT.java
@@ -20,6 +20,7 @@ import com.thoughtworks.selenium.DefaultSelenium;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.kuali.rice.core.framework.resourceloader.SpringResourceLoader;
 import org.kuali.rice.krad.uif.component.Component;
@@ -34,7 +35,10 @@ import static junit.framework.Assert.*;
  */
 public class ConfigurationTestViewIT {
     private DefaultSelenium selenium;
+    /** bean id prefix in used in view */
     private String idPrefix = "ConfigurationTestView-ProgressiveRender-";
+    /** bean id suffix for add line controls */
+    String addLineIdSuffix = "InputField_add_control";
     @Before
 	public void setUp() throws Exception {
 		selenium = new DefaultSelenium("localhost", 4444, "*chrome", System.getProperty("remote.public.url"));
@@ -42,6 +46,9 @@ public class ConfigurationTestViewIT {
 	}
 
 	@Test
+    /**
+     * test for text input field label - style setting and refreshWhenChanged for components not in collection
+     */
 	public void testConfigurationTestView() throws Exception {
         openConfigurationTestView();
 
@@ -88,32 +95,68 @@ public class ConfigurationTestViewIT {
 
     /**
      * test adding a line to a collection which uses an add line that has spring expressions that are evaluated on refresh
+     * a specific time is set
      */
     @Test
-    public void testAddLine() throws Exception{
+    public void testAddLineWithSpecificTime() throws Exception{
         openConfigurationTestView();
-        String[] addLineIds = {"StartTime", "StartTimeAmPm", "AllDay"};
-        String idSuffix = "InputField_add_control";
-        for (String id: addLineIds) {
-            String tagId = "id=" + idPrefix + id + idSuffix;
-           assertTrue("Did not find id " + tagId, selenium.isElementPresent(tagId));
-        }
+        confirmAddLineControlsPresent();
 
-        String startTimeId = "id=" +idPrefix + "StartTime" + idSuffix;
+        String startTimeId = "id=" +idPrefix + "StartTime" + addLineIdSuffix;
         selenium.focus(startTimeId);
         String inputTime = "7:06";
         selenium.type(startTimeId, inputTime);
 
-        String allDayId = "id=" + idPrefix + "AllDay" + idSuffix;
+        String amPmSelectLocator = "id=" + idPrefix + "StartTimeAmPm" + addLineIdSuffix;
+        selenium.click(amPmSelectLocator);
+        selenium.select(amPmSelectLocator, "label=PM");
+        assertEquals("PM", selenium.getSelectedLabel(amPmSelectLocator));
+
+        Thread.sleep(5000); //allow for ajax refresh
+        selenium.click("css=div#ConfigurationTestView-ProgressiveRender-TimeInfoSection button");
+        Thread.sleep(5000); //allow for line to be added
+
+        //confirm that line has been added
+        assertTrue("line is not present", selenium.isElementPresent("//input[@value='7:06']"));
+    }
+
+    /**
+     * verify that add line controls are present
+     */
+    private void confirmAddLineControlsPresent() {
+        String[] addLineIds = {"StartTime", "StartTimeAmPm", "AllDay"};
+
+        for (String id: addLineIds) {
+            String tagId = "id=" + idPrefix + id + addLineIdSuffix;
+           assertTrue("Did not find id " + tagId, selenium.isElementPresent(tagId));
+        }
+    }
+
+    /**
+     * test adding a line to a collection which uses an add line that has spring expressions that are evaluated on refresh
+     * a specific time is set
+     */
+    @Test
+    @Ignore("count rows through CSS or XPATH fails")
+    public void testAddLineAllDay() throws Exception{
+        openConfigurationTestView();
+        confirmAddLineControlsPresent();
+
+        //store number of rows before adding the lines
+        String cssCountRows = "div#ConfigurationTestView-ProgressiveRender-TimeInfoSection.uif-group div#ConfigurationTestView-ProgressiveRender-TimeInfoSection_disclosureContent.uif-disclosureContent table tbody tr";
+        int rowCount = (selenium.getCssCount(cssCountRows)).intValue();
+
+        String allDayId = "id=" + idPrefix + "AllDay" + addLineIdSuffix;
         selenium.focus(allDayId);
         Thread.sleep(5000); //allow for ajax refresh
         selenium.click(allDayId);
         selenium.click("css=div#ConfigurationTestView-ProgressiveRender-TimeInfoSection button");
         Thread.sleep(5000); //allow for line to be added
 
-        //confirm that line has been added
-        assertTrue("line is not present",selenium.isElementPresent("//input[@value='7:06']"));
+        //confirm that line has been added (by checking for the new delete button)
+        assertEquals("line was not added", rowCount + 1, (selenium.getCssCount(cssCountRows)).intValue());
     }
+
 
     @After
 	public void tearDown() throws Exception {

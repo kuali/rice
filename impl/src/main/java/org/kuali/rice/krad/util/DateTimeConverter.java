5ea6f3a9dc28cb30561845addfcd208efaecfaf1
Jeff Caddel <jcaddel@rsmart.com>
2012-04-21 00:10:29 +0000

    Automated license header update

--- impl/src/main/java/org/kuali/rice/krad/util/DateTimeConverter.java
+++ impl/src/main/java/org/kuali/rice/krad/util/DateTimeConverter.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2006-2012 The Kuali Foundation
+/**
+ * Copyright 2005-2012 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * This is a utility converter class for use with XStream serializers. This was written to deal with an issue
  * resulting from the use of JODA-Time where unmarshalling of XML was failing.

a0242ac0e621be5a25b55adda10b371975bde1b9
Peter Giles <g1l3sp@gmail.com>
2012-04-23 19:18:15 +0000

    KULRICE-6964: Update Rice default From email address
    * changed selection of row to update so that it doesn't use OBJ_ID

--- scripts/upgrades/2.0.0 to 2.1/db-updates/mysql-2012-04-16b.sql
+++ scripts/upgrades/2.0.0 to 2.1/db-updates/mysql-2012-04-16b.sql
@@ -17,5 +17,6 @@
 
 -- KULRICE-6964: Update Rice default From email address
 
-update KRCR_PARM_T set val='rice.test@kuali.org' where OBJ_ID='700AB6A6E23740D0B3E00E02A8FB6347'
+update KRCR_PARM_T set val='rice.test@kuali.org'
+where nmspc_cd='KR-WKFLW' and cmpnt_cd='Mailer' and parm_nm='FROM_ADDRESS' and appl_id='KUALI'
 ;

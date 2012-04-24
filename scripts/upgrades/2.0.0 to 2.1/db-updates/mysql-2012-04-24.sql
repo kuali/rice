ee40aecfc1c0fecd2c5b496eff4236b77eb2bdae
Jeremy Hanson <jjhanso@iastate.edu>
2012-04-24 18:00:50 +0000

    KULRICE-7059 - fix sequence value

---
+++ scripts/upgrades/2.0.0 to 2.1/db-updates/mysql-2012-04-24.sql
@@ -0,0 +1,8 @@
+drop table if exists KRIM_ROLE_PERM_ID_S;
+
+create table KRIM_ROLE_PERM_ID_S (
+  id bigint(19) not null auto_increment,
+  primary key (id)
+) ENGINE MyISAM;
+
+alter table KRIM_ROLE_PERM_ID_S auto_increment = 10000;

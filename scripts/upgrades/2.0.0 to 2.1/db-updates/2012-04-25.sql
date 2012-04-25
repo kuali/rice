ed41da42e0ab0bddd4bfba54bf812ca4ce33718b
Erik Meade <github-kuali-m@eghm.net>
2012-04-25 20:59:01 +0000

    KULRICE-6611 - Correct KRMS type attributes for Validation Action & Rule in database

---
+++ scripts/upgrades/2.0.0 to 2.1/db-updates/2012-04-25.sql
@@ -0,0 +1,15 @@
+update krms_attr_defn_t set nm='actionTypeCode' where attr_defn_id=1004
+/
+update krms_attr_defn_t set nm='actionMessage' where attr_defn_id=1005
+/
+update krms_attr_defn_t set nm='ruleTypeCode' where attr_defn_id=1001
+/
+
+delete from krms_typ_attr_t where ATTR_DEFN_ID = 1002
+/
+delete from krms_typ_attr_t where ATTR_DEFN_ID = 1003
+/
+delete from krms_attr_defn_t where ATTR_DEFN_ID = 1002
+/
+delete from krms_attr_defn_t where ATTR_DEFN_ID = 1003
+/

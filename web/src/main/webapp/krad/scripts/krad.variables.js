705a7412dc4c6e627641e14fcf8041259a974fe9
Martin Taylor <mztaylor@umd.edu>
2012-05-02 18:58:39 +0000

    KULRICE-7203: Added additional uif related classes

--- web/src/main/webapp/krad/scripts/krad.variables.js
+++ web/src/main/webapp/krad/scripts/krad.variables.js
@@ -31,15 +31,53 @@ var kradVariables = (function () {
 //    var infoImage;
 
     // constants
+    // var APPLICATION_CLASS  = "Uif-Application";
     // var APP_ID  = "Uif-Application";
-    // var VALIDATION_MESSAGES = "validationMessages";
-    // var INPUT_FIELD_SELECTOR = "[data-role='InputField']";
-
-    // var IMAGE_LOCATION = "kradImageLocation";
-    // var VALIDATION_IMAGE_CLASS = "uif-validationImage";
+    // var CHANGED_HEADER_ICON_CLASS = "uif-changedHeaderIcon";
+    // var CLIENT_MESSAGE_ITEMS_CLASS = "uif-clientMessageItems";
+    // var CLIENT_WARNING_DIV_CLASS = "uif-clientWarningDiv";
+    // var COLLAPSED_ERRORS_CLASS = "uif-collapsedErrors";
+    // var COLLAPSED_INFO_CLASS = "uif-collapsedInfo";
+    // var COLLAPSED_WARNINGS_CLASS = "uif-collapsedWarnings";
+    // var CONTROL_CLASS = "Uif-Application";
     // var DIRTY_CLASS = "dirty";
-
+    // var ERROR_HIGHLIGHT_SECTION_CLASS = "uif-errorHighlight-section";
+    // var ERROR_MESSAGE_ITEM_CLASS = "uif-errorMessageItem-field";
+    // var FIELD_CLASS = "uif-field";
+    // var HAS_ERROR_CLASS = "uif-hasError";
+    // var HAS_INFO_CLASS = "uif-hasInfo";
+    // var HAS_MODIFIED_ERROR_CLASS = "uif-hasError-modified";
+    // var HAS_WARNING_CLASS = "uif-hasWarning";
+    // var HEADER_TEXT_CLASS = "uif-headerText";
+    // var IMAGE_LOCATION = "kradImageLocation";
+    // var INCIDENT_REPORT_VIEW_CLASS = "Uif-IncidentReportView";
+    // var INFO_HIGHLIGHT_SECTION_CLASS = "uif-infoHighlight-section";
+    // var INFO_MESSAGE_ITEM_CLASS = "uif-infoMessageItem-field";
+    // var INPUT_FIELD_CLASS = "uif-inputField";
+    // var INPUT_FIELD_SELECTOR = "[data-role='InputField']";
+    // var MESSAGE_COUNT_CLASS = "uif-messageCount";
+    // var NAVIGATION_MENU_CLASS = "uif-navigationMenu";
+    // var PAGE_CONTENT_HEADER_CLASS = "Uif-PageContentWrapper";
+    // var PAGE_VALIDATION_HEADER_CLASS = "uif-pageValidationHeader";
+    // var PAGE_VALIDATION_MESSAGE_ERROR_CLASS = "uif-pageValidationMessages-error";
+    // var PAGE_VALIDATION_MESSAGE_INFO_CLASS = "uif-pageValidationMessages-info";
+    // var PAGE_VALIDATION_MESSAGE_WARNING_CLASS = "uif-pageValidationMessages-warning";
+    // var PROGRESSIVE_DISCLOSURE_HIGHLIGHT_CLASS = "uif-progressiveDisclosure-highlight";
+    // var REQUIRED_MESSAGE_CLASS = "uif-requiredMessage";
+    // var SERVER_MESSAGE_ITEMS_CLASS = "uif-serverMessageItems"
+    // var TABLE_COLLECTION_LAYOUT_CLASS = "uif-tableCollectionLayout";
+    // var TAB_GROUP_CLASS = "Uif-TabGroup";
+    // var TAB_MENU_CLASS = "uif-tabMenu";
+    // var TOOLTIP_CLASS = "uif-tooltip";
+    // var VALIDATION_IMAGE_CLASS = "uif-validationImage";
+    // var VALIDATION_MESSAGES = "validationMessages";
+    // var VALIDATION_MESSAGES_CLASS = "uif-validationMessagesList";
+    // var VALIDATION_PAGE_HEADER_CLASS = "uif-pageValidationHeader";
     // var VALIDATION_SETUP_EVENT = "validationSetup";
-
+    // var VIEW_CONTENT_HEADER_CLASS = "Uif-ViewContentWrapper";
     // var VIEW_STATE = ""ViewState";
+    // var WARNING_HIGHLIGHT_SECTION_CLASS = "uif-warningHighlight-section";
+    // var WARNING_MESSAGE_ITEM_CLASS = "uif-warningMessageItem-field";
+
+
 })();

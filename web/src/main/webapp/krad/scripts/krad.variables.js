2a6fcc42cfc7927c5dc011bd0825d19ce1b06ed1
Martin Taylor <mztaylor@umd.edu>
2012-05-08 22:00:36 +0000

    KULRICE-7203: updated kradVariables to load from prototype

--- web/src/main/webapp/krad/scripts/krad.variables.js
+++ web/src/main/webapp/krad/scripts/krad.variables.js
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005-2012 The Kuali Foundation
  *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * Licensed under the Educational Community License, Version 2.0 (the "License"),
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,67 +17,71 @@
 /**
  * Common constants and variables for KRAD
  */
-var kradVariables = (function () {
+        
+function KradVariables () {
+}
+
+KradVariables.prototype = {
 
     // validation init
-//    var pageValidatorReady = false;
-//    var validateClient = true;
-//    var messageSummariesShown = false;
-//    var pauseTooltipDisplay = false;
+//    var pageValidatorReady : false,
+//    var validateClient : true,
+//    var messageSummariesShown : false,
+//    var pauseTooltipDisplay : false,
 //
-//    var errorImage;
-//    var errorGreyImage;
-//    var warningImage;
-//    var infoImage;
+//    var errorImage,
+//    var errorGreyImage,
+//    var warningImage,
+//    var infoImage,
 
     // constants
-    // var APPLICATION_CLASS  = "Uif-Application";
-    // var APP_ID  = "Uif-Application";
-    // var CHANGED_HEADER_ICON_CLASS = "uif-changedHeaderIcon";
-    // var CLIENT_MESSAGE_ITEMS_CLASS = "uif-clientMessageItems";
-    // var CLIENT_WARNING_DIV_CLASS = "uif-clientWarningDiv";
-    // var COLLAPSED_ERRORS_CLASS = "uif-collapsedErrors";
-    // var COLLAPSED_INFO_CLASS = "uif-collapsedInfo";
-    // var COLLAPSED_WARNINGS_CLASS = "uif-collapsedWarnings";
-    // var CONTROL_CLASS = "Uif-Application";
-    // var DIRTY_CLASS = "dirty";
-    // var ERROR_HIGHLIGHT_SECTION_CLASS = "uif-errorHighlight-section";
-    // var ERROR_MESSAGE_ITEM_CLASS = "uif-errorMessageItem-field";
-    // var FIELD_CLASS = "uif-field";
-    // var HAS_ERROR_CLASS = "uif-hasError";
-    // var HAS_INFO_CLASS = "uif-hasInfo";
-    // var HAS_MODIFIED_ERROR_CLASS = "uif-hasError-modified";
-    // var HAS_WARNING_CLASS = "uif-hasWarning";
-    // var HEADER_TEXT_CLASS = "uif-headerText";
-    // var IMAGE_LOCATION = "kradImageLocation";
-    // var INCIDENT_REPORT_VIEW_CLASS = "Uif-IncidentReportView";
-    // var INFO_HIGHLIGHT_SECTION_CLASS = "uif-infoHighlight-section";
-    // var INFO_MESSAGE_ITEM_CLASS = "uif-infoMessageItem-field";
-    // var INPUT_FIELD_CLASS = "uif-inputField";
-    // var INPUT_FIELD_SELECTOR = "[data-role='InputField']";
-    // var MESSAGE_COUNT_CLASS = "uif-messageCount";
-    // var NAVIGATION_MENU_CLASS = "uif-navigationMenu";
-    // var PAGE_CONTENT_HEADER_CLASS = "Uif-PageContentWrapper";
-    // var PAGE_VALIDATION_HEADER_CLASS = "uif-pageValidationHeader";
-    // var PAGE_VALIDATION_MESSAGE_ERROR_CLASS = "uif-pageValidationMessages-error";
-    // var PAGE_VALIDATION_MESSAGE_INFO_CLASS = "uif-pageValidationMessages-info";
-    // var PAGE_VALIDATION_MESSAGE_WARNING_CLASS = "uif-pageValidationMessages-warning";
-    // var PROGRESSIVE_DISCLOSURE_HIGHLIGHT_CLASS = "uif-progressiveDisclosure-highlight";
-    // var REQUIRED_MESSAGE_CLASS = "uif-requiredMessage";
-    // var SERVER_MESSAGE_ITEMS_CLASS = "uif-serverMessageItems"
-    // var TABLE_COLLECTION_LAYOUT_CLASS = "uif-tableCollectionLayout";
-    // var TAB_GROUP_CLASS = "Uif-TabGroup";
-    // var TAB_MENU_CLASS = "uif-tabMenu";
-    // var TOOLTIP_CLASS = "uif-tooltip";
-    // var VALIDATION_IMAGE_CLASS = "uif-validationImage";
-    // var VALIDATION_MESSAGES = "validationMessages";
-    // var VALIDATION_MESSAGES_CLASS = "uif-validationMessagesList";
-    // var VALIDATION_PAGE_HEADER_CLASS = "uif-pageValidationHeader";
-    // var VALIDATION_SETUP_EVENT = "validationSetup";
-    // var VIEW_CONTENT_HEADER_CLASS = "Uif-ViewContentWrapper";
-    // var VIEW_STATE = ""ViewState";
-    // var WARNING_HIGHLIGHT_SECTION_CLASS = "uif-warningHighlight-section";
-    // var WARNING_MESSAGE_ITEM_CLASS = "uif-warningMessageItem-field";
+     APPLICATION_CLASS  : "Uif-Application",
+     APP_ID  : "Uif-Application",
+     CHANGED_HEADER_ICON_CLASS : "uif-changedHeaderIcon",
+     CLIENT_MESSAGE_ITEMS_CLASS : "uif-clientMessageItems",
+     CLIENT_WARNING_DIV_CLASS : "uif-clientWarningDiv",
+     COLLAPSED_ERRORS_CLASS : "uif-collapsedErrors",
+     COLLAPSED_INFO_CLASS : "uif-collapsedInfo",
+     COLLAPSED_WARNINGS_CLASS : "uif-collapsedWarnings",
+     CONTROL_CLASS : "Uif-Application",
+     DIRTY_CLASS : "dirty",
+     ERROR_HIGHLIGHT_SECTION_CLASS : "uif-errorHighlight-section",
+     ERROR_MESSAGE_ITEM_CLASS : "uif-errorMessageItem-field",
+     FIELD_CLASS : "uif-field",
+     HAS_ERROR_CLASS : "uif-hasError",
+     HAS_INFO_CLASS : "uif-hasInfo",
+     HAS_MODIFIED_ERROR_CLASS : "uif-hasError-modified",
+     HAS_WARNING_CLASS : "uif-hasWarning",
+     HEADER_TEXT_CLASS : "uif-headerText",
+     IMAGE_LOCATION : "kradImageLocation",
+     INCIDENT_REPORT_VIEW_CLASS : "Uif-IncidentReportView",
+     INFO_HIGHLIGHT_SECTION_CLASS : "uif-infoHighlight-section",
+     INFO_MESSAGE_ITEM_CLASS : "uif-infoMessageItem-field",
+     INPUT_FIELD_CLASS : "uif-inputField",
+     INPUT_FIELD_SELECTOR : "[data-role:'InputField']",
+     MESSAGE_COUNT_CLASS : "uif-messageCount",
+     NAVIGATION_MENU_CLASS : "uif-navigationMenu",
+     PAGE_CONTENT_HEADER_CLASS : "Uif-PageContentWrapper",
+     PAGE_VALIDATION_HEADER_CLASS : "uif-pageValidationHeader",
+     PAGE_VALIDATION_MESSAGE_ERROR_CLASS : "uif-pageValidationMessages-error",
+     PAGE_VALIDATION_MESSAGE_INFO_CLASS : "uif-pageValidationMessages-info",
+     PAGE_VALIDATION_MESSAGE_WARNING_CLASS : "uif-pageValidationMessages-warning",
+     PROGRESSIVE_DISCLOSURE_HIGHLIGHT_CLASS : "uif-progressiveDisclosure-highlight",
+     REQUIRED_MESSAGE_CLASS : "uif-requiredMessage",
+     SERVER_MESSAGE_ITEMS_CLASS : "uif-serverMessageItems",
+     TABLE_COLLECTION_LAYOUT_CLASS : "uif-tableCollectionLayout",
+     TAB_GROUP_CLASS : "Uif-TabGroup",
+     TAB_MENU_CLASS : "uif-tabMenu",
+     TOOLTIP_CLASS : "uif-tooltip",
+     VALIDATION_IMAGE_CLASS : "uif-validationImage",
+     VALIDATION_MESSAGES : "validationMessages",
+     VALIDATION_MESSAGES_CLASS : "uif-validationMessagesList",
+     VALIDATION_PAGE_HEADER_CLASS : "uif-pageValidationHeader",
+     VALIDATION_SETUP_EVENT : "validationSetup",
+     VIEW_CONTENT_HEADER_CLASS : "Uif-ViewContentWrapper",
+     VIEW_STATE : "ViewState",
+     WARNING_HIGHLIGHT_SECTION_CLASS : "uif-warningHighlight-section",
+     WARNING_MESSAGE_ITEM_CLASS : "uif-warningMessageItem-field",
+}
 
-
-})();
+var kradVariables = new KradVariables();

1376e4ff6bc7c5fb91368e7e4d9569412abe8653
Rice-CI User <riceci@kuali.org>
2012-05-12 00:09:35 +0000

    Automated license header update

--- scripts/upgrades/2.0.0 to 2.1/db-updates/2012-05-11.sql
+++ scripts/upgrades/2.0.0 to 2.1/db-updates/2012-05-11.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2012 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 insert into krim_perm_t
 (perm_id, perm_tmpl_id, nmspc_cd, nm, desc_txt, actv_ind, ver_nbr, obj_id)
 values ((select (max(to_number(perm_id)) + 1) from krim_perm_t where perm_id is not NULL and to_number(perm_id) < 10000),

a06f5c65a2cf21936e8382d9e80511f2bb4a08cc
Samuel Holtzkampf <samuel@opencollab.co.za>
2012-05-16 13:21:18 +0000

    KULRICE-6647 : Javadoc

--- krad/krad-web-framework/src/main/java/org/kuali/rice/krad/datadictionary/WorkflowPropertyGroup.java
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/krad/datadictionary/WorkflowPropertyGroup.java
@@ -23,58 +23,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This object allows for grouping of related {@link WorkflowProperty} objects.  It defines a base path to which all {@link WorkflowProperty} are
- * relative. See {@link #getBasePath()} for a explanation of the meaning of the base path
- * 
- *                 This element is used to define a set of workflowProperty tags, which are used to
-                specify which document properties should be serialized during the document serialization
-                process.  This element allows for all the nested workflowProperty tags to be relative
-                to some base path.  This base path itself is relative to the object being serialized
-                during the document serialization process (which is not necessarily the document itself,
-                but a wrapper around the document).
-                
-                If blank/missing, the base path will be assumed to be the property path to the document
+ * Allows for grouping of related {@link WorkflowProperty} objects
+ *
+ * <p>
+ * This element is used to define a set of workflowProperty tags, which are used to
+ * specify which document properties should be serialized during the document serialization
+ * process.  This element allows for all the nested workflowProperty tags to be relative
+ * to some base path.  This base path itself is relative to the object being serialized
+ * during the document serialization process (which is not necessarily the document itself,
+ * but a wrapper around the document).If blank/missing, the base path will be assumed to be
+ * the property path to the document.
+ * </p>
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public class WorkflowPropertyGroup implements Serializable {
     private static final long serialVersionUID = 1L;
 
     protected String basePath = KRADConstants.EMPTY_STRING;
     protected List<WorkflowProperty> workflowProperties = new ArrayList<WorkflowProperty>();
-        
+
     /**
-     * Returns the list of added {@link WorkflowProperty} objects.
-     * 
-     * @return list of {@link WorkflowProperty} objects.
+     * The list of {@link WorkflowProperty} objects
+     *
+     * @return List<WorkflowProperty>
      */
     public List<WorkflowProperty> getWorkflowProperties() {
         return workflowProperties;
     }
 
     /**
-     * Returns the base path of the group, which represents the path that all {@link WorkflowProperty} objects are relative to.  The base path
-     * itself should be relative from the object being serialized, which may not necessarily be the document, see {@link Document#wrapDocumentWithMetadataForXmlSerialization()}
-     * and {@link Document#getBasePathToDocumentDuringSerialization()}
-     * 
-     * @return the base path
+     * The base path of the group which all {@link WorkflowProperty} objects are relative to
+     *
+     * <p>
+     * The base path itself should be relative from the object being serialized, which may not necessarily be the
+     * document, see {@link Document#wrapDocumentWithMetadataForXmlSerialization()}
+     * and {@link Document#getBasePathToDocumentDuringSerialization()}. If blank/missing, the base path will be
+     * assumed to be the property path to the document.
+     * </p>
+     *
+     * @return String
      */
     public String getBasePath() {
         return this.basePath;
     }
 
     /**
-     * This element allows for all the nested workflowProperty tags to be relative
-                to some base path.  This base path itself is relative to the object being serialized
-                during the document serialization process (which is not necessarily the document itself,
-                but a wrapper around the document).
-                
-                If blank/missing, the base path will be assumed to be the property path to the document
-     * 
-     * @param basePath see description of {@link #getBasePath()}
+     * Setter for the base path
+     *
+     * @param basePath - the base path
      */
     public void setBasePath(String basePath) {
         this.basePath = basePath;
     }
 
+    /**
+     * Setter for workflow properties list
+     *
+     * @param workflowProperties - the list of workflow properties
+     */
     public void setWorkflowProperties(List<WorkflowProperty> workflowProperties) {
         this.workflowProperties = workflowProperties;
     }

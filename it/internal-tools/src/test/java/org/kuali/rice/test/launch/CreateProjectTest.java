eac1f2aa4be02749c0d6c64bcc55161219c34ed3
Sona Sona <sonam@kuali.org>
2012-05-17 23:30:13 +0000

    KULRICE-7099 Integration test for createproject.groovy- Made changes based on review

--- it/internal-tools/src/test/java/org/kuali/rice/test/launch/CreateProjectTest.java
+++ it/internal-tools/src/test/java/org/kuali/rice/test/launch/CreateProjectTest.java
@@ -4,6 +4,7 @@ import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -24,40 +25,27 @@ public class CreateProjectTest  {
     private static final Logger log = Logger.getLogger(CreateProjectTest.class);
 
     private static final String INTERNAL_TOOL_MODULE_NAME = "internal-tools";
+
+    //The directory where the rice code has been checked out (referred to as RICE_DIR in the createproject.groovy script)
     private String rdir;
+
+     /**
+     * maven will set this property and find resources from the config based on it. This makes eclipse testing work because
+     * we have to put the basedir in our config files in order to find things when testing from maven
+     */
+   @Before
+    public  void setBaseDirSystemProperty() {
+        if (System.getProperty("basedir") == null) {
+        	final String userDir = System.getProperty("user.dir");
+
+            System.setProperty("basedir", userDir + ((userDir.endsWith(File.separator + "it" + File.separator + INTERNAL_TOOL_MODULE_NAME)) ? "" : File.separator + "it" + File.separator + INTERNAL_TOOL_MODULE_NAME));
+        }
+       System.out.println("BASEDIR :"+System.getProperty("basedir"));
+    }
 
     @Test
     public void testCreateProjectForSampleApp() throws IOException {
-        setBaseDirSystemProperty(INTERNAL_TOOL_MODULE_NAME);
-
-        File file = new File(getBaseDir());
-        String parentDir1 = file.getParent();
-
-        file = new File(parentDir1);
-        String parentDir2 = file.getParent();
-
-        rdir =  parentDir2;
-
-        ClassLoader parent = new CreateProjectTest().getClass().getClassLoader();
-        GroovyClassLoader loader;
-        loader = new GroovyClassLoader(parent);
-        Class groovyClass = null;
-        try {
-            groovyClass = loader.parseClass(new File(rdir + "/scripts/createproject.groovy"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-       GroovyObject groovyObject = null;
-        try {
-            groovyObject = (GroovyObject) groovyClass.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        GroovyObject groovyObject = getGroovyObject();
         Object[] args = {"-name", "MyRiceSampleApp","-pdir",getBaseDir()+"/target","-rdir",rdir,"-sampleapp","-testmode"};
         groovyObject.invokeMethod("main", args);
 
@@ -65,36 +53,7 @@ public class CreateProjectTest  {
 
     @Test
     public void testCreateProjectForSampleAppNoProjectDirectory() throws IOException {
-        setBaseDirSystemProperty(INTERNAL_TOOL_MODULE_NAME);
-
-        File file = new File(getBaseDir());
-        String parentDir1 = file.getParent();
-
-        file = new File(parentDir1);
-        String parentDir2 = file.getParent();
-
-        rdir =  parentDir2;
-
-        ClassLoader parent = new CreateProjectTest().getClass().getClassLoader();
-        GroovyClassLoader loader;
-        loader = new GroovyClassLoader(parent);
-        Class groovyClass = null;
-        try {
-            groovyClass = loader.parseClass(new File(rdir + "/scripts/createproject.groovy"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-       GroovyObject groovyObject = null;
-        try {
-            groovyObject = (GroovyObject) groovyClass.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
+        GroovyObject groovyObject = getGroovyObject();
         Object[] args = {"-name", "MyRiceSampleApp","-rdir",rdir,"-sampleapp","-testmode"};
         groovyObject.invokeMethod("main", args);
 
@@ -102,35 +61,7 @@ public class CreateProjectTest  {
 
     @Test
     public void testCreateProjectForStandaloneAppProjectDirectory() throws IOException {
-        setBaseDirSystemProperty(INTERNAL_TOOL_MODULE_NAME);
-
-        File file = new File(getBaseDir());
-        String parentDir1 = file.getParent();
-
-        file = new File(parentDir1);
-        String parentDir2 = file.getParent();
-
-        rdir =  parentDir2;
-
-        ClassLoader parent = new CreateProjectTest().getClass().getClassLoader();
-        GroovyClassLoader loader;
-        loader = new GroovyClassLoader(parent);
-        Class groovyClass = null;
-        try {
-            groovyClass = loader.parseClass(new File(rdir + "/scripts/createproject.groovy"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-       GroovyObject groovyObject = null;
-        try {
-            groovyObject = (GroovyObject) groovyClass.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        GroovyObject groovyObject = getGroovyObject();
 
         Object[] args = {"-name", "MyRiceStandaloneApp","-rdir",rdir,"-standalone","-testmode"};
         groovyObject.invokeMethod("main", args);
@@ -139,54 +70,48 @@ public class CreateProjectTest  {
 
     @Test
     public void testCreateProjectForStandalone() throws IOException {
-            setBaseDirSystemProperty(INTERNAL_TOOL_MODULE_NAME);
+     GroovyObject groovyObject = getGroovyObject();
+     Object[] args = {"-name", "MyRiceStandaloneApp","-pdir",getBaseDir()+"/target","-rdir",rdir,"-standalone","-testmode"};
+     groovyObject.invokeMethod("main", args);
 
-            File file = new File(getBaseDir());
-            String parentDir1 = file.getParent();
-
-            file = new File(parentDir1);
-            String parentDir2 = file.getParent();
-
-            rdir =  parentDir2;
-
-            ClassLoader parent = new CreateProjectTest().getClass().getClassLoader();
-            GroovyClassLoader loader;
-            loader = new GroovyClassLoader(parent);
-            Class groovyClass = null;
-            try {
-                groovyClass = loader.parseClass(new File(rdir + "/scripts/createproject.groovy"));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-           GroovyObject groovyObject = null;
-            try {
-                groovyObject = (GroovyObject) groovyClass.newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-
-            Object[] args = {"-name", "MyRiceStandaloneApp","-pdir",getBaseDir()+"/target","-rdir",rdir,"-standalone","-testmode"};
-            groovyObject.invokeMethod("main", args);
-
-        }
-
-   /**
-     * maven will set this property and find resources from the config based on it. This makes eclipse testing work because
-     * we have to put the basedir in our config files in order to find things when testing from maven
-     */
-    protected void setBaseDirSystemProperty(String moduleBaseDir) {
-        if (System.getProperty("basedir") == null) {
-        	final String userDir = System.getProperty("user.dir");
-
-            System.setProperty("basedir", userDir + ((userDir.endsWith(File.separator + "it" + File.separator + moduleBaseDir)) ? "" : File.separator + "it" + File.separator + moduleBaseDir));
-        }
-    }
+     }
 
     protected String getBaseDir() {
         return System.getProperty("basedir");
+    }
+    /**
+     * Helper method to create the groovy object instance so that it can be used to call the script
+     */
+    protected GroovyObject getGroovyObject(){
+        //Get the checkout location of rice from the working directory
+        File file = new File(getBaseDir());
+        String parentDir1 = file.getParent();
+
+        file = new File(parentDir1);
+        String parentDir2 = file.getParent();
+
+        rdir =  parentDir2;
+        // Create the GroovyClassLoader which is used to load Groovy classes using the current class loader as the parent
+        ClassLoader parent = new CreateProjectTest().getClass().getClassLoader();
+        GroovyClassLoader loader;
+        loader = new GroovyClassLoader(parent);
+        Class groovyClass = null;
+        try {
+            // Parse the script into a Java class capable of being run
+            groovyClass =  loader.parseClass(new File(rdir + "/scripts/createproject.groovy"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       GroovyObject groovyObject = null;
+        try {
+            groovyObject = (GroovyObject) groovyClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+       return groovyObject;
     }
 }

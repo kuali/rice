d2c2b3582b9f38c4a473c5d73efc03676085461b
Jeff Ruch <jeff.ruch@colostate.edu>
2012-05-17 17:53:06 +0000

    KULRICE-7252 Component name criteria not working correctly in parameter search.

--- core-service/web/src/main/java/org/kuali/rice/coreservice/web/parameter/ParameterLookupableHelperServiceImpl.java
+++ core-service/web/src/main/java/org/kuali/rice/coreservice/web/parameter/ParameterLookupableHelperServiceImpl.java
@@ -44,14 +44,13 @@ public class ParameterLookupableHelperServiceImpl extends KualiLookupableHelperS
 
     private static final Log LOG = LogFactory.getLog(ParameterLookupableHelperServiceImpl.class);
     private static final String COMPONENT_NAME = "component.name";
-    private static final String DERIVED_COMPONENT_NAME = "derivedComponent.name";
-    private static final String NAMESPACE_CODE = "namespaceCode";
+    private static final String COMPONENT_CODE = "componentCode";
 
     @Override
     protected boolean allowsMaintenanceEditAction(BusinessObject businessObject) {
-    	
+
         ParameterBo parm = (ParameterBo)businessObject;
-        
+
         Map<String, String> permissionDetails = new HashMap<String, String>();
         permissionDetails.put(KimConstants.AttributeConstants.NAMESPACE_CODE, parm.getNamespaceCode());
         permissionDetails.put(KimConstants.AttributeConstants.COMPONENT_NAME, parm.getComponentCode());
@@ -61,36 +60,29 @@ public class ParameterLookupableHelperServiceImpl extends KualiLookupableHelperS
                 KimConstants.PermissionTemplateNames.MAINTAIN_SYSTEM_PARAMETER, permissionDetails,
                 Collections.<String, String>emptyMap());
     }
-    
+
     @Override
     public List<? extends BusinessObject> getSearchResults(java.util.Map<String, String> fieldValues) {
 
-        List<ParameterBo> parametersWithDerivedComponents = null;
-
-        if (fieldValues.containsKey(COMPONENT_NAME) && StringUtils.isNotBlank(fieldValues.get(COMPONENT_NAME))) {
-            // also search based on derived component name
-            Map<String, String> derivedComponentFieldValues = new HashMap<String, String>(fieldValues);
-            String componentName = derivedComponentFieldValues.remove(COMPONENT_NAME);
-            derivedComponentFieldValues.put(DERIVED_COMPONENT_NAME, componentName);
-            parametersWithDerivedComponents = (List<ParameterBo>)super.getSearchResultsUnbounded(derivedComponentFieldValues);
+        if (fieldValues.containsKey(COMPONENT_NAME) && StringUtils.isNotBlank(fieldValues.get(COMPONENT_NAME)) && fieldValues.containsKey(COMPONENT_NAME)) {
+            //remove parameter code if parameter name exists.
+            fieldValues.remove(COMPONENT_CODE);
+            fieldValues.put(COMPONENT_CODE,"");
         }
 
         List<ParameterBo> results = (List<ParameterBo>)super.getSearchResultsUnbounded(fieldValues);
-        if (parametersWithDerivedComponents != null) {
-            results.addAll(parametersWithDerivedComponents);
-        }
         normalizeParameterComponents(results);
         return results;
     }
 
-	private void normalizeParameterComponents(List<ParameterBo> parameters) {
-		// attach the derived components where needed
+    private void normalizeParameterComponents(List<ParameterBo> parameters) {
+        // attach the derived components where needed
         for (ParameterBo parameterBo : parameters) {
             if (parameterBo.getComponent() == null) {
                 parameterBo.setComponent(DerivedComponentBo.toComponentBo(parameterBo.getDerivedComponent()));
             }
         }
-	}
+    }
 
 }
 

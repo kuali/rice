cabadbe5674cf2560e9ba5602bdcd6e0d014be7a
Viral Chauhan <vchauhan@vivantech.com>
2012-06-04 10:46:02 +0000

    KULRICE-7190 : Updated selenium test Create Routing Rules Delegation Screen [Added testMethod "testCreateNewRRDTravelRequestDestRouting()"]

--- sampleapp/src/it/java/edu/samplu/mainmenu/test/WorkFlowRouteRulesDelegationIT.java
+++ sampleapp/src/it/java/edu/samplu/mainmenu/test/WorkFlowRouteRulesDelegationIT.java
@@ -85,6 +85,37 @@ public class WorkFlowRouteRulesDelegationIT {
               
     }
     
+    @Test
+    public void testCreateNewRRDTravelRequestDestRouting() throws Exception {
+        selenium.open(System.getProperty("remote.public.url"));
+        selenium.type("name=__login_user", "admin");
+        selenium.click("css=input[type=\"submit\"]");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("link=Routing Rules Delegation");
+        selenium.waitForPageToLoad("30000");
+        selenium.selectFrame("iframeportlet");
+        selenium.click("css=img[alt=\"create new\"]");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("name=methodToCall.performLookup.(!!org.kuali.rice.kew.rule.RuleBaseValues!!).(((id:parentRuleId))).((``)).((<>)).(([])).((**)).((^^)).((&&)).((//)).((~~)).(::::;;::::).anchor");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("css=td.infoline > input[name=\"methodToCall.search\"]");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("css=a[title=\"return valueRule Id=1046 \"]");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("name=parentResponsibilityId");
+        selenium.click("name=methodToCall.createDelegateRule");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("name=methodToCall.cancel");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("name=methodToCall.processAnswer.button0");
+        selenium.waitForPageToLoad("30000");
+        selenium.selectWindow("null");
+        selenium.click("xpath=(//input[@name='imageField'])[2]");
+        selenium.waitForPageToLoad("30000");
+        
+              
+    }
+    
     @After
     public void tearDown() throws Exception {
         selenium.stop();

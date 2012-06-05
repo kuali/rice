9e6b6ffe83540b2bd8c3d0508cc49ec8ee078a14
Viral Chauhan <vchauhan@vivantech.com>
2012-06-05 15:58:06 +0000

    KULRICE-7152 KULRICE-7149 : Added Selenium Test for Attribute Definition Lookup/Create New Agenda

---
+++ sampleapp/src/it/java/edu/samplu/mainmenu/test/AttributeDefinitionLookUpIT.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.samplu.mainmenu.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+
+
+/**
+ * tests whether the Attribute Definition Look UP is working ok 
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class AttributeDefinitionLookUpIT {
+
+    private Selenium selenium;
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", System.getProperty("remote.public.url"));
+        selenium.start();
+    }
+    
+    @Test
+    public void testAttributeDefinitionLookUp() throws Exception {
+        selenium.open(System.getProperty("remote.public.url"));
+        selenium.type("name=__login_user", "admin");
+        selenium.click("css=input[type=\"submit\"]");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("link=Attribute Definition Lookup");
+        selenium.waitForPageToLoad("30000");
+        selenium.selectFrame("iframeportlet");
+        selenium.click("id=u80");
+        selenium.waitForPageToLoad("30000");
+        selenium.click("id=u86");
+        selenium.waitForPageToLoad("30000");
+        selenium.selectWindow("null");
+        selenium.click("xpath=(//input[@name='imageField'])[2]");
+        selenium.waitForPageToLoad("30000");
+        
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+
+}

e1e25384c1bd2dbf210424691a7531604a3836f3
Jeff Ruch <jeff.ruch@colostate.edu>
2012-07-20 22:26:21 +0000

    KULRICE-7310 can't change only month or year with picker (jquery limitation)

--- web/src/main/webapp/krad/scripts/krad.widget.js
+++ web/src/main/webapp/krad/scripts/krad.widget.js
@@ -400,6 +400,13 @@ function createDatePicker(controlId, options) {
                     jQuery("#" + fieldId).data(kradVariables.VALIDATION_MESSAGES).messagingEnabled = false;
                 });
 
+        //KULRICE-7310 can't change only month or year with picker (jquery limitation)
+        jQuery("#" + controlId).datepicker('option', 'onChangeMonthYear',
+                function(y, m, i){
+                    var d = i.selectedDay;
+                    jQuery(this).datepicker('setDate', new Date(y, m - 1, d));
+                });
+
         //KULRICE-7261 fix date format passed back.  jquery expecting mm-dd-yy
         if(options.dateFormat=="mm-dd-yy" && jQuery("#" + controlId)[0].getAttribute("value").indexOf("/") != -1  )    {
             jQuery("#" + controlId).datepicker('setDate', new Date(jQuery("#" + controlId)[0].getAttribute("value")));

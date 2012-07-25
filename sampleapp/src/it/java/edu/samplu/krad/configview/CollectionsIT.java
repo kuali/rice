da4f54be294888d4a9a661fcd9652473bb1b88db
Erik Meade <github-kuali-m@eghm.net>
2012-07-25 06:50:31 +0000

    KULRICE-7692 : Selenium Test for Kitchen Sink Collections - Add column test of Default Tests Table Layout

--- sampleapp/src/it/java/edu/samplu/krad/configview/CollectionsIT.java
+++ sampleapp/src/it/java/edu/samplu/krad/configview/CollectionsIT.java
@@ -17,7 +17,7 @@
 package edu.samplu.krad.configview;
 
 import edu.samplu.common.UpgradedSeleniumITBase;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -30,6 +30,38 @@ public class CollectionsIT extends UpgradedSeleniumITBase {
     @Override
     public String getTestUrl() {
         return "/kr-krad/uicomponents?viewId=ConfigurationTestView-Collections&methodToCall=start";
+    }
+
+    /**
+     * Test adding a column of values to the Default Tests Table Layout
+     */
+    @Test
+    public void testDefaultTestsTableLayout() {
+        Assert.assertTrue(selenium.isTextPresent("Default Tests"));
+        Assert.assertTrue(selenium.isTextPresent("Table Layout"));
+        Assert.assertTrue(selenium.isTextPresent("* Field 1"));
+        Assert.assertTrue(selenium.isTextPresent("* Field 2"));
+        Assert.assertTrue(selenium.isTextPresent("* Field 3"));
+        Assert.assertTrue(selenium.isTextPresent("* Field 4"));
+        Assert.assertTrue(selenium.isTextPresent("Actions"));
+
+        selenium.type("name=newCollectionLines['list1'].field1", "asdf1");
+        selenium.type("name=newCollectionLines['list1'].field2", "asdf2");
+        selenium.type("name=newCollectionLines['list1'].field3", "asdf3");
+        selenium.type("name=newCollectionLines['list1'].field4", "asdf4");
+        selenium.click("//button[contains(.,'add')]"); // the first button is the one we want
+        selenium.waitForCondition("selenium.browserbot.getCurrentWindow().document.getElementsByName(\"newCollectionLines['list1'].field1\")[0].value == ''", "3000");
+
+        Assert.assertEquals("", selenium.getValue("name=newCollectionLines['list1'].field1"));
+        Assert.assertEquals("", selenium.getValue("name=newCollectionLines['list1'].field2"));
+        Assert.assertEquals("", selenium.getValue("name=newCollectionLines['list1'].field3"));
+        Assert.assertEquals("", selenium.getValue("name=newCollectionLines['list1'].field4"));
+        Assert.assertEquals("asdf1", selenium.getValue("name=list1[0].field1"));
+        Assert.assertEquals("asdf2", selenium.getValue("name=list1[0].field2"));
+        Assert.assertEquals("asdf3", selenium.getValue("name=list1[0].field3"));
+        Assert.assertEquals("asdf4", selenium.getValue("name=list1[0].field4"));
+
+        // how to figure out which delete button for the one we just added?
     }
 
     /**

544c83ee63ebafb3ac332a629f29b46204374b65
Martin Taylor <mztaylor@umd.edu>
2012-08-08 17:46:15 +0000

    KULRICE-7777: Adding deprecated package/class to point at new package/class

---
+++ impl/src/main/java/org/kuali/rice/kim/workflow/attribute/KimTypeQualifierResolver.java
@@ -0,0 +1,12 @@
+package org.kuali.rice.kim.workflow.attribute;
+
+/**
+ * Deprecated version of KualiTypeQualifierResolver for documents containing legacy path
+ *
+ * @deprecated
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ *
+ */
+public class KimTypeQualifierResolver extends org.kuali.rice.kim.impl.type.KimTypeQualifierResolver {
+
+}

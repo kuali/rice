a9522b477e8328977f9ff1f4e00025d147ece0c4
Sona Sona <sonam@kuali.org>
2012-08-15 18:36:20 +0000

    KURICE-7465 Support for Ebos

--- it/core/src/test/groovy/org/kuali/rice/core/impl/criteria/org/kuali/rice/coreservice/impl/service/CoreServiceModuleServiceIntTest.java
+++ it/core/src/test/groovy/org/kuali/rice/core/impl/criteria/org/kuali/rice/coreservice/impl/service/CoreServiceModuleServiceIntTest.java
@@ -20,13 +20,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
 import org.kuali.rice.core.test.CORETestCase;
+import org.kuali.rice.coreservice.impl.component.ComponentBo;
 import org.kuali.rice.coreservice.impl.namespace.NamespaceBo;
+import org.kuali.rice.coreservice.impl.parameter.ParameterBo;
+import org.kuali.rice.coreservice.impl.parameter.ParameterTypeBo;
 import org.kuali.rice.krad.service.KualiModuleService;
 import org.kuali.rice.krad.service.ModuleService;
 import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -36,7 +41,10 @@ import static org.junit.Assert.*;
 public class CoreServiceModuleServiceIntTest extends CORETestCase {
 
     private static final String KUALI_NAMESPACE_CODE = "KUALI";
-
+    private static final String NAMESPACE_CODE = "KR-WKFLW";
+    private static final String COMPONENT_CODE = "ActionList";
+    private static final String NAME = "ACTION_LIST_DOCUMENT_POPUP_IND";
+    private static final String APPLICATION_ID = "KUALI";
     private ModuleService coreServiceModuleService;
 
     @Before
@@ -83,4 +91,66 @@ public class CoreServiceModuleServiceIntTest extends CORETestCase {
         assertTrue("no namespaces should have been returned", CollectionUtils.isEmpty(namespaces));
     }
 
+    @Test
+    public void testGetParameterEbo() {
+        assertNotNull("coreServiceModuleService wasn't successfully configured", coreServiceModuleService);
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        fieldValues.put("namespaceCode",NAMESPACE_CODE);
+        fieldValues.put("componentCode",COMPONENT_CODE);
+        fieldValues.put("name",NAME);
+        fieldValues.put("applicationId",APPLICATION_ID);
+        ParameterBo parameter = coreServiceModuleService.getExternalizableBusinessObject(ParameterBo.class,fieldValues);
+
+        assertNotNull("parameter wasn't successfully loaded", parameter);
+        assertEquals("parameter doesn't have the requested name",
+                parameter.getName(),NAME);
+
+        List<ParameterBo> parameters = coreServiceModuleService.getExternalizableBusinessObjectsList(ParameterBo.class,
+                Collections.<String,Object>emptyMap());
+
+        assertFalse("parameters weren't successfully retrieved", CollectionUtils.isEmpty(parameters));
+        assertTrue(parameters.size() > 1);
+        assertTrue(parameters.contains(parameter));
+
+        parameters = coreServiceModuleService.getExternalizableBusinessObjectsList(ParameterBo.class,
+                Collections.<String,Object>singletonMap("name", NAME));
+
+        assertFalse("parameters weren't successfully retrieved", CollectionUtils.isEmpty(parameters));
+        assertTrue(parameters.size() == 1);
+        assertTrue(parameters.contains(parameter));
+
+        parameters = coreServiceModuleService.getExternalizableBusinessObjectsList(ParameterBo.class,
+                Collections.<String,Object>singletonMap("name", "Kwisatz Haderach"));
+
+        assertTrue("no parameters should have been returned", CollectionUtils.isEmpty(parameters));
+    }
+
+    @Test
+    public void testGetParameterTypeEbo() {
+        assertNotNull("coreServiceModuleService wasn't successfully configured", coreServiceModuleService);
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        fieldValues.put("code","CONFG");
+        ParameterTypeBo parameterType = coreServiceModuleService.getExternalizableBusinessObject(ParameterTypeBo.class,fieldValues);
+
+        assertNotNull("parameterType wasn't successfully loaded", parameterType);
+        assertEquals("parameterType doesn't have the requested code",
+                parameterType.getCode(),"CONFG");
+
+
+    }
+
+    @Test
+    public void testGetComponentEbo() {
+        assertNotNull("coreServiceModuleService wasn't successfully configured", coreServiceModuleService);
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        fieldValues.put("namespaceCode","KR-WKFLW");
+        fieldValues.put("code",COMPONENT_CODE);
+        ComponentBo component = coreServiceModuleService.getExternalizableBusinessObject(ComponentBo.class,fieldValues);
+
+        assertNotNull("component wasn't successfully loaded", component);
+        assertEquals("component doesn't have the requested name",
+                component.getCode(),COMPONENT_CODE);
+
+
+    }
 }

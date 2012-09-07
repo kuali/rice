0901ba6ef577db2e48bce55a4464ece0d875ca36
Peter Giles <g1l3sp@gmail.com>
2012-09-07 22:19:28 +0000

    KULRICE-8127: Do weekly merges (2012-09-07)
    * merging rice-2.1-kfs into rice-2.1

---
+++ kns/src/main/java/org/kuali/rice/kns/rules/DocumentRuleBase.java
@@ -0,0 +1,21 @@
+package org.kuali.rice.kns.rules;
+
+import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.kuali.rice.krad.service.DataDictionaryService;
+import org.kuali.rice.krad.service.DictionaryValidationService;
+
+/**
+ * KNS version of DocumentRuleBase
+ */
+public class DocumentRuleBase extends org.kuali.rice.krad.rules.DocumentRuleBase {
+
+    protected DictionaryValidationService getDictionaryValidationService() {
+        return LazyServicesHolder.dictionaryValidationService;
+    }
+
+    // Lazy init holder class, see Effective Java #71
+    private static class LazyServicesHolder {
+        static final DictionaryValidationService dictionaryValidationService =
+                KNSServiceLocator.getKNSDictionaryValidationService();
+    }
+}
--- krad/krad-web-framework/src/main/java/org/kuali/rice/krad/datadictionary/AttributeDefinition.java
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/krad/datadictionary/AttributeDefinition.java
@@ -398,8 +398,20 @@ public class AttributeDefinition extends AttributeDefinitionBase implements Case
 		return this.attributeSecurity;
 	}
 
-	/**
-	 * @param attributeSecurity
+    /**
+    * This overridden method applies validCharacterConstraint if legacy validation pattern in place
+    *
+    * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+    */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (StringUtils.isEmpty(name)) {
+            throw new RuntimeException("blank name for bean: " + id);
+        }
+    }
+
+    /**
+     * @param attributeSecurity
 	 *            the attributeSecurity to set
 	 */
 	public void setAttributeSecurity(AttributeSecurity attributeSecurity) {
@@ -408,21 +420,6 @@ public class AttributeDefinition extends AttributeDefinitionBase implements Case
 
 	public boolean hasAttributeSecurity() {
 		return (attributeSecurity != null);
-	}
-
-	/**
-	 * This overridden method applies validCharacterConstraint if legacy validation pattern in place
-	 * 
-	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		if (StringUtils.isEmpty(name)) {
-			throw new RuntimeException("blank name for bean: " + id);
-		}
-        if(this.validCharactersConstraint == null  && validationPattern != null) {
-            this.validCharactersConstraint = validationPattern.asValidCharactersConstraint();
-        }
 	}
 
 	/**

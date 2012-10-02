8861dd5cd3e49ab59ff0d7818410e0e567781afb
Shannon Hess <ShannonHess18@gmail.com>
2012-10-02 16:23:20 +0000

    KULRICE-8355 - Create a new service bus page which displays the cached service endpoints ; give ability to refresh them if needed

---
+++ ksb/web/src/main/java/org/kuali/rice/ksb/messaging/web/ServiceBusAction.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2005-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.ksb.messaging.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessages;
+import org.kuali.rice.core.api.config.CoreConfigHelper;
+import org.kuali.rice.core.api.config.property.ConfigContext;
+import org.kuali.rice.core.api.util.RiceUtilities;
+import org.kuali.rice.ksb.api.KsbApiServiceLocator;
+import org.kuali.rice.ksb.api.bus.Endpoint;
+import org.kuali.rice.ksb.api.bus.ServiceBus;
+import org.kuali.rice.ksb.api.bus.ServiceConfiguration;
+
+
+/**
+ * Struts action for interacting with the queue of messages.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class ServiceBusAction extends KSBAction {
+
+	public ActionForward start(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		return mapping.findForward("basic");
+	}
+
+	public ActionForward refreshServiceBus(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		KsbApiServiceLocator.getServiceBus().synchronize();
+		return mapping.findForward("basic");
+	}
+
+	public ActionMessages establishRequiredState(HttpServletRequest request, ActionForm actionForm) throws Exception {
+		ServiceBusForm form = (ServiceBusForm)actionForm;
+		form.setMyIpAddress(RiceUtilities.getIpNumber());
+		form.setMyApplicationId(CoreConfigHelper.getApplicationId());
+		form.setDevMode(ConfigContext.getCurrentContextConfig().getDevMode());
+		ServiceBus serviceBus = KsbApiServiceLocator.getServiceBus();
+		form.setMyInstanceId(serviceBus.getInstanceId());
+		form.setPublishedServices(getPublishedServices(serviceBus));
+		form.setGlobalServices(getGlobalServices(serviceBus));
+
+		return null;
+	}
+
+	private List<ServiceConfiguration> getPublishedServices(ServiceBus serviceBus) {
+		Map<QName, Endpoint> localEndpoints = serviceBus.getLocalEndpoints();
+		List<ServiceConfiguration> publishedServices = new ArrayList<ServiceConfiguration>();
+		for (Endpoint endpoint : localEndpoints.values()) {
+			publishedServices.add(endpoint.getServiceConfiguration());
+		}
+		return publishedServices;
+	}
+
+	private List<ServiceConfiguration> getGlobalServices(ServiceBus serviceBus) {
+		List<ServiceConfiguration> allServices = new ArrayList<ServiceConfiguration>();
+		for(Endpoint endpoint : serviceBus.getAllEndpoints()) {
+			allServices.add(endpoint.getServiceConfiguration());
+		}
+		return allServices;
+	}
+
+}

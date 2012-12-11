b62921ef2290c4e0522654cf650a2ab4f912a56d
Shannon Hess <ShannonHess18@gmail.com>
2012-12-11 22:47:29 +0000

    KULRICE-8648 - Update maintenance document XML update converters

--- development-tools/src/main/java/org/kuali/rice/krad/maintainablexml/ConvertMaintainableXML.java
+++ development-tools/src/main/java/org/kuali/rice/krad/maintainablexml/ConvertMaintainableXML.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -39,25 +40,44 @@ import org.w3c.dom.NodeList;
  *   <li>Add the conversion rules to the rules xml file -
  *       ..\rice\development-tools\src\main\resources\org\kuali\rice\devtools\krad\maintainablexml\MaintainableXMLUpgradeRules.xml
  *       See comments in the xml file to setup the rules.</li>
- *   <li>Run this class.</li>
+ *   <li>Run this class.  Note that any classes being converted must be on the classpath.</li>
  *   <li>Enter the rice config file location that has the database connection properties. Only enter the location relative
- *       to user.dir/kuali/main.</li>
- *   <li>Select Run mode. Mode 1 will do the xml upgrade and update the krns_maint_doc_t table with the new xml. Cannot
- *       roll back after this has been done. Mode 2 will only print out the old xml and the new xml - this can be used
- *       to test the rules xml setup.</li>
- *   <li>Select the range of document numbers to upgrade. Enter % on from range prompt to upgrade all.</li>
+ *       to user.dir.  NOTE -- If it is uncommented in ConvertMaintainableXML.java, MaintainableXMLConversionConfig.xml
+ *       in the user.dir will be used as the conversion config file.  runmode, fromRange, and toRange can be
+ *       specified in this file to avoid prompting the user for this information.  An example file can be found in
+ *        ..\rice\development-tools\src\main\resources\org\kuali\rice\devtools\krad\maintainablexml\MaintainableXMLConversionConfig.xml
+ *   </li>
+ *   <li>If it is not specified in the config file, the user will be prompted for a run mode. Mode 1 will do the xml
+ *   upgrade and update the krns_maint_doc_t table with the new xml. CANNOT ROLL BACK AFTER THIS HAS BEEN DONE.
+ *   Mode 2 will only print out the old xml and the new xml - this can be used to test the rules xml setup.</li>
+ *   <li>If it is not specified in the config file, the user will be prompted for the document number to upgrade from.
+ *   Enter "all" in the from range prompt to upgrade all documents.  Otherwise this should be an starting document ID
+ *   such as 1000</li>
+ *   <li>If it is not specified in the config file and the fromRange is not "all", the user will be prompted for the document
+ *   number to upgrade to.  If "all" was entered for the fromRange, toRange will be ignored.  toRange should be a
+ *   document ID such as 9999</li>
  * </ol>
  *
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public class ConvertMaintainableXML {
 
-    public static void main(String[] args) {
-        //  prompt the user to enter settings file
-        String settingsFile = readInput("Enter config file location relative to Kuali main user directory "
-                + "(ie. dev/sample-app-config.xml OR dev/trnapp-config.xml) : ", null, false);
+    private static final String MAINTAINABLE_CONVERSION_CONFIG_FILE = "maintainable.conversion.rule.file";
 
-        String filePath = System.getProperty("user.home") + "/kuali/main/" + settingsFile;
+    public static void main(String[] args) {
+
+        // NOTE : If you want the user to be prompted for the config settings file name, Leave this set to ""
+        // Otherwise uncomment out the second line and it will look for the file MaintainableXMLConversionConfig.xml
+
+        String settingsFile = "";
+        //settingsFile =  "MaintainableXMLConversionConfig.xml";
+
+        if (StringUtils.isEmpty(settingsFile)) {
+            settingsFile = readInput("Enter config file location relative to Kuali main user directory "
+                + "(ie. dev/sample-app-config.xml OR MaintainableXMLConversionConfig.xml) : ", null, false);
+        }
+
+        String filePath = System.getProperty("user.home") + "/" + settingsFile;
 
         File file = new File(filePath);
         if (!file.exists()) {
@@ -65,34 +85,55 @@ public class ConvertMaintainableXML {
             System.exit(1);
         }
 
-        String runMode = readInput("Run mode :\n1. Update xml in DB\n2. Print old and new xml\nEnter 1 or 2\n",
-                new String[]{"1", "2"}, false);
-
-        String fromRange = readInput("Please enter document start range value ('%' or '' for all) :\n",
-                new String[]{"%", ""}, true);
-
-        String toRange = null;
-
-        boolean hasRangeParameters = false;
-
-        if (!"".equals(fromRange) && !"%".equals(fromRange)) {
-            toRange = readInput("Please enter end range value :\n", null, true);
-            System.out.println("Upgrading document numbers from " + fromRange + " to " + toRange + "\n");
-            hasRangeParameters = true;
-        }
-
-        System.out.println("Looking up settings file... " + filePath + "\n");
-
         try {
-            HashMap map = getSettings(filePath);
-            System.out.println("Using the following settings : " + map + "\n");
+            HashMap settingsMap = getSettings(filePath);
+            System.out.println("Using the following settings : " + settingsMap + "\n");
+
+            //  RUN MODE
+            String runMode = (String)settingsMap.get("runMode");
+            if (StringUtils.isEmpty(runMode) || !(StringUtils.equals(runMode, "1") || StringUtils.equals(runMode, "2"))) {
+                runMode = readInput("Run mode :\n1. Update xml in DB\n2. Print old and new xml\nEnter 1 or 2\n",
+                    new String[]{"1", "2"}, false);
+            }
+
+            //  FROM RANGE
+            String fromRange = (String)settingsMap.get("fromRange");
+            if (StringUtils.isEmpty(fromRange) || (!StringUtils.equals(fromRange, "all") && !(isNumber(fromRange)))) {
+                fromRange = readInput("Please enter document start range value ('all', empty line for all, or the starting document number such as 1000) :\n",
+                    new String[]{"all", ""}, true);
+            }
+
+            String toRange = null;
+            boolean hasRangeParameters = false;
+
+            if (!"".equals(fromRange) && !"all".equals(fromRange)) {
+                //  TO RANGE, if needed
+                toRange = (String)settingsMap.get("toRange");
+                if (StringUtils.isEmpty(toRange) || !(isNumber(toRange))) {
+                    toRange = readInput("Please enter end range value :\n", null, true);
+                }
+                System.out.println("Upgrading document numbers from " + fromRange + " to " + toRange + "\n");
+                hasRangeParameters = true;
+            }
+
             FileConverter fileConverter = new FileConverter();
-            fileConverter.runFileConversion(map, runMode, fromRange, toRange, hasRangeParameters);
+
+            // RUN THE CONVERSION!
+            fileConverter.runFileConversion(settingsMap, runMode, fromRange, toRange, hasRangeParameters);
+
         } catch (Exception e) {
             System.out.println("Error executing conversion : " + e.getMessage());
             e.printStackTrace();
         }
+    }
 
+    public static boolean isNumber(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -150,8 +191,12 @@ public class ConvertMaintainableXML {
                 Element fstElmnt = (Element) fstNode;
                 String paramName = fstElmnt.getAttribute("name");
                 NodeList textFNList = fstElmnt.getChildNodes();
-                String paramValue = ((Node) textFNList.item(0)).getNodeValue().trim();
-                params.put(paramName, paramValue);
+                if (textFNList.item(0) != null) {
+                    String paramValue = ((Node) textFNList.item(0)).getNodeValue().trim();
+                    params.put(paramName, paramValue);
+                } else {
+                    params.put(paramName, null);
+                }
             }
         }
         return params;

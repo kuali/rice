dcf4623f488dd24783b1ab6ba2d5a611db9b7aaf
Peter Giles <g1l3sp@gmail.com>
2012-12-17 22:09:31 +0000

    KULRICE-8510: Maintenance documents blow up when a property is removed and there are pre-existing documents with that property. * added isEmpty check on docType.getParentId before trying to retrieve the parent doc type.

--- krad/krad-web-framework/src/main/java/org/kuali/rice/krad/maintenance/MaintenanceDocumentBase.java
+++ krad/krad-web-framework/src/main/java/org/kuali/rice/krad/maintenance/MaintenanceDocumentBase.java
@@ -402,7 +402,11 @@ public class MaintenanceDocumentBase extends DocumentBase implements Maintenance
                             break;
                     }
                 }
-                docType = KewApiServiceLocator.getDocumentTypeService().getDocumentTypeById(docType.getParentId());
+                if (!StringUtils.isEmpty(docType.getParentId())) {
+                    docType = KewApiServiceLocator.getDocumentTypeService().getDocumentTypeById(docType.getParentId());
+                } else {
+                    docType = null;
+                }
             }
         }
         if (!ignoreMissingFields) {

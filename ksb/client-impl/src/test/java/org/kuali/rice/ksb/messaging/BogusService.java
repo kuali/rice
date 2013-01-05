79db0634b71fdefd5ed848ec64527f1d77490832
Peter Giles <g1l3sp@gmail.com>
2013-01-05 18:30:21 +0000

    KULRICE-8728: BusClientFailureProxy doesn't try all endpoint options * Committing unit test to verify fail over logic in BusClientFailureProxy

---
+++ ksb/client-impl/src/test/java/org/kuali/rice/ksb/messaging/BogusService.java
@@ -0,0 +1,12 @@
+package org.kuali.rice.ksb.messaging;
+
+import java.io.IOException;
+
+/**
+ * Bogus service interface for use in BusClientFailureProxyTest
+ */
+public interface BogusService {
+
+    String doSomething() throws IOException;
+
+}

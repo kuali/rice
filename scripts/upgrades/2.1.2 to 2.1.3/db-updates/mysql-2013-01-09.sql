5e98f328ec9a8f6c487fff94e38ab4cfef641837
Peter Giles <g1l3sp@gmail.com>
2013-01-09 19:05:31 +0000

    KULRICE-7687: Docs that go to final status without routing should not display the recall button * adding upgrade scripts to remove this assignment of the Recall permission to the initiator role

---
+++ scripts/upgrades/2.1.2 to 2.1.3/db-updates/mysql-2013-01-09.sql
@@ -0,0 +1,6 @@
+
+-- delete the assignment of the recall from routing permission for KULRICE-7687
+delete from krim_role_perm_t where
+  role_id = (select role_id from krim_role_t where nmspc_cd = 'KR-WKFLW' and role_nm = 'Initiator') and
+  perm_id = (select PERM_ID from krim_perm_t where nmspc_cd = 'KR-WKFLW' and nm = 'Recall Document')
+;

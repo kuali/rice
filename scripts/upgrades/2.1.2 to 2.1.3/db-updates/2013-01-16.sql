0a6b48c53d88cf53c156b2d8dd4863e5a9fe34f7
Peter Giles <g1l3sp@gmail.com>
2013-01-16 18:23:46 +0000

    KULRICE-8177: CONTRIB: Identity Mgmt Section listed as "Undefined" * adding sql upgrade scripts to set the IdentityManagementDocument doc type label to 'Identity Management Document'

---
+++ scripts/upgrades/2.1.2 to 2.1.3/db-updates/2013-01-16.sql
@@ -0,0 +1,5 @@
+
+-- KULRICE-8177: CONTRIB: Identity Mgmt Section listed as "Undefined"
+update KREW_DOC_TYP_T set LBL = 'Identity Management Document' where
+  DOC_TYP_NM = 'IdentityManagementDocument' and LBL = 'Undefined'
+/

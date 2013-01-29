1724e465b4281da8805bac36811fd28a3aa9b751
Viral Chauhan <vchauhan@vivantech.com>
2013-01-29 10:57:21 +0000

    KULRICE-8517 Created Non-navigation versions of the LegacyIT Smoke tests

---
+++ sampleapp/src/it/java/edu/samplu/mainmenu/test/PeopleFlowCreateNewWDIT.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.mainmenu.test;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * TODO vchauhan don't forget to fill this in.
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class PeopleFlowCreateNewWDIT extends WebDriverLegacyITBase {
+  
+    public static final String TEST_URL = ITUtil.PORTAL + "?channelTitle=People%20Flow&channelUrl=" + ITUtil.getBaseUrlString() +
+            "/kr-krad/lookup?methodToCall=start&dataObjectClassName=org.kuali.rice.kew.impl.peopleflow.PeopleFlowBo&returnLocation=" +
+            ITUtil.PORTAL_URL + "&showMaintenanceLinks=true";
+   
+    @Override
+    public String getTestUrl() {
+        return TEST_URL;
+    }
+    
+    
+    @Test
+    public void testPeopleFlow() throws Exception {
+      super.testPeopleFlow();
+    }
+
+    /**
+     * This overridden method ...
+     * 
+     * @see edu.samplu.common.MainMenuLookupITBase#lookupAssertions()
+     */
+  
+    public void lookupAssertions() {
+        // nothing
+
+    }
+
+    /**
+     * This overridden method ...
+     * 
+     * @see edu.samplu.common.MenuITBase#getLinkLocator()
+     */
+
+    protected String getLinkLocator() {
+        return "People Flow";
+    }
+
+    
+    @Ignore // No Need to     
+    @Test
+ 
+    public void testLookUp() throws Exception {}
+}
---
+++ sampleapp/src/it/java/edu/samplu/mainmenu/test/TermSpecificationLookUpWDIT.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2005-2011 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.mainmenu.test;
+
+import org.junit.Test;
+
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * tests that user 'admin' can display the Term Specification lookup screen, search,
+ * initiate an Term Specification maintenance document via an edit action on the search results and
+ * finally cancel the maintenance document
+ * 
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class TermSpecificationLookUpWDIT extends WebDriverLegacyITBase {
+    public static final String TEST_URL = ITUtil.PORTAL + "?channelTitle=Term%20Specification%20Lookup&channelUrl=" + ITUtil.getBaseUrlString() +
+            "/kr-krad/lookup?methodToCall=start&dataObjectClassName=org.kuali.rice.krms.impl.repository.TermSpecificationBo&showMaintenanceLinks=true&returnLocation=" +
+            ITUtil.PORTAL_URL + "&hideReturnLink=true";
+    
+    @Override
+    public String getTestUrl() {
+        return TEST_URL;
+    }
+    
+   @Test
+    public void testTermSpecificationLookupAssertions() throws Exception{
+        super.testTermSpecificationLookupAssertions();
+    }
+}

01d255066a479746c8252b5398b6650ede0f9edb
Eric Westfall <ewestfal@iu.edu>
2013-01-31 19:02:23 +0000

    Fixing some old broken comments.

--- core/api/src/main/java/org/kuali/rice/core/api/resourceloader/ParentChildResourceLoader.java
+++ core/api/src/main/java/org/kuali/rice/core/api/resourceloader/ParentChildResourceLoader.java
@@ -22,10 +22,10 @@ import javax.xml.namespace.QName;
 import org.kuali.rice.core.api.reflect.ObjectDefinition;
 
 /**
- * This is a description of what this class does - ewestfal don't forget to fill this in.
+ * A resource loader implementation which checks a child resource loader first and if the resource is not found there,
+ * checks a parent resource loader.
  *
  * @author Kuali Rice Team (rice.collab@kuali.org)
- *
  */
 class ParentChildResourceLoader implements ResourceLoader {
 
@@ -124,14 +124,8 @@ class ParentChildResourceLoader implements ResourceLoader {
 		this.child.removeResourceLoader(name);
     }
 
-	/**
-	 * This overridden method ...
-	 * 
-	 * @see org.kuali.rice.core.api.lifecycle.Lifecycle#isStarted()
-	 */
 	@Override
 	public boolean isStarted() {
-		// TODO ewestfal - THIS METHOD NEEDS JAVADOCS
 		return false;
 	}
 

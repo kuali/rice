ce11a8c65f8edfa8f5320239e785011f2e068611
Jeff Caddel <jcaddel@rsmart.com>
2013-02-15 22:38:05 +0000

    Automated license header update

--- rice-middleware/it/config/src/test/groovy/org/kuali/rice/config/at/OutputAwareMvnContextImpl.groovy
+++ rice-middleware/it/config/src/test/groovy/org/kuali/rice/config/at/OutputAwareMvnContextImpl.groovy
@@ -1,5 +1,5 @@
-/*
- * Copyright 2006-2012 The Kuali Foundation
+/**
+ * Copyright 2005-2013 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,12 +8,11 @@
  * http://www.opensource.org/licenses/ecl2.php
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License  distributed on an "AS " BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kuali.rice.config.at
 
 import groovy.transform.Canonical

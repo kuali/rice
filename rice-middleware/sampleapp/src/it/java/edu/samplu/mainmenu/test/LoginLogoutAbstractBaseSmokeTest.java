a5b4891837285b61ceaad9b9ea9ded814dea3bde
Erik Meade <github-kuali-m@eghm.net>
2013-03-23 03:19:38 +0000

    KULRICE-9203: Create or Generate JUnit and TestNG Smoke Tests from WebDriverLegacyITBase - LoginLogout

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/mainmenu/test/LoginLogoutAbstractBaseSmokeTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.mainmenu.test;
+
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.SeleneseFailable;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * Abstract base class for LoginLogout Smoke tests.  Framework specific classes should not be depended upon in this
+ * class but abstracted behind SeleneseFailable.
+ *
+ * @see SeleneseFailable
+ * @see LoginLogoutSTNGBase
+ * @see LoginLogoutSTJUnitBase
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public abstract class LoginLogoutAbstractBaseSmokeTest extends WebDriverLegacyITBase {
+
+    @Override
+    public String getTestUrl() {
+        return ITUtil.PORTAL;
+    }
+
+    @Override
+    public String getUserName() {
+        return "admin";
+    }
+
+    /**
+     * Navigate to the page under test and call testLogout(SeleneseFailable)
+
+     * @param failable SeleneseFailable
+     * @throws Exception
+     */
+    public void testNavLogout(SeleneseFailable failable) throws Exception {
+        navigate(failable);
+        testLogout(failable);
+    }
+
+    private void navigate(SeleneseFailable failable) throws InterruptedException {
+        waitAndClickMainMenu(failable);
+        waitForPageToLoad();
+    }
+
+    /**
+     * Test Logout on the current page.  Sets the passed flag (used with SauceLabs, reporting, etc.) as appropriate.
+     *
+     * @param failable SeleneseFailable
+     * @throws Exception
+     */
+    public void testLogout(SeleneseFailable failable) throws Exception {
+        waitAndClickLogout(failable);
+        passed();
+    }
+}
---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/mainmenu/test/LoginLogoutSTJUnitNavGen.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.mainmenu.test;
+
+import org.junit.Test;
+
+/**
+ * JUnit implementation of LoginLogoutSTJUnitBase that navigates through the UI to the page under test.  In the future
+ * the idea is to generate this class using the test methods from LoginLogoutAbstractBaseSmokeTest and following the  simple pattern of
+ * super.testNavTestMethod(SeleneseFailable).
+ *
+ * @see LoginLogoutSTJUnitBase
+ * @see LoginLogoutSTJUnitBkMrkGen
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LoginLogoutSTJUnitNavGen extends LoginLogoutSTJUnitBase {
+
+    @Test
+    public void testNavLogout() throws Exception {
+        super.testNavLogout(this);
+    }
+}

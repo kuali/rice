d6abb05520b7d26b21c4d42b19b5aba63c28876e
Jeff Caddel <jcaddel@rsmart.com>
2013-04-02 04:20:43 +0000

    Automated license header update

--- scripts/upgrades/2.2 to 2.3/db-updates/2013-04-01.sql
+++ scripts/upgrades/2.2 to 2.3/db-updates/2013-04-01.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 
 INSERT INTO KRIM_GRP_T (GRP_ID,GRP_NM,GRP_DESC,ACTV_IND,KIM_TYP_ID,NMSPC_CD,VER_NBR,OBJ_ID) VALUES ('10000','OAA_Workgroup_Approvers','workgroup approval group for OAA Recruitment eDoc Lite documents.','Y','1','DFAC_OAA',1,'dd1b202c-f2b4-4f3c-88a3-8882318f41ec')
 /

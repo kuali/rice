80769ea7ee12d4fee5838b89d373f7a21896f518
Deep Moteria <dmoteria@vivantech.com>
2013-05-21 04:47:13 +0000

    STJUnit and STNG smoketests generated for configview package.

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/configview/CollectionsAbstractSmokeTestBase.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.samplu.krad.configview;
+
+import org.junit.Test;
+
+import com.thoughtworks.selenium.SeleneseTestBase;
+import edu.samplu.common.Failable;
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public abstract class CollectionsAbstractSmokeTestBase extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/uicomponents?viewId=ConfigurationTestView-Collections&methodToCall=start
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/uicomponents?viewId=ConfigurationTestView-Collections&methodToCall=start";
+
+    /**
+     * (//a[contains(text(),'Collections Configuration Test View')])[2]
+     */
+    public static final String TEXT_COLLECTIONS_CONFIGURATION_TEST_VIEW_XPATH =
+            "(//a[contains(text(),'Collections Configuration Test View')])[2]";
+
+    /**
+     * Kuali :: Collection Test View
+     */
+    public static final String KUALI_COLLECTION_WINDOW_TITLE = "Kuali :: Collection Test View";
+    
+    /**
+     * Nav tests start at {@link edu.samplu.common.ITUtil#PORTAL}.  Bookmark Tests should override and return {@link CollectionsAbstractSmokeTestBase#BOOKMARK_URL}
+     * {@inheritDoc}
+     * @return
+     */
+    @Override
+    public String getTestUrl() {
+        return ITUtil.PORTAL;
+    }
+
+    protected void navigation() throws Exception {
+        waitAndClickKRAD();
+        waitAndClickByXpath(TEXT_COLLECTIONS_CONFIGURATION_TEST_VIEW_XPATH);
+        switchToWindow(KUALI_COLLECTION_WINDOW_TITLE);
+    }
+
+    protected void testCollectionsNav(Failable failable) throws Exception {
+        navigation();
+        testDefaultTestsTableLayout();
+        navigation();
+        testAddBlankLine();
+        navigation();
+        testActionColumnPlacement();
+        navigation();
+        testAddViaLightbox();
+        navigation();
+        testColumnSequence();
+        navigation();
+        testSequencerow();        
+    }
+
+    protected void testCollectionsBookmark(Failable failable) throws Exception {
+        testDefaultTestsTableLayout();
+        testAddBlankLine();        
+        testActionColumnPlacement();        
+        testAddViaLightbox();        
+        testColumnSequence();        
+        testSequencerow();
+    }
+    
+    /**
+     * Test adding a column of values to the Default Tests Table Layout
+     */
+    public void testDefaultTestsTableLayout() throws Exception {
+        testDefaultTestsTableLayout();
+        passed();
+    }
+
+    /**
+     * Test adding a column of values to the Add Blank Line Tests Table Layout
+     */
+    public void testAddBlankLine() throws Exception {
+        testAddBlankLine();
+        passed();
+    }
+
+    /**
+     * Test action column placement in table layout collections
+     */
+    public void testActionColumnPlacement() throws Exception {
+        testActionColumnPlacement();
+        passed();
+    }
+
+    public void testAddViaLightbox() throws Exception {
+        testAddViaLightbox();
+        passed();
+    }
+
+    public void testColumnSequence() throws Exception {
+        testColumnSequence();
+        passed();
+    }
+    
+    public void testSequencerow() throws Exception {
+        testSequencerow();
+        passed();
+    }
+}
---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/configview/Help2AbstractSmokeTestBase.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.samplu.krad.configview;
+
+import org.junit.Test;
+
+import com.thoughtworks.selenium.SeleneseTestBase;
+import edu.samplu.common.Failable;
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public abstract class Help2AbstractSmokeTestBase extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/configuration-test-view-uif-controller?viewId=ConfigurationTestView-Help&methodToCall=start
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/configuration-test-view-uif-controller?viewId=ConfigurationTestView-Help&methodToCall=start";
+   
+    /**
+     * Nav tests start at {@link edu.samplu.common.ITUtil#PORTAL}.  Bookmark Tests should override and return {@link Help2AbstractSmokeTestBase#BOOKMARK_URL}
+     * {@inheritDoc}
+     * @return
+     */
+    @Override
+    public String getTestUrl() {
+        return ITUtil.PORTAL;
+    }
+
+    protected void navigation() throws Exception {
+        waitAndClickKRAD();
+        waitAndClickByXpath(CONFIGURATION_VIEW_XPATH);
+        switchToWindow(CONFIGURATION_VIEW_WINDOW_TITLE);
+        waitAndClickByLinkText("Help");
+        Thread.sleep(5000);
+        selectFrameIframePortlet();
+    }
+
+    protected void testHelp2Nav(Failable failable) throws Exception {
+        navigation();
+        testViewHelp2();
+        navigation();
+        testExternalHelp2();
+        navigation();
+        testDisplayOnlyExternalHelp2();
+        navigation();
+        testMissingExternalHelp2();       
+    }
+
+    protected void testHelp2Bookmark(Failable failable) throws Exception {
+        testViewHelp();
+        testPageHelp();        
+        testTooltipHelp();        
+        testDisplayOnlyTooltipHelp();        
+        testMissingTooltipHelp();       
+    }
+    
+    /**
+     * Test the tooltip and external help on the view
+     */
+    public void testViewHelp2() throws Exception {
+        testViewHelp2();
+        passed();
+    }
+
+    /**
+     * Test the tooltip and external help on the page
+     */
+    public void testExternalHelp2() throws Exception {
+        testExternalHelp2();
+        passed();
+    }
+
+    /**
+     * Test the tooltip help on the section and fields
+     */
+    public void testDisplayOnlyExternalHelp2() throws Exception {
+        testDisplayOnlyExternalHelp2();
+        passed();
+    }
+
+    /**
+     * Test the tooltip help on the sub-section and fields that are display only
+     */
+    public void testMissingExternalHelp2() throws Exception {
+        testMissingExternalHelp2();
+        passed();
+    }    
+}

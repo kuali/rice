97de1b268c3c21d19354b930ea9b411555b1f4e8
Peter Giles <g1l3sp@gmail.com>
2013-06-10 06:50:49 +0000

    KULRICE-9712: Do weekly merges (2013-06-09) * merging r39478:39570 from rice-2.2 branch into trunk

--- rice-middleware/web/src/main/webapp/kr/scripts/jscalendar-1.0/calendar.js
+++ rice-middleware/web/src/main/webapp/kr/scripts/jscalendar-1.0/calendar.js
@@ -1,3 +1,19 @@
+/*  Copyright Mihai Bazon, 2002-2005  |  www.bazon.net/mishoo
+ * -----------------------------------------------------------
+ *
+ * The DHTML Calendar, version 1.0 "It is happening again"
+ *
+ * Details and latest version at:
+ * www.dynarch.com/projects/calendar
+ *
+ * This script is developed by Dynarch.com.  Visit us at www.dynarch.com.
+ *
+ * This script is distributed under the GNU Lesser General Public License.
+ * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
+ */
+
+// $Id: calendar.js,v 1.51 2005/03/07 16:44:31 mishoo Exp $
+
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
 	// member variables

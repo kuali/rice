7891d4182c8df46a9caf368e8b349dd5b39b7f8d
Erik Meade <github-kuali-m@eghm.net>
2013-06-18 18:12:47 +0000

    KULRICE-9749 : Create Smoke Tests for KRAD Component Library - Navigation - draft TestNG Bookmark

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/demo/uif/library/navigation/DemoLibraryNavigationSTNGBkMrkGen.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.demo.uif.library.navigation;
+
+import edu.samplu.common.WebDriverLegacyITBase;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLibraryNavigationSTNGBkMrkGen extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=ComponentLibraryHome
+     */
+    public final static String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=ComponentLibraryHome";
+
+    @Override
+    public String getTestUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Test(groups = { "all", "fast", "default", "bookmark" }, description = "testNavigationMenuBookmark")
+    @Parameters( { "seleniumHost", "seleniumPort", "os", "browser", "version", "webSite" })
+    public void testNavigationMenuBookmark() throws Exception {
+        waitAndClickByLinkText("Navigation");
+        waitAndClickByLinkText("Left Menu");
+        waitAndClickByLinkText("Navigation Group Tab Example");
+    }
+}

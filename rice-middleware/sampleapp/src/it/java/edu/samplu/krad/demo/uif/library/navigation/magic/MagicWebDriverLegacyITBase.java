063fefc35618ae6c7a53964ab48d69fd45372a85
Eric Westfall <ewestfal@iu.edu>
2013-06-24 21:06:05 +0000

    KULRICE-9751 - Committing simplified smoke test stuff

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/demo/uif/library/navigation/magic/MagicWebDriverLegacyITBase.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2006-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.samplu.krad.demo.uif.library.navigation.magic;
+
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.WebDriverLegacyITBase;
+import org.junit.runner.RunWith;
+
+@RunWith(MagicSmokeTestRunner.class)
+public abstract class MagicWebDriverLegacyITBase extends WebDriverLegacyITBase {
+
+    private String testUrl = ITUtil.KRAD_PORTAL;
+    private boolean shouldNavigate = false;
+
+    @Override
+    public String getTestUrl() {
+        return testUrl;
+    }
+
+    protected void enableBookmarkMode() {
+        this.testUrl = getBookmarkUrl();
+    }
+
+    protected void enableNavigationMode() {
+        this.shouldNavigate = true;
+        this.testUrl = ITUtil.KRAD_PORTAL;
+    }
+
+    @Override
+    protected void navigateInternal() throws Exception {
+        if (this.shouldNavigate) {
+            navigate();
+        }
+    }
+
+    protected abstract String getBookmarkUrl();
+
+    protected abstract void navigate() throws Exception;
+
+}

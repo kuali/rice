0525e762742c8c0bdeb4d0d11e5bff714e53b464
Jeremy Hanson <jjhanso@iastate.edu>
2013-07-05 15:33:15 +0000

    KULRICE-9865 - fix call to sequence accessor service so correct db call used in all situations KULIRCE-9860 - add krad component scans to base krad-servlet.xml in rice-web module

--- rice-middleware/impl/src/main/java/org/kuali/rice/kim/document/GenericPermissionMaintainable.java
+++ rice-middleware/impl/src/main/java/org/kuali/rice/kim/document/GenericPermissionMaintainable.java
@@ -82,7 +82,8 @@ public class GenericPermissionMaintainable extends KualiMaintainableImpl {
     public void processAfterCopy(MaintenanceDocument document, Map<String, String[]> parameters) {
         super.processAfterCopy(document,parameters);
         // get id for new permission
-        String newId = getSequenceAccessorService().getNextAvailableSequenceNumber(KimConstants.SequenceNames.KRIM_PERM_ID_S).toString();
+        String newId = getSequenceAccessorService()
+                .getNextAvailableSequenceNumber(KimConstants.SequenceNames.KRIM_PERM_ID_S, PermissionBo.class).toString();
         ((GenericPermissionBo)document.getNewMaintainableObject().getDataObject()).setId(newId);
     }
 

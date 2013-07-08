ead752ba836feaefdc37249e452541728c484d4c
Erik Meade <github-kuali-m@eghm.net>
2013-07-08 21:59:19 +0000

    KULRICE-8146 : Implement performance monitoring - rename KRAD Sample Breadcrumbs to start with Demo so it can be picked up in rice-2.3-smoke-test-krad which runs Demo*SmokeTest.java

--- rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/kitchensink/compview/DemoBreadcrumbKradSTJUnitBkMrkGen.java
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/kitchensink/compview/DemoBreadcrumbKradSTJUnitBkMrkGen.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 /**
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
-public class BreadcrumbKradSTJUnitBkMrkGen extends BreadcrumbKradSTJUnitBase {
+public class DemoBreadcrumbKradSTJUnitBkMrkGen extends BreadcrumbKradSTJUnitBase {
 
     /**
      * {@inheritDoc}
--- rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/kitchensink/compview/DemoBreadcrumbKradSTJUnitNavGen.java
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/kitchensink/compview/DemoBreadcrumbKradSTJUnitNavGen.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 /**
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
-public class BreadcrumbKradSTJUnitNavGen extends BreadcrumbKradSTJUnitBase {
+public class DemoBreadcrumbKradSTJUnitNavGen extends BreadcrumbKradSTJUnitBase {
 
     @Test
     public void testBreadcrumbNav() throws Exception {

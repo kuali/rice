0204041118fcb11275227e21bc980063b3beb3cb
Deep Moteria <dmoteria@vivantech.com>
2013-07-11 05:32:21 +0000

    Constants added and formatting done!

--- rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/clientside/stateview/ClientSideStateViewSmokeTest.java
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/clientside/stateview/ClientSideStateViewSmokeTest.java
@@ -17,29 +17,39 @@ package edu.samplu.krad.labs.clientside.stateview;
 
 import org.junit.Test;
 import org.junit.Assert;
-import edu.samplu.common.Failable;
 import edu.samplu.common.SmokeTestBase;
 
 /**
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
-public class ClientSideStateViewSmokeTest extends SmokeTestBase{
+public class ClientSideStateViewSmokeTest extends SmokeTestBase {
 
     /**
-     * /kr-krad/labs?viewId=Lab-ClientSideState&formKey=ff000d97-13e9-4130-81a3-bc0217e8e0eb&cacheKey=otutyty24mo0f76n59ebqxvtpg&pageId=Lab-ClientSideState-Page1  
+     * /kr-krad/labs?viewId=Lab-ClientSideState&formKey=ff000d97-13e9-4130-81a3-bc0217e8e0eb&
+     * cacheKey=otutyty24mo0f76n59ebqxvtpg&pageId=Lab-ClientSideState-Page1
      */
-    public static final String BOOKMARK_URL =  "/kr-krad/labs?viewId=Lab-ClientSideState&formKey=ff000d97-13e9-4130-81a3-bc0217e8e0eb&cacheKey=otutyty24mo0f76n59ebqxvtpg&pageId=Lab-ClientSideState-Page1";
-    
+    public static final String BOOKMARK_URL = "/kr-krad/labs?viewId=Lab-ClientSideState&formKey=ff000d97-13e9-4130-81a3-bc0217e8e0eb&cacheKey=otutyty24mo0f76n59ebqxvtpg&pageId=Lab-ClientSideState-Page1";
+
     /**
      * inactivatableCollection[0].active
      */
-    private static final String ACTIVE_COMPONENT_NAME= "inactivatableCollection[0].active";
-    
+    private static final String ACTIVE_COMPONENT_NAME = "inactivatableCollection[0].active";
+
     /**
      * inactivatableCollection[0].active
      */
-    private static final String ACTIVE_COMPONENT2_NAME= "inactivatableCollection2[0].active";
+    private static final String ACTIVE_COMPONENT2_NAME = "inactivatableCollection2[0].active";
     
+    /**
+     * //div[4]/div[2]/button
+     */
+    private static final String AJAX_BUTTON_XPATH = "//div[4]/div[2]/button";
+    
+    /**
+     * //div[4]/div[2]/button[2]
+     */
+    private static final String SUBMIT_BUTTON_XPATH = "//div[4]/div[2]/button[2]";
+
     /**
      * This overridden method ...
      * 
@@ -50,31 +60,29 @@ public class ClientSideStateViewSmokeTest extends SmokeTestBase{
         // TODO deep - THIS METHOD NEEDS JAVADOCS
         //Navigation is not defined for this functionality so we only do have URL in this Smoke Test.
     }
-    
+
     @Override
     protected String getBookmarkUrl() {
         return BOOKMARK_URL;
     }
-    
+
     @Test
-    public void testClientSideStateViewBookmark() throws Exception {     
+    public void testClientSideStateViewBookmark() throws Exception {
         testClientSideStateView();
     }
-    
+
     public void testClientSideStateView() throws Exception
     {
         selectFrameIframePortlet();
         waitAndClickByName(ACTIVE_COMPONENT_NAME);
         waitAndClickByName(ACTIVE_COMPONENT2_NAME);
-        waitAndClickByXpath("//div[4]/div[2]/button");
+        waitAndClickByXpath(AJAX_BUTTON_XPATH);
         Thread.sleep(5000);
         Assert.assertFalse(isElementPresentByName(ACTIVE_COMPONENT_NAME));
         Assert.assertFalse(isElementPresentByName(ACTIVE_COMPONENT2_NAME));
-        waitAndClickByXpath("//div[4]/div[2]/button[2]");
+        waitAndClickByXpath(SUBMIT_BUTTON_XPATH);
         Thread.sleep(5000);
         Assert.assertFalse(isElementPresentByName(ACTIVE_COMPONENT_NAME));
         Assert.assertFalse(isElementPresentByName(ACTIVE_COMPONENT2_NAME));
     }
-
-    
 }

4fdc750aab938de290aac2e223e602bb03571a91
Corey Pedersen <corey.pedersen@utah.edu>
2013-07-23 18:06:12 +0000

    KULRICE-9892 Enhance PeopleFlowTypeService and PeopleFlowRequestGeneratorImpl to allow resolving/accepting multiple maps of role qualifiers

--- rice-middleware/impl/src/main/java/org/kuali/rice/kew/impl/peopleflow/PeopleFlowRequestGeneratorImpl.java
+++ rice-middleware/impl/src/main/java/org/kuali/rice/kew/impl/peopleflow/PeopleFlowRequestGeneratorImpl.java
@@ -17,14 +17,17 @@ package org.kuali.rice.kew.impl.peopleflow;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.core.api.CoreConstants;
 import org.kuali.rice.core.api.config.ConfigurationException;
 import org.kuali.rice.core.api.membership.MemberType;
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.core.api.util.VersionHelper;
 import org.kuali.rice.kew.actionrequest.ActionRequestFactory;
 import org.kuali.rice.kew.actionrequest.ActionRequestValue;
 import org.kuali.rice.kew.actionrequest.KimGroupRecipient;
 import org.kuali.rice.kew.actionrequest.KimPrincipalRecipient;
 import org.kuali.rice.kew.actionrequest.Recipient;
+import org.kuali.rice.kew.api.KewApiConstants;
 import org.kuali.rice.kew.api.action.ActionRequestType;
 import org.kuali.rice.kew.api.document.Document;
 import org.kuali.rice.kew.api.document.DocumentContent;
@@ -40,8 +43,11 @@ import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValueContent;
 import org.kuali.rice.kim.api.role.Role;
 import org.kuali.rice.kim.api.role.RoleMembership;
 import org.kuali.rice.kim.api.role.RoleService;
+import org.kuali.rice.ksb.api.KsbApiServiceLocator;
+import org.kuali.rice.ksb.api.bus.Endpoint;
 
 import javax.xml.namespace.QName;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -87,22 +93,38 @@ public class PeopleFlowRequestGeneratorImpl implements PeopleFlowRequestGenerato
     }
 
     protected void generateRequestForRoleMember(Context context, PeopleFlowMember member, String actionRequestPolicyCode) {
-        Map<String, String> roleQualifiers = loadRoleQualifiers(context, member);
+        List<Map<String, String>> roleQualifierList = loadRoleQualifiers(context, member);
         Role role = getRoleService().getRole(member.getMemberId());
+
         if (role == null) {
             throw new IllegalStateException("Failed to locate a role with the given role id of '" + member.getMemberId() + "'");
         }
-        List<RoleMembership> memberships = getRoleService().getRoleMembers(Collections.singletonList(
-                member.getMemberId()), roleQualifiers);
-        if (!CollectionUtils.isEmpty(memberships)) {
-            context.getActionRequestFactory().addKimRoleRequest(context.getActionRequested().getCode(), member.getPriority(),
-                    role, memberships, null, member.getResponsibilityId(), true, actionRequestPolicyCode, null);
+
+        if (CollectionUtils.isEmpty(roleQualifierList)) {
+            addKimRoleRequest(context, role, member, Collections.<String, String>emptyMap(), actionRequestPolicyCode );
+        } else {
+            for (Map<String, String> roleQualifiers : roleQualifierList) {
+                addKimRoleRequest(context, role, member, roleQualifiers, actionRequestPolicyCode );
+            }
         }
         // TODO - KULRICE-5726 - still need to implement support for ignoring built-in kim delegates whenever peopleflow delegate(s) are defined
     }
 
-    protected Map<String, String> loadRoleQualifiers(Context context, PeopleFlowMember member) {
+
+    private void addKimRoleRequest(Context context, Role role, PeopleFlowMember member, Map<String, String> roleQualifiers, String actionRequestPolicyCode) {
+        List<RoleMembership> memberships = getRoleService().getRoleMembers(Collections.singletonList(
+                member.getMemberId()), roleQualifiers);
+
+        if (!CollectionUtils.isEmpty(memberships)) {
+            context.getActionRequestFactory().addKimRoleRequest(context.getActionRequested().getCode(), member.getPriority(),
+                    role, memberships, null, member.getResponsibilityId(), true, actionRequestPolicyCode, null);
+        }
+    }
+
+    protected List<Map<String, String>> loadRoleQualifiers(Context context, PeopleFlowMember member) {
         PeopleFlowTypeService peopleFlowTypeService = context.getPeopleFlowTypeService();
+        List<Map<String, String>> roleQualifierList = new ArrayList<Map<String, String>>();
+
         if (peopleFlowTypeService != null) {
             Document document = DocumentRouteHeaderValue.to(context.getRouteContext().getDocument());
             DocumentRouteHeaderValueContent content = new DocumentRouteHeaderValueContent(document.getDocumentId());
@@ -110,11 +132,24 @@ public class PeopleFlowRequestGeneratorImpl implements PeopleFlowRequestGenerato
             DocumentContent documentContent = DocumentRouteHeaderValueContent.to(content);
             Map<String, String> roleQualifiers = peopleFlowTypeService.resolveRoleQualifiers(
                     context.getPeopleFlow().getTypeId(), member.getMemberId(), document, documentContent);
+
             if (roleQualifiers != null) {
-                return roleQualifiers;
+                roleQualifierList.add(roleQualifiers);
             }
+
+            boolean versionOk = VersionHelper.compareVersion(context.getPeopleFlowTypeServiceVersion(), CoreConstants.Versions.VERSION_2_3_0)!=-1? true:false;
+            if(versionOk) {
+                List<Map<String, String>> multipleRoleQualifiers = peopleFlowTypeService.resolveMultipleRoleQualifiers(
+                        context.getPeopleFlow().getTypeId(), member.getMemberId(), document, documentContent);
+
+                if (multipleRoleQualifiers != null) {
+                    roleQualifierList.addAll(multipleRoleQualifiers);
+                }
+            }
+
         }
-        return Collections.emptyMap();
+
+        return roleQualifierList;
     }
 
     private Recipient toRecipient(PeopleFlowMember member) {
@@ -173,6 +208,7 @@ public class PeopleFlowRequestGeneratorImpl implements PeopleFlowRequestGenerato
         // lazily loaded
         private PeopleFlowTypeService peopleFlowTypeService;
         private boolean peopleFlowTypeServiceLoaded = false;
+        private String peopleFlowTypeServiceVersion;
 
         Context(RouteContext routeContext, PeopleFlowDefinition peopleFlow, ActionRequestType actionRequested) {
             if (routeContext == null) {
@@ -216,21 +252,36 @@ public class PeopleFlowRequestGeneratorImpl implements PeopleFlowRequestGenerato
             if (peopleFlowTypeServiceLoaded) {
                 return this.peopleFlowTypeService;
             }
+
             if (getPeopleFlow().getTypeId() != null) {
                 KewTypeDefinition typeDefinition = getTypeRepositoryService().getTypeById(getPeopleFlow().getTypeId());
+
                 if (typeDefinition == null) {
                     throw new IllegalStateException("Failed to locate a PeopleFlow type for the given type id of '" + getPeopleFlow().getTypeId() + "'");
                 }
+
                 if (StringUtils.isNotBlank(typeDefinition.getServiceName())) {
-                    this.peopleFlowTypeService = GlobalResourceLoader.getService(QName.valueOf(typeDefinition.getServiceName()));
-                    if (this.peopleFlowTypeService == null) {
+                    Endpoint endpoint = KsbApiServiceLocator.getServiceBus().getEndpoint(QName.valueOf(typeDefinition.getServiceName()));
+
+                    if (endpoint == null) {
                         throw new IllegalStateException("Failed to load the PeopleFlowTypeService with the name '" + typeDefinition.getServiceName() + "'");
-                     }
+                    }
+
+                    this.peopleFlowTypeService = (PeopleFlowTypeService)endpoint.getService();
+                    this.peopleFlowTypeServiceVersion = endpoint.getServiceConfiguration().getServiceVersion();
                 }
             }
             peopleFlowTypeServiceLoaded = true;
             return this.peopleFlowTypeService;
         }
 
+        String getPeopleFlowTypeServiceVersion() {
+            if (!this.peopleFlowTypeServiceLoaded) {
+                // execute getPeopleFlowTypeService first to lazy load
+                getPeopleFlowTypeService();
+            }
+
+            return this.peopleFlowTypeServiceVersion;
+        }
     }
 }

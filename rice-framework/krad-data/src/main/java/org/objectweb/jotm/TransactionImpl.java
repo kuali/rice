37ee52132b256d4580864455ddbdfdc1f8fbb477
Jeff Caddel <jcaddel@rsmart.com>
2013-07-26 06:05:45 +0000

    Automated license header update

--- rice-framework/krad-data/src/main/java/org/objectweb/jotm/TransactionImpl.java
+++ rice-framework/krad-data/src/main/java/org/objectweb/jotm/TransactionImpl.java
@@ -1,54 +1,17 @@
-/*
- * @(#) TransactionImpl.java
+/**
+ * Copyright 2005-2013 The Kuali Foundation
  *
- * JOTM: Java Open Transaction Manager
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ * http://www.opensource.org/licenses/ecl2.php
  *
- * This module was originally developed by
- *
- *  - Bull S.A. as part of the JOnAS application server code released in
- *    July 1999 (www.bull.com)
- *
- * --------------------------------------------------------------------------
- *  The original code and portions created by Bull SA are
- *  Copyright (c) 1999 BULL SA
- *  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * -Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * -Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * --------------------------------------------------------------------------
- * Contributor(s):
- * 01/11/06 Christophe Ney cney@batisseurs.com
- *          Added ResourceManagerListener mechanism to remove ThreadData
- *          dependency.
- *
- * 02/01/15 Dean Jennings - List  instead of Vector for enlistedXARes delistedXARes
- *
- * 02/06/18 Marek Prochazka - in timeoutExpired() removed code
- *          preventing deadlock by rolling back transaction
- * --------------------------------------------------------------------------
- * $Id: TransactionImpl.java,v 1.51 2006-09-07 00:18:41 tonyortiz Exp $
- * --------------------------------------------------------------------------
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.objectweb.jotm;
 

bcb7b9968138220d59a0fe208dc075b60e39d401
Eric Westfall <ewestfal@iu.edu>
2013-07-30 21:31:12 +0000

    KULRICE-9925 - improved javadoc on MaxValueIncrementerFactory and implemented a unit test.

--- rice-framework/krad-data/src/main/java/org/kuali/rice/krad/data/platform/MaxValueIncrementerFactory.java
+++ rice-framework/krad-data/src/main/java/org/kuali/rice/krad/data/platform/MaxValueIncrementerFactory.java
@@ -20,13 +20,39 @@ import java.sql.Statement;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * Factory for obtaining instances of {@link DataFieldMaxValueIncrementer} for a given {@link DataSource} and
+ * incrementer name. These incrementers are used for getting generated incrementing values like that provided by a
+ * database-level sequence generator.
+ *
+ * <p>Note that not all database platforms support sequences natively, so incrementers can be returned that emulate
+ * sequence-like behavior. The Spring Framework provides incrementer implementations for numerous different database
+ * platforms. This classes uses {@link DatabasePlatforms} to determine the platform of the given DataSource.</p>
+ *
+ * <p>Note that this class will cache internally any incrementers for a given DataSource + Incrementer Name combination.
+ * </p>
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
 public final class MaxValueIncrementerFactory {
 
-    private static final String ID_COLUMN_NAME = "id";
+    private static final String ID_COLUMN_NAME = "ID";
 
     private static final ConcurrentMap<DataSource, ConcurrentMap<String, DataFieldMaxValueIncrementer>> cache =
             new ConcurrentHashMap<DataSource, ConcurrentMap<String, DataFieldMaxValueIncrementer>>(8, 0.9f, 1);
 
+    /**
+     * Either constructs a new incrementer or retrieves a cached instance for the given DataSource and target
+     * incrementer name.
+     *
+     * @param dataSource the DataSource for which to retrieve the incrementer
+     * @param incrementerName the case-insensitive name of the incrementer to use, this will generally be the name of the database
+     *        object which is used to implement the incrementer
+     * @return an incrementer that can be used to generate the next incremented value for the given incrementer against
+     *         the specified DataSource
+     *
+     * @throws IllegalArgumentException if dataSource or incrementerName are null or blank
+     */
     public static DataFieldMaxValueIncrementer getIncrementer(DataSource dataSource, String incrementerName) {
         if (dataSource == null) {
             throw new IllegalArgumentException("DataSource must not be null");
@@ -84,7 +110,7 @@ public final class MaxValueIncrementerFactory {
         return incrementer;
     }
 
-    private static final class EnhancedMySQLMaxValueIncrementer extends AbstractColumnMaxValueIncrementer {
+    static final class EnhancedMySQLMaxValueIncrementer extends AbstractColumnMaxValueIncrementer {
 
         private JdbcTemplate template;
 
---
+++ rice-framework/krad-data/src/test/java/org/kuali/rice/krad/data/platform/MaxValueIncrementerFactoryTest.java
@@ -0,0 +1,115 @@
+package org.kuali.rice.krad.data.platform;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer;
+import org.springframework.jdbc.support.incrementer.OracleSequenceMaxValueIncrementer;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit test for {@link MaxValueIncrementerFactory}.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MaxValueIncrementerFactoryTest {
+
+    @Mock private DataSource mysql;
+    @Mock private DataSource oracle;
+    @Mock private DataSource bad;
+
+    @Before
+    public void setUp() throws Exception {
+        setUpMetaData(mysql, DatabasePlatforms.MYSQL, 5);
+        setUpMetaData(oracle, DatabasePlatforms.ORACLE, 11);
+        setUpMetaData(bad, "BAD!!!!!", 1);
+    }
+
+    private void setUpMetaData(DataSource dataSource, String platformName, int version) throws SQLException {
+        DatabaseMetaData metaData = mock(DatabaseMetaData.class);
+        when(metaData.getDatabaseProductName()).thenReturn(platformName);
+        when(metaData.getDatabaseMajorVersion()).thenReturn(version);
+        Connection connection = mock(Connection.class);
+        when(connection.getMetaData()).thenReturn(metaData);
+        when(dataSource.getConnection()).thenReturn(connection);
+    }
+
+
+    @Test
+    public void testGetIncrementer_Oracle() throws Exception {
+        DataFieldMaxValueIncrementer incrementer = MaxValueIncrementerFactory.getIncrementer(oracle, "MY_SEQUENCE");
+        assertTrue(incrementer instanceof OracleSequenceMaxValueIncrementer);
+        OracleSequenceMaxValueIncrementer oracleIncrementer = (OracleSequenceMaxValueIncrementer)incrementer;
+        assertEquals("MY_SEQUENCE", oracleIncrementer.getIncrementerName());
+
+        // ensure that it's caching the incrementer
+        assertSame(incrementer, MaxValueIncrementerFactory.getIncrementer(oracle, "MY_SEQUENCE"));
+        // ensure that different sequence gives a different incrementer
+        assertNotSame(incrementer, MaxValueIncrementerFactory.getIncrementer(oracle, "MY_SEQUENCE_2"));
+
+    }
+
+    @Test
+    public void testGetIncrementer_MySQL() throws Exception {
+        DataFieldMaxValueIncrementer incrementer = MaxValueIncrementerFactory.getIncrementer(mysql, "MY_SEQUENCE");
+        assertTrue(incrementer instanceof MaxValueIncrementerFactory.EnhancedMySQLMaxValueIncrementer);
+        MaxValueIncrementerFactory.EnhancedMySQLMaxValueIncrementer mysqlIncrementer =
+                (MaxValueIncrementerFactory.EnhancedMySQLMaxValueIncrementer)incrementer;
+        assertEquals("MY_SEQUENCE", mysqlIncrementer.getIncrementerName());
+        assertEquals("ID", mysqlIncrementer.getColumnName());
+
+        // ensure that it's caching the incrementer
+        assertSame(incrementer, MaxValueIncrementerFactory.getIncrementer(mysql, "MY_SEQUENCE"));
+        // ensure that different sequence gives a different incrementer
+        assertNotSame(incrementer, MaxValueIncrementerFactory.getIncrementer(mysql, "MY_SEQUENCE_2"));
+
+    }
+
+    @Test(expected = UnsupportedDatabasePlatformException.class)
+    public void testGetIncrementer_Bad() throws Exception {
+        MaxValueIncrementerFactory.getIncrementer(bad, "MY_SEQUENCE");
+    }
+
+    @Test
+    public void testGetIncrementer_CaseInsensitive() throws Exception {
+        DataFieldMaxValueIncrementer incrementer1 = MaxValueIncrementerFactory.getIncrementer(mysql, "MY_SEQUENCE");
+        DataFieldMaxValueIncrementer incrementer2 = MaxValueIncrementerFactory.getIncrementer(mysql, "MY_sequence");
+        DataFieldMaxValueIncrementer incrementer3 = MaxValueIncrementerFactory.getIncrementer(mysql, "my_sequence");
+        assertSame(incrementer1, incrementer2);
+        assertSame(incrementer2, incrementer3);
+    }
+
+    @Test
+    public void testGetIncrementer_CacheByDataSource() throws Exception {
+        DataFieldMaxValueIncrementer incrementer1 = MaxValueIncrementerFactory.getIncrementer(mysql, "MY_SEQUENCE");
+        DataFieldMaxValueIncrementer incrementer2 = MaxValueIncrementerFactory.getIncrementer(oracle, "MY_SEQUENCE");
+        assertNotSame(incrementer1, incrementer2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetIncrementer_NullDataSource() throws Exception {
+        MaxValueIncrementerFactory.getIncrementer(null, "MY_SEQUENCE");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetIncrementer_NullIncrementerName() throws Exception {
+        MaxValueIncrementerFactory.getIncrementer(mysql, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetIncrementer_BlankIncrementerName() throws Exception {
+        MaxValueIncrementerFactory.getIncrementer(mysql, "");
+    }
+
+}

308a13540eba1eeb84633e6ba4927fd8fab6bfbd
Jeff Caddel <jcaddel@rsmart.com>
2013-08-09 06:06:26 +0000

    Automated license header update

--- scripts/upgrades/2.2 to 2.3/db-updates/mysql-2013-08-08.sql
+++ scripts/upgrades/2.2 to 2.3/db-updates/mysql-2013-08-08.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 -- updating permissions for edit document KULRICE-9643
 delete from krim_perm_attr_data_t where perm_id = (select distinct a.perm_id from krim_perm_t a,krim_perm_tmpl_t b where a.perm_tmpl_id = b.perm_tmpl_id and b.nmspc_cd = 'KR-NS' and b.nm = 'Edit Document' and a.nmspc_cd = 'KUALI' and a.nm = 'Edit Kuali ENROUTE Document Node Name PreRoute');
 

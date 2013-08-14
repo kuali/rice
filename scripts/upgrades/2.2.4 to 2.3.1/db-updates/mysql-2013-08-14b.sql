e3ef11a32e79cb566edc9c1dbf62227b013eeaaf
Peter Giles <g1l3sp@gmail.com>
2013-08-14 23:54:11 +0000

    KULRICE-9034: KR-KRAD - RESULTS_LIMIT parameter should be added and the code should be changed to use it * adding database scripts

---
+++ scripts/upgrades/2.2.4 to 2.3.1/db-updates/mysql-2013-08-14b.sql
@@ -0,0 +1,9 @@
+--
+-- KULRICE-9034: KR-KRAD - RESULTS_LIMIT parameter should be added and the code should be changed to use it
+--
+
+INSERT INTO KRCR_PARM_T
+  (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+  VALUES ('KR-KRAD', 'Lookup', 'RESULTS_LIMIT', uuid(), '200', 'CONFG', '1',
+          'Maximum number of results returned in a look-up query.', 'A', 'KUALI')
+;

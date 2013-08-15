571618b070a53bf0b3d7c6b1bfb6aa7997d4272e
Peter Giles <g1l3sp@gmail.com>
2013-08-15 21:58:25 +0000

    KULRICE-9034: KR-KRAD - RESULTS_LIMIT parameter should be added and the code should be changed to use it * correcting script bug, the version number was set to '200' and the value to '1'.

--- scripts/upgrades/2.2.4 to 2.3.1/db-updates/2013-08-14b.sql
+++ scripts/upgrades/2.2.4 to 2.3.1/db-updates/2013-08-14b.sql
@@ -20,6 +20,6 @@
 
 INSERT INTO KRCR_PARM_T
   (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-  VALUES ('KR-KRAD', 'Lookup', 'RESULTS_LIMIT', SYS_GUID(), '200', 'CONFG', '1',
+  VALUES ('KR-KRAD', 'Lookup', 'RESULTS_LIMIT', SYS_GUID(), 1, 'CONFG', '200',
           'Maximum number of results returned in a look-up query.', 'A', 'KUALI')
 /

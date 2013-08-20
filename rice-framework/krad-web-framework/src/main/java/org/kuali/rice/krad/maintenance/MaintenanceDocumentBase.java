919a92a044e643ea890e678c30c0b1de2ecb495a
Jonathan Keller <keller.jonathan@gmail.com>
2013-08-20 15:58:29 +0000

    Added protection from attempt to delete when there is no attachment.  (Since the LegacyDataAdapter returns an error in that case.)

--- rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/maintenance/MaintenanceDocumentBase.java
+++ rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/maintenance/MaintenanceDocumentBase.java
@@ -15,6 +15,30 @@
  */
 package org.kuali.rice.krad.maintenance;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.PostLoad;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.ojb.broker.core.proxy.ProxyHelper;
@@ -56,29 +80,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.PostLoad;
-import javax.persistence.PreUpdate;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.persistence.UniqueConstraint;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Document class for all maintenance documents which wraps the maintenance object in
@@ -828,8 +829,10 @@ public class MaintenanceDocumentBase extends DocumentBase implements Maintenance
     public void populateBoAttachmentListBeforeSave() { }
 
     public void deleteDocumentAttachment() {
-        KRADServiceLocatorWeb.getLegacyDataAdapter().delete(attachment);
-        attachment = null;
+        if ( attachment != null ) {
+            KRADServiceLocatorWeb.getLegacyDataAdapter().delete(attachment);
+            attachment = null;
+        }
     }
 
     public void deleteDocumentAttachmentList() {

e0e96fabc669b315ba72233ee8fa7212cb5d1aa3
Martin Taylor <mztaylor@umd.edu>
2013-09-16 20:05:20 +0000

    KULRICE-10453: Added sql related to app specific copy of 'RESULT_LIMIT' parameters from kns to krad

---
+++ scripts/upgrades/2.3 to 2.4/db-updates/2013-09-16.sql
@@ -0,0 +1,27 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+INSERT INTO KRCR_PARM_T (OBJ_ID, NMSPC_CD, CMPNT_CD, PARM_NM, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+    SELECT sys_guid(), 'KR-KRAD', CMPNT_CD, PARM_NM, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID
+      FROM KRCR_PARM_T
+     WHERE NMSPC_CD = 'KR-NS'
+       AND CMPNT_CD = 'Lookup'
+       AND PARM_NM = 'RESULTS_LIMIT'
+       AND APPL_ID NOT IN (SELECT APPL_ID FROM KRCR_PARM_T
+                        WHERE NMSPC_CD = 'KR-KRAD'
+                          AND CMPNT_CD = 'Lookup'
+                          AND PARM_NM = 'RESULTS_LIMIT')
+/

657b713e5449dd353588e081efa74ed83e4f8185
Deep Moteria <dmoteria@vivantech.com>
2013-09-16 08:06:47 +0000

    KULRICE-10471 : Create Smoke Tests for KRAD Component Library - Containers Accordion Group Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/library/containers/DemoLibraryContainerAccordionSmokeTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.library.containers;
+
+import org.junit.Test;
+
+import edu.samplu.common.Failable;
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.SmokeTestBase;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLibraryContainerAccordionSmokeTest extends SmokeTestBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-AccordionGroup-View&methodToCall=start
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-AccordionGroup-View&methodToCall=start";
+ 
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Containers");
+        waitAndClickByLinkText("Accordion Group");
+    }
+
+    protected void testLibraryContainerAccordion() throws Exception {
+        waitAndClickByLinkText("Accordion Item 1");
+        assertElementPresentByXpath("//div[@id='Demo-AccordionGroup-SubList1' and @style='display: block;']");
+        waitAndClickByLinkText("Accordion Item 1");
+        assertElementPresentByXpath("//div[@id='Demo-AccordionGroup-SubList1' and @style='display: none;']");
+        waitAndClickByLinkText("Accordion Item 2");
+        assertElementPresentByXpath("//div[@id='Demo-AccordionGroup-SubList2' and @style='display: block;']");
+        waitAndClickByLinkText("Accordion Item 2");
+        assertElementPresentByXpath("//div[@id='Demo-AccordionGroup-SubList2' and @style='display: none;']");
+    
+    }
+    
+    @Test
+    public void testContainerAccordionBookmark() throws Exception {
+        testLibraryContainerAccordion();
+        passed();
+    }
+
+    @Test
+    public void testContainerAccordionNav() throws Exception {
+        testLibraryContainerAccordion();
+        passed();
+    }  
+}

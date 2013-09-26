73430e08b4194f0f1f7a05e2ae7e25c9110270e6
Erik Meade <github-kuali-m@eghm.net>
2013-09-26 03:08:22 +0000

    Rename AFT directory collectionfeatures to collections

--- rice-middleware/sampleapp/src/it/java/edu/samplu/krad/library/collections/DemoLibraryCollectionFeaturesColumnCalculationsSmokeTest.java
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/library/collections/DemoLibraryCollectionFeaturesColumnCalculationsSmokeTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.samplu.krad.library.collectionfeatures;
+package edu.samplu.krad.library.collections;
 
 import edu.samplu.common.SmokeTestBase;
 import org.apache.commons.lang.StringUtils;

f4261fd661aa5ea53e9dfba6b059402c712c7855
Deep Moteria <dmoteria@vivantech.com>
2013-09-27 10:35:35 +0000

    KULRICE-10477 : Create Smoke Tests for KRAD Component Library - Controls Select Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/library/controls/DemoLibraryControlSelectSmokeTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.library.controls;
+
+import org.junit.Test;
+
+import edu.samplu.common.Failable;
+import edu.samplu.common.ITUtil;
+import edu.samplu.common.SmokeTestBase;
+import edu.samplu.common.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLibraryControlSelectSmokeTest extends SmokeTestBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-SelectControl-View&methodToCall=start
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-SelectControl-View&methodToCall=start";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Controls");
+        waitAndClickByLinkText("Select");
+    }
+
+    protected void testLibraryControlSelectDefault() throws Exception {
+        assertElementPresentByXpath("//select[@name='inputField1']");
+    }
+    
+    protected void testLibraryControlSelectMultiSelect() throws Exception {
+        waitAndClickByLinkText("MultiSelect");
+        assertElementPresentByXpath("//select[@name='inputField2' and @multiple='multiple']");
+    }
+    
+    protected void testLibraryControlSelectDisabled() throws Exception {
+        waitAndClickByLinkText("Disabled");
+        assertElementPresentByXpath("//select[@name='inputField1' and @disabled='disabled']");
+    }
+    
+    protected void testLibraryControlSelectNavigation() throws Exception {
+        waitAndClickByXpath("//li[@data-tabfor='Demo-SelectControl-Example4']/a");
+        assertElementPresentByXpath("//div[@data-parent='Demo-SelectControl-Example4']/select/option[@data-location='http://www.kuali.org']");
+    }
+    
+    @Test
+    public void testControlSelectBookmark() throws Exception {
+        testLibraryControlSelectDefault();
+        testLibraryControlSelectMultiSelect();
+        testLibraryControlSelectDisabled();
+        testLibraryControlSelectNavigation();
+        passed();
+    }
+
+    @Test
+    public void testControlSelectNav() throws Exception {
+        testLibraryControlSelectDefault();
+        testLibraryControlSelectMultiSelect();
+        testLibraryControlSelectDisabled();
+        testLibraryControlSelectNavigation();
+        passed();
+    }  
+}

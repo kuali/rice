6c8c85be9012b0e3e47db2c32d84135421c56483
Deep Moteria <dmoteria@vivantech.com>
2013-10-01 10:53:26 +0000

    KULRICE-10789 : Create Functional Test for KRAD Demo Labs Lookups - with Conditional Criteria Field Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/lookups/DemoLabsLookUpConditionalCriteriaSmokeTest.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.labs.lookups;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLabsLookUpConditionalCriteriaSmokeTest extends DemoLabsLookupBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LabsLookup-ConditionalCriteriaView&hideReturnLink=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LabsLookup-ConditionalCriteriaView&hideReturnLink=true";
+
+    /**
+     *  lookupCriteria[number]
+     */
+    private static final String LOOKUP_CRITERIA_NUMBER_NAME="lookupCriteria[number]";
+    
+    /**
+     *  Search
+     */
+    private static final String SEARCH="Search";
+    
+    /**
+     *  lookupCriteria[rangeLowerBoundKeyPrefix_createDate]
+     */
+    private static final String LOOKUP_CRITERIA_DATE_NAME="lookupCriteria[rangeLowerBoundKeyPrefix_createDate]";
+    
+    /**
+     *  Not read only. Date input field present.
+     */
+    private static final String FAILURE_MESSAGE="Not read only. Date input field present.";
+    
+    /**
+     *  Date Created:
+     */
+    private static final String DATE_CREATED_LABEL="Date Created:";
+    
+    /**
+     *  Date Created is a required field.
+     */
+    private static final String DATE_REQUIRED_MESSAGE="Date Created is a required field.";
+    
+    /**
+     *  LookUp Criteria Name 
+     */
+    private static final String LOOKUP_CRITERIA_NAME_NAME="lookupCriteria[name]";
+    
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToLookup("Lookup Conditional Criteria");
+    }
+
+    protected void testLookUpConditionalCriteria() throws InterruptedException {
+        //Case 1 - Date field required by number a1
+        //It requires "Search" to be clicked twice, for the date required message to show.
+        waitAndTypeByName(LOOKUP_CRITERIA_NUMBER_NAME,"a1");
+        waitAndClickButtonByText(SEARCH);
+        Thread.sleep(10000); // If we don't wait long enough we'll get concurrency issues.
+        checkForIncidentReport(SEARCH, "DemoLookUpConditionalCriteriaSmokeTest testLookUpConditionalCriteria");
+        waitAndClickButtonByText(SEARCH);
+        Thread.sleep(3000);
+        checkForIncidentReport();
+        assertTextPresent(DATE_REQUIRED_MESSAGE);
+        
+        //Case 2 - Date field read only by number a2
+        clearTextByName(LOOKUP_CRITERIA_NUMBER_NAME);
+        waitAndTypeByName(LOOKUP_CRITERIA_NUMBER_NAME,"a2");
+        waitAndTypeByName(LOOKUP_CRITERIA_NAME_NAME,"");
+        Thread.sleep(2000);
+        if(isElementPresentByName(LOOKUP_CRITERIA_DATE_NAME)) {
+            fail(FAILURE_MESSAGE);
+        }
+        
+        //Case 3 - Date field hide by number a3
+        clearTextByName(LOOKUP_CRITERIA_NUMBER_NAME);
+        waitAndTypeByName(LOOKUP_CRITERIA_NUMBER_NAME,"a3");
+        waitAndClickButtonByText(SEARCH);
+        Thread.sleep(3000);
+        assertTextNotPresent(DATE_CREATED_LABEL);
+    }
+
+    @Test
+    public void testLookUpConditionalCriteriaBookmark() throws Exception {
+        testLookUpConditionalCriteria();
+        passed();
+    }
+
+    @Test
+    public void testLookUpConditionalCriteriaNav() throws Exception {
+        testLookUpConditionalCriteria();
+        passed();
+    }
+}

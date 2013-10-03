093f65f40a421493d491a712741ff89de1889628
Deep Moteria <dmoteria@vivantech.com>
2013-10-03 10:34:42 +0000

    KULRICE-10797 : Create Functional Test for KRAD Demo Labs Lookups - Inquiry Disabled Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/lookups/DemoLabsLookupInquiryDisabledSmokeTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2006-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.labs.lookups;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+
+public class DemoLabsLookupInquiryDisabledSmokeTest extends DemoLabsLookupBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LabsLookup-InquiryDisabledView&hideReturnLink=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LabsLookup-InquiryDisabledView&hideReturnLink=true";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToLookup("Lookup Inquiry Disabled");
+    }
+
+    @Test
+    public void testLabsLookupInquiryDisabledBookmark() throws Exception {
+        testLabsLookupInquiryDisabled();
+        passed();
+    }
+
+    @Test
+    public void testLabsLookupInquiryDisabledNav() throws Exception {
+        testLabsLookupInquiryDisabled();
+        passed();
+    }
+    
+    protected void testLabsLookupInquiryDisabled()throws Exception {
+        waitAndClickButtonByText("Search");
+        Thread.sleep(2000);
+        if(isElementPresentByXpath("//a[contains(text(), 'a1')]"))
+        {
+            fail("Inquiry has not been disabled.");
+        }
+    }
+}

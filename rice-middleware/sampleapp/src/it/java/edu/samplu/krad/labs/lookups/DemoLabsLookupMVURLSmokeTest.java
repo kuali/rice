58155427e04b97991f0b367c72e4860e42057aac
Deep Moteria <dmoteria@vivantech.com>
2013-10-04 06:14:55 +0000

    KULRICE-10800 : Create Functional Test for KRAD Demo Labs Lookups - MV URL Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/labs/lookups/DemoLabsLookupMVURLSmokeTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.labs.lookups;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+
+public class DemoLabsLookupMVURLSmokeTest extends DemoLabsLookupBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LabsLookup-ResultsLimitView&hideReturnLink=true&multipleValuesSelect=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LabsLookup-ResultsLimitView&hideReturnLink=true&multipleValuesSelect=true";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToLookup("Lookup MV URL");
+    }
+
+    @Test
+    public void testLabsLookupMVURLBookmark() throws Exception {
+        testLabsLookupMVURL();
+        passed();
+    }
+
+    @Test
+    public void testLabsLookupMVURLNav() throws Exception {
+        testLabsLookupMVURL();
+        passed();
+    }
+    
+    protected void testLabsLookupMVURL()throws Exception {
+        waitAndTypeByName("lookupCriteria[name]","*");
+        waitAndTypeByName("lookupCriteria[fiscalOfficer.principalName]","fra*");
+        waitAndClickButtonByText("Search");
+        Thread.sleep(3000);
+        assertTextPresent("4 items retrieved, displaying all items.");
+    }
+}

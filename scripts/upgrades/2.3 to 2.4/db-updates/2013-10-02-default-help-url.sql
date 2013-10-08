2549fd5b1e965735594e9f2014c3fe6fdbe46928
Dan Seibert <dseibert@vivantech.com>
2013-10-08 16:35:20 +0000

    KULRICE-9252 Removed default help for inquiry and maintenance views.

--- scripts/upgrades/2.3 to 2.4/db-updates/2013-10-02-default-help-url.sql
+++ scripts/upgrades/2.3 to 2.4/db-updates/2013-10-02-default-help-url.sql
@@ -20,20 +20,6 @@ INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_
 VALUES ('KR-KRAD', 'Lookup', 'DEFAULT_HELP_URL','69A9BABE4A0FBD56E0404F8189D82B0D', 1, 'HELP',
         'http://site.kuali.org/rice/latest/reference/html/Help.html', 'Default External Help Url', 'A', 'KUALI')
 /
-INSERT INTO KRCR_CMPNT_T (NMSPC_CD, CMPNT_CD, OBJ_ID, VER_NBR, NM, ACTV_IND)
-VALUES ('KR-KRAD', 'Inquiry', '69A9BABE4A0FBD56E0404F8189D82B0C', 1, 'Inquiry','Y')
-/
-INSERT INTO KRCR_CMPNT_T (NMSPC_CD, CMPNT_CD, OBJ_ID, VER_NBR, NM, ACTV_IND)
-VALUES ('KR-SAP', 'Maintenance', '69A9BABE4A0FBD56E0404F8189D82B0B', 1, 'Maintenance','Y')
-/
-INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KR-KRAD', 'Inquiry', 'DEFAULT_HELP_URL','69A9BABE4A0FBD56E0404F8189D82B09', 1, 'HELP',
-        'http://site.kuali.org/rice/latest/reference/html/Help.html', 'Default External Help Url', 'A', 'KUALI')
-/
-INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KR-KRAD', 'Maintenance', 'DEFAULT_HELP_URL','69A9BABE4A0FBD56E0404F8189D82B08', 1, 'HELP',
-        'http://site.kuali.org/rice/latest/reference/html/Help.html', 'Default External Help Url', 'A', 'KUALI')
-/
 -- fixes config parameter for kitchen sink help test
 INSERT INTO KRCR_CMPNT_T (NMSPC_CD, CMPNT_CD, OBJ_ID, VER_NBR, NM, ACTV_IND)
 VALUES ('KR-SAP', 'TestComponent', '69A9BABE4A0FBD56E0404F8189D82B0A', 1, 'Test Component','Y')

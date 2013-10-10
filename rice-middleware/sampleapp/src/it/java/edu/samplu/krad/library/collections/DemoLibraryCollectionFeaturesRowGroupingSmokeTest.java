73a385ac66cd2855ff29f0691ffacdf155f123e4
Deep Moteria <dmoteria@vivantech.com>
2013-10-10 06:27:25 +0000

    KULRICE-10499 : Create Smoke Tests for KRAD Component Library - Collection Row Grouping Completed

---
+++ rice-middleware/sampleapp/src/it/java/edu/samplu/krad/library/collections/DemoLibraryCollectionFeaturesRowGroupingSmokeTest.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.samplu.krad.library.collections;
+
+import org.junit.Test;
+
+import edu.samplu.common.SmokeTestBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLibraryCollectionFeaturesRowGroupingSmokeTest extends SmokeTestBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-TableLayoutGrouping-View
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-TableLayoutGrouping-View";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Collection Features");
+        waitAndClickByLinkText("Row Grouping");
+    }
+
+    protected void testCollectionFeaturesRowGroupingBaseFunctionality() throws Exception {
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow a']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='a']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow b']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='b']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow c']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section1']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='c']");
+    }
+    
+    protected void testCollectionFeaturesRowGrouping2FieldGrouping() throws Exception {
+        selectByName("exampleShown", "2 Field Grouping");
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow 2001-fall']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-groupvalue='2001-fall']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow 2001-spring']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-groupvalue='2001-spring']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow 2002-fall']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section2']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-groupvalue='2002-fall']");
+    }
+    
+    protected void testCollectionFeaturesRowGroupingOmitGroupField() throws Exception {
+        selectByName("exampleShown", "Omit Group Field");
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow a']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='a']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow b']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='b']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow c']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='c']");
+        if(isElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section3']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-groupvalue]"))
+        {
+            fail("Group Field Not Omited.");
+        }
+    }
+    
+    protected void testCollectionFeaturesRowGroupingAddToGrouped() throws Exception {
+        selectByName("exampleShown", "Add to Grouped");
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section4']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow a']");
+        assertElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section4']/div[@class='dataTables_wrapper']/table/tbody/tr[@data-group='a']/td/div/span[contains(text(), 'A')]");
+    }
+    
+    protected void testCollectionFeaturesRowGroupingPrefixOption() throws Exception {
+        selectByName("exampleShown", "Prefix option");
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section5']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow a']/td[contains(text(),'Lines with value A')]");
+    }
+    
+    protected void testCollectionFeaturesRowGroupingCustomGroupTitle() throws Exception {
+        selectByName("exampleShown", "Custom Group title");
+        waitForElementPresentByXpath("//div[@id='Demo-TableLayoutGrouping-Section6']/div[@class='dataTables_wrapper']/table/tbody/tr[@class='uif-groupRow letter-a-in-item']/td[contains(text(),'Letter A in item')]");
+    }
+    
+    @Test
+    public void testCollectionFeaturesRowGroupingBookmark() throws Exception {
+        testCollectionFeaturesRowGroupingBaseFunctionality();
+        testCollectionFeaturesRowGrouping2FieldGrouping();
+        testCollectionFeaturesRowGroupingOmitGroupField();
+        testCollectionFeaturesRowGroupingAddToGrouped();
+        testCollectionFeaturesRowGroupingPrefixOption();
+        testCollectionFeaturesRowGroupingCustomGroupTitle();
+        passed();
+    }
+
+    @Test
+    public void testCollectionFeaturesRowGroupingNav() throws Exception {
+        testCollectionFeaturesRowGroupingBaseFunctionality();
+        testCollectionFeaturesRowGrouping2FieldGrouping();
+        passed();
+    }  
+}

aad931eb6bc97b4742942949ee7412893e04e6ee
Eric Westfall <ewestfal@iu.edu>
2013-10-14 19:28:36 +0000

    KULRICE-10952 - fixed a precision-related bug in InactivableFromToUtils so that active date changes work properly immediately after a change has occurred

--- rice-middleware/core/api/src/main/java/org/kuali/rice/core/api/mo/common/active/InactivatableFromToUtils.java
+++ rice-middleware/core/api/src/main/java/org/kuali/rice/core/api/mo/common/active/InactivatableFromToUtils.java
@@ -24,16 +24,21 @@ public final class InactivatableFromToUtils {
     }
 
     public static boolean isActive(DateTime activeFromDate, DateTime activeToDate, DateTime activeAsOfDate) {
-        long asOfDate = System.currentTimeMillis();
-        if (activeAsOfDate != null) {
-            asOfDate = activeAsOfDate.getMillis();
+        if (activeAsOfDate == null) {
+            activeAsOfDate = DateTime.now();
         }
-
-        return computeActive(activeFromDate, activeToDate, asOfDate);
+        return computeActive(activeFromDate, activeToDate, activeAsOfDate);
     }
 
-    private static boolean computeActive(DateTime activeFromDate, DateTime activeToDate, long asOfDate) {
-        return (activeFromDate == null || asOfDate >= activeFromDate.getMillis()) &&
-                (activeToDate == null || asOfDate < activeToDate.getMillis());
+    private static boolean computeActive(DateTime activeFromDate, DateTime activeToDate, DateTime activeAsOfDate) {
+        // the precision of this check should be to the second, not milliseconds, so we want to chop off any
+        // milliseconds and do a ceiling of our seconds. Sometimes changes are made in near real time after a record
+        // becomes activated or inactivated so we want to have the best result possible if they are still within the
+        // same second, so we essentially always round up to ensure that this check will never fail in high throughput
+        // environments
+        activeAsOfDate = activeAsOfDate.secondOfDay().roundCeilingCopy();
+        return (activeFromDate == null || activeAsOfDate.getMillis() >= activeFromDate.getMillis()) &&
+                (activeToDate == null || activeAsOfDate.getMillis() < activeToDate.getMillis());
     }
+
 }

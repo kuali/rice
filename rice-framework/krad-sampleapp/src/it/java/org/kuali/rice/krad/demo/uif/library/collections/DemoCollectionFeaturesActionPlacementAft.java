6248362bb33c200aaad805e1ee50d1966d4f223b
Deep Moteria <dmoteria@vivantech.com>
2013-10-22 09:08:02 +0000

    KULRICE-10501 : Create Smoke Tests for KRAD Component Library - Collection Select Line Completed KULRICE-10501 : Create Smoke Tests for KRAD Component Library - Collection Action Placement Completed

---
+++ rice-framework/krad-sampleapp/src/it/java/org/kuali/rice/krad/demo/uif/library/collections/DemoCollectionFeaturesActionPlacementAft.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.uif.library.collections;
+
+import org.junit.Test;
+
+import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoCollectionFeaturesActionPlacementAft extends AutomatedFunctionalTestBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-CollectionActionPlacement-View
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-CollectionActionPlacement-View";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Collection Features");
+        waitAndClickByLinkText("Action Placement");
+    }
+
+    protected void testCollectionFeaturesActionPlacement() throws Exception {
+    	waitForElementPresentByXpath("//table/tbody/tr[1]/td[1]/div/fieldset/div/div[@class='uif-horizontalBoxLayout clearfix']/button");
+    	waitForElementPresentByXpath("//table/tbody/tr[1]/td[2]/div/fieldset/div/div[@class='uif-horizontalBoxLayout clearfix']/button");
+    }
+    
+    @Test
+    public void testCollectionFeaturesActionPlacementBookmark() throws Exception {
+        testCollectionFeaturesActionPlacement();
+        passed();
+    }
+
+    @Test
+    public void testCollectionFeaturesActionPlacementNav() throws Exception {
+        testCollectionFeaturesActionPlacement();
+        passed();
+    }  
+}

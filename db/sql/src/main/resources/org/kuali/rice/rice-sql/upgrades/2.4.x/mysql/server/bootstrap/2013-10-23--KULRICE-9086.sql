db140c8aaef82885c079037c6768498d778905a8
Jeff Caddel <jcaddel@rsmart.com>
2013-10-23 04:34:08 +0000

    Automated license header update

--- db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.x/mysql/server/bootstrap/2013-10-23--KULRICE-9086.sql
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.x/mysql/server/bootstrap/2013-10-23--KULRICE-9086.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 -- KULRICE-9086 - Adding a parameter for the maximum number of rows that will be displayed in the lookup results
 
 INSERT INTO KRCR_PARM_T (OBJ_ID, NMSPC_CD, CMPNT_CD, PARM_NM, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)

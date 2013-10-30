f0365b4de4ab2fc0fb8913203cfebb5650ad4548
Travis Schneeberger <tschneeb@gmail.com>
2013-10-30 01:10:42 +0000

    KULRICE-9513 cleanup

--- development-tools/src/main/java/org/kuali/rice/devtools/jpa/eclipselink/conv/common/CommonUtil.java
+++ development-tools/src/main/java/org/kuali/rice/devtools/jpa/eclipselink/conv/common/CommonUtil.java
@@ -1,9 +1,5 @@
 package org.kuali.rice.devtools.jpa.eclipselink.conv.common;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Class to hold generic helper methods.
  */
@@ -14,15 +10,5 @@ public final class CommonUtil {
 
     public static String toFilePath(String className, String projectDir, String sourceDir) {
         return projectDir + sourceDir + "/" + className.replace('.', '/') + ".java";
-    }
-
-    public static Collection<String> toFilePaths(Collection<String> classNames, String projectDir, String sourceDir) {
-        Set<String> filePaths = new HashSet<String>();
-
-        for (String clazz : classNames) {
-            filePaths.add(toFilePath(clazz, projectDir, sourceDir));
-        }
-
-        return filePaths;
     }
 }

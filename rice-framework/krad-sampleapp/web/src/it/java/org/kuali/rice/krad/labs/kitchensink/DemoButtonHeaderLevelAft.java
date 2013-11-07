c2a9cd8bff8c47b24ffcb04b8aa06cb6d7fd4df8
Deep Moteria <dmoteria@vivantech.com>
2013-11-07 05:07:32 +0000

    KULRICE-10946 : Create Automated Functional (Smoke) Tests for KRAD Labs - Kitchen Sink Button/Header Levels Completed

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/kitchensink/DemoButtonHeaderLevelAft.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.kitchensink;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+
+public class DemoButtonHeaderLevelAft extends DemoKitchenSinkBase {
+
+    public static final String BOOKMARK_URL = "/kr-krad/uicomponents?viewId=UifCompView&formKey=a9ce648c-c2e6-408f-ab0c-7628f99ee6a0&cacheKey=oc9kb09wqws0vik3bdahh&pageId=UifCompView-Page8#UifCompView-Page8";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+	@Override
+	protected void navigate() throws Exception {
+		navigateToKitchenSink("Button/Header Levels");
+	}
+	
+	@Test
+    public void testButtonHeaderLevelBookmark() throws Exception {
+        testButtonHeaderLevel();
+        passed();
+    }
+
+    @Test
+    public void testButtonHeaderLevelNav() throws Exception {
+        testButtonHeaderLevel();
+        passed();
+    }
+    
+    protected void testButtonHeaderLevel() throws InterruptedException 
+    {
+    	//Buttons
+    	waitAndClickByXpath("//button[@id='Demo-Primary-Action1' and @class='btn btn-primary uif-primaryActionButton uif-boxLayoutVerticalItem clearfix']");
+    	alertAccept();
+    	waitAndClickByXpath("//button[@id='Demo-Primary-Action2' and @class='btn btn-primary uif-primaryActionButton uif-boxLayoutVerticalItem clearfix disabled' and @disabled]");
+    	waitAndClickByXpath("//button[@id='Demo-Secondary-Action1' and @class='btn uif-secondaryActionButton uif-boxLayoutVerticalItem clearfix']");
+    	alertAccept();
+    	waitAndClickByXpath("//button[@id='Demo-Secondary-Action2' and @class='btn uif-secondaryActionButton uif-boxLayoutVerticalItem clearfix disabled' and @disabled]");
+    	assertElementPresentByXpath("//a[@id='Demo-Links-Action1' and @class='uif-actionLink uif-boxLayoutVerticalItem clearfix']");
+    	waitAndClickByXpath("//button[@id='Demo-Primary-Action3' and @class='btn btn-primary btn-small uif-smallActionButton uif-boxLayoutVerticalItem clearfix']");
+    	alertAccept();
+    	waitAndClickByXpath("//button[@id='Demo-Primary-Action4' and @class='btn btn-primary btn-small uif-smallActionButton uif-boxLayoutVerticalItem clearfix disabled' and @disabled]");
+    
+    	//Header
+    	assertElementPresentByXpath("//h3/span[contains(text(),'H3 Header Title')]");
+    	assertElementPresentByXpath("//h4/span[contains(text(),'H4 Header Title')]");
+    	assertElementPresentByXpath("//h5/span[contains(text(),'H5 Header Title')]");
+    	assertElementPresentByXpath("//h6/span[contains(text(),'H6 Header Title')]");
+    	assertElementPresentByXpath("//h3/a/span[contains(text(),'H3 Header Title')]");
+    	assertElementPresentByXpath("//h4/a/span[contains(text(),'H4 Header Title')]");
+    	assertElementPresentByXpath("//h5/a/span[contains(text(),'H5 Header Title')]");
+    	assertElementPresentByXpath("//h6/a/span[contains(text(),'H6 Header Title')]");
+    }
+}

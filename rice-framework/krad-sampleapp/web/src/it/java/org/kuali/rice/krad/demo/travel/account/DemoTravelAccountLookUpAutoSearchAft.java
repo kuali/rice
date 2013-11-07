165cab9c2a0376685ab4638429cbee0af91e0b5c
Erik Meade <github-kuali-m@eghm.net>
2013-11-07 04:25:48 +0000

    Move DemoTravelAccountLookupAutoSearchAft out of sub package to see if it will get run

--- rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/travel/account/DemoTravelAccountLookUpAutoSearchAft.java
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/travel/account/DemoTravelAccountLookUpAutoSearchAft.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.krad.demo.travel.account.lookup.autosearch;
+package org.kuali.rice.krad.demo.travel.account;
 
 import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
 import org.junit.Test;

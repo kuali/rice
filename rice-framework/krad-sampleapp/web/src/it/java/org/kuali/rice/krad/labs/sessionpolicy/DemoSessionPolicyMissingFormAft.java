4b3813644febae53af471b0beb0a7a433d7fe8c5
Deep Moteria <dmoteria@vivantech.com>
2013-11-18 04:42:31 +0000

    KULRICE-10854 : Create Automated Functional (Smoke) Tests for KRAD Labs - Session Policy Redirect Home - Completed KULRICE-10855 : Create Automated Functional (Smoke) Tests for KRAD Labs - Session Policy Redirect URL View - Completed KULRICE-10856 : Create Automated Functional (Smoke) Tests for KRAD Labs - Session Policy Timeout View - Completed KULRICE-10858 : Create Automated Functional (Smoke) Tests for KRAD Labs - Session Policy Missing Form - Completed
    Added a base class for the same.

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/sessionpolicy/DemoSessionPolicyMissingFormAft.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.sessionpolicy;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoSessionPolicyMissingFormAft extends DemoLabsSessionPolicyBase {
+
+    /**
+     * /kr-krad/sessionPolicy?viewId=Lab-SessionPolicy-MissingForm
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/sessionPolicy?viewId=Lab-SessionPolicy-MissingForm";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	navigateToSessionPolicy("Session Policy Missing Form");
+    }
+
+    protected void testDemoSessionPolicyMissingForm() throws InterruptedException {
+    	waitAndClickByLinkText("Post Missing Form");
+    	Thread.sleep(3000);
+    	assertTextPresent("Your Session has timed out and the request could not be completed.");
+    }
+
+    @Test
+    public void testDemoSessionPolicyMissingFormBookmark() throws Exception {
+    	testDemoSessionPolicyMissingForm();
+        passed();
+    }
+
+    @Test
+    public void testDemoSessionPolicyMissingFormNav() throws Exception {
+    	testDemoSessionPolicyMissingForm();
+        passed();
+    }
+}
---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/sessionpolicy/DemoSessionPolicyTimeoutViewAft.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.sessionpolicy;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoSessionPolicyTimeoutViewAft extends DemoLabsSessionPolicyBase {
+
+    /**
+     * /kr-krad/sessionPolicy?viewId=Lab-SessionPolicy-TimeoutView
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/sessionPolicy?viewId=Lab-SessionPolicy-TimeoutView";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	navigateToSessionPolicy("Session Policy Timeout View");
+    }
+
+    protected void testDemoSessionPolicyTimeoutView() throws InterruptedException {
+    	waitForElementPresentByXpath("//button[contains(text(),'Kill Session')]");
+    	assertElementPresentByXpath("//button[contains(text(),'Ajax Request')]");
+    	assertElementPresentByXpath("//button[contains(text(),'NonAjax Request')]");
+    	waitAndClickByXpath("//button[contains(text(),'Kill Session')]");
+    	waitAndClickByXpath("//button[contains(text(),'Ajax Request')]");
+    	Thread.sleep(3000);
+    	waitAndTypeByName("login_user","admin");
+    	waitAndClickByXpath("//button[@id='Rice-LoginButton']");
+    	Thread.sleep(3000);
+    	assertTextPresent("Your Session has timed out and the request could not be completed.");
+    }
+
+    @Test
+    public void testDemoSessionPolicyTimeoutViewBookmark() throws Exception {
+    	testDemoSessionPolicyTimeoutView();
+        passed();
+    }
+
+    @Test
+    public void testDemoSessionPolicyTimeoutViewNav() throws Exception {
+    	testDemoSessionPolicyTimeoutView();
+        passed();
+    }
+}

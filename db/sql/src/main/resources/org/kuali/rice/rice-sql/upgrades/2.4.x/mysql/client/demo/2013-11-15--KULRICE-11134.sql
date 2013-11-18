4d9ad1fc41bc71157c3270471d6cd20837a16547
Mark Fyffe <mark.w.fyffe@gmail.com>
2013-11-18 12:41:56 +0000

    trunk merge

---
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.x/mysql/client/demo/2013-11-15--KULRICE-11134.sql
@@ -0,0 +1,55 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+-- KULRICE-11134: samples for I31, I32
+-- adding permissions and role assignment to unmask TravelerDetail phoneNumber field
+
+
+INSERT INTO KRIM_PERM_T (PERM_ID, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND, OBJ_ID, VER_NBR)
+VALUES('KRSAP2100', '27', 'KR-SYS', 'Full Unmask TravelerDetail Phone', 'Authorizes users to view the entire Phone Number on the Travel Detail document and inquiry.','Y', uuid(), 1)
+/
+
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL, OBJ_ID, VER_NBR)
+VALUES('KRSAP2101', 'KRSAP2100', '11', '5', 'TravelerDetail', uuid(), 1)
+/
+
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL, OBJ_ID, VER_NBR)
+VALUES('KRSAP2102', 'KRSAP2100', '11', '6', 'phoneNumber', uuid(), 1)
+/
+
+INSERT INTO KRIM_PERM_T (PERM_ID, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND, OBJ_ID, VER_NBR)
+VALUES('KRSAP2200', '28', 'KR-SYS', 'Partial Unmask TravelerDetail Phone', 'Authorizes users to paritally view the Phone Number on the Travel Detail document and inquiry.','Y', uuid(), 1)
+/
+
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL, OBJ_ID, VER_NBR)
+VALUES('KRSAP2201', 'KRSAP2200', '11', '5', 'TravelerDetail', uuid(), 1)
+/
+
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL, OBJ_ID, VER_NBR)
+VALUES('KRSAP2202', 'KRSAP2200', '11', '6', 'phoneNumber', uuid(), 1)
+/
+
+INSERT INTO KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND)
+VALUES('KRSAP2000', uuid(), 1, 'KRSAP10003', 'KRSAP2100', 'Y')
+/
+
+INSERT INTO KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND)
+VALUES('KRSAP2001', uuid(), 1, 'KRSAP10004', 'KRSAP2200', 'Y')
+/
+
+
+
+

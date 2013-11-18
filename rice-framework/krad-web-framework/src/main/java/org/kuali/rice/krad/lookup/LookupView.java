88b454d48efb3c03f3b473cbca1fd1d5456fde0a
Mark Fyffe <mark.w.fyffe@gmail.com>
2013-11-18 11:42:42 +0000

    trunk merge

--- rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/lookup/LookupView.java
+++ rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/lookup/LookupView.java
@@ -70,6 +70,9 @@ public class LookupView extends FormView {
     private List<Component> criteriaFields;
     private Group criteriaGroup;
 
+    @RequestParameter
+    private boolean hideCriteriaOnSearch;
+
     private List<Component> resultFields;
     private CollectionGroup resultsGroup;
 
@@ -153,8 +156,12 @@ public class LookupView extends FormView {
             criteriaGroup.getFooter().setRender(false);
         }
 
-        if (!renderLookupCriteria) {
+        if (!renderLookupCriteria || (hideCriteriaOnSearch && lookupForm.isDisplayResults())) {
             criteriaGroup.setRender(false);
+        }
+
+        if (hideCriteriaOnSearch && !lookupForm.isDisplayResults()) {
+            resultsGroup.setRender(false);
         }
 
         boolean returnLinkAllowed = false;
@@ -576,6 +583,14 @@ public class LookupView extends FormView {
         this.criteriaGroup = criteriaGroup;
     }
 
+    public boolean isHideCriteriaOnSearch() {
+        return hideCriteriaOnSearch;
+    }
+
+    public void setHideCriteriaOnSearch(boolean hideCriteriaOnSearch) {
+        this.hideCriteriaOnSearch = hideCriteriaOnSearch;
+    }
+
     /**
      * List of fields that will be rendered for the result collection group, each field will be a column
      * (assuming table layout is used).
@@ -835,6 +850,8 @@ public class LookupView extends FormView {
             List<Component> criteriaFieldsCopy = ComponentUtils.copy(criteriaFields);
             lookupViewCopy.setCriteriaFields(criteriaFieldsCopy);
         }
+
+        lookupViewCopy.setHideCriteriaOnSearch(this.hideCriteriaOnSearch);
 
         if (this.resultFields != null) {
             List<Component> resultFieldsCopy = ComponentUtils.copy(resultFields);

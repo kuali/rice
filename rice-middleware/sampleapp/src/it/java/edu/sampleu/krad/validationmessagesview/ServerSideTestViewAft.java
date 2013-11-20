e2938d5d26001d2544e7af1865272b15460c3ff8
Deep Moteria <dmoteria@vivantech.com>
2013-11-20 12:48:46 +0000

    Package name modified

--- rice-middleware/sampleapp/src/it/java/edu/sampleu/krad/validationmessagesview/ServerSideTestViewAft.java
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/krad/validationmessagesview/ServerSideTestViewAft.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.sampleu.travel;
+package edu.sampleu.krad.validationmessagesview;
 
 import com.thoughtworks.selenium.SeleneseTestBase;
 import org.junit.Test;

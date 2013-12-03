62eabe9327d108784f768239994dfbb7cf619ab6
Deep Moteria <dmoteria@vivantech.com>
2013-12-03 06:40:03 +0000

    KULRICE-10794 : Create Functional Test for KRAD Demo Labs Lookups - With User Control - Completed

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/lookups/LabsLookupWithUserControlAft.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2006-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.lookups;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+
+public class LabsLookupWithUserControlAft extends LabsLookupBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LabsGroupControl-LookupView&hideReturnLink=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LabsUserControl-LookupView";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToLookup("Lookup with User Control");
+    }
+
+    @Test
+    public void testLabsLookupWithUserControlBookmark() throws Exception {
+        testLabsLookupWithUserControl();
+        passed();
+    }
+
+    @Test
+    public void testLabsLookupWithUserControlNav() throws Exception {
+        testLabsLookupWithUserControl();
+        passed();
+    }
+    
+    protected void testLabsLookupWithUserControl()throws Exception {
+       waitAndTypeByName("lookupCriteria[myPrincipalName]","admin");
+       waitAndClickButtonByText("Search");
+       Thread.sleep(3000);
+       assertTextPresent("1100");
+       assertTextPresent("admin");
+       waitAndClickButtonByText("Clear Values");
+       Thread.sleep(2000);
+       waitAndClickButtonByText("Search");
+       Thread.sleep(3000);
+       assertTextPresent("Showing 1 to 10 of");
+    }
+}

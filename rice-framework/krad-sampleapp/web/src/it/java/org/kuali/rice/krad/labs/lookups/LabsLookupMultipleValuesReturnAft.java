b3b18cab58833613d266904ca43c2e231b65de8c
Deep Moteria <dmoteria@vivantech.com>
2013-12-04 08:13:02 +0000

    KULRICE-11337 : Create Automated Functional Tests for KRAD Labs - Lookup Multiple Values Return

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/lookups/LabsLookupMultipleValuesReturnAft.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2006-2012 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.lookups;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsLookupMultipleValuesReturnAft extends LabsLookupBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LabsLookup-MultipleValuesReturnView
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LabsLookup-MultipleValuesReturnView";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToLookup("Lookup Multiple Values Return");
+    }
+
+    @Test
+    public void testLabsLookupMultipleValuesReturnBookmark() throws Exception {
+        testLabsLookupMultipleValuesReturn();
+        passed();
+    }
+
+    @Test
+    public void testLabsLookupMultipleValuesReturnNav() throws Exception {
+        testLabsLookupMultipleValuesReturn();
+        passed();
+    }
+    
+    protected void testLabsLookupMultipleValuesReturn()throws Exception {
+    	waitAndClickByLinkText("Lookup/Add Multiple Lines");
+    	gotoIframeByXpath("//iframe[@class='fancybox-iframe']");
+        waitAndTypeByName("lookupCriteria[name]","*");
+        waitAndTypeByName("lookupCriteria[fiscalOfficer.principalName]","eri*");
+        waitAndClickButtonByText("Search");
+        assertResultCount("6");
+        waitAndClickByXpath("//input[@type='checkbox' and @value='CAT:Travel Account 10:a10']");
+        waitAndClickByXpath("//input[@type='checkbox' and @value='EAT:Travel Account 11:a11']");
+        waitAndClickButtonByText("return selected");
+    }
+}

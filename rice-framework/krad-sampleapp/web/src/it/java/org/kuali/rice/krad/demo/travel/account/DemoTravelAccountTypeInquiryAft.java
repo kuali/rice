3acd8175203df06c56acfe917160e33edaf4180f
Deep Moteria <dmoteria@vivantech.com>
2013-12-05 06:18:44 +0000

    KULRICE-10867 : Create Automated Functional (Smoke) Tests for KRAD Demo - Travel Application:Travel Account Type Inquiry

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/travel/account/DemoTravelAccountTypeInquiryAft.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.travel.account;
+
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoTravelAccountTypeInquiryAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/inquiry?methodToCall=start&accountTypeCode=IAT&dataObjectClassName=org.kuali.rice.krad.demo.travel.dataobject.TravelAccountType
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/inquiry?methodToCall=start&accountTypeCode=IAT&dataObjectClassName=org.kuali.rice.krad.demo.travel.dataobject.TravelAccountType";
+    
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-DemoLink", "");
+        waitAndClickByLinkText("Travel Account Type Inquiry");
+    }
+
+    @Test
+    public void testTravelAccountTypeInquiryNav() throws Exception {
+        testTravelAccountTypeInquiry();
+        passed();
+    }
+
+    @Test
+    public void testTravelAccountTypeInquiryBookmark() throws Exception {
+        testTravelAccountTypeInquiry();
+        passed();
+    }
+    
+    protected void testTravelAccountTypeInquiry() throws Exception {
+        waitForElementPresentByXpath("//a[contains(text(),'IAT')]");
+        assertTextPresent("Travel Account Type Code:");
+        assertTextPresent("Account Type Name:");
+        assertTextPresent("Account Type:");
+        assertTextPresent("IAT - Income");
+     }
+}

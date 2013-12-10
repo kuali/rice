963895ab6256d8878c322a848d135a0d7283d36b
Deep Moteria <dmoteria@vivantech.com>
2013-12-10 06:09:05 +0000

    KULRICE-11315 : Create Automated Functional Tests for KRAD Labs - Inquiry - AttributionDefinition Formatting

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/inquiries/LabsInquiryAttributionDefinitionFormattingAft.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.inquiries;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsInquiryAttributionDefinitionFormattingAft extends LabsInquiryBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=KradInquirySample-PageR2C1
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=KradInquirySample-PageR2C1";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	navigateToInquiry("Inquiry - AttributionDefinition Formatting");
+    }
+
+    protected void testInquiryAttributionDefinitionFormatting() throws InterruptedException {
+    	
+    	// Inquiry - AttributionDefinition Formatting
+    	waitAndClickByLinkText("Inquiry - AttributionDefinition Formatting");
+        
+    	// Lightbox
+        waitAndClickLinkContainingText("10000");
+        gotoLightBox();
+        String[][] lightBoxLabeledText = {{"ID:", "10000"},
+                                          {"Travel Authorization Document Id:", "10000"},
+                                          {"Travel Company Name:", "Discount Travel"},
+                                          {"Expense Type:", "ME"},
+                                          {"Expense Description:", "Family Related"}, 
+                                          {"Expense Date:", "12/09/2013"},
+                                          {"Expense Amount:", "1,278.97"},
+                                          {"Reimbursable:", "true"},
+                                          {"Taxable:", "true"}};
+        assertLabeledTextPresent(lightBoxLabeledText);
+        clickCollapseAll();
+        assertLabeledTextNotPresent(lightBoxLabeledText);
+        clickExpandAll();
+        assertLabeledTextPresent(lightBoxLabeledText);
+        waitAndClickButtonByText("Close");
+        selectTopFrame();
+        assertLabeledTextPresent(lightBoxLabeledText);
+        waitAndClickButtonByText("< Back");
+        
+        //Inquiry - AttributionDefinition Formatting (Partial Attribute Masking, Additional Display Attribute Name)
+        waitAndClickByLinkText("Inquiry - AttributionDefinition Formatting (Partial Attribute Masking, Additional Display Attribute Name)");
+        String[][] LabeledText = {{"Id:", "1"},
+                {"Document Number:", "??"},
+                {"Principal Id:", "fred"},
+                {"Traveler Name:", ""},
+                {"Traveler User ID:", ""}, 
+                {"First and Last Name (additionalDisplayAttributeName example):", "Test *-* Traveler"},
+                {"Middle Name:", "A"},
+                {"Last Name:", "Traveler"},
+                {"Street Address Line1:", "123 Nowhere St."},
+                {"Street Address Line2:", ""},
+                {"City Name:", "Davis"},
+                {"State Code:", "CA"},
+                {"Zip Code:", "95616"},
+                {"Country Code:", "US"},
+                {"Citizenship:", "US"},
+                {"Email Address:", ""},
+                {"Gender:", "M"},
+                {"Phone Number:", "******1212"},
+                {"Traveler Type Code:", "123"},
+                {"Customer Number:", "CUST"},
+                {"Drivers License:", "CA12345678"},
+                {"Drivers License Exp Date:", "06/30/2015"},
+                {"Active:", "true"},
+                {"Non Resident Alien:", "false"},
+                {"Liability Insurance:", "false"},};
+        assertLabeledTextPresent(LabeledText);
+    }
+
+    @Test
+    public void testInquiryAttributionDefinitionFormattingBookmark() throws Exception {
+    	testInquiryAttributionDefinitionFormatting();
+        passed();
+    }
+
+    @Test
+    public void testInquiryAttributionDefinitionFormattingNav() throws Exception {
+    	testInquiryAttributionDefinitionFormatting();
+        passed();
+    }
+}

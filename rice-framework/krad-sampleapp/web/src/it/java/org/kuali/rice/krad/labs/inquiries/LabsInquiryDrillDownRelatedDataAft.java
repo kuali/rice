0d810961915117d83b9abf9050ef858a6ad3654e
Deep Moteria <dmoteria@vivantech.com>
2013-12-10 06:10:25 +0000

    KULRICE-11316 : Create Automated Functional Tests for KRAD Labs - Inquiry - Drill-down related data

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/inquiries/LabsInquiryDrillDownRelatedDataAft.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2005-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.inquiries;
+
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsInquiryDrillDownRelatedDataAft extends LabsInquiryBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=KradInquirySample-PageR3C1
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=KradInquirySample-PageR3C1";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	navigateToInquiry("Inquiry Drill-down related data");
+    }
+
+    protected void testInquiryDrillDownRelatedData() throws InterruptedException {
+    	waitAndClickByLinkText("KRAD Inquiry TravelPerDiemExpense = 10000");
+        
+    	// Lightbox
+        waitAndClickByXpath("//a[@title='Travel Destination ']");
+        gotoLightBox();
+        String[][] lightBoxLabeledText = {{"id:", "10000"},
+                                          {"Destination name:", "Colorado"},
+                                          {"Country:", "United States"},
+                                          {"State:", "Colorado"},
+                                          {"Active:", "true"}};
+        assertLabeledTextPresent(lightBoxLabeledText);
+        clickCollapseAll();
+        assertLabeledTextNotPresent(lightBoxLabeledText);
+        clickExpandAll();
+        assertLabeledTextPresent(lightBoxLabeledText);
+        waitAndClickButtonByText("Close");
+        selectTopFrame();
+        String[][] LabeledText = {{"ID:", "10000"},
+                {"Travel Authorization Document Id:", "10000"},
+                {"Primary Destination:", "10000"},
+                {"Date of Use:", "12/09/2013"},
+                {"Breakfast Value:", "10"}, 
+                {"Lunch Value:", "10"},
+                {"Dinner Value:", "15"},
+                {"Amount estimated for incidentals:", "20"},
+                {"Mileage rate:", "DO"},
+                {"Number of estimated miles:", "30"}};
+        assertLabeledTextPresent(LabeledText);
+        clickCollapseAll();
+        assertLabeledTextNotPresent(LabeledText);
+        clickExpandAll();
+        assertLabeledTextPresent(LabeledText);
+    }
+
+    @Test
+    public void testInquiryDrillDownRelatedDataBookmark() throws Exception {
+    	testInquiryDrillDownRelatedData();
+        passed();
+    }
+
+    @Test
+    public void testInquiryDrillDownRelatedDataNav() throws Exception {
+    	testInquiryDrillDownRelatedData();
+        passed();
+    }
+}

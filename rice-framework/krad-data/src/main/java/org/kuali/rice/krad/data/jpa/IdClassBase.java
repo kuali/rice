88ba42569068a1a4b231be555adf96f7e71d26b5
Eric Westfall <ewestfal@iu.edu>
2014-01-18 15:07:40 +0000

    KULRICE-9337 - created IdClassBase as a replacement for CompositePrimaryKeyBase

---
+++ rice-framework/krad-data/src/main/java/org/kuali/rice/krad/data/jpa/IdClassBase.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2006-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.rice.krad.data.jpa;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+/**
+ * A base class for creating ID class implementations for JPA.
+ *
+ * These ID classes are required to have proper equals and
+ * hashcode method implementations, so this base class provides reflection-based defaults for both of those as well as
+ * toString.
+ *
+ * @see javax.persistence.IdClass
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public abstract class IdClassBase implements Serializable {
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(obj, this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+}

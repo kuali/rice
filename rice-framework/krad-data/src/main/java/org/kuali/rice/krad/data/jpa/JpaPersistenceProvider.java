4ca011ea6d00f397b6194c6c219df74974285f82
Eric Westfall <ewestfal@iu.edu>
2014-01-19 23:21:40 +0000

    KULRICE-9798 - renabled persistence exception translation for JpaPersistenceProvider

--- rice-framework/krad-data/src/main/java/org/kuali/rice/krad/data/jpa/JpaPersistenceProvider.java
+++ rice-framework/krad-data/src/main/java/org/kuali/rice/krad/data/jpa/JpaPersistenceProvider.java
@@ -20,6 +20,7 @@ import org.kuali.rice.core.api.config.property.ConfigContext;
 import org.kuali.rice.core.api.criteria.LookupCustomizer;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.core.api.criteria.QueryResults;
+import org.kuali.rice.core.api.exception.RiceRuntimeException;
 import org.kuali.rice.krad.data.CompoundKey;
 import org.kuali.rice.krad.data.DataObjectService;
 import org.kuali.rice.krad.data.PersistenceOption;
@@ -27,24 +28,52 @@ import org.kuali.rice.krad.data.config.ConfigConstants;
 import org.kuali.rice.krad.data.metadata.DataObjectMetadata;
 import org.kuali.rice.krad.data.provider.PersistenceProvider;
 import org.kuali.rice.krad.data.provider.util.ReferenceLinker;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.support.ChainedPersistenceExceptionTranslator;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.dao.support.PersistenceExceptionTranslator;
+import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NonUniqueResultException;
 import javax.persistence.metamodel.ManagedType;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 
 /**
- * JPA PersistenceProvider impl
+ * Java Persistence API (JPA) implementation of {@link PersistenceProvider}.
+ *
+ * <p>When creating a new instance of this provider, a reference to a "shared" entity manager (like that created by
+ * Spring's {@link org.springframework.orm.jpa.support.SharedEntityManagerBean} must be injected. Additionally, a
+ * reference to the {@link DataObjectService} must be injected as well.</p>
+ *
+ * <p>This class will perform persistence exception translation (converting JPA exceptions to
+ * {@link org.springframework.dao.DataAccessException}s. It will scan the
+ * {@link org.springframework.beans.factory.BeanFactory} in which it was created to find beans which implement
+ * {@link org.springframework.dao.support.PersistenceExceptionTranslator} and use those translators for translation.</p>
+ *
+ * @see org.springframework.orm.jpa.support.SharedEntityManagerBean
+ * @see org.springframework.dao.support.PersistenceExceptionTranslator
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 @Transactional
-public class JpaPersistenceProvider implements PersistenceProvider, InitializingBean {
+public class JpaPersistenceProvider implements PersistenceProvider, InitializingBean, BeanFactoryAware {
 
     private EntityManager sharedEntityManager;
     private DataObjectService dataObjectService;
+
     private ReferenceLinker referenceLinker;
+    private PersistenceExceptionTranslator persistenceExceptionTranslator;
 
     /**
      * Initialization-on-demand holder idiom for thread-safe lazy loading of configuration.
@@ -72,71 +101,124 @@ public class JpaPersistenceProvider implements PersistenceProvider, Initializing
     }
 
     @Override
-    public <T> T save(T dataObject, PersistenceOption... options) {
-        verifyDataObjectWritable(dataObject);
-
-		Set<PersistenceOption> optionSet = Sets.newHashSet(options);
-
-		dataObject = sharedEntityManager.merge(dataObject);
-
-        if (optionSet.contains(PersistenceOption.LINK)) {
-            referenceLinker.linkObjects(dataObject);
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        if (!(beanFactory instanceof ListableBeanFactory)) {
+            throw new IllegalArgumentException(
+                    "Cannot use PersistenceExceptionTranslator autodetection without ListableBeanFactory");
         }
+        this.persistenceExceptionTranslator = detectPersistenceExceptionTranslators((ListableBeanFactory)beanFactory);
+    }
 
-        if(optionSet.contains(PersistenceOption.FLUSH) || LazyConfigHolder.autoFlush){
-			sharedEntityManager.flush();
+    protected PersistenceExceptionTranslator detectPersistenceExceptionTranslators(ListableBeanFactory beanFactory) {
+        // Find all translators, being careful not to activate FactoryBeans.
+        Map<String, PersistenceExceptionTranslator> pets = BeanFactoryUtils.beansOfTypeIncludingAncestors(beanFactory,
+                PersistenceExceptionTranslator.class, false, false);
+        ChainedPersistenceExceptionTranslator cpet = new ChainedPersistenceExceptionTranslator();
+        for (PersistenceExceptionTranslator pet : pets.values()) {
+            cpet.addDelegate(pet);
         }
-
-		return dataObject;
+        // always add one last persistence exception translator as a catch all
+        cpet.addDelegate(new DefaultPersistenceExceptionTranslator());
+        return cpet;
     }
 
     @Override
-    public <T> T find(Class<T> type, Object id) {
-        if (id instanceof CompoundKey) {
-			QueryResults<T> results = findMatching(type,
-					QueryByCriteria.Builder.andAttributes(((CompoundKey) id).getKeys()).build());
-			if (results.getResults().size() > 1) {
-				throw new NonUniqueResultException("Error Compound Key: " + id + " on class " + type.getName()
-						+ " returned more than one row.");
-			}
-            if (!results.getResults().isEmpty()) {
-				return results.getResults().get(0);
+    public <T> T save(final T dataObject, final PersistenceOption... options) {
+        return doWithExceptionTranslation(new Callable<T>() {
+            public T call() {
+                verifyDataObjectWritable(dataObject);
+
+        		Set<PersistenceOption> optionSet = Sets.newHashSet(options);
+
+		        T mergedDataObject = sharedEntityManager.merge(dataObject);
+
+                if (optionSet.contains(PersistenceOption.LINK)) {
+                    referenceLinker.linkObjects(mergedDataObject);
+                }
+
+                if(optionSet.contains(PersistenceOption.FLUSH) || LazyConfigHolder.autoFlush){
+			        sharedEntityManager.flush();
+                }
+
+        		return mergedDataObject;
             }
-			return null;
-        } else {
-            return sharedEntityManager.find(type, id);
-        }
+        });
     }
 
     @Override
-    public <T> QueryResults<T> findMatching(Class<T> type, QueryByCriteria queryByCriteria) {
-        return new JpaCriteriaQuery(sharedEntityManager).lookup(type, queryByCriteria);
+    public <T> T find(final Class<T> type, final Object id) {
+        return doWithExceptionTranslation(new Callable<T>() {
+            public T call() {
+                if (id instanceof CompoundKey) {
+			        QueryResults<T> results = findMatching(type,
+				        	QueryByCriteria.Builder.andAttributes(((CompoundKey) id).getKeys()).build());
+			        if (results.getResults().size() > 1) {
+				        throw new NonUniqueResultException("Error Compound Key: " + id + " on class " + type.getName()
+					        	+ " returned more than one row.");
+			        }
+                    if (!results.getResults().isEmpty()) {
+				        return results.getResults().get(0);
+                    }
+			        return null;
+                } else {
+                    return sharedEntityManager.find(type, id);
+                }
+            }
+        });
     }
 
     @Override
-    public <T> QueryResults<T> findMatching(Class<T> type, QueryByCriteria queryByCriteria, LookupCustomizer<T> lookupCustomizer) {
-        return new JpaCriteriaQuery(sharedEntityManager).lookup(type, queryByCriteria, lookupCustomizer);
+    public <T> QueryResults<T> findMatching(final Class<T> type, final QueryByCriteria queryByCriteria) {
+        return doWithExceptionTranslation(new Callable<QueryResults<T>>() {
+            public QueryResults<T> call() {
+                return new JpaCriteriaQuery(sharedEntityManager).lookup(type, queryByCriteria);
+            }
+        });
     }
 
     @Override
-    public void delete(Object dataObject) {
-        verifyDataObjectWritable(dataObject);
-        sharedEntityManager.remove(sharedEntityManager.merge(dataObject));
+    public <T> QueryResults<T> findMatching(final Class<T> type, final QueryByCriteria queryByCriteria,
+            final LookupCustomizer<T> lookupCustomizer) {
+        return doWithExceptionTranslation(new Callable<QueryResults<T>>() {
+            public QueryResults<T> call() {
+                return new JpaCriteriaQuery(sharedEntityManager).lookup(type, queryByCriteria, lookupCustomizer);
+            }
+        });
     }
 
     @Override
-    public boolean handles(Class<?> type) {
-        try {
-            ManagedType<?> managedType = sharedEntityManager.getMetamodel().managedType(type);
-            return managedType != null;
-        } catch (IllegalArgumentException iae) {
-            return false;
-        }
+    public void delete(final Object dataObject) {
+        doWithExceptionTranslation(new Callable<Object>() {
+            public Object call() {
+                verifyDataObjectWritable(dataObject);
+                sharedEntityManager.remove(sharedEntityManager.merge(dataObject));
+                return null;
+            }
+        });
     }
 
     @Override
-    public void flush(Class<?> type){
-        sharedEntityManager.flush();
+    public boolean handles(final Class<?> type) {
+        return doWithExceptionTranslation(new Callable<Boolean>() {
+            public Boolean call() {
+                try {
+                    ManagedType<?> managedType = sharedEntityManager.getMetamodel().managedType(type);
+                    return Boolean.valueOf(managedType != null);
+                } catch (IllegalArgumentException iae) {
+                    return Boolean.FALSE;
+                }
+            }
+        }).booleanValue();
+    }
+
+    @Override
+    public void flush(final Class<?> type) {
+        doWithExceptionTranslation(new Callable<Object>() {
+            public Object call() {
+                sharedEntityManager.flush();
+                return null;
+            }
+        });
     }
 
     protected void verifyDataObjectWritable(Object dataObject) {
@@ -147,6 +229,27 @@ public class JpaPersistenceProvider implements PersistenceProvider, Initializing
         if (metaData.isReadOnly()) {
             throw new UnsupportedOperationException(dataObject.getClass() + " is read-only");
         }
+    }
+
+    protected <T> T doWithExceptionTranslation(Callable<T> callable) {
+        try {
+            return callable.call();
+        }
+        catch (RuntimeException ex) {
+            throw DataAccessUtils.translateIfNecessary(ex, this.persistenceExceptionTranslator);
+        } catch (Exception ex) {
+            // this should really never happen based on the internal usage in this class
+            throw new RiceRuntimeException("Unexpected checked exception during data access.", ex);
+        }
+    }
+
+    private static final class DefaultPersistenceExceptionTranslator implements PersistenceExceptionTranslator {
+
+        @Override
+        public DataAccessException translateExceptionIfPossible(RuntimeException ex) {
+            return EntityManagerFactoryUtils.convertJpaAccessExceptionIfPossible(ex);
+        }
+
     }
 
 }

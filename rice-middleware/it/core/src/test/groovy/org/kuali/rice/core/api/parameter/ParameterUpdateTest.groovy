02e4feb9d9a60a8ccfc270af658b25b6f46f7c69
Shannon Hess <ShannonHess18@gmail.com>
2014-01-22 20:53:05 +0000

    KULRICE-11653 - IT Failure ParameterUpdateTest parameter should not be Null.  Creating the parameter right before the test to verify missing data is not the issue.   Will revert if this does not fix the problem.

--- rice-middleware/it/core/src/test/groovy/org/kuali/rice/core/api/parameter/ParameterUpdateTest.groovy
+++ rice-middleware/it/core/src/test/groovy/org/kuali/rice/core/api/parameter/ParameterUpdateTest.groovy
@@ -21,32 +21,52 @@ import org.kuali.rice.coreservice.framework.CoreFrameworkServiceLocator
 import org.kuali.rice.coreservice.api.parameter.Parameter
 import org.kuali.rice.krad.util.KRADConstants
 import org.kuali.rice.kew.api.KewApiConstants
+import org.kuali.rice.test.data.PerSuiteUnitTestData
+import org.kuali.rice.test.data.UnitTestData
+import org.kuali.rice.test.data.UnitTestSql
 
 import static org.junit.Assert.assertNotNull
 import org.kuali.rice.coreservice.framework.parameter.ParameterService
 import org.apache.commons.lang.time.StopWatch
 
+@PerSuiteUnitTestData(
+    value = [
+        @UnitTestData(
+            sqlStatements = [
+                @UnitTestSql("INSERT INTO KRCR_NMSPC_T(NMSPC_CD, OBJ_ID, VER_NBR, NM, ACTV_IND, APPL_ID) VALUES('KR-CACHE-TST', '5685fbb89b054a24b6174ee3213b8f91', 1, 'Test Namespace', 'Y', 'RICE')"),
+                @UnitTestSql("INSERT INTO KRCR_PARM_TYP_T(PARM_TYP_CD, OBJ_ID, VER_NBR, NM, ACTV_IND) VALUES('CACHE', '4b424baa4f51429ab43f6816c300230e', 1, 'Cache Test Type', 'Y')"),
+                @UnitTestSql("INSERT INTO KRCR_CMPNT_T(NMSPC_CD, CMPNT_CD, OBJ_ID, VER_NBR, NM, ACTV_IND) VALUES('KR-CACHE-TST', 'CMPNT-FOR-CACHE-TEST', '015a6f78a85548ac84011646e3866b6c', 1, 'CACHE-TEST', 'Y')"),
+                @UnitTestSql("INSERT INTO KRCR_PARM_T(NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID) VALUES('KR-CACHE-TST', 'CMPNT-FOR-CACHE-TEST', 'PARM-NM-FOR-CACHE-TEST', '41e65ffd34cc49a49fb2dea99266bfe2', 1, 'CACHE', 'Y', 'Parameter for cache test.', 'A', 'RICE')")
+            ]
+        )
+    ]
+)
 
 class ParameterUpdateTest extends CORETestCase {
+
+    public static final String KUALI_RICE_TEST_NAMESPACE = "KR-CACHE-TST";
+    public static final String KUALI_RICE_TEST_COMPONENT_CD = "CMPNT-FOR-CACHE-TEST";
+    public static final String KUALI_RICE_TEST_PARAMETER = "PARM-NM-FOR-CACHE-TEST";
 
     @Test
     void parameterCachingTest ( ){
 
         ParameterService parameterService = CoreFrameworkServiceLocator.getParameterService();
-        Parameter parameter = parameterService.getParameter(KRADConstants.KUALI_RICE_WORKFLOW_NAMESPACE,
-                KRADConstants.DetailTypes.BACKDOOR_DETAIL_TYPE, KewApiConstants.SHOW_BACK_DOOR_LOGIN_IND);
+        Parameter parameter = parameterService.getParameter(KUALI_RICE_TEST_NAMESPACE,
+                KUALI_RICE_TEST_COMPONENT_CD, KUALI_RICE_TEST_PARAMETER);
 
 
-        String value = parameterService.getParameterValueAsString(KRADConstants.KUALI_RICE_WORKFLOW_NAMESPACE,
-                KRADConstants.DetailTypes.BACKDOOR_DETAIL_TYPE, KewApiConstants.SHOW_BACK_DOOR_LOGIN_IND);
+        String value = parameterService.getParameterValueAsString(KUALI_RICE_TEST_NAMESPACE,
+                KUALI_RICE_TEST_COMPONENT_CD, KUALI_RICE_TEST_PARAMETER);
         assertNotNull("parameter should not be Null", parameter)
 
         //loop and get the same parameter to test caching
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         0.step(10000, 1) {
-            parameter = parameterService.getParameter(KRADConstants.KUALI_RICE_WORKFLOW_NAMESPACE,
-                KRADConstants.DetailTypes.BACKDOOR_DETAIL_TYPE, KewApiConstants.SHOW_BACK_DOOR_LOGIN_IND);
+            parameter = parameterService.getParameter(KUALI_RICE_TEST_NAMESPACE,
+                    KUALI_RICE_TEST_COMPONENT_CD, KUALI_RICE_TEST_PARAMETER);
+            assertNotNull("parameter should not be Null", parameter)
         }
         stopWatch.stop()
         LOG.info("loop time: " + stopWatch.getTime() + "ms");

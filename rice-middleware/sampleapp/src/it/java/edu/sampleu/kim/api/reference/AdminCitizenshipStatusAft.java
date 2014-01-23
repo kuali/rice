931e48247adbf294081d0dc26f6d8e5ec09198b6
Deep Moteria <dmoteria@vivantech.com>
2014-01-23 05:01:18 +0000

    KULRICE-10929 : Create Automated Functional (Smoke) Tests for Administration Reference (old sample app)

---
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/kim/api/reference/AdminCitizenshipStatusAft.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.sampleu.kim.api.reference;
+
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.kuali.rice.testtools.selenium.WebDriverUtils;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class AdminCitizenshipStatusAft extends WebDriverLegacyITBase {
+
+    /**
+     *  AutomatedFunctionalTestUtils.PORTAL + "?channelTitle=Citizenship%20Status&channelUrl=" + WebDriverUtils
+     *   .getBaseUrlString() + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.rice.kim.impl.identity.citizenship.EntityCitizenshipStatusBo&docFormKey=88888888&returnLocation=" +
+     *   AutomatedFunctionalTestUtils.PORTAL + AutomatedFunctionalTestUtils.HIDE_RETURN_LINK;
+     */
+    public static final String BOOKMARK_URL =
+            AutomatedFunctionalTestUtils.PORTAL + "?channelTitle=Citizenship%20Status&channelUrl=" + WebDriverUtils
+                    .getBaseUrlString() + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.rice.kim.impl.identity.citizenship.EntityCitizenshipStatusBo&docFormKey=88888888&returnLocation=" +
+                    AutomatedFunctionalTestUtils.PORTAL + AutomatedFunctionalTestUtils.HIDE_RETURN_LINK;
+    
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws InterruptedException {
+        waitAndClickAdministration();
+        waitAndClickByLinkText("Citizenship Status");
+    }
+    
+    private void testCitizenshipStatus() throws Exception{
+        selectFrameIframePortlet();
+        waitAndClickByXpath("//input[@name='active' and @title='Active Indicator - Both']");
+        waitAndClickByXpath("//input[@value='search']");
+        waitForTextPresent("No values match this search.");
+    }
+    
+    @Test
+    public void testCitizenshipStatusBookmark() throws Exception {
+        testCitizenshipStatus();
+        passed();
+    }
+
+    @Test
+    public void testCitizenshipStatusNav() throws Exception {
+        testCitizenshipStatus();
+        passed();
+    }
+    
+}

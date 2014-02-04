03d96984508945f79a93c92aae530c0bbe1bc03c
Sona Sona <sonam@kuali.org>
2014-02-04 17:48:21 +0000

    KULRICE-11737- AFT Failure Lookup with User/Group ClassCastException LabsGroupControl/LabsUserControl cannot be cast to BusinessObject. Removed the OJB mapping so the KRADAdapter is used and extended the Group and User Controls from AbstractMemberBo and DataObjectBase instead

--- rice-framework/krad-sampleapp/web/src/main/java/org/kuali/rice/krad/labs/LabsUserControl.java
+++ rice-framework/krad-sampleapp/web/src/main/java/org/kuali/rice/krad/labs/LabsUserControl.java
@@ -15,16 +15,98 @@
  */
 package org.kuali.rice.krad.labs;
 
+import org.kuali.rice.kim.api.identity.principal.Principal;
+import org.kuali.rice.kim.api.identity.principal.PrincipalContract;
 import org.kuali.rice.kim.impl.identity.principal.PrincipalBo;
+import org.kuali.rice.krad.bo.DataObjectBase;
+import org.kuali.rice.krad.data.jpa.PortableSequenceGenerator;
+import org.kuali.rice.krad.data.jpa.converters.BooleanYNConverter;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
+@Entity
+@Table(name = "KRIM_PRNCPL_T")
 
-public class LabsUserControl extends PrincipalBo {
+public class LabsUserControl extends DataObjectBase implements PrincipalContract {
 
     @Transient
     private String myPrincipalName;
     @Transient
     private String myPersonName;
+
+    private static final long serialVersionUID = 1L;
+
+    @PortableSequenceGenerator(name = "KRIM_PRNCPL_ID_S")
+    @GeneratedValue(generator = "KRIM_PRNCPL_ID_S")
+    @Id
+    @Column(name = "PRNCPL_ID")
+    private String principalId;
+
+    @Column(name = "PRNCPL_NM")
+    private String principalName;
+
+    @Column(name = "ENTITY_ID")
+    private String entityId;
+
+    @Column(name = "PRNCPL_PSWD")
+    private String password;
+
+    @Column(name = "ACTV_IND")
+    @Convert(converter = BooleanYNConverter.class)
+    private boolean active;
+
+    @Override
+    public String getPrincipalId() {
+        return principalId;
+    }
+
+    public void setPrincipalId(String principalId) {
+        this.principalId = principalId;
+    }
+
+    @Override
+    public String getPrincipalName() {
+        return principalName;
+    }
+
+    public void setPrincipalName(String principalName) {
+        this.principalName = principalName;
+    }
+
+    @Override
+    public String getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(String entityId) {
+        this.entityId = entityId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    @Override
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
     public String getMyPrincipalName() {
         return myPrincipalName;

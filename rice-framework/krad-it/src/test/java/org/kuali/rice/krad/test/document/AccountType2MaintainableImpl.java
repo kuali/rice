9436ceec293ab540397e0d30ca56501ef1a2d135
Kristina Taylor <kpbtaylor@gmail.com>
2014-02-10 19:36:35 +0000

    KULRICE-11817: Reverted library change and moved maintenance document to KRAD

--- rice-framework/krad-it/src/test/java/org/kuali/rice/krad/test/document/AccountType2MaintainableImpl.java
+++ rice-framework/krad-it/src/test/java/org/kuali/rice/krad/test/document/AccountType2MaintainableImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.rice.kns.maintainable;
+package org.kuali.rice.krad.test.document;
 
 import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;

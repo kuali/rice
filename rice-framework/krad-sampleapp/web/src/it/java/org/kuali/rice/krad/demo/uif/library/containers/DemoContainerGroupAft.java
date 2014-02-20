85360043829f4c139d21cb3406a31d8a249c56c5
Deep Moteria <dmoteria@vivantech.com>
2014-02-20 06:02:07 +0000

    KULRICE-11570 : Update AFT for Demo Library Container Group

--- rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/containers/DemoContainerGroupAft.java
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/containers/DemoContainerGroupAft.java
@@ -43,52 +43,68 @@ public class DemoContainerGroupAft extends WebDriverLegacyITBase {
     protected void testLibraryContainerGroupBasic() throws Exception {
         selectByName("exampleShown","Basic Grid");
         assertElementPresentByXpath("//input[@name='inputField1']");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example1']/div[2]/table");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example1']/div/table");
     }
     
     protected void testLibraryContainerGroupBasicVerticalBox() throws Exception {
         selectByName("exampleShown","Basic Vertical Box");
         assertElementPresentByXpath("//input[@name='inputField3']");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example2']/div[2]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example2']/div");
     }
     
     protected void testLibraryContainerGroupBasicCssGrid() throws Exception {
         selectByName("exampleShown","Basic CSS Grid");
         assertElementPresentByXpath("//input[@name='inputField5']");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example3']/div[2]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example3']/div");
     }
     
-    protected void testLibraryContainerGroupSections() throws Exception {
-        selectByName("exampleShown","Sections");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example4']/div/h3");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example4']/div[2]/div/h3");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example4']/div[2]/div[2]/div/h4");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example4']/div[2]/div[3]/div/h4");
+    protected void testLibraryContainerGroupSectionVertical() throws Exception {
+        selectByName("exampleShown","Section Vertical");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example4']/header/h3");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example4']/section/header/h3/span[contains(text(),'Section 1')]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example4']/section/header/h3/span[contains(text(),'Section 2')]");
+    }
+    
+    protected void testLibraryContainerGroupSectionHorizontal() throws Exception {
+        selectByName("exampleShown","Section Horizontal");
+        assertElementPresentByXpath("//main[@id='Demo-Group-Example5']/header/h2");
+        assertElementPresentByXpath("//main[@id='Demo-Group-Example5']/section/header/h3/span[contains(text(),'Section 1')]");
+        assertElementPresentByXpath("//main[@id='Demo-Group-Example5']/section/header/h3/span[contains(text(),'Section 2')]");
+    }
+    
+    protected void testLibraryContainerGroupSubSection() throws Exception {
+        selectByName("exampleShown","Section Horizontal");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example6']/header/h3");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example6']/section/header/h3/span[contains(text(),'Section 1')]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example6']/section/section/header/h4/span[contains(text(),'SubSection 1')]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example6']/section/section/header/h4/span[contains(text(),'SubSection 2')]");
+        assertElementPresentByXpath("//section[@id='Demo-Group-Example6']/section/header/h3/span[contains(text(),'Section 2')]");
     }
     
     protected void testLibraryContainerGroupDisclosure() throws Exception {
         selectByName("exampleShown","Disclosure");
         waitAndClickByLinkText("Disclosure Section");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example5']/div[2]/div[@data-role='disclosureContent' and @style='display: none;']");
+        waitForElementPresentByXpath("//section[@id='Demo-Group-Example7']/section/div[@class='uif-disclosureContent' and @style='overflow: hidden; display: none;']");
         waitAndClickByLinkText("Disclosure Section");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example5']/div[2]/div[@data-role='disclosureContent' and @style='display: block;']");
+        waitForElementPresentByXpath("//section[@id='Demo-Group-Example7']/section/div[@class='uif-disclosureContent' and @style='overflow: hidden; display: block;']");
         waitAndClickByLinkText("Predefined Disclosure Section");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example5']/div[3]/div[@data-role='disclosureContent' and @style='display: none;']");
+        waitForElementPresentByXpath("//section[@id='Demo-Group-Example7']/section[2]/div[@class='uif-disclosureContent' and @style='overflow: hidden; display: none;']");
         waitAndClickByLinkText("Predefined Disclosure Section");
-        assertElementPresentByXpath("//div[@id='Demo-Group-Example5']/div[3]/div[@data-role='disclosureContent' and @style='display: block;']");
+        waitForElementPresentByXpath("//section[@id='Demo-Group-Example7']/section[2]/div[@class='uif-disclosureContent' and @style='overflow: hidden; display: block;']");
     }
     
     protected void testLibraryContainerGroupScrollpane() throws Exception {
         selectByName("exampleShown","Scrollpane");
-        //No demo of Scroll is available in the test page.
-        //assertElementPresentByXpath("//div[@style='height: 100px;overflow: auto;']");
+        waitForElementPresentByXpath("//section[@id='Demo-Group-Example8']/div[@style='height: 100px;overflow: auto;']");
     }
 
     private void testAllGroups() throws Exception {
         testLibraryContainerGroupBasic();
         testLibraryContainerGroupBasicVerticalBox();
         testLibraryContainerGroupBasicCssGrid();
-        testLibraryContainerGroupSections();
+        testLibraryContainerGroupSectionVertical();
+        testLibraryContainerGroupSectionHorizontal();
+        testLibraryContainerGroupSubSection();
         testLibraryContainerGroupDisclosure();
         testLibraryContainerGroupScrollpane();
     }

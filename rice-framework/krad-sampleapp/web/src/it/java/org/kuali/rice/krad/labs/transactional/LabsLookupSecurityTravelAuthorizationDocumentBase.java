9d829c0730bf2a3cc8fd8b0282f7d84a5c2e8fbc
Kristina Taylor <kpbtaylor@gmail.com>
2014-02-28 19:55:42 +0000

    KULRICE-11952: Adding AFT base files

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/transactional/LabsLookupSecurityTravelAuthorizationDocumentBase.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs.transactional;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Base class for testing lookup security.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsLookupSecurityTravelAuthorizationDocumentBase extends LabsTransactionalBase {
+
+    /**
+     * /kr-krad/approval?methodToCall=docHandler&command=initiate&docTypeName=TravelAuthorization&viewName=LabsLookupSecurityTravelAuthorization
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/approval?methodToCall=docHandler&command=initiate&docTypeName=TravelAuthorization&viewName=LabsLookupSecurityTravelAuthorization";
+
+    private static final String PHONE_NUMBER_NAME = "document.travelerDetail.phoneNumber";
+    private static final String PHONE_NUMBER_MASKED = "(xxx)xxx-xxxx";
+    private static final String PHONE_NUMBER_DECRYPTED = "8005551212";
+
+    private static final String CUSTOMER_NUMBER_NAME = "document.travelerDetail.customerNumber";
+    private static final String CUSTOMER_NUMBER_DECRYPTED = "CUST";
+
+    private static final String EMAIL_ADDRESS_NAME = "document.travelerDetail.emailAddress";
+
+    private static final String TRAVELER_TYPE_CODE_NAME = "travelerType.code";
+    private static final String TRAVELER_TYPE_CODE_DECRYPTED = StringUtils.EMPTY;
+
+    private static final String CONVERSION_FIELDS = "conversionFields=";
+    private static final String ERRANT_CONVERSION_FIELD = TRAVELER_TYPE_CODE_NAME + "%3A" + EMAIL_ADDRESS_NAME + "%2C";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        navigateToTransactional("Transactional Sample - Lookup Security");
+        waitAndClickByLinkText("Travel Authorization Transactional Sample - Lookup Security");
+    }
+
+    /**
+     * Tests the basic case in which the phone number is masked and does not appear anywhere on the page decrypted.
+     *
+     * @throws Exception
+     */
+    protected void testTransactionalLookupSecurity(boolean authorized) throws Exception {
+        waitAndClick(By.id("travelerQuickfinder_quickfinder_act"));
+        waitAndClickSearch3();
+        waitAndClickReturnValue();
+
+        if (authorized) {
+            assertTextNotPresent(PHONE_NUMBER_MASKED);
+            assertTextPresent(PHONE_NUMBER_DECRYPTED);
+        } else {
+            assertTextPresent(PHONE_NUMBER_MASKED);
+            assertTextNotPresent(PHONE_NUMBER_DECRYPTED);
+        }
+    }
+
+    /**
+     * Tests the case in which the data dictionary phone number conversion field is changed to have it appear in the
+     * email address field, which is not secured.
+     *
+     * @throws Exception
+     */
+    protected void testTransactionalLookupSecurityAddDataDictionaryConversionField() throws Exception {
+        waitAndClick(By.id("travelerQuickfinder_quickfinder_act"));
+        waitForPageToLoad();
+
+        String newUrl = StringUtils.replace(driver.getCurrentUrl(), PHONE_NUMBER_NAME, EMAIL_ADDRESS_NAME);
+        open(newUrl);
+        waitForPageToLoad();
+
+        waitAndClickSearch3();
+        waitAndClickReturnValue();
+
+        assertElementPresentByName(EMAIL_ADDRESS_NAME);
+        WebElement element = findElement(By.name(EMAIL_ADDRESS_NAME));
+        String emailAddress = element.getAttribute("value");
+
+        assertTrue("Non-secure field emailAddress was empty", StringUtils.isNotBlank(emailAddress));
+        assertFalse("Secure field phoneNumber was placed in the non-secure field emailAddress and decrypted",
+                StringUtils.equals(emailAddress, PHONE_NUMBER_DECRYPTED));
+    }
+
+    /**
+     * Tests the case in which the UIf customer number conversion field is changed to have it appear in the email
+     * address field, which is not secured.
+     *
+     * @throws Exception
+     */
+    protected void testTransactionalLookupSecurityAddUifConversionField() throws Exception {
+        waitAndClick(By.id("travelerQuickfinder_quickfinder_act"));
+        waitForPageToLoad();
+
+        String newUrl = StringUtils.replace(driver.getCurrentUrl(), CUSTOMER_NUMBER_NAME, EMAIL_ADDRESS_NAME);
+        open(newUrl);
+        waitForPageToLoad();
+
+        waitAndClickSearch3();
+        waitAndClickReturnValue();
+
+        assertElementPresentByName(EMAIL_ADDRESS_NAME);
+        WebElement element = findElement(By.name(EMAIL_ADDRESS_NAME));
+        String emailAddress = element.getAttribute("value");
+
+        assertTrue("Non-secure field emailAddress was empty", StringUtils.isNotBlank(emailAddress));
+
+        assertFalse("Secure field customerNumber was placed in the non-secure field emailAddress and decrypted",
+                StringUtils.equals(emailAddress, CUSTOMER_NUMBER_DECRYPTED));
+    }
+
+    /**
+     * Tests the case in which the a new conversion field is added so that a field that is not referenced in either the
+     * data dictionary or the Uif (the traveler type code) appears in the email address field, which is not secured.
+     *
+     * @throws Exception
+     */
+    protected void testTransactionalLookupSecurityAddHiddenConversionField() throws Exception {
+        waitAndClick(By.id("travelerQuickfinder_quickfinder_act"));
+        waitForPageToLoad();
+
+        int splitPosition = StringUtils.indexOf(driver.getCurrentUrl(), CONVERSION_FIELDS) + CONVERSION_FIELDS.length();
+        String before = StringUtils.substring(driver.getCurrentUrl(), 0, splitPosition);
+        String after = StringUtils.substring(driver.getCurrentUrl(), splitPosition);
+        String newUrl = before + ERRANT_CONVERSION_FIELD + after;
+        open(newUrl);
+        waitForPageToLoad();
+
+        waitAndClickSearch3();
+        waitAndClickReturnValue();
+
+        assertElementPresentByName(EMAIL_ADDRESS_NAME);
+        WebElement element = findElement(By.name(EMAIL_ADDRESS_NAME));
+        String emailAddress = element.getAttribute("value");
+
+        assertTrue("Non-secure field emailAddress was empty", StringUtils.isNotBlank(emailAddress));
+
+        assertFalse("Secure field travelerType.code was placed in the non-secure field emailAddress and decrypted",
+                StringUtils.equals(emailAddress, TRAVELER_TYPE_CODE_DECRYPTED));
+    }
+
+}

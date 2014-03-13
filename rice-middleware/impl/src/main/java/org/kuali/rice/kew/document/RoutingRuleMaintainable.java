52b08b368002f14ce2e6c9861bc337253d074ac9
Jonathan Keller <keller.jonathan@gmail.com>
2014-03-13 16:34:50 +0000

    KULRICE-11417 : Adjusting business rules to work when data is still in pre-saving form

--- rice-middleware/impl/src/main/java/org/kuali/rice/kew/document/RoutingRuleMaintainable.java
+++ rice-middleware/impl/src/main/java/org/kuali/rice/kew/document/RoutingRuleMaintainable.java
@@ -26,24 +26,24 @@ import org.kuali.rice.kew.rule.RuleResponsibilityBo;
 import org.kuali.rice.kew.rule.bo.RuleTemplateBo;
 import org.kuali.rice.kew.rule.web.WebRuleUtils;
 import org.kuali.rice.kew.service.KEWServiceLocator;
+import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
+import org.kuali.rice.kns.maintenance.Maintainable;
 import org.kuali.rice.kns.web.ui.Section;
 import org.kuali.rice.krad.bo.PersistableBusinessObject;
-import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.krad.maintenance.MaintenanceLock;
-import org.kuali.rice.kns.maintenance.Maintainable;
 
 /**
- * This class is the maintainable implementation for Routing Rules 
- * in KEW (represented by the {@link RuleBaseValues} business object). 
- * 
+ * This class is the maintainable implementation for Routing Rules
+ * in KEW (represented by the {@link RuleBaseValues} business object).
+ *
  * @author Kuali Rice Team (rice.collab@kuali.org)
  *
  */
 public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 
 	private static final long serialVersionUID = -5920808902137192662L;
-    
+
 	/**
 	 * Override the getSections method on this maintainable so that the Section Containing the various Rule Attributes
 	 * can be dynamically generated based on the RuleTemplate which is selected.
@@ -52,11 +52,11 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 	public List getSections(MaintenanceDocument document, Maintainable oldMaintainable) {
 		List<Section> sections = super.getSections(document, oldMaintainable);
 		return WebRuleUtils.customizeSections(getThisRule(), sections, false);
-		
+
 	}
-	
+
 	/**
-	 * On creation of a new rule document, we must validate that a rule template and document type are set. 
+	 * On creation of a new rule document, we must validate that a rule template and document type are set.
 	 */
 	@Override
 	public void processAfterNew(MaintenanceDocument document,
@@ -65,7 +65,7 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 		WebRuleUtils.establishDefaultRuleValues(getNewRule(document));
 		getNewRule(document).setDocumentId(document.getDocumentHeader().getDocumentNumber());
 	}
-	
+
 	/**
 	 * This is a hack to get around the fact that when a document is first created, this value is
  	 * true which causes issues if you want to be able to initialize fields on  the document using
@@ -73,10 +73,10 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
  	 * Field.propertyValue to see why this causes problems
 	 */
 	@Override
-	public void setGenerateBlankRequiredValues(String docTypeName) {		
-		
+	public void setGenerateBlankRequiredValues(String docTypeName) {
+
 	}
-			
+
     /**
      * A complete override of the implementation for saving a Rule
      */
@@ -93,7 +93,7 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
     	WebRuleUtils.processRuleForCopy(document.getDocumentNumber(), getOldRule(document), getNewRule(document));
         super.processAfterCopy(document, parameters);
     }
-    
+
 	@Override
 	public void processAfterEdit(MaintenanceDocument document,
 			Map<String, String[]> parameters) {
@@ -101,7 +101,7 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 			throw new RiceRuntimeException("Cannot edit a non-current version of a rule.");
 		}
 		WebRuleUtils.populateForCopyOrEdit(getOldRule(document), getNewRule(document));
-		
+
 		getNewRule(document).setPreviousRuleId(getOldRule(document).getId());
 
 		getNewRule(document).setDocumentId(document.getDocumentHeader().getDocumentNumber());
@@ -114,14 +114,14 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 	protected RuleBaseValues getNewRule(MaintenanceDocument document) {
 		return (RuleBaseValues)document.getNewMaintainableObject().getBusinessObject();
 	}
-	
+
 	/**
 	 * Returns the old RuleBaseValues business object.
 	 */
 	protected RuleBaseValues getOldRule(MaintenanceDocument document) {
 		return (RuleBaseValues)document.getOldMaintainableObject().getBusinessObject();
 	}
-	
+
 	/**
 	 * Returns the RuleBaseValues business object associated with this Maintainable.
 	 */
@@ -131,12 +131,12 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 
 	/**
 	 * Overridden implementation of maintenance locks.  The default locking for Routing Rules
-	 * is based on previous version (can't route more than one rule based off the same 
+	 * is based on previous version (can't route more than one rule based off the same
 	 * previous verison).  However, for the first version of a rule, the previous version id
 	 * will be null.
-	 * 
+	 *
 	 * So for a new Route Rule maintenance document we don't want any locks generated.
-	 * 
+	 *
 	 * TODO can we just let the locking key be the primary key? (ruleBaseValuesId)
 	 */
 	@Override
@@ -159,16 +159,16 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
         return title.toString();
 	}
 
-	/**
-	 * This overridden method ...
-	 *
-	 * @see org.kuali.rice.krad.maintenance.KualiMaintainableImpl#prepareForSave()
-	 */
-	@Override
-	public void prepareForSave() {
-		super.prepareForSave();
-		WebRuleUtils.translateResponsibilitiesForSave(getThisRule());
-	}
+//	/**
+//	 * This overridden method ...
+//	 *
+//	 * @see org.kuali.rice.krad.maintenance.KualiMaintainableImpl#prepareForSave()
+//	 */
+//	@Override
+//	public void prepareForSave() {
+//		super.prepareForSave();
+//		WebRuleUtils.translateResponsibilitiesForSave(getThisRule());
+//	}
 
     /**
 	 * @see org.kuali.rice.krad.maintenance.KualiMaintainableImpl#setNewCollectionLineDefaultValues(java.lang.String, org.kuali.rice.krad.bo.PersistableBusinessObject)
@@ -184,5 +184,5 @@ public class RoutingRuleMaintainable extends KualiMaintainableImpl {
 	        }
 		}
 	}
-	
+
 }

8cfb2832e1ec6a6f4ad518d93bc0a7fcb1f3aa71
Erik Meade <github-kuali-m@eghm.net>
2014-03-14 23:04:25 +0000

    Whitespace change to trigger CI build

--- rice-middleware/sampleapp/src/it/java/edu/sampleu/admin/ConfigViewConfiguredPropertiesAftBase.java
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/admin/ConfigViewConfiguredPropertiesAftBase.java
@@ -20,7 +20,7 @@ import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
 import org.kuali.rice.testtools.selenium.WebDriverUtils;
 
 /**
- *  @author Kuali Rice Team (rice.collab@kuali.org)
+ * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public abstract class ConfigViewConfiguredPropertiesAftBase extends AdminTmplMthdAftNavBase {
 

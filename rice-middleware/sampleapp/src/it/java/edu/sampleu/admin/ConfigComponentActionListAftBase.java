5e87406c96c078133e1beee6aae8e9ba360e785b
Erik Meade <github-kuali-m@eghm.net>
2014-04-02 17:02:52 +0000

    whitespace bump

--- rice-middleware/sampleapp/src/it/java/edu/sampleu/admin/ConfigComponentActionListAftBase.java
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/admin/ConfigComponentActionListAftBase.java
@@ -19,7 +19,7 @@ import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
 import org.kuali.rice.testtools.selenium.WebDriverUtils;
 
 /**
- *  @author Kuali Rice Team (rice.collab@kuali.org)
+ * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public abstract class ConfigComponentActionListAftBase extends ConfigComponentAftBase {
 

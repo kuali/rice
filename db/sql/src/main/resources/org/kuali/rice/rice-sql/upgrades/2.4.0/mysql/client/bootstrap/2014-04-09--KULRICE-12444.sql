2a9171af06fd136f60ddb5aad3a8138737e5ccd1
James Bennett <jawbenne@iu.edu>
2014-04-09 19:37:10 +0000

    KULRICE-12444: Added a new table which will be optionally used to hold attachment content in the database instead of the filesystem

---
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.0/mysql/client/bootstrap/2014-04-09--KULRICE-12444.sql
@@ -0,0 +1,3 @@
+-- Create a new table for holding attachment content which can be used in place of storing them on the filesystem
+CREATE TABLE krns_att_cntnt_t (att_id VARCHAR(36), att_cntnt BLOB)
+/

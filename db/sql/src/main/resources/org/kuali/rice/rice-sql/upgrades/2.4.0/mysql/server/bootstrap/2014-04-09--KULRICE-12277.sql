fde34f8b7468d8d7ad0ecacfcc4e6719de4abeff
Jeff Caddel <jcaddel@rsmart.com>
2014-04-10 04:20:12 +0000

    Automated license header update

--- db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.0/mysql/server/bootstrap/2014-04-09--KULRICE-12277.sql
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.0/mysql/server/bootstrap/2014-04-09--KULRICE-12277.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 -- Create new KR-NTFCN:Channel KIM type
 INSERT INTO krim_typ_t (kim_typ_id, obj_id, ver_nbr, nmspc_cd, nm, srvc_nm, actv_ind) VALUES ('KR1002', uuid(), 1, 'KR-NTFCN', 'Channel', '{http://rice.kuali.org/ken/v2_0}channelPermissionTypeService', 'Y')
 /
--- db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.0/oracle/server/bootstrap/2014-04-09--KULRICE-12281.sql
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.4.0/oracle/server/bootstrap/2014-04-09--KULRICE-12281.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 -- This creates a new permission template which controls which users can export results from a lookup and sets up a single permission which allows all users to export from all lookups
 INSERT INTO krim_perm_tmpl_t (perm_tmpl_id, obj_id, ver_nbr, nmspc_cd, nm, desc_txt, kim_typ_id, actv_ind) VALUES ('KR1004', sys_guid(), '1', 'KR-NS', 'Export Records', 'Ability to export results from the lookup screen.', (SELECT kim_typ_id FROM krim_typ_t WHERE nmspc_cd = 'KR-NS' AND nm = 'Namespace or Component'), 'Y')
 /

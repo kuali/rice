a866821df0df6ec15c3b53471fe5fc5a20d4f608
Deep Moteria <dmoteria@vivantech.com>
2014-04-22 04:51:51 +0000

    RICEQA-316 : Fill AFT Per-Screen Item Gap: Detailed Doc Search Added methods to test all working fields. Few fields are not working and few are unidentified.

---
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/admin/DetailedDocSearchAft.java
@@ -0,0 +1,188 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.sampleu.admin;
+
+import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.kuali.rice.testtools.selenium.WebDriverUtils;
+import org.junit.Test;
+import org.kuali.rice.krad.util.KRADConstants;
+
+/**
+ * Tests whether the ENABLE_FIELD_LEVEL_HELP_IND parameter is being considered and loaded on each request.
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DetailedDocSearchAft extends WebDriverLegacyITBase {
+    /*
+     * AutomatedFunctionalTestUtils.PORTAL + "?channelTitle=Document%20Search&channelUrl=" + WebDriverUtils
+     * .getBaseUrlString() + "/kew/DocumentSearch.do?docFormKey=88888888&returnLocation=" + AutomatedFunctionalTestUtils.PORTAL_URL + AutomatedFunctionalTestUtils.HIDE_RETURN_LINK;
+     */
+    
+    public static String BOOKMARK_URL = AutomatedFunctionalTestUtils.PORTAL + "?channelTitle=Document%20Search&channelUrl=" + WebDriverUtils
+            .getBaseUrlString() + "/kew/DocumentSearch.do?docFormKey=88888888&returnLocation=" + AutomatedFunctionalTestUtils.PORTAL_URL + AutomatedFunctionalTestUtils.HIDE_RETURN_LINK;
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+       waitAndClickByXpath("//img[@alt='doc search']"); 
+       waitAndClickByName("toggleAdvancedSearch");
+    }
+
+//    @Test
+    public void testAdvancedDocSearchNav() throws Exception {
+        advancedDocSearchAll();
+    }
+    
+    @Test
+    public void testAdvancedDocSearchBookmark() throws Exception {
+        selectFrameIframePortlet();
+        waitAndClickByName("toggleAdvancedSearch");
+        advancedDocSearchAll();
+    }
+    
+    private void advancedDocSearchAll() throws Exception{
+        searchByDocumentType();
+        searchByInitiator();
+        searchByApprover();
+        searchByViewer();
+        searchByGroupViewer();
+        searchByDocumentId();
+        searchByApplicationDocumentId();
+        searchByDocumentStatus();
+        searchByDateCreatedFrom();
+        searchByDateCreatedTo();
+        searchByDateApprovedFrom();
+        searchByDateApprovedTo();
+        searchByDateLastModifiedFrom();
+        searchByDateLastModifiedTo();
+        searchByDateLastFinalizedFrom();
+        searchByDateLastFinalizedTo();
+        searchByTitle();
+    }
+
+    private void searchByDocumentType() throws Exception {
+        //Search by Document Type not working. Sometimes also gives 500 Server error while searching and returning value for the field
+    }
+    
+    private void searchByInitiator() throws Exception {
+        selectFrameIframePortlet();
+        waitAndTypeByName("initiatorPrincipalName","admin");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitForElementPresentByXpath("//a[contains(text(),'admin, admin')]");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+
+    private void searchByApprover() throws Exception {
+        selectFrameIframePortlet();
+        waitAndTypeByName("approverPrincipalName","admin");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitForElementPresentByXpath("//a[contains(text(),'admin, admin')]");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+
+    private void searchByViewer() throws Exception {
+        //Unknown - no idea from where can we get these fields
+    }
+    
+    private void searchByGroupViewer() throws Exception {
+        //Unknown - no idea from where can we get these fields        
+    }
+    
+    private void searchByDocumentId() throws Exception {
+        waitAndTypeByName("documentId","2700");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForElementPresentByXpath("//a[contains(text(),'2700')]");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByApplicationDocumentId() throws Exception {
+        //Unknown - no idea from where can we get these fields
+    }
+    
+    private void searchByDocumentStatus() throws Exception {
+        selectByName("statusCode","Successful Statuses");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("FINAL");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateCreatedFrom() throws Exception {
+        waitAndTypeByName("rangeLowerBoundKeyPrefix_dateCreated","04/17/2005");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateCreatedTo() throws Exception {
+        waitAndTypeByName("dateCreated","04/17/2014");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateApprovedFrom() throws Exception {
+        waitAndTypeByName("rangeLowerBoundKeyPrefix_dateApproved","04/17/2005");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateApprovedTo() throws Exception {
+        waitAndTypeByName("dateApproved","04/17/2014");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateLastModifiedFrom() throws Exception {
+        waitAndTypeByName("rangeLowerBoundKeyPrefix_dateLastModified","04/17/2005");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+ 
+    private void searchByDateLastModifiedTo() throws Exception {
+        waitAndTypeByName("dateLastModified","04/17/2014");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByDateLastFinalizedFrom() throws Exception {
+        waitAndTypeByName("rangeLowerBoundKeyPrefix_dateFinalized","04/17/2005");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+
+    private void searchByDateLastFinalizedTo() throws Exception {
+        waitAndTypeByName("dateFinalized","04/17/2014");
+        waitAndClickByXpath("//td/input[@type='image' and @name='methodToCall.search']");
+        waitForTextPresent("items retrieved");
+        waitAndClickByName("methodToCall.clearValues");
+    }
+    
+    private void searchByTitle() throws Exception {
+        //Search By Title not working
+    }
+}

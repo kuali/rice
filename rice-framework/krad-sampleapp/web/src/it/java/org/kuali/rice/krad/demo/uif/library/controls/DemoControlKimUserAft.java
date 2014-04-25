4e7d57b5f8164601a47be4294924397ea38547bf
Deep Moteria <dmoteria@vivantech.com>
2014-04-25 04:12:54 +0000

    RICEQA-329 : Fill AFT Gap: KRAD Library: Controls: KIM User

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/controls/DemoControlKimUserAft.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.uif.library.controls;
+
+import org.junit.Test;
+
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoControlKimUserAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-KIMUserControlView
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-KIMUserControlView";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Controls");
+        waitAndClickByLinkText("KIM User");
+    }
+
+    protected void testControlKimUserDefault() throws Exception {
+        waitAndTypeByName("testPerson.principalName","admin");
+        waitAndClickByLinkText("Documentation");
+        waitForTextPresent("admin, admin");
+    }
+    
+    protected void testControlKimUserWidgetInputOnly() throws Exception {
+        waitAndClickByLinkText("Widget Input Only");
+        waitAndClickByXpath("//section[@id='Demo-KIMUserControl-Example2']/div/div/span/a");
+        gotoLightBox();
+        waitAndClickButtonByText("Search");
+        waitAndClickLinkContainingText("return value");
+        waitForTextPresent("admin, admin");
+    }
+    
+    @Test
+    public void testControlKimUserBookmark() throws Exception {
+        testControlKimUserDefault();
+        testControlKimUserWidgetInputOnly();
+        passed();
+    }
+
+    @Test
+    public void testControlKimUserNav() throws Exception {
+    	testControlKimUserDefault();
+        testControlKimUserWidgetInputOnly();
+        passed();
+    }  
+}

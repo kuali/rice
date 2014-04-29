fe7bed855672f2cc4ff3ae3abf146964cbc7f5f8
Deep Moteria <dmoteria@vivantech.com>
2014-04-29 04:08:20 +0000

    RICEQA-336 : Fill AFT Gap: KRAD Labs: Meta tags

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/LabsAdditionalHtmlMetaTagsAft.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs;
+
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsAdditionalHtmlMetaTagsAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/labs?viewId=Lab-Meta-Tags
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/labs?viewId=Lab-Meta-Tags";
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	waitAndClickByLinkText("Meta tags");
+    }
+
+    protected void testDemoAdditionalHtmlMetaTags() throws InterruptedException {
+    	waitForElementPresentByXpath("//meta[@name='description' and @content='Kuali Labs Page']");
+    }
+
+    @Test
+    public void testDemoAdditionalHtmlMetaTagsBookmark() throws Exception {
+    	testDemoAdditionalHtmlMetaTags();
+        passed();
+    }
+
+    @Test
+    public void testDemoAdditionalHtmlMetaTagsNav() throws Exception {
+    	testDemoAdditionalHtmlMetaTags();
+        passed();
+    }
+}

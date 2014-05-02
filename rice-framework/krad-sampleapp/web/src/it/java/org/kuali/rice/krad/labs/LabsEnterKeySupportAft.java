dac10d680364b7e6c449f4830c45dfb36a8809d9
Deep Moteria <dmoteria@vivantech.com>
2014-05-02 06:26:05 +0000

    RICEQA-340 : Fill AFT Gap: KRAD Labs: Enter Key Support

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/LabsEnterKeySupportAft.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.labs;
+
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class LabsEnterKeySupportAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/labs?viewId=Lab-LayoutTest-EnterKey
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/labs?viewId=Lab-LayoutTest-EnterKey";
+   
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	waitAndClickByLinkText("Enter Key Support");
+    }
+
+    protected void testDemoEnterKeySupport() throws InterruptedException {
+    	waitAndTypeByName("inputField12","");
+    	pressEnterByName("inputField12");
+    	acceptAlertIfPresent();
+    	waitAndTypeByName("inputField14","");
+    	pressEnterByName("inputField14");
+    	acceptAlertIfPresent();
+    	waitAndTypeByName("inputField16","");
+    	pressEnterByName("inputField16");
+    	acceptAlertIfPresent();
+    	waitAndTypeByName("inputField18","");
+    	pressEnterByName("inputField18");
+    	acceptAlertIfPresent();
+    	waitAndTypeByName("inputField20","");
+    	pressEnterByName("inputField20");
+    	acceptAlertIfPresent();
+    }
+
+    @Test
+    public void testDemoEnterKeySupportBookmark() throws Exception {
+    	testDemoEnterKeySupport();
+        passed();
+    }
+
+    @Test
+    public void testDemoEnterKeySupportNav() throws Exception {
+    	testDemoEnterKeySupport();
+        passed();
+    }
+}

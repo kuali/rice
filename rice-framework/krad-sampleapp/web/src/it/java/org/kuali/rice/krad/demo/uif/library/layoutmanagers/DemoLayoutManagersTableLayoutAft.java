3b9c6a83bb6c6270a80da49e71b58c6e1094b2f0
Deep Moteria <dmoteria@vivantech.com>
2014-05-07 05:59:08 +0000

    RICEQA-349 : Fill AFT Gap: KRAD Library: Layouts: Table Layouts

--- rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/layoutmanagers/DemoLayoutManagersTableLayoutAft.java
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/layoutmanagers/DemoLayoutManagersTableLayoutAft.java
@@ -45,18 +45,102 @@ public class DemoLayoutManagersTableLayoutAft extends WebDriverLegacyITBase {
 
     protected void testLayoutManagersBasicTableLayout() throws Exception {
         selectByName("exampleShown","Basic Table Layout");
+        waitForElementPresentByXpath("//section[@id='Demo-TableLayoutManager-Example1']/div/div/div/table");
         waitForElementPresentByXpath("//button[@id='Demo-TableLayoutManager-Example1_add']");
+    }
+    
+    protected void testLayoutManagersJqueryTableFeatures() throws Exception {
+        selectByName("exampleShown","jQuery Table Features");
+        waitForElementPresentByXpath("//section[@id='Demo-TableLayoutManager-Example2']/div/div/div/table");
+    }
+    
+    protected void testLayoutManagersAddBlankLineTop() throws Exception {
+        selectByName("exampleShown","Add Blank Line Top");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example3']/div/div/table/tbody/tr/td[2]/div/input[@value='A']");
+        waitAndClickByXpath("//section[@data-parent='Demo-TableLayoutManager-Example3']/div/button[contains(text(),'Add Line')]");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example3']/div/div/table/tbody/tr/td[2]/div/input[@value='']");
+    }
+    
+    protected void testLayoutManagersAddBlankLineBottom() throws Exception {
+        selectByName("exampleShown","Add Blank Line Bottom");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example4']/div/div/table/tbody/tr/td[2]/div/input[@value='A']");
+        waitAndClickByXpath("//section[@data-parent='Demo-TableLayoutManager-Example4']/div/button[contains(text(),'Add Line')]");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example4']/div/div/table/tbody/tr[3]/td[2]/div/input[@value='']");
+    }
+    
+    protected void testLayoutManagersAddViaLightBoxTop() throws Exception {
+        selectByName("exampleShown","Add Via Lightbox TOP");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example5']/div/div/table/tbody/tr/td[2]/div/input[@value='a']");
+        waitAndClickByXpath("//section[@data-parent='Demo-TableLayoutManager-Example5']/div/button[contains(text(),'Add Line')]");
+        waitForElementPresentByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[2]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[4]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[6]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[8]/div/input","1");
+        waitAndClickByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-footer']/button[2]");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example5']/div/div/table/tbody/tr/td[2]/div/input[@value='1']");
+    }
+    
+    protected void testLayoutManagersAddViaLightBoxBottom() throws Exception {
+        selectByName("exampleShown","Add Via Lightbox BOTTOM");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example6']/div/div/table/tbody/tr/td[2]/div/input[@value='a']");
+        waitAndClickByXpath("//section[@data-parent='Demo-TableLayoutManager-Example6']/div/button[contains(text(),'Add Line')]");
+        waitForElementPresentByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[2]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[4]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[6]/div/input","1");
+        waitAndTypeByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-body']/div[8]/div/input","1");
+        waitAndClickByXpath("//section[@class='modal fade uif-cssGridGroup in' and @style='display: block;']/div/div/div[@class='modal-footer']/button[2]");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example6']/div/div/table/tbody/tr[4]/td[2]/div/input[@value='1']");
+    }
+    
+    protected void testLayoutManagersActionColumnLeft() throws Exception {
+        selectByName("exampleShown","Action Column Left");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example7']/div/div/table/tbody/tr/td/div/fieldset/div/button");
+    }
+    
+    protected void testLayoutManagersActionColumn3() throws Exception {
+        selectByName("exampleShown","Action Column 3");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example8']/div/div/table/tbody/tr/td[3]/div/fieldset/div/button");
+    }
+    
+    protected void testLayoutManagersSaveRow() throws Exception {
+        selectByName("exampleShown","Save Row");
+        waitForElementPresentByXpath("//section[@data-parent='Demo-TableLayoutManager-Example9']/div/div/table/tbody/tr/td[6]/div/fieldset/div/button[contains(text(),'Save')]");
+    }
+    
+    protected void testLayoutManagersMultirow() throws Exception {
+        selectByName("exampleShown","Multirow");
+        waitForElementPresentByXpath("//section[@id='Demo-TableLayoutManager-Example10']/div/div/div/table/thead/tr/th[@rowspan='2']");
+    }
+    
+    protected void testLayoutManagersSeparateAddLine() throws Exception {
+        selectByName("exampleShown","Separate Add Line");
+        waitForElementPresentByXpath("//section[@class='uif-collectionItem uif-tableCollectionItem uif-collectionAddItem']");
     }
 
     @Test
     public void testLayoutManagersBoxLayoutBookmark() throws Exception {
-        testLayoutManagersBasicTableLayout();
-        passed();
+    	testLayoutManagersAll();
     }
 
     @Test
     public void testLayoutManagersBoxLayoutNav() throws Exception {
-        testLayoutManagersBasicTableLayout();
-        passed();
+    	testLayoutManagersAll();
+    }
+    
+    private void testLayoutManagersAll() throws Exception {
+    	testLayoutManagersBasicTableLayout();
+    	testLayoutManagersJqueryTableFeatures();
+    	testLayoutManagersAddBlankLineTop();
+    	testLayoutManagersAddBlankLineBottom();
+    	testLayoutManagersAddViaLightBoxTop();
+    	testLayoutManagersAddViaLightBoxBottom();
+    	testLayoutManagersActionColumnLeft();
+    	testLayoutManagersActionColumn3();
+    	testLayoutManagersSaveRow();
+    	testLayoutManagersMultirow();
+    	testLayoutManagersSeparateAddLine();
+    	passed();
     }
 }

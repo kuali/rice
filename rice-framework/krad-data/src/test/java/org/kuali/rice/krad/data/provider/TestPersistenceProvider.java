b6af62b8ce436d239456e5ffd2a9cb9a9f245be3
Kurt McNew <mcnewk@gmail.com>
2014-05-08 17:34:18 +0000

    Removed Intellij auto generated comments.

--- rice-framework/krad-data/src/test/java/org/kuali/rice/krad/data/provider/TestPersistenceProvider.java
+++ rice-framework/krad-data/src/test/java/org/kuali/rice/krad/data/provider/TestPersistenceProvider.java
@@ -19,32 +19,25 @@ import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.core.api.criteria.QueryResults;
 import org.kuali.rice.krad.data.PersistenceOption;
 
-/**
- * Created with IntelliJ IDEA.
- * User: aaron
- * Date: 2/20/13
- * Time: 2:47 PM
- * To change this template use File | Settings | File Templates.
- */
 public class TestPersistenceProvider implements PersistenceProvider {
     @Override
     public <T> T save(T dataObject, PersistenceOption... options) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return null;
     }
 
     @Override
     public <T> T find(Class<T> type, Object id) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return null;
     }
 
     @Override
     public <T> QueryResults<T> findMatching(Class<T> type, QueryByCriteria queryByCriteria) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return null;
     }
 
     @Override
     public void delete(Object dataObject) {
-        //To change body of implemented methods use File | Settings | File Templates.
+
     }
 
     @Override
@@ -59,7 +52,7 @@ public class TestPersistenceProvider implements PersistenceProvider {
 
     @Override
     public boolean handles(Class<?> type) {
-        return true;  //To change body of implemented methods use File | Settings | File Templates.
+        return true;
     }
 
     @Override

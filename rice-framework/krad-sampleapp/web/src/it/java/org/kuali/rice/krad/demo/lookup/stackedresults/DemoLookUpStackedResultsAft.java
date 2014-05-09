3643910bfd9958645b03f9c98b1b19933c299732
Martin Taylor <mztaylor@umd.edu>
2014-05-09 17:07:19 +0000

    KULRICE-12623: Added new view along with AFT

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/lookup/stackedresults/DemoLookUpStackedResultsAft.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.lookup.stackedresults;
+
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.openqa.selenium.By;
+
+/**
+ * This class performs simple tests on the stacked results lookup
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoLookUpStackedResultsAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/lookup?methodToCall=start&viewId=LookupSampleViewMultipleValuesSelectLimit&hideReturnLink=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=LookupSampleViewStackedResults&hideReturnLink=true";
+
+    /**
+     *  lookupCriteria[number]
+     */
+    private static final String LOOKUP_CRITERIA_NUMBER_NAME="lookupCriteria[number]";
+
+    /**
+     *  Search
+     */
+    private static final String SEARCH="Search";
+
+    /**
+     * Clear Values
+     */
+    private static final String CLEAR_VALUES="Clear Values";
+
+
+    @Override
+    public String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-DemoLink", "");
+        waitAndClickByLinkText("Lookup Stacked Results");
+    }
+
+    protected void testLookUpStackedResults() throws InterruptedException {
+        waitAndTypeByName(LOOKUP_CRITERIA_NUMBER_NAME, "a1");
+        waitAndClickButtonByText(SEARCH);
+        waitForElementPresentByXpath("//a[contains(text(), 'a1')]");
+        waitAndClickButtonByText(CLEAR_VALUES);
+        waitAndClickButtonByText(SEARCH);
+    }
+
+    // No Nav test, there is no link to this page
+    @Test
+    public void testLookUpMultiValueBookmark() throws Exception {
+        testLookUpStackedResults();
+        passed();
+    }
+}

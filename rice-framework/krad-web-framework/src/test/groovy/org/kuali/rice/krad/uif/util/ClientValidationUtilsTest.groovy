b3fba763b97a95b829be275cdd7105823b01787c
Kristina Taylor <kpbtaylor@gmail.com>
2014-05-13 18:35:34 +0000

    Removing resource loader entirely to hopefully fix test for good

--- rice-framework/krad-web-framework/src/test/groovy/org/kuali/rice/krad/uif/util/ClientValidationUtilsTest.groovy
+++ rice-framework/krad-web-framework/src/test/groovy/org/kuali/rice/krad/uif/util/ClientValidationUtilsTest.groovy
@@ -15,50 +15,14 @@
  */
 package org.kuali.rice.krad.uif.util
 
-import org.junit.After
 import org.junit.Test
 import org.kuali.rice.krad.datadictionary.validation.constraint.ValidCharactersConstraint
 
 import org.kuali.rice.krad.uif.component.BindingInfo
-import org.junit.Before
-import org.kuali.rice.core.api.util.RiceConstants
-import javax.xml.namespace.QName
-import org.kuali.rice.core.framework.resourceloader.BaseResourceLoader
-import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader
-import groovy.mock.interceptor.MockFor
-import org.kuali.rice.core.api.config.property.ConfigContext
-import org.kuali.rice.core.api.CoreConstants
-import org.kuali.rice.core.api.config.property.Config
 import static org.junit.Assert.assertEquals
 import org.kuali.rice.krad.uif.field.InputFieldBase
 
 class ClientValidationUtilsTest {
-    static final def MOCK_ATTR_NAME = "mock_attr_name"
-    static final def MOCK_APP_ID = "mock_app_id"
-
-    private static void mockTheConfig() {
-        def mock_config = new MockFor(Config)
-        mock_config.demand.getProperty(CoreConstants.Config.APPLICATION_ID) { app_id -> MOCK_APP_ID }
-        ConfigContext.init(mock_config.proxyDelegateInstance())
-    }
-
-    private static void mockTheResourceLoader() {
-        mockTheConfig()
-        GlobalResourceLoader.addResourceLoader(new BaseResourceLoader(new QName(MOCK_APP_ID, RiceConstants.DEFAULT_ROOT_RESOURCE_LOADER_NAME)));
-        GlobalResourceLoader.start();
-    }
-
-    // FIXME: ClientValidationUtils statically initializes a ConfigurationService singleton on class initialization
-    // so we have to mock out the GRL so that code does not bomb
-    @Before
-    void init() {
-        mockTheResourceLoader()
-    }
-
-    @After
-    void destroy() {
-        GlobalResourceLoader.stop();
-    }
 
     @Test
     void testGetRegexMethod() {

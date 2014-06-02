fd6a1033cde402b2aa54aceffa837bc1fa7835ec
Brian Smith <bsmith83@ad3.ucdavis.edu>
2014-06-02 17:47:26 +0000

    [KULRICE-12514] MultiFileUpload - cleanup

--- rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/file/FileMetaBlob.java
+++ rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/file/FileMetaBlob.java
@@ -125,12 +125,15 @@ public class FileMetaBlob implements Serializable, FileMeta {
     @Override
     public String getSizeFormatted() {
         DecimalFormat format = new DecimalFormat("0.#");
+
         if (size >= 1000000000) {
             return format.format((((double)size) / 1000000000)) + " GB";
         }
+
         if (size >= 1000000) {
             return format.format((((double)size) / 1000000)) + " MB";
         }
+
         return format.format((((double)size) / 1000)) + " KB";
     }
 

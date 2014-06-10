6ffe8e92db4481791f593f5e5b9c0220e3b4f42b
Dan Seibert <dseibert@vivantech.com>
2014-06-10 22:55:52 +0000

    RICEQA-429 convenience script to pull down jmeter and jmeter plugin CMD tool into the CI job workspace.

---
+++ rice-tools-test/src/test/resources/scripts/get-jmeter.sh
@@ -0,0 +1,9 @@
+
+wget http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-2.11.tgz
+tar -xf apache-jmeter-2.11.tgz
+wget http://jmeter-plugins.org/downloads/file/JMeterPlugins-Standard-1.1.3.zip
+unzip -o JMeterPlugins-Standard-1.1.3.zip -d apache-jmeter-2.11
+wget http://jmeter-plugins.org/downloads/file/JMeterPlugins-Extras-1.1.3.zip
+unzip -o JMeterPlugins-Extras-1.1.3.zip -d apache-jmeter-2.11
+wget http://jmeter-plugins.org/downloads/file/JMeterPlugins-ExtrasLibs-1.1.3.zip
+unzip -o JMeterPlugins-ExtrasLibs-1.1.3.zip -d apache-jmeter-2.11

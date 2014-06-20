f8fdaf20c23f00175af42296a11c8d39bb8153cf
Deep Moteria <dmoteria@vivantech.com>
2014-06-20 06:08:00 +0000

    RICEQA-439 : Fill AFT Gap: KRAD Library - Field Group

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/containers/DemoContainerFieldGroupAft.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.uif.library.containers;
+
+import org.junit.Test;
+
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoContainerFieldGroupAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/kradsampleapp?viewId=Demo-FieldGroupView
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/kradsampleapp?viewId=Demo-FieldGroupView";
+    
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitAndClickById("Demo-LibraryLink", "");
+        waitAndClickByLinkText("Containers");
+        waitAndClickByLinkText("Field Group");
+    }
+
+    protected void testLibraryContainerFieldGroupVertical() throws Exception {
+       waitForElementPresentByXpath("//div[@class='uif-inputField uif-boxLayoutVerticalItem pull-left clearfix']/input[@name='inputField1']");
+       waitForElementPresentByXpath("//div[@class='uif-inputField uif-boxLayoutVerticalItem pull-left clearfix']/input[@name='inputField2']");
+    }
+    
+    protected void testLibraryContainerFieldGroupHorizontal() throws Exception {
+    	waitAndClickByXpath("//a[contains(text(),'Field Group Horizontal')]");
+        waitForElementPresentByXpath("//div[@class='uif-inputField uif-boxLayoutHorizontalItem']/input[@name='inputField1']");
+        waitForElementPresentByXpath("//div[@class='uif-inputField uif-boxLayoutHorizontalItem']/input[@name='inputField2']");
+     }
+    
+    @Test
+    public void testContainerFieldGroupBookmark() throws Exception {
+    	testLibraryContainerFieldGroupVertical();
+    	testLibraryContainerFieldGroupHorizontal();
+        passed();
+    }
+
+    @Test
+    public void testContainerFieldGroupNav() throws Exception {
+    	testLibraryContainerFieldGroupVertical();
+    	testLibraryContainerFieldGroupHorizontal();
+        passed();
+    }  
+}

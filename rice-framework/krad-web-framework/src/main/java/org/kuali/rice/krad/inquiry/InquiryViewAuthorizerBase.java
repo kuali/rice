5bfba251e46c4981fdb5dc6875b4ce775d596a53
Jonathan Keller <keller.jonathan@gmail.com>
2014-07-11 17:19:40 +0000

    KULRICE-11438 : Added use of the KR-NS / Inquire Into Records to the KRAD Inquiry view (needed for KNS-equivalency)

--- rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/inquiry/InquiryViewAuthorizerBase.java
+++ rice-framework/krad-web-framework/src/main/java/org/kuali/rice/krad/inquiry/InquiryViewAuthorizerBase.java
@@ -15,7 +15,14 @@
  */
 package org.kuali.rice.krad.inquiry;
 
+import org.kuali.rice.kim.api.KimConstants;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.uif.view.View;
 import org.kuali.rice.krad.uif.view.ViewAuthorizerBase;
+import org.kuali.rice.krad.uif.view.ViewModel;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.web.form.InquiryForm;
 
 /**
  * Implementation of {@link org.kuali.rice.krad.uif.view.ViewAuthorizer} for
@@ -26,4 +33,43 @@ import org.kuali.rice.krad.uif.view.ViewAuthorizerBase;
 public class InquiryViewAuthorizerBase extends ViewAuthorizerBase {
     private static final long serialVersionUID = 5853518191618440332L;
 
+
+    /**
+     * Augmenting the base Open View check with an additional check against the KR-NS / Inquire Into Records
+     * permission template.
+     *
+     * This check will fail if the user is not allowed by *either* the View
+     *
+     * @see org.kuali.rice.krad.uif.view.ViewAuthorizerBase#canOpenView(org.kuali.rice.krad.uif.view.View, org.kuali.rice.krad.uif.view.ViewModel, org.kuali.rice.kim.api.identity.Person)
+     */
+    @Override
+    public boolean canOpenView(View view, ViewModel model, Person user) {
+        boolean canOpenViewPerViewId = super.canOpenView(view, model, user);
+        // if the user is blocked out of the view by it's ID, we'll respect that and stop access here
+        if ( !canOpenViewPerViewId ) {
+            return false;
+        }
+
+        // If we get here - then the view permission is not blocking access - so we check the KNS inquiry permission
+        if ( model instanceof InquiryForm ) {
+            InquiryForm inquiryForm = (InquiryForm) model;
+            if ( inquiryForm.getDataObject() != null ) {
+                // but - we only block if a permission which handles this data object exists
+                // at some level
+                if ( permissionExistsByTemplate(inquiryForm.getDataObject(),
+                        KRADConstants.KNS_NAMESPACE,
+                        KimConstants.PermissionTemplateNames.INQUIRE_INTO_RECORDS ) ) {
+
+                    if ( !isAuthorizedByTemplate( inquiryForm.getDataObject(),
+                            KRADConstants.KNS_NAMESPACE,
+                            KimConstants.PermissionTemplateNames.INQUIRE_INTO_RECORDS,
+                            GlobalVariables.getUserSession().getPrincipalId() ) ) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }

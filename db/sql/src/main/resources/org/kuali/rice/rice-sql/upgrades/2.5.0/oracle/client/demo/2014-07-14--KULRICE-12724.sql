86b5199fe0b8ec7e365fc656bb530307f3cbd9f4
Martin Taylor <mztaylor@umd.edu>
2014-07-28 15:08:43 +0000

    KULRICE-12724: Adding properly formatted files for sql loading

---
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.5.0/oracle/client/demo/2014-07-14--KULRICE-12724.sql
@@ -0,0 +1,19 @@
+--
+-- Copyright 2005-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+insert into KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KR-KRAD', 'TravelAccount', 'DEFAULT_HELP_URL', SYS_GUID(), 1, 'CONFG', 'http://www.kuali.org', 'Default Help URL', 'A', 'KUALI')
+/

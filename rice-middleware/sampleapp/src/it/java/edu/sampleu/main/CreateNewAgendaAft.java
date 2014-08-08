70a8f921bc641136b833f53f74f47f83439cbe5b
Deep Moteria <dmoteria@vivantech.com>
2014-08-08 07:50:48 +0000

    RICEQA-503 : Fill AFT Gap: KRMS Rules (Save)

---
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/main/CreateNewAgendaAft.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.sampleu.main;
+
+import org.kuali.rice.testtools.common.JiraAwareFailable;
+import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.kuali.rice.testtools.selenium.WebDriverUtils;
+import org.junit.Test;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class CreateNewAgendaAft extends WebDriverLegacyITBase {
+
+    /**
+     * ITUtil.PORTAL + "?channelTitle=Create%20New%20Agenda&channelUrl=" + WebDriverUtils.getBaseUrlString() +
+     *  "/kr-krad/krmsAgendaEditor?methodToCall=start&dataObjectClassName=org.kuali.rice.krms.impl.ui.AgendaEditor&returnLocation=" +
+     *  ITUtil.PORTAL_URL + ITUtil.HIDE_RETURN_LINK;
+     */
+    public static final String BOOKMARK_URL = AutomatedFunctionalTestUtils.PORTAL + "?channelTitle=Create%20New%20Agenda&channelUrl=" + WebDriverUtils
+            .getBaseUrlString() +"/kr-krad/krmsAgendaEditor?methodToCall=start&dataObjectClassName=org.kuali.rice.krms.impl.ui.AgendaEditor&returnLocation=" +
+            AutomatedFunctionalTestUtils.PORTAL_URL + AutomatedFunctionalTestUtils.HIDE_RETURN_LINK;
+
+    @Override
+    protected String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+        waitForTitleToEqualKualiPortalIndex();
+        selectTopFrame();
+        waitAndClickByLinkText("Main Menu","");
+        waitForTitleToEqualKualiPortalIndex();
+        waitAndClickByLinkText("Create New Agenda", "");
+        waitForTitleToEqualKualiPortalIndex("");
+        selectFrameIframePortlet();
+        checkForIncidentReport("Create New Agenda", "");
+    }
+
+    //    @Override
+    //    protected String getBookmarkUrl() {
+    //        return BOOKMARK_URL;
+    //    }
+    //
+    //    /**
+    //     * {@inheritDoc}
+    //     * Create New Agenda
+    //     * @return
+    //     */
+    //    @Override
+    //    protected String getLinkLocator() {
+    //        return "Create New Agenda";
+    //    }
+
+
+    protected void createNewEnterDetails() throws InterruptedException {
+        selectFrameIframePortlet();
+        selectByName("document.newMaintainableObject.dataObject.namespace","Kuali Rules Test");
+        waitAndTypeByName("document.newMaintainableObject.dataObject.agenda.name",getDescriptionUnique());
+        waitAndTypeByName("document.newMaintainableObject.dataObject.contextName","Context1");
+    }
+
+    @Test
+    public void testCreateNewAgendaSaveBookmark() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Save')]");
+        passed();
+    }
+
+    @Test
+    public void testCreateNewAgendaSaveNav() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Save')]");
+        passed();
+    }
+
+    @Test
+    public void testCreateNewAgendaSubmitBookmark() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Submit')]");
+        waitAndClickConfirmationOk();
+        passed();
+    }
+
+    @Test
+    public void testCreateNewAgendaSubmitNav() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Submit')]");
+        waitAndClickConfirmationOk();
+        passed();
+    }
+
+    @Test
+    public void testCreateNewAgendaSaveSubmitBookmark() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Save')]");
+        waitAndClickByXpath("//button[contains(text(),'Submit')]");
+        waitAndClickConfirmationOk();
+        passed();
+    }
+
+    @Test
+    public void testCreateNewAgendaSaveSubmitNav() throws Exception {
+        createNewEnterDetails();
+        waitAndClickByXpath("//button[contains(text(),'Save')]");
+        waitAndClickByXpath("//button[contains(text(),'Submit')]");
+        waitAndClickConfirmationOk();
+        passed();
+    }
+
+    public void testCreateNewAgendaBookmark(JiraAwareFailable failable) throws Exception {
+        testCreateNewAgenda();
+        passed();
+    }
+    public void testCreateNewAgendaNav(JiraAwareFailable failable) throws Exception {
+        testCreateNewAgenda();
+        passed();
+    }
+}

bd9a7a5828b1354eda096862b96706db8f5c8b87
Shannon Hess <ShannonHess18@gmail.com>
2014-08-12 21:28:21 +0000

    KULRICE-13015 - Create necessary tests for Super User tab

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/travel/application/DemoTravelCompanySuperUserTabAft.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright 2006-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.rice.krad.demo.travel.application;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Test;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.kuali.rice.testtools.selenium.WebDriverUtils;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoTravelCompanySuperUserTabAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/maintenance?methodToCall=start&dataObjectClassName=edu.sampleu.travel.dataobject.TravelCompany
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/maintenance?methodToCall=start&dataObjectClassName=edu.sampleu.travel.dataobject.TravelCompany";
+
+    /**
+     *  Approve
+     */
+    private static final String APPROVE="Approve";
+
+    /**
+     *  Disapprove
+     */
+    private static final String DISAPPROVE="Disapprove";
+
+    /**
+     *  Take Selected Actions
+     */
+    private static final String TAKE_SELECTED_ACTIONS="Take Selected Actions";
+
+    /**
+     * Description field
+     */
+    public static final String DESCRIPTION_FIELD = "document.documentHeader.documentDescription";
+
+    /**
+     * Travel account name field
+     */
+    public static final String COMPANY_NAME_FIELD = "document.newMaintainableObject.dataObject.travelCompanyName";
+
+    @Override
+    public String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    protected void navigate() throws Exception {
+        waitAndClickDemoLink();
+        waitAndClickByLinkText("Travel Company Lookup");
+        waitAndClickByLinkText("Create New");
+    }
+
+    @Override
+    public String getUserName() {
+        if ((testName.getMethodName().equals("testDemoTravelCompanySuperUserTabNotAppearBookmark"))||
+            (testName    .getMethodName().equals("testDemoTravelCompanySuperUserTabNotAppearNav"))) {
+            return "testUser2";
+        } else {
+            return super.getUserName();
+        }
+    }
+
+    protected void testTravelAccountCreateDocument() throws Exception {
+        waitForDocIdKrad();
+        waitAndTypeByName(DESCRIPTION_FIELD,"Travel Company Super User Test");
+        String randomCode = RandomStringUtils.randomAlphabetic(9).toUpperCase();
+        waitAndTypeByName(COMPANY_NAME_FIELD, "Company Name " + randomCode);
+
+        waitAndClickByLinkText("Ad Hoc Recipients");
+        waitAndTypeByName("newCollectionLines['document.adHocRoutePersons'].id", "user1");
+        jGrowl("Click Add button");
+        waitAndClickById("Uif-AdHocPersonCollection_add");
+        waitForElementPresentByXpath(
+                "//div[@data-parent=\"Uif-AdHocPersonCollection\"]/div/span[contains(text(), 'user1']");
+
+        waitAndTypeByName("newCollectionLines['document.adHocRoutePersons'].id", "user2");
+        jGrowl("Click Add button");
+        waitAndClickById("Uif-AdHocPersonCollection_add");
+        waitForElementPresentByXpath(
+                "//div[@data-parent=\"Uif-AdHocPersonCollection\"]/div/span[contains(text(), 'user2']");
+        waitAndClickByLinkText("Ad Hoc Recipients");
+
+        waitAndClickSubmitByText();
+        waitAndClickConfirmationOk();
+        waitForTextPresent("Document was successfully submitted.", WebDriverUtils.configuredImplicityWait() * 2);
+    }
+
+    protected void testTravelAccountSuperUserApprove() throws Exception {
+        waitAndClickByLinkText("Super User Actions");
+        waitAndTypeByName("document.superUserAnnotation","Reason for approval");
+        waitAndClickButtonByText(APPROVE);
+        waitForTextPresent("was superuser approved.");
+        reloadAndCheckDocStatus("FINAL");
+    }
+
+    protected void testTravelAccountSuperUserDisapprove() throws Exception {
+        waitAndClickByLinkText("Super User Actions");
+        waitAndTypeByName("document.superUserAnnotation","Reason For disapproval");
+        waitAndClickButtonByText(DISAPPROVE);
+        waitForTextPresent("was superuser disapproved.");
+        reloadAndCheckDocStatus("DISAPPROVED");
+    }
+
+    protected void testTravelAccountSuperUserTakeActions() throws Exception {
+        waitAndClickByLinkText("Super User Actions");
+        waitAndTypeByName("document.superUserAnnotation","Reason For Taking Action for user1");
+
+        // Submitted document throws an error when no checkbox is selected and the user presses the Take Action button
+        waitAndClickButtonByText(TAKE_SELECTED_ACTIONS);
+        waitForProgressLoading();
+        waitForTextPresent("Super User Actions: 1 error");
+        waitForTextPresent("Please select an action to perform");
+
+        checkByName("selectedCollectionLines['document.actionRequests']");
+        waitAndClickButtonByText(TAKE_SELECTED_ACTIONS);
+        waitForTextPresent("Action request");
+        waitForTextPresent("was superuser approved in Document");
+
+        // This is currently broken - Once KULRICE-13093 is fixed this line can be uncommented
+        //assertFalse("Remaining action should not be checked", isCheckedByName("selectedCollectionLines['document.actionRequests']"));
+
+        waitAndTypeByName("document.superUserAnnotation","Reason For Taking Action for user2");
+        checkByName("selectedCollectionLines['document.actionRequests']");
+        waitAndClickButtonByText(TAKE_SELECTED_ACTIONS);
+        waitForProgressLoading();
+        waitForTextPresent("Action request");
+        waitForTextPresent("was superuser approved in Document");
+
+        reloadAndCheckDocStatus("FINAL");
+    }
+
+    protected void testTravelAccountSuperUserErrors() throws Exception {
+        waitAndClickByLinkText("Super User Actions");
+
+        // Submitted document throws an error when a checkbox is selected and the Approve button is pressed
+        checkByName("selectedCollectionLines['document.actionRequests']");
+        waitAndClickButtonByText(APPROVE);
+        waitForProgressLoading();
+        waitForTextPresent(
+                "button was selected but there are actions checked. Please either uncheck the actions or select the");
+        uncheckByName("selectedCollectionLines['document.actionRequests']");
+
+        // Submitted document throws an error when the annotation is not filled in and the Approve button is pressed
+        waitAndClickButtonByText(APPROVE);
+        waitForProgressLoading();
+        waitForTextPresent("Annotation: Please provide an annotation to continue with the approval of this document");
+
+        // Submitted document throws an error when the annotation is not filled in and the Disapprove button is pressed
+        waitAndClickButtonByText(DISAPPROVE);
+        waitForProgressLoading();
+        waitForTextPresent(
+                "Annotation: Please provide an annotation to continue with the disapproval of this document");
+
+        // Submitted document throws an error when the annotation is not filled in and the Take Selected Action
+        // button is pressed
+        waitAndClickButtonByText(TAKE_SELECTED_ACTIONS);
+        waitForProgressLoading();
+        waitForTextPresent("Annotation: Please provide an annotation to continue taking these actions on the document");
+
+        //Set value in annotation field
+        waitAndTypeByName("document.superUserAnnotation", "Reason for approval");
+
+        // Submitted document throws an error when a checkbox is selected and the user presses the Disapprove button
+        checkByName("selectedCollectionLines['document.actionRequests']");
+        waitAndClickButtonByText(DISAPPROVE);
+        waitForProgressLoading();
+        waitForTextPresent(
+                "button was selected but there are actions checked. Please either uncheck the actions or select the");
+        uncheckByName("selectedCollectionLines['document.actionRequests']");
+
+        // Go ahead and approve the document
+        waitAndClickButtonByText(APPROVE);
+        reloadAndCheckDocStatus("FINAL");
+    }
+
+    protected void testTravelAccountSuperUserTabNotAppear() throws Exception {
+        // Super User tab does not show up when regular user routes the document
+        assertTextNotPresent("Super User Actions");
+        reloadAndCheckDocStatus("ENROUTE");
+    }
+
+    private void reloadAndCheckDocStatus(String docStatus) throws Exception {
+        waitAndClickButtonByText("Reload");
+        waitForTextPresent("Document was successfully reloaded.");
+        assertTextPresent(docStatus);
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserApproveBookmark() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserApprove();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserApproveNav() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserApprove();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserDisapproveBookmark() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserDisapprove();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserDisapproveNav() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserDisapprove();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserTakeActionsBookmark() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserTakeActions();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserTakeActionsNav() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserTakeActions();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserErrorsBookmark() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserErrors();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserErrorsNav() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserErrors();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserTabNotAppearBookmark() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserTabNotAppear();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelCompanySuperUserTabNotAppearNav() throws Exception {
+        testTravelAccountCreateDocument();
+        testTravelAccountSuperUserTabNotAppear();
+        passed();
+    }
+}

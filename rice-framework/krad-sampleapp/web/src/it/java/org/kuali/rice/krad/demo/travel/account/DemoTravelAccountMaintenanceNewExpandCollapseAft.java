30b378eca696bac137b068e2c573115d73fd51d9
Deep Moteria <dmoteria@vivantech.com>
2014-08-20 05:51:50 +0000

    RICEQA-44 : Create Smoke Tests for KRAD Maintenance Document Tab Expand on Error

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/travel/account/DemoTravelAccountMaintenanceNewExpandCollapseAft.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad.demo.travel.account;
+
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.kuali.rice.testtools.selenium.WebDriverUtils;
+import org.openqa.selenium.By;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoTravelAccountMaintenanceNewExpandCollapseAft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/maintenance?methodToCall=start&dataObjectClassName=org.kuali.rice.krad.demo.travel.dataobject.TravelAccount&hideReturnLink=true
+     */
+    public static final String BOOKMARK_URL = "/kr-krad/maintenance?methodToCall=start&dataObjectClassName=org.kuali.rice.krad.demo.travel.dataobject.TravelAccount&hideReturnLink=true";
+
+    @Override
+    public String getBookmarkUrl() {
+        return BOOKMARK_URL;
+    }
+
+    protected void navigate() throws Exception {
+        waitAndClickDemoLink();
+        waitAndClickByLinkText("Travel Account Maintenance (New)");
+    }
+
+    protected void testTravelAccountMaintenanceNewExpandCollapse() throws Exception {
+        waitAndClickByXpath("//a/span[contains(text(),'Document Overview')]");
+        waitAndClickByXpath("//a/span[contains(text(),'Account Information')]");
+        waitAndClickByXpath("//a/span[contains(text(),'Sub Accounts')]");
+        waitAndClickButtonByExactText("Submit");
+        waitForElementPresentByXpath("//div[@style='overflow: hidden; display: block;']/div/ul/li/a[contains(text(),'Description: Required')]");
+        waitForElementPresentByXpath("//div[@style='overflow: hidden; display: block;']/div/ul/li/a[contains(text(),'Travel Account Number: Required')]");
+        waitForElementPresentByXpath("//div[@style='overflow: hidden; display: block;']/div/ul/li/a[contains(text(),'Travel Account Name: Required')]");
+        waitForElementPresentByXpath("//div[@style='overflow: hidden; display: block;']/div/ul/li/a[contains(text(),'Travel Account Type Code: Required')]");
+    }
+
+    @Test
+    public void testDemoTravelAccountMaintenanceNewExpandCollapseBookmark() throws Exception {
+        testTravelAccountMaintenanceNewExpandCollapse();
+        passed();
+    }
+
+    @Test
+    public void testDemoTravelAccountMaintenanceNewExpandCollapseNav() throws Exception {
+        testTravelAccountMaintenanceNewExpandCollapse();
+        passed();
+    }
+}

f9ab2b8d280a721269dbee0ab257f7558e6d4392
Deep Moteria <dmoteria@vivantech.com>
2014-08-21 04:20:23 +0000

    RICEQA-519 : Create Freemarker 404 AFT

---
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/DemoFreemarker404Aft.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.rice.krad;
+
+import org.kuali.rice.krad.demo.ViewDemoAftBase;
+import org.kuali.rice.testtools.selenium.WebDriverLegacyITBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+/**
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class DemoFreemarker404Aft extends WebDriverLegacyITBase {
+
+    /**
+     * /kr-krad/lab
+     */
+    public static final String BAD_BOOKMARK_URL = "/kr-krad/lab";
+    
+    @Override
+    public String getBookmarkUrl() {
+        return BAD_BOOKMARK_URL;
+    }
+
+    @Override
+    protected void navigate() throws Exception {
+    	//No need for navigation test as we just need to test Freemarker Error on BAD URL.
+    }
+
+    @Test
+    public void testLookUpConditionalCriteriaBookmark() throws Exception {
+        //Need to test the BAD URL for Freemarker Error.
+    }
+}

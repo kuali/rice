d9a4c1359eeab498cfd9a27db72ced41656e6c58
Christopher Wade <christopher.d.wade@wustl.edu>
2014-09-10 16:47:43 +0000

    KULRICE-13212 :  fix for 'AFT Failure LabsLookupTravelAuthorizationDocumentCopyActionAft copy link no longer present'

--- rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/transactional/LabsLookupTravelAuthorizationDocumentCopyActionAft.java
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/labs/transactional/LabsLookupTravelAuthorizationDocumentCopyActionAft.java
@@ -27,13 +27,8 @@ import java.util.List;
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
 public class LabsLookupTravelAuthorizationDocumentCopyActionAft extends LabsTransactionalBase {
-
     public static final String BOOKMARK_URL = "/kr-krad/lookup?methodToCall=start&viewId=TravelAuthorization-LookupView";
-
     public static final String COPIED_FROM_DOCUMENT_ID = "3034";
-
-
-
 
     @Override
     protected String getBookmarkUrl() {
@@ -43,7 +38,6 @@ public class LabsLookupTravelAuthorizationDocumentCopyActionAft extends LabsTran
     @Override
     protected void navigate() throws Exception {
         navigateToTransactional("Transactional Sample - Lookup With Copy Actions");
-
     }
 
     @Test
@@ -59,70 +53,17 @@ public class LabsLookupTravelAuthorizationDocumentCopyActionAft extends LabsTran
     }
 
     public void testTransactionalLookupAndCopy() throws Exception {
-
-        // Lookup Page
         waitAndClickButtonByExactText("Search");
         waitForElementPresent(By.id("uLookupResults_layout"));
-        WebElement resultsTable = driver.findElement(By.id("uLookupResults_layout"));
-        selectActionLinkByTableRowData(resultsTable, "View", "Document Number", getCopiedFromDocumentId() );
-
-        // Original Document Page
+        jGrowl("Click on 2nd View Link");
+        waitAndClick(By.xpath("(//a[contains(text(),'View')])[2]"));
         waitForPageToLoad();
         waitAndClickButtonByExactText("Copy");
-
-        // Copied Document Page
         waitForPageToLoad();
         waitAndClickByLinkText("Notes and Attachments (1)");
-
     }
 
     public String getCopiedFromDocumentId() {
         return COPIED_FROM_DOCUMENT_ID;
     }
-
-    /** Helper function used with results table for finding a row with the right data
-     * and selecting its link
-     *
-     * @param linkText
-     * @param headerLabel
-     * @param columnText
-     */
-    protected void selectActionLinkByTableRowData(WebElement resultsTable, String linkText, String headerLabel, String columnText) {
-        int index = 1;
-        int headerIndex = -1;
-        int actionIndex = -1;
-        String actionLabel = "Actions";
-
-        List<WebElement> tableRows = resultsTable.findElements(By.tagName("tr"));
-        List<WebElement> headerColumns = tableRows.get(0).findElements(By.tagName("th"));
-
-        // identify the correct header and actions columns for review
-        if(tableRows != null && tableRows.size() > 0) {
-            for (int columnIndex = 0; columnIndex < headerColumns.size(); columnIndex++) {
-                if (headerColumns.get(columnIndex).getText().contains(headerLabel)) {
-                    headerIndex = columnIndex;
-                }
-                // Actions are generally in the first column but this is not guaranteed
-                if (headerColumns.get(columnIndex).getText().contains(actionLabel)) {
-                    actionIndex = columnIndex;
-                }
-            }
-
-            if(headerIndex > 0 && actionIndex >= 0 ) {
-                boolean hasClicked = false;
-                for (int rowIndex = 1; rowIndex < tableRows.size() && !hasClicked; rowIndex++) {
-                    List<WebElement> dataColumns = tableRows.get(rowIndex).findElements(By.tagName("td"));
-                    if (dataColumns.get(headerIndex).getText().contains(columnText)) {
-                        dataColumns.get(actionIndex).findElement(By.linkText(linkText)).click();
-                        hasClicked = true;
-                    }
-                }
-            }
-            else {
-                // Error: header and/or actions column not found
-            }
-
-        }
-    }
-
 }

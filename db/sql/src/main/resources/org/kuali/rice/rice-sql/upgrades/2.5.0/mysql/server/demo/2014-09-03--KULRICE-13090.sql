4499f4176f65dbb74c1a97d6e3e834552c30424b
Jeff Caddel <jcaddel@rsmart.com>
2014-09-11 05:08:47 +0000

    Automated license header update

--- db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.5.0/mysql/server/demo/2014-09-03--KULRICE-13090.sql
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.5.0/mysql/server/demo/2014-09-03--KULRICE-13090.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2005-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND)
 VALUES ('KRSAP100030', UUID(), '1', '46', 'KR-SAP', 'View Note / Attachment Travel Document', 'Applies to travel document', 'Y')
 /

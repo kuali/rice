1db7cc14a5f9c1b2366cc4c67f64524580c1e882
Shannon Hess <ShannonHess18@gmail.com>
2014-09-29 15:00:30 +0000

    KULRICE-13002 - Object on EntityTypeContactInfoBo are not update able from the UI

--- rice-middleware/sampleapp/src/it/java/edu/sampleu/kim/api/identity/IdentityPersonLookUpEditAftBase.java
+++ rice-middleware/sampleapp/src/it/java/edu/sampleu/kim/api/identity/IdentityPersonLookUpEditAftBase.java
@@ -16,6 +16,8 @@
 package edu.sampleu.kim.api.identity;
 
 import edu.sampleu.admin.AdminTmplMthdAftNavBase;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.kuali.rice.testtools.common.JiraAwareFailable;
 import org.kuali.rice.testtools.selenium.AutomatedFunctionalTestUtils;
 import org.kuali.rice.testtools.selenium.WebDriverUtils;
@@ -61,35 +63,106 @@ public abstract class IdentityPersonLookUpEditAftBase extends AdminTmplMthdAftNa
     }
     
     public void testIdentityPersonLookUpEdit() throws Exception {
+        String randomSalary = RandomStringUtils.randomNumeric(6);
+        String randomMiddleName = RandomStringUtils.randomAlphabetic(6);;
+        String randomAddress = RandomStringUtils.randomNumeric(6);;
+        String randomExtension = RandomStringUtils.randomNumeric(6);;
+        String randomEmail = RandomStringUtils.randomAlphabetic(6);;
+
         selectFrameIframePortlet();
         waitAndTypeByName("principalName","fran");
         waitAndClickSearchSecond();
         selectFrameIframePortlet();
         waitAndClickByXpath("//a[@title='edit Person withPrincipal ID=fran ']");
         waitAndTypeByName("document.documentHeader.documentDescription", "Test description of person");
-        selectByName("newAffln.affiliationTypeCode", "Staff");
-        selectByName("newAffln.campusCode","BL - BLOOMINGTON");
-        waitAndClickByName("newAffln.dflt");
-        waitAndClickByName("methodToCall.addAffln.anchor");
-        waitAndTypeByName("document.affiliations[0].newEmpInfo.employeeId","9999999999");
-        waitAndClickByName("document.affiliations[0].newEmpInfo.primary");
-        selectByName("document.affiliations[0].newEmpInfo.employmentStatusCode","Active");
-        selectByName("document.affiliations[0].newEmpInfo.employmentTypeCode","Professional");
-        waitAndTypeByName("document.affiliations[0].newEmpInfo.baseSalaryAmount","99999");
-        waitAndTypeByXpath("//*[@id='document.affiliations[0].newEmpInfo.primaryDepartmentCode']", "BL-BUS");
-        waitAndClickByName("methodToCall.addEmpInfo.line0.anchor");
-        waitAndClickByName("methodToCall.showAllTabs");
-        waitAndClickByName("methodToCall.route");
 
-        if (hasDocError()) {
-            // After the second run of this test, there will be 2 Errors.  Requires a decent amount of work to satisfy constraints.
-            if (!extractErrorText().startsWith("3 error(s) found on page")) {
-                checkForDocError();
-            }
+        // Add new information if it is not present.  Otherwise, edit the existing information.
+        if (isElementPresentByName("document.affiliations[0].campusCode")) {
+            clearTextByName("document.affiliations[0].empInfos[0].baseSalaryAmount");
+            waitAndTypeByName("document.affiliations[0].empInfos[0].baseSalaryAmount", "1" + randomSalary);
         } else {
-            assertTextPresent("Document was successfully submitted.");
+            selectByName("newAffln.affiliationTypeCode", "Staff");
+            selectByName("newAffln.campusCode", "BL - BLOOMINGTON");
+            waitAndClickByName("newAffln.dflt");
+            waitAndClickByName("methodToCall.addAffln.anchor");
+            waitAndTypeByName("document.affiliations[0].newEmpInfo.employeeId", "9999999999");
+            waitAndClickByName("document.affiliations[0].newEmpInfo.primary");
+            selectByName("document.affiliations[0].newEmpInfo.employmentStatusCode", "Active");
+            selectByName("document.affiliations[0].newEmpInfo.employmentTypeCode", "Professional");
+            waitAndTypeByName("document.affiliations[0].newEmpInfo.baseSalaryAmount", "1" + randomSalary);
+            waitAndTypeByXpath("//*[@id='document.affiliations[0].newEmpInfo.primaryDepartmentCode']", "BL-BUS");
+            waitAndClickByName("methodToCall.addEmpInfo.line0.anchor");
         }
-        waitAndClickByName("methodToCall.close");
-//         waitAndClickByName("methodToCall.processAnswer.button1");
+
+        waitAndClickByName("methodToCall.showAllTabs");
+
+        // Add nick name or edit the existing one
+        if (isTextPresent("CrazyNickName")) {
+            clearTextByName("document.names[1].middleName");
+            waitAndTypeByName("document.names[1].middleName", randomMiddleName);
+        } else {
+            waitAndTypeByName("newName.firstName", "CrazyNickName");
+            waitAndTypeByName("newName.middleName", randomMiddleName);
+            waitAndTypeByName("newName.lastName", "fran");
+            waitAndClickByName("methodToCall.addName.anchor");
+        }
+
+        // Add address or edit the existing one
+        if (isTextPresent("CrazyHomeAddress")) {
+            clearTextByName("document.addrs[0].line1");
+            waitAndTypeByName("document.addrs[0].line1", randomAddress + " Main St");
+        } else {
+            waitAndTypeByName("newAddress.line1", randomAddress + " Main St");
+            waitAndTypeByName("newAddress.line2", "CrazyHomeAddress");
+            waitAndTypeByName("newAddress.city", "Bloomington");
+            selectByName("newAddress.stateProvinceCode", "ALASKA");
+            waitAndTypeByName("newAddress.postalCode", "61821");
+            selectByName("newAddress.countryCode", "United States");
+            waitAndClickByName("newAddress.dflt");
+            waitAndClickByName("methodToCall.addAddress.anchor");
+        }
+
+        // Add phone number or edit the existing one
+        if (isTextPresent("555-555-5555")) {
+            clearTextByName("document.phones[0].extensionNumber");
+            waitAndTypeByName("document.phones[0].extensionNumber", randomExtension);
+        } else {
+            waitAndTypeByName("newPhone.phoneNumber", "555-555-5555");
+            waitAndTypeByName("newPhone.extensionNumber", randomExtension);
+            selectByName("newPhone.countryCode", "United States");
+            waitAndClickByName("newPhone.dflt");
+            waitAndClickByName("methodToCall.addPhone.anchor");
+        }
+
+        // Add home email or edit the existing one
+        if (isTextPresent("@gmailCrazy.com")) {
+            clearTextByName("document.emails[1].emailAddress");
+            waitAndTypeByName("document.emails[1].emailAddress", randomEmail + "@gmailCrazy.com");
+        } else {
+            waitAndTypeByName("newEmail.emailAddress", randomEmail + "@gmailCrazy.com");
+            waitAndClickByName("methodToCall.addEmail.anchor");
+        }
+
+        waitAndClickByName("methodToCall.route");
+        assertTextPresent("Document was successfully submitted.");
+
+        selectTopFrame();
+        waitAndClickAdministration();
+        waitAndClickByLinkText("Person");
+        selectFrameIframePortlet();
+        waitAndTypeByName("principalName","fran");
+        waitAndClickSearchSecond();
+        selectFrameIframePortlet();
+        waitAndClickByXpath("//a[@title='edit Person withPrincipal ID=fran ']");
+        waitAndClickByName("methodToCall.showAllTabs");
+
+        assertTextPresent("1," + StringUtils.substring(randomSalary, 0, 3) + ","
+                + StringUtils.substring(randomSalary, 3, 6) + ".00");
+        assertTextPresent(randomMiddleName);
+        assertTextPresent(randomAddress);
+        assertTextPresent(randomExtension);
+        assertTextPresent(randomEmail);
+
+        testCancelConfirmation();
     }
 }

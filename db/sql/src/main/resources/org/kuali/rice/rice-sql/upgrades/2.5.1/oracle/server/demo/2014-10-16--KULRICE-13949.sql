a688a5c1759f4531bee2f3c6c026cf6a521fcfb8
Kristina Taylor <kpbtaylor@gmail.com>
2014-10-17 21:34:07 +0000

    KULRICE-13949: Updating demo data to remove error

---
+++ db/sql/src/main/resources/org/kuali/rice/rice-sql/upgrades/2.5.1/oracle/server/demo/2014-10-16--KULRICE-13949.sql
@@ -0,0 +1,50 @@
+--
+-- Copyright 2005-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License")/
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1000'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1001'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1002'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1003'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1004'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1005'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1006'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1007'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1008'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1011'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1012'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '2' WHERE PROP_ID = 'T1013'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '3' WHERE PROP_ID = 'T1014'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1015'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '2' WHERE PROP_ID = 'T1016'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '1' WHERE PROP_ID = 'T1018'
+/
+UPDATE KRMS_PROP_T SET CMPND_SEQ_NO = '2' WHERE PROP_ID = 'T1019'
+/

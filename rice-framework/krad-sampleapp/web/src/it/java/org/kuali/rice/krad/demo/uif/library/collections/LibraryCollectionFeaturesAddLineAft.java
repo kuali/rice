5dfb87da46ec694ccab0bf435e03ff9f03994bb0
Deep Moteria <dmoteria@vivantech.com>
2014-10-29 04:55:00 +0000

    KULRICE-13992 : Fill AFT Gap: Add Line ("Collection Add Line with Dialog and Custom Actions")

--- rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/collections/LibraryCollectionFeaturesAddLineAft.java
+++ rice-framework/krad-sampleapp/web/src/it/java/org/kuali/rice/krad/demo/uif/library/collections/LibraryCollectionFeaturesAddLineAft.java
@@ -42,119 +42,106 @@ public class LibraryCollectionFeaturesAddLineAft extends WebDriverLegacyITBase {
         waitAndClickByLinkText("Add Line");
     }
 
-    protected void testCollectionFeaturesAddLine() throws Exception {
-        selectByName("exampleShown", "Collection Default Add Line");
-
-        // add some values to the collection
-        waitAndTypeByName("newCollectionLines['collection1'].field1", "12");
-        waitAndTypeByName("newCollectionLines['collection1'].field2", "5");
-        jGrowl("Click Add button.");
-        waitAndClickByXpath("//section[@id = 'Demo-CollectionAddLine-Example1']//button[contains(text(), 'Add')]");
-
-        // assert that the values have been added
-        waitForElementPresent("input[name = 'collection1[0].field1'][value = '12']");
-        waitForElementPresent("input[name = 'collection1[0].field2'][value = '5']");
+    protected void testCollectionFeaturesDefaultAddLine() throws Exception {
+        selectByName("exampleShown","Collection Default Add Line");
+        waitAndTypeByXpath("//section[@id='Demo-CollectionAddLine-Example1']/section/div/div/table/tbody/tr[1]/td[2]/div/input","12");
+        waitAndTypeByXpath("//section[@id='Demo-CollectionAddLine-Example1']/section/div/div/table/tbody/tr[1]/td[3]/div/input","5");
+        waitAndClickButtonByText("Add");
+        waitForElementPresentByXpath("//input[@name='collection1[0].field1' and @value='12']");
+        assertElementPresentByXpath("//input[@name='collection1[0].field2' and @value='5']");
     }
 
-    protected void testCollectionFeaturesAddLineWithDuplicatePropertyName() throws Exception {
-        selectByName("exampleShown", "Collection Add Line with duplicateLinePropertyNames");
-
-        // attempt to add some values to the collection that have already been added
-        waitAndTypeByName("newCollectionLines['collection1_2'].field1", "13");
-        waitAndTypeByName("newCollectionLines['collection1_2'].field2", "14");
-        jGrowl("Click Add button.");
-        waitAndClickByXpath("//section[@id = 'Demo-CollectionAddLine-Example2']//button[contains(text(), 'Add')]");
-
-        // assert that an error message is shown
-        waitForTextPresent("Duplicate Default Add Line with duplicateLinePropertyNames property configured");
+    protected void testCollectionFeaturesDefaultAddLineDuplicatePropertyName() throws Exception {
+        selectByName("exampleShown","Collection Add Line w duplicateLinePropertyNames");
+        waitAndTypeByXpath("//section[@id='Demo-CollectionAddLine-Example6']/section/div/div/table/tbody/tr[1]/td[2]/div/input","13");
+        waitAndTypeByXpath("//section[@id='Demo-CollectionAddLine-Example6']/section/div/div/table/tbody/tr[1]/td[3]/div/input","14");
+        waitAndClickByXpath("//section[@id='Demo-CollectionAddLine-Example6']/section/div/div/table/tbody/tr[1]/td[4]/div/fieldset/div/button");
+        waitForElementPresentByXpath("//li[@class='uif-errorMessageItem' and contains(text(),'Duplicate Default Add Line with duplicateLinePropertyNames property configured')]");
     }
 
-    protected void testCollectionFeaturesAddLineWithDialog() throws Exception {
-        selectByName("exampleShown", "Collection Add Line with Dialog");
-
-        // open the add line dialog
-        jGrowl("Click Add Line button.");
-        waitAndClickByXpath("//section[@id = 'Demo-CollectionAddLine-Example3']//button[contains(text(), 'Add Line')]");
-
-        // attempt to enter an empty value via the dialog for the required field
-        waitForElementPresentByName("newCollectionLines['collection1_3'].field1");
-        jGrowl("Click Add button.");
-        waitAndClickByXpath("//div[@id = 'Uif-Dialogs']//button[contains(text(), 'Add')]");
-
-        // assert that an error message is shown
-        waitIsVisible("div[data-label = 'Field 1'] li[class = 'uif-errorMessageItem-field]");
-
-        // add some values to the collection via the dialog
-        waitAndTypeByName("newCollectionLines['collection1_3'].field1", "42");
-        waitAndTypeByName("newCollectionLines['collection1_3'].field2", "55");
-        jGrowl("Click Add button.");
-        waitAndClickByXpath("//div[@id = 'Uif-Dialogs']//button[contains(text(), 'Add')]");
-
-        // assert that the values have been added
-        waitForElementPresent("input[name = 'collection1_3[0].field1'][value = '42']");
-        waitForElementPresent("input[name = 'collection1_3[0].field2'][value = '55']");
+    protected void testCollectionFeaturesDefaultAddBlankLine() throws Exception {
+        selectByName("exampleShown","Collection Add Blank Line");
+        waitAndClickByXpath("//section[@id='Demo-CollectionAddLine-Example3']/section/div/button");
+        assertElementPresentByXpath("//input[@name='collection1_6[0].field1' and @value]");
+        assertElementPresentByXpath("//input[@name='collection1_6[0].field2' and @value]");
     }
 
-    protected void testCollectionFeaturesAddBlankLine() throws Exception {
-        selectByName("exampleShown", "Collection Add Blank Line");
-
-        // click the add line button
-        jGrowl("Click Add Line button.");
-        waitAndClickByXpath("//section[@id = 'Demo-CollectionAddLine-Example4']//button[contains(text(), 'Add Line')]");
-
-        // assert that the blank line has been added
-        waitForElementPresent("input[name = 'collection1_7[0].field1'][value = '']");
-        waitForElementPresent("input[name = 'collection1_7[0].field2'][value = '']");
-    }
-
-    protected void testCollectionFeaturesAddLineWithCustomActions() throws Exception {
-        selectByName("exampleShown", "Collection Add Line with Custom Actions");
+    protected void testCollectionFeaturesDefaultAddViaLightbox() throws Exception {
+        selectByName("exampleShown","Collection Add with Dialog");
+        jGrowl("Click Add Line");
+        waitAndClickByXpath("//section[@data-parent=\"Demo-CollectionAddLine-Example2\"]/div/button");
+        waitAndTypeByName("newCollectionLines['collection1_2'].field1", "");
+        waitAndTypeByName("newCollectionLines['collection1_2'].field2", "");
+        fireMouseOverEventByName("newCollectionLines['collection1_2'].field1");
+        waitIsVisible("//div[@class='jquerybubblepopup jquerybubblepopup-kr-error-cs' and @style='margin: 0px 0px 0px 72.16666662693024px; opacity: 1; top: 61px; left: 834px; position: absolute; display: block;']");
+        waitAndTypeByName("newCollectionLines['collection1_2'].field1", "42");
+        waitAndTypeByName("newCollectionLines['collection1_2'].field2", "55");
+        jGrowl("Click Add");
+        waitAndClickByXpath("//div[@class=\"modal-footer\"]/button[@data-loadingmessage=\"Adding Line...\"]");
+        waitForElementPresentByXpath("//input[@name='collection1_2[0].field1' and @value='42']");
+        waitForElementPresentByXpath("//input[@name='collection1_2[0].field2' and @value='55']");
     }
 
     protected void testCollectionFeaturesAddLineWithDialogAndCustomActions() throws Exception {
-        selectByName("exampleShown", "Collection Add Line with Dialog and Custom Actions");
+        selectByName("exampleShown","Collection Add Line with Dialog and Custom Actions");
+
+        // Add Income Cancel
+        waitAndClickByXpath("//section[@data-parent='Demo-CollectionAddLine-Example4']/button/span[@class='icon-plus']");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field3","999");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field1","999");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field2","999");
+        waitAndClickByXpath("//section[@style='display: block;']/div/div/div[@class='modal-footer']/button[contains(text(),'Cancel')]");
+        waitForElementNotPresent(By.xpath("//section[@style='display: block;']"));
+        waitForElementNotPresent(By.xpath("//input[@value='999']"));
+
+        //Add Income and Verify
+        waitAndClickByXpath("//section[@data-parent='Demo-CollectionAddLine-Example4']/button/span[@class='icon-plus']");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field3","999");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field1","999");
+        waitAndTypeByName("newCollectionLines['collection1_7'].field2","999");
+        waitAndClickByXpath("//section[@style='display: block;']/div/div/div[@class='modal-footer']/button[contains(text(),'Cancel')]");
+        waitForElementPresentByXpath("//input[@value='999']");
+
+        //Trash Check
+        waitAndClickByXpath("//button[@class='btn btn-default btn-xs uif-action uif-boxLayoutHorizontalItem icon-trash']");
+        waitForElementNotPresent(By.xpath("//input[@value='999']"));
+        waitForTextPresent("You have deleted an item from Project Income.");
     }
 
-    protected void testCollectionFeaturesAddRefreshingDialog() throws Exception {
-        selectByName("exampleShown", "Collection Add Line with Refreshing Dialog");
+    protected void testCollectionFeaturesAddLineWithCustomActions() throws Exception {
+        selectByName("exampleShown","Collection Add Line with Custom Actions");
 
-        // open the add line dialog
-        jGrowl("Click Add Income button.");
-        waitAndClickByXpath("//section[@id = 'Demo-CollectionAddLine-Example7']//button/text()[contains(., 'Add Income')]/..");
+        // Add Income Verify
+        waitAndTypeByName("newCollectionLines['collection1_8'].field1","999");
+        waitAndTypeByName("newCollectionLines['collection1_8'].field2","999");
+        waitAndClickByXpath("//section[@data-parent='Demo-CollectionAddLine-Example5']/div/table/tbody/tr/td[4]/div/fieldset/div/button[contains(text(),'Add')]");
+        waitForElementPresentByXpath("//input[@value='999']");
 
-        // assert that the button does not exist
-        waitForElementNotPresent(By.xpath("//section[@id = 'Demo-CollectionAddLine-Example7-AddLineDialog']//button[contains(text(), 'Add Income')]"));
-
-        // enter the required value and trigger the change
-        waitAndTypeByName("newCollectionLines['collection1_10'].field1", "77");
-        waitAndTypeByName("newCollectionLines['collection1_10'].field2", "");
-        waitForProgressLoading();
-
-        // assert that the button now exists
-        waitForElementPresent(By.xpath("//section[@id = 'Demo-CollectionAddLine-Example7-AddLineDialog']//button[contains(text(), 'Add Income')]"));
+        //Trash Check
+        waitAndClickByXpath("//button[@class='btn btn-default btn-xs uif-action uif-boxLayoutHorizontalItem icon-trash']");
+        waitForElementNotPresent(By.xpath("//input[@value='999']"));
+        waitForTextPresent("You have deleted an item from Project Income.");
     }
 
     @Test
     public void testCollectionFeaturesAddLineBookmark() throws Exception {
-        testCollectionFeaturesAddLine();
-        testCollectionFeaturesAddLineWithDuplicatePropertyName();
-        testCollectionFeaturesAddLineWithDialog();
-        testCollectionFeaturesAddBlankLine();
-        testCollectionFeaturesAddLineWithCustomActions();
+        testCollectionFeaturesDefaultAddLine();
+        testCollectionFeaturesDefaultAddLineDuplicatePropertyName();
+        testCollectionFeaturesDefaultAddBlankLine();
+        testCollectionFeaturesDefaultAddViaLightbox();
         testCollectionFeaturesAddLineWithDialogAndCustomActions();
-        testCollectionFeaturesAddRefreshingDialog();
+        testCollectionFeaturesAddLineWithCustomActions();
         passed();
     }
 
     @Test
     public void testCollectionFeaturesAddLineNav() throws Exception {
-        testCollectionFeaturesAddLine();
-        testCollectionFeaturesAddLineWithDuplicatePropertyName();
-        testCollectionFeaturesAddLineWithDialog();
-        testCollectionFeaturesAddBlankLine();
-        testCollectionFeaturesAddLineWithCustomActions();
+        testCollectionFeaturesDefaultAddLine();
+        testCollectionFeaturesDefaultAddLineDuplicatePropertyName();
+        testCollectionFeaturesDefaultAddBlankLine();
+        testCollectionFeaturesDefaultAddViaLightbox();
         testCollectionFeaturesAddLineWithDialogAndCustomActions();
-        testCollectionFeaturesAddRefreshingDialog();
+        testCollectionFeaturesAddLineWithCustomActions();
         passed();
     }
 

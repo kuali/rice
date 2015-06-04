5abcb70b2a07cc52da17dc93a61969ead253aa6b
Shannon Hess <shannonhess18@gmail.com>
2015-06-04 01:17:23 -0400

    KULRICE-14255 - Fix for PersonServiceImplTest and RoleRouteModuleTest for 2.6

--- rice-middleware/it/kew/src/test/java/org/kuali/rice/kew/role/RoleRouteModuleTest.java
+++ rice-middleware/it/kew/src/test/java/org/kuali/rice/kew/role/RoleRouteModuleTest.java
@@ -164,7 +164,7 @@ public class RoleRouteModuleTest extends KEWTestCase {
         role.setActive(true);
         role.setKimTypeId(kimType.getId());
 
-        String roleMemberId1 = "" + getNextSequenceLongValue("KRIM_ROLE_ID_S");
+        String roleMemberId1 = "" + getNextSequenceLongValue("KRIM_ROLE_MBR_ID_S");
         RoleMemberBo adminRolePrincipal = new RoleMemberBo();
         adminRolePrincipal.setId(roleMemberId1);
         adminRolePrincipal.setRoleId(roleId);
@@ -173,7 +173,7 @@ public class RoleRouteModuleTest extends KEWTestCase {
         adminRolePrincipal.setMemberId(adminPrincipal.getPrincipalId());
         adminRolePrincipal.setType( MemberType.PRINCIPAL );
 
-        String roleMemberId2 = "" + getNextSequenceLongValue("KRIM_ROLE_ID_S");
+        String roleMemberId2 = "" + getNextSequenceLongValue("KRIM_ROLE_MBR_ID_S");
         RoleMemberBo user2RolePrincipal = new RoleMemberBo();
         user2RolePrincipal.setId(roleMemberId2);
         user2RolePrincipal.setRoleId(roleId);
@@ -182,7 +182,7 @@ public class RoleRouteModuleTest extends KEWTestCase {
         user2RolePrincipal.setMemberId(user2Principal.getPrincipalId());
         user2RolePrincipal.setType( MemberType.PRINCIPAL );
 
-        String roleMemberId3 = "" + getNextSequenceLongValue("KRIM_ROLE_ID_S");
+        String roleMemberId3 = "" + getNextSequenceLongValue("KRIM_ROLE_MBR_ID_S");
         RoleMemberBo user1RolePrincipal = new RoleMemberBo();
         user1RolePrincipal.setId(roleMemberId3);
         user1RolePrincipal.setRoleId(roleId);

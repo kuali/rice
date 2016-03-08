5e136fabf4ab5168db32eb2d1b251367a6b4da60
James Bennett <jwbennet@gmail.com>
2016-03-07 23:39:35 -0500

    RICE-8: Added a whitelist for the form/lookup return location
    * Added a white list which the back location (aka returnLocation query
    parameter) must return the user to an expected location
    * Primarily used in "return value" links in lookups and the cancel
    button which appears on many screens
    * Defaults to the application.url, rice.url or anything matching the
    appserver.url in the application's configuration

--- rice-middleware/kns/src/main/java/org/kuali/rice/kns/lookup/AbstractLookupableHelperServiceImpl.java
+++ rice-middleware/kns/src/main/java/org/kuali/rice/kns/lookup/AbstractLookupableHelperServiceImpl.java
@@ -52,6 +52,7 @@ import org.kuali.rice.kns.service.KNSServiceLocator;
 import org.kuali.rice.kns.service.MaintenanceDocumentDictionaryService;
 import org.kuali.rice.kns.util.FieldUtils;
 import org.kuali.rice.kns.util.KNSConstants;
+import org.kuali.rice.kns.util.WebUtils;
 import org.kuali.rice.kns.web.comparator.CellComparatorHelper;
 import org.kuali.rice.kns.web.struts.form.LookupForm;
 import org.kuali.rice.kns.web.struts.form.MultipleValueLookupForm;
@@ -737,7 +738,7 @@ public abstract class AbstractLookupableHelperServiceImpl implements LookupableH
      * @return Returns the backLocation.
      */
     public String getBackLocation() {
-        return backLocation;
+        return WebUtils.sanitizeBackLocation(backLocation);
     }
 
     /**

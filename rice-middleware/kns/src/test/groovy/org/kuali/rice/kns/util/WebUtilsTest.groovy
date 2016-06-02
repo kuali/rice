5e2210100d65df367912a00900b16d0d54d969c7
Eric Westfall <ewestfal@gmail.com>
2016-06-01 22:26:32 -0400

    RICE-25 - Added a test for the changes to WebUtils.sanitizeBackLocation to fix relative URLs

--- rice-middleware/kns/src/test/groovy/org/kuali/rice/kns/util/WebUtilsTest.groovy
+++ rice-middleware/kns/src/test/groovy/org/kuali/rice/kns/util/WebUtilsTest.groovy
@@ -14,29 +14,27 @@
  * limitations under the License.
  */
 package org.kuali.rice.kns.util
-
-import org.kuali.rice.kns.web.ui.Section
-import org.junit.Test
-import org.kuali.rice.kns.web.ui.Row
-import org.kuali.rice.kns.web.ui.Field
-
-import static org.junit.Assert.assertEquals
 import org.apache.struts.action.ActionForm
 import org.apache.struts.config.ControllerConfig
 import org.junit.Before
-import org.kuali.rice.core.framework.config.property.SimpleConfig
+import org.junit.Test
 import org.kuali.rice.core.api.CoreConstants
 import org.kuali.rice.core.api.config.property.ConfigContext
 import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader
+import org.kuali.rice.core.framework.config.property.SimpleConfig
 import org.kuali.rice.core.framework.resourceloader.BaseResourceLoader
-import javax.xml.namespace.QName
-import org.kuali.rice.krad.util.KRADConstants
 import org.kuali.rice.coreservice.framework.parameter.ParameterService
 import org.kuali.rice.kim.api.identity.PersonService
 import org.kuali.rice.kim.impl.identity.TestPerson
 import org.kuali.rice.kns.web.struts.form.KualiMaintenanceForm
-import org.kuali.rice.kew.api.KewApiConstants
+import org.kuali.rice.kns.web.ui.Field
+import org.kuali.rice.kns.web.ui.Row
+import org.kuali.rice.kns.web.ui.Section
+import org.kuali.rice.krad.util.KRADConstants
 
+import javax.xml.namespace.QName
+
+import static org.junit.Assert.assertEquals
 /**
  * Unit tests WebUtils
  *
@@ -44,10 +42,13 @@ import org.kuali.rice.kew.api.KewApiConstants
  */
 @Deprecated
 class WebUtilsTest {
-	
-	private static final String DEFAULT_URL = "default";
-	private static final String TEST_URL = "http://test/url";
-	
+
+    private static final String DEFAULT_URL = "default";
+    private static final String TEST_URL = "http://test/url";
+    private static final String RELATIVE_URL_1 = "relative";
+    private static final String RELATIVE_URL_2 = "/relative";
+    private static final String RELATIVE_URL_3 = "/relative/url";
+
     def strutsControllerConfig = { "250M" } as ControllerConfig;
     String maxUploadSize;
     String maxAttachmentSize;
@@ -65,13 +66,13 @@ class WebUtilsTest {
         GlobalResourceLoader.addResourceLoader(new BaseResourceLoader(new QName("Foo", "Bar")) {
             def getService(QName name) {
                 [ parameterService:
-                        [ getParameterValueAsString: { ns, cmp, param ->
-                            [ (KRADConstants.MAX_UPLOAD_SIZE_PARM_NM): maxUploadSize,
-                                    (KRADConstants.ATTACHMENT_MAX_FILE_SIZE_PARM_NM): maxAttachmentSize ][param]
-                        } ] as ParameterService,
-                        // KualiMaintenanceForm -> Note -> AdHocRoutePerson -> PersonService getPersonImplementationClass lookup :(
-                        // stub impl class
-                        personService: { TestPerson.class } as PersonService
+                          [ getParameterValueAsString: { ns, cmp, param ->
+                              [ (KRADConstants.MAX_UPLOAD_SIZE_PARM_NM): maxUploadSize,
+                                (KRADConstants.ATTACHMENT_MAX_FILE_SIZE_PARM_NM): maxAttachmentSize ][param]
+                          } ] as ParameterService,
+                  // KualiMaintenanceForm -> Note -> AdHocRoutePerson -> PersonService getPersonImplementationClass lookup :(
+                  // stub impl class
+                  personService: { TestPerson.class } as PersonService
                 ][name.getLocalPart()]
             }
         })
@@ -97,26 +98,26 @@ class WebUtilsTest {
 
     protected Row generateRow(String sectionNo, String rowNo) {
         new Row([
-            generateContainerField("sections[$sectionNo].rows[$rowNo].field0", "field-$sectionNo-$rowNo-0", "field_${sectionNo}_${rowNo}_0"),
-            generateContainerField("sections[$sectionNo].rows[$rowNo].field1", "field-$sectionNo-$rowNo-1", "field_${sectionNo}_${rowNo}_1"),
-            generateContainerField("sections[$sectionNo].rows[$rowNo].field2", "field-$sectionNo-$rowNo-2", "field_${sectionNo}_${rowNo}_2")
+                generateContainerField("sections[$sectionNo].rows[$rowNo].field0", "field-$sectionNo-$rowNo-0", "field_${sectionNo}_${rowNo}_0"),
+                generateContainerField("sections[$sectionNo].rows[$rowNo].field1", "field-$sectionNo-$rowNo-1", "field_${sectionNo}_${rowNo}_1"),
+                generateContainerField("sections[$sectionNo].rows[$rowNo].field2", "field-$sectionNo-$rowNo-2", "field_${sectionNo}_${rowNo}_2")
         ])
     }
 
     protected Section generateSection(String num) {
         new Section([
-            generateRow(num, "0"),
-            generateRow(num, "Uno"),
-            generateRow(num, "Dos")
+                generateRow(num, "0"),
+                generateRow(num, "Uno"),
+                generateRow(num, "Dos")
         ])
     }
 
     @Test
     void testReopenInactiveRecords() {
         def sections = [
-            generateSection("0"),
-            generateSection("Un"),
-            generateSection("Deux")
+                generateSection("0"),
+                generateSection("Un"),
+                generateSection("Deux")
         ]
         def tabstates = [:]
 
@@ -156,10 +157,10 @@ class WebUtilsTest {
     @Test
     void testHandleNullValues() {
         def sections = [ new Section([ new Row([
-            null, // null field
-            new Field(fieldDataType:  null, containerName:  "containerName"), // null type
-            new Field(), // null containerName
-            generateContainerField("containerField", "containerField", "containerField") // legit
+                null, // null field
+                new Field(fieldDataType:  null, containerName:  "containerName"), // null type
+                new Field(), // null containerName
+                generateContainerField("containerField", "containerField", "containerField") // legit
         ])]) ]
 
         assertEquals("containerName", sections[0].rows[0].fields[1].containerName)
@@ -173,35 +174,47 @@ class WebUtilsTest {
         WebUtils.reopenInactiveRecords(sections, tabstates, "containerField")
         assertEquals([containerFieldelementcontainedvalueonecontainedvaluetwocontainedvaluethree:"OPEN"], tabstates)
     }
-	
-	@Test
-	void testSanitizeBackLocation() {
-		def config = new SimpleConfig()
-		config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
-		config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
-		ConfigContext.init(config);
-		
-		assertEquals(TEST_URL, WebUtils.sanitizeBackLocation(TEST_URL));
-	}
-	
-	@Test
-	void testSanitizeBackLocationWithDisallowedUrl() {
-		def config = new SimpleConfig()
-		config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
-		config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
-		ConfigContext.init(config);
-		
-		assertEquals(DEFAULT_URL, WebUtils.sanitizeBackLocation("http://disallowed"));
-	}
-	
-	@Test
-	void testSanitizeBackLocationWithJavaScriptProtocol() {
-		def config = new SimpleConfig()
-		config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
-		config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
-		ConfigContext.init(config);
-		
-		assertEquals(DEFAULT_URL, WebUtils.sanitizeBackLocation("javascript:alert('test')"));
-	}
+
+    @Test
+    void testSanitizeBackLocation() {
+        def config = new SimpleConfig()
+        config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
+        config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
+        ConfigContext.init(config);
+
+        assertEquals(TEST_URL, WebUtils.sanitizeBackLocation(TEST_URL));
+    }
+
+    @Test
+    void testSanitizeBackLocationWithDisallowedUrl() {
+        def config = new SimpleConfig()
+        config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
+        config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
+        ConfigContext.init(config);
+
+        assertEquals(DEFAULT_URL, WebUtils.sanitizeBackLocation("http://disallowed"));
+    }
+
+    @Test
+    void testSanitizeBackLocationWithJavaScriptProtocol() {
+        def config = new SimpleConfig()
+        config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
+        config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
+        ConfigContext.init(config);
+
+        assertEquals(DEFAULT_URL, WebUtils.sanitizeBackLocation("javascript:alert('test')"));
+    }
+
+    @Test
+    void testSanitizeBackLocationWithRelativeUrls() {
+        def config = new SimpleConfig()
+        config.putProperty(KRADConstants.BACK_LOCATION_ALLOWED_REGEX, "^http://test.*");
+        config.putProperty(KRADConstants.BACK_LOCATION_DEFAULT_URL, DEFAULT_URL);
+        ConfigContext.init(config);
+
+        assertEquals(RELATIVE_URL_1, WebUtils.sanitizeBackLocation(RELATIVE_URL_1));
+        assertEquals(RELATIVE_URL_2, WebUtils.sanitizeBackLocation(RELATIVE_URL_2));
+        assertEquals(RELATIVE_URL_3, WebUtils.sanitizeBackLocation(RELATIVE_URL_3));
+    }
 
 }

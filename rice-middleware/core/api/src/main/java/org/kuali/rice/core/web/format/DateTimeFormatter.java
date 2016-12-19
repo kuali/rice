aabe3a76b0acfa3da36351fe8b9523fa09ad0791
Eric Westfall <ewestfal@gmail.com>
2016-12-19 01:46:07 -0500

    RICE-7 - ported 06989d852bed913ba4b5554686d00bf9864ff8d8 from rice-2.5
    Implemented handling in data formatting for document search so that it can handle java.util.Date.
    
    Also added some additional support for better handling DataType.TIMESTAMP and adding a formatter for joda time DateTime.

--- rice-middleware/core/api/src/main/java/org/kuali/rice/core/web/format/DateTimeFormatter.java
+++ rice-middleware/core/api/src/main/java/org/kuali/rice/core/web/format/DateTimeFormatter.java
@@ -18,27 +18,26 @@ package org.kuali.rice.core.web.format;
 import org.joda.time.DateTime;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 
-@Deprecated
-public class DateViewTimestampObjectFormatter extends DateFormatter {
+public class DateTimeFormatter extends DateFormatter {
 
-	private static final long serialVersionUID = 2069562793196038664L;
+    @Override
+    protected Object convertToObject(String target) {
+        Date date = (Date) super.convertToObject(target);
+        return new DateTime(date.getTime());
+    }
 
-	@Override
-	protected Object convertToObject(String target) {
-		return new Timestamp(((Date) super.convertToObject(target)).getTime());
-	}
-
-	@Override
-	public Object format(Object value) {
-		if (value == null) {
+    @Override
+    public Object format(Object value) {
+        if (value == null) {
             return null;
+        } else if (value instanceof String) {
+            return value;
+        } else if (value instanceof java.util.Date) {
+            return getDateTimeService().toDateTimeString((java.util.Date)value);
+        } else {
+            return getDateTimeService().toDateTimeString(((DateTime) value).toDate());
         }
-        if (value instanceof DateTime) {
-			value = new Timestamp(((DateTime)value).getMillis());
-        }
-        return getDateTimeService().toDateTimeString((Timestamp) value);
-	}
+    }
 
 }
